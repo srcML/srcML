0a0ca8c9ee0dd350e3a24348b31d38e9fb9238e5
Michael L. Collard, Ph.D <collard@uakron.edu>
2006-01-11 03:29:31 +0000

    Document hidden srcml2src option for nested unit count in help
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/trunk@2449 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcml2src.cpp
+++ src/srcml2src.cpp
@@ -75,6 +75,7 @@ void output_help(const char* name) {
 	       << "  " << SRCVERSION_FLAG_SHORT << ", " << setw(COL) << SRCVERSION_FLAG << "display source version and exit\n"
 	       << "  " << ENCODING_FLAG_SHORT   << ", " << setw(COL) << ENCODING_FLAG   << "display xml encoding and exit\n"
 	       << '\n'
+	       << "  " << NESTED_FLAG_SHORT     << ", " << setw(COL) << NESTED_FLAG     << "display number of nested units and exit\n"
 	       << "  " << UNIT_FLAG_SHORT       << ", " << setw(COL) << UNIT_FLAG_FULL  << "process nested unit NUM from a compound srcML document\n"
 	       << "  " << XML_FLAG_SHORT        << ", " << setw(COL) << XML_FLAG        << "output is in XML instead of text\n"
 	       << "  " << EXPAND_FLAG_SHORT     << ", " << setw(COL) << EXPAND_FLAG     << "extract all files from a compound srcML document\n"
