dd16fd86495501114366b212545bb343a278157d
Michael L. Collard, Ph.D <collard@uakron.edu>
2006-02-06 11:09:12 +0000

    Reformat comment
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/trunk@2798 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcMLTranslator.cpp
+++ src/srcMLTranslator.cpp
@@ -18,10 +18,8 @@
   You should have received a copy of the GNU General Public License
   along with the srcML translator; if not, write to the Free Software
   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-*/
 
-/*
-  Class for straightforward translation
+  Class for straightforward translation from source code to srcML
 */
 
 #include <iostream>
