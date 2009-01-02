83762072f5106ad26ca8dc12d0bef546ac3f9e90
Michael L. Collard, Ph.D <collard@uakron.edu>
2009-01-02 04:52:27 +0000

    Get encoding from input state of parser
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/trunk@4115 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/SAX2TextWriter.cpp
+++ src/SAX2TextWriter.cpp
@@ -61,7 +61,7 @@ namespace SAX2TextWriter {
     // start this document the same as the current document
     xmlTextWriterStartDocument(pstate->writer,
 			       (const char*) pstate->ctxt->version,
-			       (const char*) pstate->ctxt->encoding,
+			       (const char*) pstate->ctxt->input->encoding,
 			       pstate->ctxt->standalone ? "yes" : "no");
   }
 
