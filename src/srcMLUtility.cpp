19656dfebc17986056e66a66b3fc10e4f945e57c
Michael L. Collard, Ph.D <collard@uakron.edu>
2009-01-02 14:31:09 +0000

    Remove old textreader code
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/trunk@4120 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcMLUtility.cpp
+++ src/srcMLUtility.cpp
@@ -41,11 +41,6 @@
 
 #include "Options.h"
 
-// directory permission for expand
-#ifdef __GNUC__
-const int EXPAND_DIR_PERM = S_IRWXU | S_IRWXG;
-#endif
-
 const char* const XML_DECLARATION_STANDALONE = "yes";
 const char* const XML_VERSION = "1.0";
 
@@ -61,10 +56,6 @@ bool srcMLUtility::checkEncoding(const char* encoding) {
   return xmlFindCharEncodingHandler(encoding) != 0;
 
 }
-
-//void skiptonextunit(xmlTextReaderPtr reader) throw (LibXMLError);
-//void skiptounit(xmlTextReaderPtr reader, const char* filename) throw (LibXMLError);
-//void skiptounit(xmlTextReaderPtr reader, int number) throw (LibXMLError);
 
 #include "SAX2ExtractUnitsSrc.h"
 #include "SAX2ExtractUnitSrc.h"
@@ -82,45 +73,6 @@ srcMLUtility::srcMLUtility(const char* infilename, const char* encoding, int& op
 
   // setup an output handler
   handler = xmlFindCharEncodingHandler(output_encoding);
-  /*
-  // get out now for expand since switched to sax handling
-  if (isoption(options, OPTION_EXPAND) || !(
-					    //      isoption(options, OPTION_UNIT) ||
-      isoption(options, OPTION_INFO) ||
-      isoption(options, OPTION_LONG_INFO) ||
-      isoption(options, OPTION_NAMESPACE) ||
-      //      isoption(options, OPTION_XML) ||
-      //      isoption(options, OPTION_LANGUAGE) ||
-      isoption(options, OPTION_FILENAME) ||
-      isoption(options, OPTION_DIRECTORY) ||
-      isoption(options, OPTION_VERSION) ||
-      isoption(options, OPTION_XML_ENCODING) ||
-      isoption(options, OPTION_NESTED)
-					    ) )
-    return;
-
-  // create the reader
-  reader = xmlNewTextReaderFilename(infile);
-  if (reader == NULL)
-    throw LibXMLError(0);
-
-  // read the outer unit
-  try {
-    skiptounit(reader, 1);
-  } catch (...) {
-    throw "Unable to find starting unit element";
-  }
-
-  // record all attributes for future use
-  // don't use the TextReaderMoveToNextAttribute as it messes up the future expand
-  for (xmlAttrPtr pAttr = xmlTextReaderCurrentNode(reader)->properties; pAttr; pAttr = pAttr->next)
-      attrv[(const char*) pAttr->name] = (const char*) xmlGetProp(xmlTextReaderCurrentNode(reader), pAttr->name);
-
-  // record all namespaces for future use
-  // don't use the TextReaderMoveToNextAttribute as it messes up the future expand
-  for (xmlNsPtr pAttr = xmlTextReaderCurrentNode(reader)->nsDef; pAttr; pAttr = pAttr->next)
-    nsv[(const char*) pAttr->href] = pAttr->prefix ? (const char*) pAttr->prefix : "";
-  */
 }
 
 // destructor
@@ -132,21 +84,7 @@ srcMLUtility::~srcMLUtility() {
 
 // attribute
 std::string srcMLUtility::attribute(const char* attribute_name, bool& nonnull) {
-  /*
-  if (moved) {
-    // extract attribute from unit tag
-    xmlChar* value = xmlTextReaderGetAttribute(reader, BAD_CAST attribute_name);
- 
-    // return the extracted attribute or a blank string if it doesn't exist
-    std::string s(value != 0 ? (const char*)value : "");
- 
-    nonnull = value != 0;
 
-    xmlFree(value);
-
-    return s;
-  }
-  */
   // extract attribute from unit tag
   nonnull = attrv.count(attribute_name) > 0;
 
@@ -317,157 +255,3 @@ const char* srcMLUtility::getencoding() {
 const std::map<std::string, std::string> srcMLUtility::getNS() const {
   return nsv;
 }
-
-/*
-// output current unit element in XML
-void srcMLUtility::outputUnit(const char* filename, xmlTextReaderPtr reader) {
-
-  // new document
-  xmlDocPtr doc;
-  xmlTextWriterPtr writer = xmlNewTextWriterDoc(&doc, isoption(options, OPTION_COMPRESSED));
-
-  // issue the xml declaration
-  int save_options = XML_SAVE_NO_DECL;
-  if (isoption(options, OPTION_XMLDECL)) {
-      xmlTextWriterStartDocument(writer, XML_VERSION, output_encoding, XML_DECLARATION_STANDALONE);
-      save_options = 0;
-  }
-
-  // end the document so we get the correct xml declaration
-  xmlTextWriterEndDocument(writer);
-
-  // generate the full tree in the reader of the unit
-  // so that we can move it to the output
-  xmlTextReaderExpand(reader);
-
-  // set the expanded reader as the root element of the new document
-  xmlDocSetRootElement(doc, xmlTextReaderCurrentNode(reader));
-
-  // output namespaces from outer unit tag
-  if (isoption(options, OPTION_NAMESPACEDECL)) {
-
-    // starting with the namespaces on the root element, update the namespaces
-    // with the ones from this unit
-    std::map<std::string, std::string> nnsv = nsv;
-    for (xmlNsPtr pAttr = xmlTextReaderCurrentNode(reader)->nsDef; pAttr; pAttr = pAttr->next)
-      nnsv[(const char*) pAttr->href] = pAttr->prefix ? (const char*) pAttr->prefix : "";
-
-    // output the standard namespaces, if they exist
-    const char* stdns[] = { SRCML_SRC_NS_URI, SRCML_CPP_NS_URI, SRCML_ERR_NS_URI };
-    for (int i = 0; i < (int) (sizeof(stdns) / sizeof(stdns[0])); ++i) {
-      
-      std::map<std::string, std::string>::iterator pos = nnsv.find(stdns[i]);
-      if (pos != nnsv.end()) {
-
-	xmlNewNs(xmlDocGetRootElement(doc), BAD_CAST stdns[i],
-		 pos->second == "" ? NULL : BAD_CAST pos->second.c_str());
-
-	nnsv.erase(pos);
-      }
-    }
-
-    // output any other namespaces that may exist
-    for (std::map<std::string, std::string>::const_iterator iter = nsv.begin(); iter != nsv.end(); ++iter) {
-      std::string uri = (*iter).first;
-      std::string prefix = (*iter).second;
-
-      xmlNewNs(xmlDocGetRootElement(doc), BAD_CAST uri.c_str(), prefix == "" ? NULL : BAD_CAST prefix.c_str());
-    }
-  }
-
-  // starting with the attributes on the root element, update the attributes
-  // with the ones from this unit
-  std::map<std::string, std::string> nattrv = attrv;
-  for (xmlAttrPtr pAttr = xmlDocGetRootElement(doc)->properties; pAttr; pAttr = pAttr->next)
-    nattrv[(const char*) pAttr->name] = (const char*) xmlGetProp(xmlDocGetRootElement(doc), pAttr->name);
-
-  // wipe out all the attributes
-  xmlAttrPtr pAttr;
-  while ((pAttr = xmlDocGetRootElement(doc)->properties)) {
-    xmlDocGetRootElement(doc)->properties = pAttr->next;
-    xmlRemoveProp(pAttr);
-  }
-
-  // put back the standard attributes based on a merge of the root unit and this unit
-  const char* stdattr[] = { UNIT_ATTRIBUTE_LANGUAGE, UNIT_ATTRIBUTE_DIRECTORY,
-			    UNIT_ATTRIBUTE_FILENAME, UNIT_ATTRIBUTE_VERSION };
-  for (int i = 0; i < (int) (sizeof(stdattr) / sizeof(stdattr[0])); ++i) {
-
-    std::map<std::string, std::string>::iterator pos = nattrv.find(stdattr[i]);
-    if (pos != nattrv.end()) {
-
-	xmlSetProp(xmlDocGetRootElement(doc), BAD_CAST stdattr[i], BAD_CAST pos->second.c_str());
-
-	nattrv.erase(pos);
-    }
-  }
-
-  // put in the rest of the attributes
-  for (std::map<std::string, std::string>::const_iterator iter = nattrv.begin(); iter != nattrv.end(); iter++)
-      xmlSetProp(xmlDocGetRootElement(doc), BAD_CAST (*iter).first.c_str(), BAD_CAST (*iter).second.c_str());
-
-  // save the created document
-  // "-" filename is standard output
-  xmlSaveCtxtPtr saver = xmlSaveToFilename(filename, output_encoding, save_options);
-  xmlSaveDoc(saver, doc);
-  xmlSaveClose(saver);
-}
-*/
-/*
-  // skip to the next unit
-  void skiptonextunit(xmlTextReaderPtr reader) throw (LibXMLError) {
-
-    // skip to the correct unit
-    while (1) {
-      // read a node
-      int ret = xmlTextReaderRead(reader);
-      if (ret != 1)
-	throw LibXMLError(ret);
-
-      if (xmlTextReaderNodeType(reader) == XML_READER_TYPE_ELEMENT &&
-	  xmlTextReaderDepth(reader) <= 1)
-	break;
-    }
-  }
-
-  // skip to a particular unit
-  void skiptounit(xmlTextReaderPtr reader, int number) throw (LibXMLError) {
-
-    // skip to the correct unit
-    int count = 0;
-    try {
-    while (1) {
-      skiptonextunit(reader);
-
-      ++count;
-
-      // did we find it?
-      if (count == number)
-	break;
-
-      // skip past this unit
-      xmlTextReaderNext(reader);
-    }
-    } catch (...) {
-      throw OutOfRangeUnitError(count);
-    }
-  }
-
-  // skip to a particular unit
-  void skiptounit(xmlTextReaderPtr reader, const char* filename) throw (LibXMLError) {
-
-    // skip to the correct unit
-    while (1) {
-      skiptonextunit(reader);
-
-      // did we find it?
-      if (strcmp((const char*)
-		 xmlTextReaderGetAttribute(reader, BAD_CAST UNIT_ATTRIBUTE_FILENAME), filename) == 0)
-	break;
-
-      // skip past this unit
-      xmlTextReaderNext(reader);
-    }
-}
-
-*/
