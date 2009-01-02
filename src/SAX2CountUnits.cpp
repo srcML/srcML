0868143c791ed9f94d7d9456558c8242414db878
Michael L. Collard, Ph.D <collard@uakron.edu>
2009-01-02 19:53:16 +0000

    Counting units sax handler
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/trunk@4135 f70b1346-9fe7-0310-9bf3-95a0de396e6b

---
+++ src/SAX2CountUnits.cpp
@@ -0,0 +1,81 @@
+/*
+  srcMLUtility.cpp
+
+  Copyright (C) 2004-2006  SDML (www.sdml.info)
+
+  This file is part of the srcML translator.
+
+  The srcML translator is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  The srcML translator is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with the srcML translator; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+*/
+
+#include "SAX2CountUnits.h"
+#include "SAX2Utilities.h"
+
+#include <iostream>
+#include "Options.h"
+
+namespace SAX2CountUnits {
+
+  static int placescount = 1;
+  static int placesunit = 10;
+
+  xmlSAXHandler factory() {
+
+    xmlSAXHandler sax = { 0 };
+
+    sax.initialized    = XML_SAX2_MAGIC;
+    sax.endElementNs = &endElementNs;
+    sax.endDocument  = &endDocument;
+
+    return sax;
+  }
+
+  // end document
+  void endDocument(void *user_data) {
+
+    State* pstate = (State*) user_data;
+
+    // output file status message if in verbose mode
+    if (isoption(*(pstate->poptions), OPTION_VERBOSE)) {
+      for (int i = 0; i < placescount; ++i)
+	std::clog << '\b';
+    }
+  }
+
+  // end unit element and current file/buffer (started by startElementNs
+  void endElementNs(void *ctx, const xmlChar *localname, const xmlChar *prefix, const xmlChar *URI) {
+
+    State* pstate = (State*) ctx;
+
+    if (pstate->ctxt->nameNr != 2)
+      return;
+
+    ++(pstate->count);
+
+    // output file status message if in verbose mode
+    if (isoption(*(pstate->poptions), OPTION_VERBOSE)) {
+      for (int i = 0; i < placescount; ++i)
+	std::clog << '\b';
+      std::clog << pstate->count;
+
+      if (pstate->count == placesunit) {
+	placesunit *= 10;
+	++placescount;
+      }
+    }
+  }
+};
---
+++ src/SAX2CountUnits.h
@@ -0,0 +1,60 @@
+/*
+  SAX2CountUnits.h
+
+  Copyright (C) 2008 SDML (www.sdml.info)
+
+  This file is part of the srcML translator.
+
+  The srcML translator is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  The srcML translator is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with the srcML translator; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+  A SAX2 handler for libxml2 that converts input to CountUnits.  It performs
+  a (attempted) identical copy of the input.  Typically used for part of
+  an xml document, e.g., an individual subpart.  It can also be customized
+  by overriding individual callbacks.
+*/
+
+#ifndef INCLUDED_SAX2COUNTUNITS_H
+#define INCLUDED_SAX2COUNTUNITS_H
+
+#include <libxml/parser.h>
+#include <libxml/xmlwriter.h>
+
+#include <string>
+#include <map>
+
+namespace SAX2CountUnits {
+
+  xmlSAXHandler factory();
+
+  struct State {
+    const char * root_filename;
+    const char * ofilename;
+    xmlCharEncodingHandlerPtr handler;
+    int* poptions;
+    const char* filename;      // output filename
+    xmlTextWriterPtr writer;   // output text writer
+    xmlOutputBufferPtr output;
+    long count;
+    xmlParserCtxtPtr ctxt;
+    std::map<std::string, std::string> nsv;
+    std::map<std::string, std::string> attrv;
+  };
+
+  void endDocument(void *user_data);
+
+  void endElementNs(void *ctx, const xmlChar *localname, const xmlChar *prefix, const xmlChar *URI);
+};
+
+#endif
