d02ecb43adadfce55d4cc582436dccd1c5960fd6
Michael L. Collard, Ph.D <collard@uakron.edu>
2009-01-14 00:36:11 +0000

    Size static array by static initialization
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/trunk@4240 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcMLOutput.h
+++ src/srcMLOutput.h
@@ -93,7 +93,7 @@ class srcMLOutput : public srcMLParserTokenTypes {
 
   // List of element names
   static const char* ElementNames[];
-  static int ElementPrefix[END_ELEMENT_TOKEN];
+  static int ElementPrefix[];
 
   void processText(const antlr::RefToken& token);
 
