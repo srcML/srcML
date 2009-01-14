c1cd454db8021b8de817bb2c061f89d8a2ad0a10
Michael L. Collard, Ph.D <collard@uakron.edu>
2009-01-14 12:32:50 +0000

    Get process table right
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/trunk@4246 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcMLOutput.h
+++ src/srcMLOutput.h
@@ -93,7 +93,7 @@ class srcMLOutput : public srcMLParserTokenTypes {
 
   // List of element names
   static const char* const ElementNames[];
-  static const char ElementPrefix[];
+  static const char ElementPrefix[END_ELEMENT_TOKEN];
 
  public:
   void processText(const antlr::RefToken& token);
