0d64ff03a84049fa3c1e52566e392ee573919f4d
Michael L. Collard, Ph.D <collard@uakron.edu>
2009-01-17 04:06:04 +0000

    Compact newlines
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/trunk@4270 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/src2srcml.cpp
+++ src/src2srcml.cpp
@@ -106,7 +106,7 @@ void output_help(const char* name) {
 	      << "with support for creating compound srcML documents.\n\n"
 
 	      << "When no filenames are given read from standard input and write to standard output.\n"
-	      << "When only one filename is given write to standard output."  << '\n'
+	      << "When only one filename is given write to standard output.\n"
 	      << "An input filename of '-' also reads from standard input.\n\n"
 
 	      << "Options:\n"
