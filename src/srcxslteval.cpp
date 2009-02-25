fa6b67788a528e5adab7ba77687e6785236bef6e
Michael L. Collard, Ph.D <collard@uakron.edu>
2009-02-25 13:07:14 +0000

    xslt processing
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/trunk@4647 f70b1346-9fe7-0310-9bf3-95a0de396e6b

---
+++ src/srcxslteval.cpp
@@ -0,0 +1,125 @@
+/*
+  xpathproc
+
+  A simple XPath expression evaluator.
+
+  Usage:  xpathproc xpath-expression xml-file
+*/
+
+#include "srcxslteval.h"
+
+#include <string>
+#include <list>
+
+#include <libxml/xpath.h>
+#include <libxml/xmlsave.h>
+#include <libxml/xpathInternals.h>
+
+#include <libxslt/xslt.h>
+#include <libxslt/xsltInternals.h>
+#include <libxslt/transform.h>
+#include <libxslt/xsltutils.h>
+
+#include <libexslt/exslt.h>
+#include <libexslt/exsltconfig.h>
+
+#include "xmlsavebuf.h"
+
+/* srcML unit attributes */
+/*
+const char* UNIT_ATTRIBUTE_LANGUAGE = "language";
+const char* UNIT_ATTRIBUTE_DIRECTORY = "dir";
+const char* UNIT_ATTRIBUTE_FILENAME = "filename";
+const char* UNIT_ATTRIBUTE_VERSION = "version";
+*/
+
+int srcxslteval(const char* xpath, xmlTextReaderPtr reader) {
+
+  exsltRegisterAll();
+
+  xsltStylesheetPtr xslt = xsltParseStylesheetFile(BAD_CAST xpath);
+
+  // read the first node
+  xmlTextReaderRead(reader);
+
+  // setup output
+  xmlSaveCtxtPtr ctxt = xmlSaveToFd(STDOUT_FILENO, "UTF-8", 0);
+
+  // register the namespaces on the root element
+  xmlOutputBufferWriteString(xmlSaveGetBuffer(ctxt), "<unit");
+  for (xmlNsPtr pAttr = xmlTextReaderCurrentNode(reader)->nsDef; pAttr; pAttr = pAttr->next) {
+
+	xmlOutputBufferWriteString(xmlSaveGetBuffer(ctxt), " xmlns");
+	if (pAttr->prefix)
+	  xmlOutputBufferWriteString(xmlSaveGetBuffer(ctxt), ":");
+	xmlOutputBufferWriteString(xmlSaveGetBuffer(ctxt), (const char*) pAttr->prefix);
+	xmlOutputBufferWriteString(xmlSaveGetBuffer(ctxt), "=\"");
+	xmlOutputBufferWriteString(xmlSaveGetBuffer(ctxt), (const char*) pAttr->href);
+	xmlOutputBufferWriteString(xmlSaveGetBuffer(ctxt), "\"");
+  }
+
+  // copy all attributes
+  for (xmlAttrPtr pAttr = xmlTextReaderCurrentNode(reader)->properties; pAttr; pAttr = pAttr->next) {
+
+        char* ac = (char*) xmlGetProp(xmlTextReaderCurrentNode(reader), pAttr->name);
+
+	xmlOutputBufferWriteString(xmlSaveGetBuffer(ctxt), " ");
+	xmlOutputBufferWriteString(xmlSaveGetBuffer(ctxt), (const char*) pAttr->name);
+	xmlOutputBufferWriteString(xmlSaveGetBuffer(ctxt), "=\"");
+	xmlOutputBufferWriteString(xmlSaveGetBuffer(ctxt), (const char*) ac);
+	xmlOutputBufferWriteString(xmlSaveGetBuffer(ctxt), "\"");
+  }
+  xmlOutputBufferWriteString(xmlSaveGetBuffer(ctxt), ">\n\n");
+
+  // doc for applying stylesheet to
+  xmlDocPtr doc = xmlNewDoc(NULL);
+
+  while (1) {
+
+     // read a node
+     int ret = xmlTextReaderRead(reader);
+     if (ret != 1)
+       break;
+
+     // nested unit tag
+     if (xmlTextReaderDepth(reader) == 1 &&
+	 xmlTextReaderNodeType(reader) == XML_READER_TYPE_ELEMENT &&
+	 xmlTextReaderConstName(reader)[0] == 'u') {
+
+       // expand this unit to make it the context
+       xmlNodePtr node = xmlTextReaderExpand(reader);
+
+       // create a separate document with the expanded unit
+       xmlDocSetRootElement(doc, xmlCopyNode(node, 1));
+
+       // apply the style sheet
+       xmlDocPtr res = xsltApplyStylesheet(xslt, doc, NULL);
+
+       // save the transformed tree
+       xsltSaveResultTo(xmlSaveGetBuffer(ctxt), res, xslt);
+       xmlOutputBufferWriteString(xmlSaveGetBuffer(ctxt), "\n");
+
+       // finished with the result of the transformation
+       xmlFreeDoc(res);
+
+       // cleanup our doc
+       xmlNodePtr oldnode = xmlDocGetRootElement(doc);
+       xmlUnlinkNode(oldnode);
+       xmlFreeNode(oldnode);
+
+       // move over this expanded node
+       xmlTextReaderNext(reader);
+     }
+  }
+
+  // root unit end tag
+  xmlOutputBufferWriteString(xmlSaveGetBuffer(ctxt), "</unit>\n");
+
+  // all done with the buffer
+  xmlOutputBufferClose(xmlSaveGetBuffer(ctxt));
+
+  // all done with the doc
+  xmlFreeDoc(doc);
+
+  return 0;
+}
---
+++ src/srcxslteval.h
@@ -0,0 +1,16 @@
+/*
+  xpathproc
+
+  A simple XPath expression evaluator.
+
+  Usage:  xpathproc xpath-expression xml-file
+*/
+
+#ifndef INCLUDED_SRCXSLTEVAL_H
+#define INCLUDED_SRCXSLTEVAL_H
+
+#include <libxml/xmlreader.h>
+
+int srcxslteval(const char* xpath, xmlTextReaderPtr reader);
+
+#endif
