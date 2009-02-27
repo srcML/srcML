8967ae5db4f221afa43c829bb667b8f9d6b8b2d9
Michael L. Collard, Ph.D <collard@uakron.edu>
2009-02-27 14:32:13 +0000

    Comment and fix option value choices
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/trunk@4674 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/Options.h
+++ src/Options.h
@@ -32,115 +32,170 @@ inline bool isoption(int options, int flag) {
 enum Options {
 
   // issue debugging elements and declare debugging element namespace
+  // @src2srcml
   OPTION_DEBUG       = 0x00000001,
 
   // unit is nested a root unit
+  // @src2srcml
+  // @srcml2src
   OPTION_NESTED      = 0x00000002,
 
-  // markup literal values @src2srcml
+  // markup literal values
+  // @src2srcml
   OPTION_LITERAL     = 0x00000004,
 
   // output is compressed (gzip)
+  // @src2srcml
+  // @srcml2src
   OPTION_COMPRESSED  = 0x00000008,
 
   // output is interactive (good for editing applications) @src2srcml
+  // @src2srcml
   OPTION_INTERACTIVE = 0x00000010,
 
   // input filenames is a file list
+  // @src2srcml
   OPTION_FILELIST    = 0x00000020,
 
   // declared filename
+  // @src2srcml
+  // @srcml2src
   OPTION_FILENAME    = 0x00000040,
 
   // declared directory
+  // @src2srcml
+  // @srcml2src
   OPTION_DIRECTORY   = 0x00000080,
 
   // declared version
+  // @src2srcml
+  // @srcml2src
   OPTION_VERSION     = 0x00000100,
 
   // verbose translation
+  // @src2srcml
+  // @srcml2src
   OPTION_VERBOSE     = 0x00000200,
 
   // help
+  // @src2srcml
+  // @srcml2src
   OPTION_HELP        = 0x00000400,
 
   // program version
+  // @src2srcml
+  // @srcml2src
   OPTION_PVERSION    = 0x00000800,
 
   // language
+  // @src2srcml
+  // @srcml2src
   OPTION_LANGUAGE    = 0x00001000,
 
   // xml encoding
+  // @src2srcml
+  // @srcml2src
   OPTION_XML_ENCODING= 0x00002000,
 
   // language
+  // @src2srcml
+  // @srcml2src
   OPTION_TEXT_ENCODING= 0x00004000,
 
   // text extraction
+  // @src2srcml
+  // @srcml2src
   OPTION_TEXT         = 0x00008000,
 
   // language
+  // @srcml2src
   OPTION_UNIT         = 0x00010000,
 
   // expand nested units into separate files
+  // @srcml2src
   OPTION_EXPAND       = 0x00020000,
 
   // output is xml
+  // @src2srcml
+  // @srcml2src
   OPTION_XML          = 0x00040000,
 
   // info
+  // @srcml2src
   OPTION_INFO         = 0x00080000,
 
   // long info
+  // @srcml2src
   OPTION_LONG_INFO    = 0x00100000,
 
   // skip encoding
+  // @src2srcml
+  // @srcml2src
   OPTION_SKIP_ENCODING = 0x00200000,
 
   // cpp markup #else sections
+  // @src2srcml
   OPTION_CPP_MARKUP_ELSE = 0x00400000,
 
   // cpp markup #if 0 sections
+  // @src2srcml
   OPTION_CPP_MARKUP_IF0 = 0x00800000,
 
   // terminate after this file
+  // @src2srcml
+  // @srcml2src
   OPTION_TERMINATE = 0x01000000,
 
   // expression mode
+  // @src2srcml
   OPTION_EXPRESSION = 0x02000000,
 
   // namespace mode
+  // @src2srcml
+  // @srcml2src
   OPTION_NAMESPACE = 0x04000000,
 
   // xmlns mode
+  // @src2srcml
   OPTION_XMLNS = 0x08000000,
 
   // cpp mode
+  // @src2srcml
   OPTION_CPP = 0x10000000,
 
   // xml declaration
+  // @src2srcml
+  // @srcml2src
   OPTION_XMLDECL = 0x20000000,
 
   // namespace declaration
+  // @src2srcml
+  // @srcml2src
   OPTION_NAMESPACEDECL = 0x40000000,
 
   // self version
   //  OPTION_SELF_VERSION = 0x02000000,
 
   // markup operators
-  OPTION_OPERATOR     = OPTION_INTERACTIVE,
+  // @src2srcml
+  OPTION_OPERATOR     = OPTION_UNIT,
+
+  // markup type modifiers
+  // @src2srcml
+  OPTION_MODIFIER     = OPTION_EXPAND,
 
   // xpath evaluation
+  // @srcml2src
   OPTION_XPATH        = OPTION_LITERAL,
 
   // xslt evaluation
+  // @srcml2src
   OPTION_XSLT         = OPTION_CPP,
 
   // relaxng evaluation
+  // @srcml2src
   OPTION_RELAXNG      = OPTION_EXPRESSION,
 
-  // markup type modifiers
-  OPTION_MODIFIER     = OPTION_TEXT,
 };
 
 #endif
