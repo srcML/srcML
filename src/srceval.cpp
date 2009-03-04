6a0fdb5d02a6a559d566d7e0c47d514ef90e932e
Michael L. Collard, Ph.D <collard@uakron.edu>
2009-03-04 11:36:11 +0000

    Replace specialized output of namespaces and attributes with xmlNodeDump
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/trunk@4708 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srceval.cpp
+++ src/srceval.cpp
@@ -1,40 +1,16 @@
 #include "srceval.h"
 
-#include <libxslt/xslt.h>
-#include <libxslt/transform.h>
-#include <libxslt/xsltutils.h>
-
-#include <libexslt/exslt.h>
-
-void xmlAttrDumpOutputBuffer(xmlOutputBufferPtr buf, xmlNodePtr node, xmlAttrPtr pAttr) {
-
-  xmlOutputBufferWrite(buf, 1, " ");
-  xmlOutputBufferWriteString(buf, (const char*) pAttr->name);
-  xmlOutputBufferWrite(buf, 2, "=\"");
-  xmlOutputBufferWriteString(buf, (const char*) xmlGetProp(node, pAttr->name));
-  xmlOutputBufferWrite(buf, 1, "\"");
-}
-
-void xmlNsDumpOutputBuffer(xmlOutputBufferPtr buf, xmlNsPtr pAttr) {
-
-  xmlOutputBufferWrite(buf, 6, " xmlns");
-  if (pAttr->prefix)
-    xmlOutputBufferWrite(buf, 1, ":");
-  xmlOutputBufferWriteString(buf, (const char*) pAttr->prefix);
-  xmlOutputBufferWrite(buf, 2, "=\"");
-  xmlOutputBufferWriteString(buf, (const char*) pAttr->href);
-  xmlOutputBufferWrite(buf, 1, "\"");
-}
-
 void xmlUnitDumpOutputBuffer(xmlOutputBufferPtr buf, xmlNodePtr node) {
 
-  // register the namespaces on the root element
   xmlOutputBufferWrite(buf, 5, "<unit");
+
+  // register the namespaces on the root element
   for (xmlNsPtr pAttr =  node->nsDef; pAttr; pAttr = pAttr->next)
-    xmlNsDumpOutputBuffer(buf, pAttr);
+    xmlNodeDumpOutput(buf, node->doc, (xmlNodePtr) pAttr, 0, 0, 0);
 
   // copy all attributes
   for (xmlAttrPtr pAttr = node->properties; pAttr; pAttr = pAttr->next)
-    xmlAttrDumpOutputBuffer(buf, node, pAttr);
+    xmlNodeDumpOutput(buf, node->doc, (xmlNodePtr) pAttr, 0, 0, 0);
+
   xmlOutputBufferWrite(buf, 3, ">\n\n");
 }
--- src/srceval.h
+++ src/srceval.h
@@ -2,11 +2,6 @@
 #define INCLUDED_SRCEVAL_H
 
 #include <libxml/xpath.h>
-#include <libxml/xpathInternals.h>
-
-void xmlAttrDumpOutputBuffer(xmlOutputBufferPtr buf, xmlNodePtr node, xmlAttrPtr pAttr);
-
-void xmlNsDumpOutputBuffer(xmlOutputBufferPtr buf, xmlNsPtr pAttr);
 
 void xmlUnitDumpOutputBuffer(xmlOutputBufferPtr buf, xmlNodePtr node);
 
