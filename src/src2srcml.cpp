7722cbcf6bbbb1274ceae8cf5ee2bd6c712df81c
Michael L. Collard, Ph.D <collard@uakron.edu>
2010-06-14 15:47:42 +0000

    Allow for --output at any point in file list
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/trunk@4963 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/src2srcml.cpp
+++ src/src2srcml.cpp
@@ -309,26 +309,17 @@ int main(int argc, char* argv[]) {
   int input_arg_start = curarg;
   int input_arg_end = -1;
   int input_arg_count = 0;
+  int input_arg_skip_start = -1;
+  int input_arg_skip_end = -1;
   int numout = srcml_filename ? 0 : 1;
   if (argc - curarg == 1)
     numout = 0;
   while ((argc - curarg) > numout) {
 
-    // mark last input filename assuming output srcml filename is last
-    input_arg_end = curarg;
+    // can have an output flag in the list of input filenames
+    if (compare_flags(argv[curarg], OUTPUT_FLAG, OUTPUT_FLAG_SHORT)) {
 
-    // calculate the total number of input files
-    ++input_arg_count;
-    //    input_arg_count = input_arg_end - input_arg_start + 1;
-
-    // update the argument count with the input filenames
-    //    curarg += input_arg_count;
-    ++curarg;
-  }
-
-  /*
-    // language is based on parameter
-    } else if (compare_flags(argv[curarg], OUTPUT_FLAG, OUTPUT_FLAG_SHORT)) {
+      input_arg_skip_end = input_arg_skip_start = curarg;
 
       char* embedded = extract_option(argv[curarg]);
 
@@ -346,8 +337,27 @@ int main(int argc, char* argv[]) {
 
 	// extract parameter
 	srcml_filename = argv[(++curarg)++];
+
+	++input_arg_skip_end;
       }
-*/
+
+      numout = 0;
+
+      continue;
+    }
+
+    // mark last input filename assuming output srcml filename is last
+    input_arg_end = curarg;
+
+    // calculate the total number of input files
+    ++input_arg_count;
+    //    input_arg_count = input_arg_end - input_arg_start + 1;
+
+    // update the argument count with the input filenames
+    //    curarg += input_arg_count;
+    ++curarg;
+  }
+
   // last command line parameter is output srcml filename
   if (!srcml_filename) {
     srcml_filename = "-";
@@ -367,6 +377,9 @@ int main(int argc, char* argv[]) {
   struct stat outstat;
   stat(srcml_filename, &outstat);
   for (int i = input_arg_start; i <= input_arg_end; ++i) {
+
+    if (i >= input_arg_skip_start && i <= input_arg_skip_end)
+      continue;
 
     struct stat instat;
     stat(argv[i], &instat);
@@ -542,6 +555,9 @@ int main(int argc, char* argv[]) {
     // translate in batch the input files on the command line extracting the directory and filename attributes
     // from the full path
     for (int i = input_arg_start; i <= input_arg_end; ++i) {
+
+      if (i >= input_arg_skip_start && i <= input_arg_skip_end)
+	continue;
 
       char* path = argv[i];
 
