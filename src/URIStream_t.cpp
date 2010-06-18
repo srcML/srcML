d0884186af89a311cdd6d4de62686d55b064b297
Michael John Decker <mdecker6@kent.edu>
2010-06-18 20:53:00 +0000

    Edited the URIStream_test.cpp to recursively get website
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/trunk@5014 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/URIStream_t.cpp
+++ src/URIStream_t.cpp
@@ -1,11 +1,22 @@
 #include "URIStream.h"
 #include <iostream>
+#include <vector>
 
 int main() {
 
-  URIStream stream("/dev/stdin");
+  std::vector<URIStream *> streams;
+  URIStream * stream = new URIStream("http://www.cs.uakron.edu/~mjd52/test");
+  streams.push_back(stream);
 
   char* line;
-  while (line = stream.getline())
-    std::cout << line << '\n';
+  while (!streams.empty())
+  {
+	  while(line = streams.back()->getline())
+	   //std::cout <<line <<'\n';
+		  if(std::strstr(line, "://") != NULL)
+			  streams.push_back(new URIStream(line));
+		  else
+			  std::cout << line << '\n';
+	  streams.pop_back();
+  }
 }
