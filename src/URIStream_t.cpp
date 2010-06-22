722db7c2bb9648cc384d141d0983d88d03f8ec5f
Michael L. Collard, Ph.D <collard@uakron.edu>
2010-06-22 20:55:07 +0000

    Options for sed and grep
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/trunk@5039 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/URIStream_t.cpp
+++ src/URIStream_t.cpp
@@ -1,22 +1,13 @@
 #include "URIStream.h"
 #include <iostream>
 #include <vector>
+#include <cstring>
 
 int main() {
 
-  std::vector<URIStream *> streams;
-  URIStream * stream = new URIStream("http://www.cs.uakron.edu/~mjd52/test");
-  streams.push_back(stream);
+  URIStream stream("/dev/stdin");
 
   char* line;
-  while (!streams.empty())
-  {
-	  while(line = streams.back()->getline())
-	   //std::cout <<line <<'\n';
-		  if(std::strstr(line, "://") != NULL)
-			  streams.push_back(new URIStream(line));
-		  else
-			  std::cout << line << '\n';
-	  streams.pop_back();
-  }
+  while (line = stream.getline())
+    std::cout << "|" << line << "|" << '\n';
 }
