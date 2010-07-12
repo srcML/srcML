201168a95a1f1382a30a9eca91e4774ecd90b807
Michael John Decker <mdecker6@kent.edu>
2010-07-12 18:04:21 +0000

    Changed extract-all to extract all files
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/trunk@5374 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcml2src.cpp
+++ src/srcml2src.cpp
@@ -37,7 +37,7 @@ int option_error_status(int optopt);
 
 char const * const NAME = "srcml2src";
 
-char const * const EXPAND_FLAG = "extract-all";
+char const * const EXPAND_FLAG = "extract-all-files";
 char const EXPAND_FLAG_SHORT = 'a';
 
 char const * const UNIT_FLAG = "unit";
