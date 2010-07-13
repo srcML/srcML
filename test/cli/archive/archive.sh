cbc00109bf89e9d5ef7101f35b306fe8f9746bd4
Michael John Decker <mdecker6@kent.edu>
2010-07-13 19:47:49 +0000

    Added more archives
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/trunk@5416 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- test/cli/archive/archive.sh
+++ test/cli/archive/archive.sh
@@ -10,9 +10,9 @@ gzip -cv $FILES > $NAME.gz
 
 bzip2 -cv $FILES > $NAME.bz2
 
-#gzip -cv $FILES.bz2 > $NAME.bz2.gz
+gzip -cv $FILES.bz2 > $NAME.bz2.gz
 
-#bzip2 -cv $FILES.gz > $NAME.gz.bz2
+bzip2 -cv $FILES.gz > $NAME.gz.bz2
 
 tar -cvf $NAME.tar $FILES
 
@@ -20,4 +20,10 @@ tar -cvzf $NAME.tgz $FILES
 
 tar -cvzf $NAME.tar.gz $FILES
 
+bzip2 -cv $FILES.tar > $NAME.tar.bz2
+
 echo $FILES | tr " " "\n" | cpio -ov > $NAME.cpio
+
+gzip -cv $FILES.cpio > $NAME.cpio.gz
+
+bzip2 -cv $FILES.cpio > $NAME.cpio.bz2
