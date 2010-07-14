4bf240806d9172b243571bfa5b1c46bddcee4395
Michael L. Collard, Ph.D <collard@uakron.edu>
2010-07-14 15:34:51 +0000

    Hide call to archive raw for old versions of libarchive
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/trunk@5448 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/libxml_archive_io.cpp
+++ src/libxml_archive_io.cpp
@@ -75,10 +75,9 @@ void* archiveOpenRoot(const char * URI) {
 
   a = archive_read_new();
   archive_read_support_compression_all(a);
-
-  //#if ARCHIVE_VERSION_STAMP > 2008000
+#if ARCHIVE_VERSION_STAMP > 2008000
   archive_read_support_format_raw(a);
-  //#endif
+#endif
   archive_read_support_format_all(a);
 
   int r = archive_read_open_filename(a, URI, 4000);
@@ -106,7 +105,9 @@ void* archiveOpen(const char * URI) {
   if (!a) {
     a = archive_read_new();
     archive_read_support_compression_all(a);
-    archive_read_support_format_raw(a);
+#if ARCHIVE_VERSION_STAMP > 2008000
+  archive_read_support_format_raw(a);
+#endif
     archive_read_support_format_all(a);
 
     int r = archive_read_open_filename(a, URI, 4000);
