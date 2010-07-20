c7ea43befde7fd7b9718fa53bae68d43b5cff05a
Michael John Decker <mdecker6@kent.edu>
2010-07-20 14:52:02 +0000

    Submit working regular expression test program
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/trunk@5554 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/regext.cpp
+++ src/regext.cpp
@@ -1,28 +1,51 @@
 #include <stdio.h>
 #include <regex.h>
 #include <sys/types.h>
+#include <cstring>
 
 int main(int argc, char ** argv)
 {
-  const char * string = argv[1];
+  int length = strlen(argv[1]);
+  char * string = argv[1];
+  for(int i = 0; i < length / 2; ++i)
+    {
+      char temp = string[i];
+      string[i] = string[length - (i + 1)];
+      string[length - (i + 1)] = temp;
+    }
+  fprintf(stderr, "%s\n", string);
 
-  const char * regex = ".*\\.([:alnum:]*)";
+  //  const char * regex = "(zg\\.|2zb\\.)*([:alnum:]*)\\.";
+  const char * regex = "(zg\\.|2zb\\.)*([^\\.]*)";
   regex_t preg;
   int errorcode = regcomp(&preg, regex, REG_EXTENDED);
   if(errorcode == 0)
     fprintf(stderr, "Compiled\n");
 
-  regmatch_t pmatch[2];
-  errorcode = regexec(&preg, string, 2, pmatch, 0);
+  regmatch_t pmatch[3];
+  errorcode = regexec(&preg, string, 3, pmatch, 0);
   if(errorcode == 0){
     fprintf(stderr, "Ran\n");
-    fprintf(stderr, "Match: %d\n", pmatch[0].rm_so);
-    fprintf(stderr, "Match: %d\n", pmatch[1].rm_so);
+    fprintf(stderr, "Match: %d:%d\n", pmatch[0].rm_so, pmatch[0].rm_eo);
+    fprintf(stderr, "Match: %d:%d\n", pmatch[1].rm_so, pmatch[1].rm_eo);
+    fprintf(stderr, "Match: %d:%d\n", pmatch[2].rm_so, pmatch[2].rm_eo);
   }
   else{
     fprintf(stderr, "No Match\n");
   }
-    regfree(&preg);
+  
+  for(int i = pmatch[2].rm_so; i < (pmatch[2].rm_eo + pmatch[2].rm_so) / 2; ++i)
+  {
+      char temp = string[i];
+      fprintf(stderr, "%c\n", temp);
+      fprintf(stderr, "%c\n", string[pmatch[2].rm_eo - ((i + 1) - pmatch[2].rm_so)]);
+      string[i] = string[pmatch[2].rm_eo - ((i + 1) - pmatch[2].rm_so)];
+      string[pmatch[2].rm_eo - ((i + 1) - pmatch[2].rm_so)] = temp;
+  }
 
+    string [pmatch[2].rm_eo] = '\0';
+
+  fprintf(stderr, "%*s\n", pmatch[2].rm_eo - pmatch[2].rm_so, string + pmatch[2].rm_so);
+    regfree(&preg);
   return 0;
 }
