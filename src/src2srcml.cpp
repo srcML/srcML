c72bd33c9b01f0bcdbf370872e09f337ad5d83db
Michael L. Collard, Ph.D <collard@uakron.edu>
2010-07-27 03:37:11 +0000

    Add prefixes to uridata array
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/trunk@5737 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/src2srcml.cpp
+++ src/src2srcml.cpp
@@ -127,19 +127,20 @@ const char FILELIST_COMMENT = '#';
 
 struct uridata {
   char const * const uri;
+  char const * const prefix;
   int option;
   const char* description;
 };
 
 uridata uris[] = {
 
-  { SRCML_SRC_NS_URI,          0,               "primary srcML namespace" },
-  { SRCML_CPP_NS_URI,          OPTION_CPP,      "namespace for cpreprocessing elements" },
-  { SRCML_ERR_NS_URI,          OPTION_DEBUG,    "namespace for srcML debugging elements" },
-  { SRCML_EXT_LITERAL_NS_URI,  OPTION_LITERAL,  "namespace for optional literal elements" },
-  { SRCML_EXT_OPERATOR_NS_URI, OPTION_OPERATOR, "namespace for optional operator element"},
-  { SRCML_EXT_MODIFIER_NS_URI, OPTION_MODIFIER, "namespace for optional modifier element"},
-  { SRCML_EXT_POSITION_NS_URI, OPTION_POSITION, "namespace for optional position element and attributes" },
+  { SRCML_SRC_NS_URI,          SRCML_SRC_NS_PREFIX_DEFAULT, 0,               "primary srcML namespace" },
+  { SRCML_CPP_NS_URI,          SRCML_CPP_NS_PREFIX_DEFAULT, OPTION_CPP,      "namespace for cpreprocessing elements" },
+  { SRCML_ERR_NS_URI,          SRCML_ERR_NS_PREFIX_DEFAULT, OPTION_DEBUG,    "namespace for srcML debugging elements" },
+  { SRCML_EXT_LITERAL_NS_URI,  SRCML_EXT_LITERAL_NS_PREFIX_DEFAULT, OPTION_LITERAL,  "namespace for optional literal elements" },
+  { SRCML_EXT_OPERATOR_NS_URI, SRCML_EXT_OPERATOR_NS_PREFIX_DEFAULT, OPTION_OPERATOR, "namespace for optional operator element"},
+  { SRCML_EXT_MODIFIER_NS_URI, SRCML_EXT_MODIFIER_NS_PREFIX_DEFAULT, OPTION_MODIFIER, "namespace for optional modifier element"},
+  { SRCML_EXT_POSITION_NS_URI, SRCML_EXT_POSITION_NS_PREFIX_DEFAULT, OPTION_POSITION, "namespace for optional position element and attributes" },
 };
 
 const char* urisprefix[] = {
