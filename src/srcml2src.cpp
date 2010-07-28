93179ce001ce54024582f76c30725c2fb0243a86
Michael John Decker <mdecker6@kent.edu>
2010-07-28 20:57:08 +0000

    Turned on libarchive handling
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/trunk@5787 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcml2src.cpp
+++ src/srcml2src.cpp
@@ -262,11 +262,10 @@ int main(int argc, char* argv[]) {
   initGenericErrorDefaultFunc(&handler);
 
 #ifdef LIBARCHIVE
-  /*
   if (xmlRegisterInputCallbacks(archiveReadMatch, archiveReadOpen, archiveRead, archiveReadClose) < 0) {
     fprintf(stderr, "%s: failed to register archive handler\n", argv[0]);
     exit(1);
-  }
+  }/*
   if (xmlRegisterOutputCallbacks(archiveWriteDiskMatch, archiveWriteDiskOpen, archiveWriteDisk, archiveWriteDiskClose) < 0) {
     fprintf(stderr, "%s: failed to register archive handler\n", argv[0]);
     exit(1);
