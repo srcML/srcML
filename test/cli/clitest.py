a15c68db3de65316eef52bdf4db3c2328ac912d6
Michael John Decker <mdecker6@kent.edu>
2010-08-03 18:24:07 +0000

    Changed so test single unit
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/trunk@5876 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- test/cli/clitest.py
+++ test/cli/clitest.py
@@ -1952,12 +1952,10 @@ validate(open('sub/b.cpp.xml').read(), srcml)
 # no xml declaration srcml2src
 
 srcml = """<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" language="C++">
-</unit>
+<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" language="C++"/>
 """
 
-srcmlout = """<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" language="C++">
-</unit>
+srcmlout = """<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" language="C++"/>
 """
 
 f = open('sub/a.cpp.xml', 'w')
