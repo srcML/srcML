2b346c11b9f0c749b8da88ad030a5e936ef47619
Michael L. Collard, Ph.D <collard@uakron.edu>
2010-08-05 18:22:13 +0000

    Don't repeat src xmlns
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/trunk@5902 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/SAX2UnitDOMXPath.cpp
+++ src/SAX2UnitDOMXPath.cpp
@@ -235,11 +235,13 @@ void SAX2UnitDOMXPath::endElementNs(void *ctx, const xmlChar *localname, const x
         xmlNsPtr cur = savens;
         while (cur != NULL) {
           xmlNsPtr q = xmlCopyNamespace(cur);
-          if (p == NULL) {
-            ret = p = q;
-          } else {
-            p->next = q;
-            p = q;
+          if (strcmp((const char*) q->href, "http://www.sdml.info/srcML/src") != 0) {
+            if (p == NULL) {
+              ret = p = q;
+            } else {
+              p->next = q;
+              p = q;
+            }
           }
           cur = cur->next;
         }
