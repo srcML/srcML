1c364bce2e8540f42a7cae18899c45addfd2e8dd
Michael John Decker <mdecker6@kent.edu>
2010-08-12 17:23:06 +0000

    Added simple test case for empty unit for xslt src:archive
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/trunk@6001 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- test/cli/clitest.py
+++ test/cli/clitest.py
@@ -2544,6 +2544,32 @@ validate(open('sub/b.cpp.xml', 'r').read(), xslt)
 validate(getreturn([srcml2src, option.XSLT_FLAG + '=copy.xsl', option.STRING_PARAM_FLAG], srcml), status.STATUS_ERROR)
 validate(getreturn([srcml2src, option.XSLT_FLAG + '=copy.xsl', option.STRING_PARAM_FLAG, "name"], srcml), status.STATUS_ERROR)
 
+# src:archive
+
+srcml = xml_declaration + """
+<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" filename="a.cpp" language="C++"/>
+"""
+
+xslt = """a.cpp
+C++
+"""
+
+file = open('sub/a.cpp.xml', 'w')
+file.write(srcml)
+file.close()
+
+# empty test
+
+check([srcml2src, option.XSLT_FLAG + '=archive.xsl'], srcml, xslt)
+check([srcml2src, option.XSLT_FLAG + '=archive.xsl', 'sub/a.cpp.xml'], "", xslt)
+check([srcml2src, option.XSLT_FLAG + '=archive.xsl', '-o', 'sub/b.cpp.xml'], srcml, "")
+validate(open('sub/b.cpp.xml', 'r').read(), xslt)
+check([srcml2src, option.XSLT_FLAG + '=archive.xsl', 'sub/a.cpp.xml', '-o', 'sub/b.cpp.xml'], "", "")
+validate(open('sub/b.cpp.xml', 'r').read(), xslt)
+
+validate(getreturn([srcml2src, option.XSLT_FLAG], srcml), status.STATUS_ERROR)
+validate(getreturn([srcml2src, option.XSLT_FLAG + '='], srcml), status.STATUS_ERROR)
+
 # relaxng
 
 # position
