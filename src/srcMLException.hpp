a85e8a57d431355447b7c08a7efcafbad542d6b5
Louis Scicchitano <ljs41@zips.uakron.edu>
2011-06-23 16:16:22 +0000

    updated/added licensing information in to the header comment
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/trunk@6849 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcMLException.hpp
+++ src/srcMLException.hpp
@@ -1,7 +1,7 @@
 /*
   srcMLException.hpp
 
-  Copyright (C) 2004, 2005 SDML (www.sdml.info)
+  Copyright (C) 2004-2010 SDML (www.sdml.info)
 
   This include file is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
