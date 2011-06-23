4c064c3bf8280a4bb1f476d8edc059cd90847c3a
Louis Scicchitano <ljs41@zips.uakron.edu>
2011-06-23 16:16:28 +0000

    updated/added licensing information in to the header comment
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/trunk@6856 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcMLStateStack.hpp
+++ src/srcMLStateStack.hpp
@@ -1,7 +1,7 @@
 /*
   srcMLStateStack.hpp
 
-  Copyright (C) 2002, 2003, 2004, 2005  SDML (www.sdml.info)
+  Copyright (C) 2004-2010  SDML (www.sdml.info)
 
   This file is part of the srcML translator.
 
