3a003361b638642e50f737410cdfadafd316ffd2
Michael L. Collard, Ph.D <collard@uakron.edu>
2011-07-21 01:23:30 +0000

    Move back to old xpath processing
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/trunk@7244 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcMLUtility.cpp
+++ src/srcMLUtility.cpp
@@ -340,7 +340,7 @@ void srcMLUtility::set_input_filename(const char* infilename) {
 
 // xpath evaluation of the nested units
 void srcMLUtility::xpath(const char* ofilename, const char* context_element, const char* xpaths[]) {
-
+  /*
   // relative xpath changed to at any level
   std::string s = xpaths[0];
   //  if (s[0] != '/')
@@ -375,8 +375,8 @@ void srcMLUtility::xpath(const char* ofilename, const char* context_element, con
 
   // all done with parsing
   xmlFreeParserCtxt(ctxt);
+  */
 
-  /*
   xmlSAXHandler sax = SAX2UnitDOMXPath::factory();
 
   SAX2UnitDOMXPath state(context_element, xpaths, ofilename, options);
@@ -403,7 +403,6 @@ void srcMLUtility::xpath(const char* ofilename, const char* context_element, con
   ctxt->sax = NULL;
 
   xmlFreeParserCtxt(ctxt);
-  */
 }
 
 // xslt evaluation of the nested units
