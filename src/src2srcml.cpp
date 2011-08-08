d7fe76ebf96e5de68c9656eb4704f18503019f17
Michael John Decker <mdecker6@kent.edu>
2011-08-08 19:41:01 +0000

    Added a possible fix for windows when using directory feature
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/trunk@7358 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/src2srcml.cpp
+++ src/src2srcml.cpp
@@ -1426,6 +1426,7 @@ void src2srcml_dir(srcMLTranslator& translator, const char* directory, process_o
       continue;
 #endif
 
+#ifndef __MINGW32__
     // make sure that we are not processing the output file
     if (instat.st_ino == outstat.st_ino && instat.st_dev == outstat.st_dev) {
       fprintf(stderr, !shownumber ? "Skipped '%s':  Output file.\n" :
@@ -1434,6 +1435,7 @@ void src2srcml_dir(srcMLTranslator& translator, const char* directory, process_o
       ++skipped;
       continue;
     }
+#endif
 
     // translate the file listed in the input file using the directory and filename extracted from the path
     src2srcml_text(translator,
