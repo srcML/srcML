912f861efa965e2895bd1d5502c111f4a6d0ff5d
Michael John Decker <mdecker6@kent.edu>
2011-08-23 14:57:13 +0000

    Added Core files
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/trunk@7522 f70b1346-9fe7-0310-9bf3-95a0de396e6b

---
+++ src/srcMLTranslatorCore.cpp
@@ -0,0 +1,141 @@
+/*
+  srcMLTranslator.cpp
+
+  Copyright (C) 2003-2011  SDML (www.sdml.info)
+
+  This file is part of the srcML translator.
+
+  The srcML translator is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  The srcML translator is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with the srcML translator; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+  Class for straightforward translation from source code to srcML
+*/
+
+#include "srcMLTranslator.hpp"
+#include "KeywordCPPLexer.hpp"
+#include "srcMLParser.hpp"
+#include "StreamMLParser.hpp"
+#include "srcMLOutput.hpp"
+#include "srcmlns.hpp"
+
+// constructor
+srcMLTranslator::srcMLTranslator(int language, const char* srcml_filename, OPTION_TYPE& op)
+  : Language(language), first(true),
+    root_directory(""), root_filename(""), root_version(""),
+    encoding(DEFAULT_TEXT_ENCODING), options(op),
+    out(0, srcml_filename, getLanguageString(), DEFAULT_XML_ENCODING, options, (const char**)DEFAULT_URI_PREFIX, 8) {
+}
+
+// constructor
+srcMLTranslator::srcMLTranslator(int language,                // programming language of source code
+				 const char* src_encoding,    // text encoding of source code
+				 const char* xml_encoding,    // xml encoding of result srcML file
+				 const char* srcml_filename,  // filename of result srcML file
+				 OPTION_TYPE& op,             // many and varied options
+				 const char* directory,       // root unit directory
+				 const char* filename,        // root unit filename
+				 const char* version,         // root unit version
+				 const char* uri[],           // uri prefixes
+				 int tabsize                  // size of tabs
+				 )
+  : Language(language), pinput(0), first(true),
+    root_directory(directory), root_filename(filename), root_version(version),
+    encoding(src_encoding), options(op),
+    out(0, srcml_filename, getLanguageString(), xml_encoding, options, uri, tabsize), tabsize(tabsize) {
+
+}
+
+// translate from input stream to output stream
+void* srcMLTranslator::setInput(const char* path) {
+
+  try {
+    pinput = new UTF8CharBuffer(path, encoding);
+
+    // return the libxml context of the file
+    if (pinput)
+      return pinput->getContext();
+
+  } catch (const std::exception& e) {
+    fprintf(stderr, "SRCML Exception: %s\n", e.what());
+  }
+  catch (UTF8FileError) {
+    throw FileError();
+  }
+  catch (...) {
+    fprintf(stderr, "ERROR\n");
+  }
+
+  return 0;
+}
+
+// close the output
+void srcMLTranslator::close() {
+
+  out.close();
+}
+
+// translate from input stream to output stream
+void srcMLTranslator::translate(const char* path, const char* unit_directory,
+				const char* unit_filename, const char* unit_version,
+				int language) {
+
+  // root unit for compound srcML documents
+  if (first && ((options & OPTION_NESTED) > 0))
+    out.startUnit(0, root_directory, root_filename, root_version, true);
+
+  first = false;
+
+  try {
+
+      // master lexer with multiple streams
+      antlr::TokenStreamSelector selector;
+
+      // srcML lexical analyzer from standard input
+      KeywordCPPLexer lexer(pinput, encoding, language);
+      lexer.setSelector(&selector);
+      lexer.setTabsize(tabsize);
+
+      // pure block comment lexer
+      PureCommentLexer textlexer(lexer.getInputState());
+      textlexer.setSelector(&selector);
+
+      // switching between lexers
+      selector.addInputStream(&lexer, "main");
+      selector.addInputStream(&textlexer, "text");
+      selector.select(&lexer);
+
+      // base stream parser srcML connected to lexical analyzer
+      StreamMLParser<srcMLParser> parser(selector, language, options);
+
+      // connect local parser to attribute for output
+      out.setTokenStream(parser);
+
+      // parse and form srcML output with unit attributes
+      Language l(language);
+      out.consume(l.getLanguageString(), unit_directory, unit_filename, unit_version);
+
+  } catch (const std::exception& e) {
+    fprintf(stderr, "SRCML Exception: %s\n", e.what());
+  }
+  catch (UTF8FileError) {
+    throw FileError();
+  }
+  catch (...) {
+    fprintf(stderr, "ERROR\n");
+  }
+}
+
+// destructor
+srcMLTranslator::~srcMLTranslator() {
+}
---
+++ src/srcMLTranslatorCore.hpp
@@ -0,0 +1,82 @@
+/*
+  srcMLTranslator.hpp
+
+  Copyright (C) 2003-2011  SDML (www.sdml.info)
+
+  This file is part of the srcML translator.
+
+  The srcML translator is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  The srcML translator is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with the srcML translator; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+/*
+  Class for straightforward source code to srcML translation
+*/
+
+#ifndef SRCMLTRANSLATOR_HPP
+#define SRCMLTRANSLATOR_HPP
+
+#include "Language.hpp"
+#include "srcMLOutput.hpp"
+
+class UTF8CharBuffer;
+class FileError {};
+
+class srcMLTranslator : public Language {
+ public:
+
+  // constructor
+  srcMLTranslator(int language, const char* srcml_filename, OPTION_TYPE& op);
+
+  // constructor
+  srcMLTranslator(int language,
+		  const char* src_encoding,
+		  const char* xml_encoding,
+		  const char* srcml_filename,
+		  OPTION_TYPE& options,
+		  const char* directory,
+		  const char* filename,
+		  const char* version,
+		  const char* uri[],
+		  int tabsize
+		  );
+
+  void* setInput(const char* path);
+
+  void close();
+
+  // translate from input stream to output stream
+  void translate(const char* path,
+		 const char* unit_directory = 0,
+		 const char* unit_filename = 0,
+		 const char* unit_version = 0,
+		 int language = 0);
+
+  // destructor
+  ~srcMLTranslator();
+
+ private:
+  UTF8CharBuffer* pinput;
+  bool first;
+  const char* root_directory;
+  const char* root_filename;
+  const char* root_version;
+  const char* encoding;
+  int language;
+  OPTION_TYPE& options;
+  srcMLOutput out;
+  int tabsize;
+};
+
+#endif
