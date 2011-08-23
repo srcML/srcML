fba5a015c6bce06eca3d9fd33fba3eac4c742dcb
Michael John Decker <mdecker6@kent.edu>
2011-08-23 15:08:21 +0000

    Made the function call the Core class
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/trunk@7525 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcMLTranslator.cpp
+++ src/srcMLTranslator.cpp
@@ -23,18 +23,12 @@
 */
 
 #include "srcMLTranslator.hpp"
-#include "KeywordCPPLexer.hpp"
-#include "srcMLParser.hpp"
-#include "StreamMLParser.hpp"
-#include "srcMLOutput.hpp"
-#include "srcmlns.hpp"
+#include "srcMLTranslatorCore.hpp"
 
 // constructor
-srcMLTranslator::srcMLTranslator(int language, const char* srcml_filename, OPTION_TYPE& op)
-  : Language(language), first(true),
-    root_directory(""), root_filename(""), root_version(""),
-    encoding(DEFAULT_TEXT_ENCODING), options(op),
-    out(0, srcml_filename, getLanguageString(), DEFAULT_XML_ENCODING, options, (const char**)DEFAULT_URI_PREFIX, 8) {
+srcMLTranslator::srcMLTranslator(int language, const char* srcml_filename, OPTION_TYPE& op) {
+
+  translator = new srcMLTranslatorCore(language, srcml_filename, op);
 }
 
 // constructor
@@ -48,94 +42,32 @@ srcMLTranslator::srcMLTranslator(int language,                // programming lan
 				 const char* version,         // root unit version
 				 const char* uri[],           // uri prefixes
 				 int tabsize                  // size of tabs
-				 )
-  : Language(language), pinput(0), first(true),
-    root_directory(directory), root_filename(filename), root_version(version),
-    encoding(src_encoding), options(op),
-    out(0, srcml_filename, getLanguageString(), xml_encoding, options, uri, tabsize), tabsize(tabsize) {
+				 ) {
 
+  translator = new srcMLTranslatorCore(language, src_encoding, xml_encoding, srcml_filename, op, directory, filename, version, uri, tabsize);
 }
 
 // translate from input stream to output stream
 void* srcMLTranslator::setInput(const char* path) {
 
-  try {
-    pinput = new UTF8CharBuffer(path, encoding);
-
-    // return the libxml context of the file
-    if (pinput)
-      return pinput->getContext();
-
-  } catch (const std::exception& e) {
-    fprintf(stderr, "SRCML Exception: %s\n", e.what());
-  }
-  catch (UTF8FileError) {
-    throw FileError();
-  }
-  catch (...) {
-    fprintf(stderr, "ERROR\n");
-  }
-
-  return 0;
+  return translator->setInput(path);
 }
 
 // close the output
 void srcMLTranslator::close() {
 
-  out.close();
+  translator->close();
 }
 
 // translate from input stream to output stream
 void srcMLTranslator::translate(const char* path, const char* unit_directory,
 				const char* unit_filename, const char* unit_version,
 				int language) {
-
-  // root unit for compound srcML documents
-  if (first && ((options & OPTION_NESTED) > 0))
-    out.startUnit(0, root_directory, root_filename, root_version, true);
-
-  first = false;
-
-  try {
-
-      // master lexer with multiple streams
-      antlr::TokenStreamSelector selector;
-
-      // srcML lexical analyzer from standard input
-      KeywordCPPLexer lexer(pinput, encoding, language);
-      lexer.setSelector(&selector);
-      lexer.setTabsize(tabsize);
-
-      // pure block comment lexer
-      PureCommentLexer textlexer(lexer.getInputState());
-      textlexer.setSelector(&selector);
-
-      // switching between lexers
-      selector.addInputStream(&lexer, "main");
-      selector.addInputStream(&textlexer, "text");
-      selector.select(&lexer);
-
-      // base stream parser srcML connected to lexical analyzer
-      StreamMLParser<srcMLParser> parser(selector, language, options);
-
-      // connect local parser to attribute for output
-      out.setTokenStream(parser);
-
-      // parse and form srcML output with unit attributes
-      Language l(language);
-      out.consume(l.getLanguageString(), unit_directory, unit_filename, unit_version);
-
-  } catch (const std::exception& e) {
-    fprintf(stderr, "SRCML Exception: %s\n", e.what());
-  }
-  catch (UTF8FileError) {
-    throw FileError();
-  }
-  catch (...) {
-    fprintf(stderr, "ERROR\n");
-  }
+  translator->translate(path, unit_directory, unit_filename, unit_version, language);
 }
 
 // destructor
 srcMLTranslator::~srcMLTranslator() {
+
+  delete translator;
 }
