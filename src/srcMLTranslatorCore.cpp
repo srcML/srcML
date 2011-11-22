e6cc9a589b310ac632a828dd79d2a3156b00e94c
Michael John Decker <mdecker6@kent.edu>
2011-11-22 17:57:41 +0000

    Made constructor
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/trunk@10317 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcMLTranslatorCore.cpp
+++ src/srcMLTranslatorCore.cpp
@@ -64,6 +64,23 @@ srcMLTranslatorCore::srcMLTranslatorCore(int language,                // program
     encoding(src_encoding), options(op),
     out(0, srcml_filename, getLanguageString(), xml_encoding, options, uri, tabsize), tabsize(tabsize) {
 
+// constructor
+srcMLTranslatorCore::srcMLTranslatorCore(int language,                // programming language of source code
+				 const char* src_encoding,    // text encoding of source code
+				 const char* xml_encoding,    // xml encoding of result srcML file
+                                 xmlBuffer * output_buffer,
+				 OPTION_TYPE& op,             // many and varied options
+				 const char* directory,       // root unit directory
+				 const char* filename,        // root unit filename
+				 const char* version,         // root unit version
+				 const char* uri[],           // uri prefixes
+				 int tabsize                  // size of tabs
+				 )
+  : Language(language), pinput(0), first(true),
+    root_directory(directory), root_filename(filename), root_version(version),
+    encoding(src_encoding), options(op),
+  out(0, 0, getLanguageString(), xml_encoding, options, uri, tabsize, output_buffer), tabsize(tabsize) {
+
 }
 
 // translate from input stream to output stream
