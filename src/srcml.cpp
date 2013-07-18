fea577b5e4c83bcc090cedef91dc30992abeab55
Michael John Decker <mdecker6@kent.edu>
2013-07-18 14:29:23 +0000

    Added srcml.cpp and added to build.  Just enough now to build srcml project
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/trunk@15013 f70b1346-9fe7-0310-9bf3-95a0de396e6b

---
+++ src/srcml.cpp
@@ -0,0 +1,19 @@
+/*
+    Class.cpp
+
+    This is a generic Main template.
+
+    Michael J. Decker
+    mdecker6@kent.edu
+*/
+
+#include <iostream>
+#include "srcMLUtility.hpp"
+
+void output_info(srcMLUtility& su, int options, int optioncount, int optionorder[]) {}
+
+int main(int argc, char * argv[]) {
+
+  return 0;
+}
+
