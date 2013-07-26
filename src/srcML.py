b131095759e3da32b037af4a931d9f320f6083d8
Michael John Decker <mdecker6@kent.edu>
2013-07-26 14:15:59 +0000

    Changed to have one python file for both translator and utility
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/trunk@15137 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcML.py
+++ src/srcML.py
@@ -10,6 +10,7 @@ elif os.path.exists('../bin/libsrcml.dll') :
 
 libsrcml = cdll.LoadLibrary(LIBSRCML_PATH)
 
+# srcMLTranslator functions
 libsrcml.srcml_new.restype = c_void_p
 libsrcml.srcml_new.argtypes = [c_int, c_ulonglong]
 
@@ -33,6 +34,22 @@ libsrcml.srcml_get_srcml.argtypes = [c_void_p]
 
 libsrcml.srcml_delete.restype = None
 libsrcml.srcml_delete.argtypes = [c_void_p]
+
+# srcMLUtility Functions
+libsrcml.srcml_utility_file_new.restype = c_void_p
+libsrcml.srcml_utility_file_new.argtypes = [c_char_p, c_char_p, c_ulonglong, c_char_p]
+
+libsrcml.srcml_utility_memory_new.restype = c_void_p
+libsrcml.srcml_utility_memory_new.argtypes = [c_char_p, c_int, c_char_p, c_ulonglong, c_char_p]
+
+libsrcml.srcml_extract_text_file.restype = None
+libsrcml.srcml_extract_text_file.argtypes = [c_void_p, c_char_p, c_char_p, c_int]
+
+libsrcml.srcml_extract_text_buffer.restype = c_char_p
+libsrcml.srcml_extract_text_buffer.argtypes = [c_void_p, c_int]
+
+libsrcml.srcml_delete.restype = None
+libsrcml.srcml_delete.argtypes = [c_ulonglong]
 
 class srcMLTranslator(object):
 
@@ -60,6 +77,24 @@ class srcMLTranslator(object):
     def delete(self) :
         libsrcml.srcml_delete(self.translator)
 
+
+class srcMLUtility(object):
+
+    def __init__(self, filename, src_encoding, options, diff_version) :
+        self.utility = c_void_p(libsrcml.srcml_utility_file_new(filename, src_encoding, options, diff_version))
+
+    def __init__(self, buffer, size, src_encoding, options, diff_version) :
+        self.utility = c_void_p(libsrcml.srcml_utility_memory_new(buffer, size, src_encoding, options, diff_version))
+
+    def extract_text(self, to_dir, ofilename, unit) :
+        libsrcml.srcml_extract_text_file(self.utility, to_dir, ofilename, unit) 
+
+    def extract_text(self, unit) :
+        return libsrcml.srcml_extract_text_buffer(self.utility, unit) 
+
+    def delete(self) :
+        libsrcml.srcml_utility_delete(self.utility)
+
 # test
 #from srcMLapps import *
 #translator = srcMLTranslator(2, "ISO-8859-1", "ISO-8859-1", 0, "directory", "file", "version", URI_PREFIX, 8)
@@ -68,3 +103,11 @@ class srcMLTranslator(object):
 #translator.close()
 #print translator.getsrcML()
 #translator.delete()
+
+#srcml = """<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+#<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" language="C++"><expr_stmt><expr><name>a</name></expr>;</expr_stmt>
+#</unit>"""
+
+#utility = srcMLUtility(srcml, len(srcml) + 1, "UTF-8", 0, "")
+#print utility.extract_text(1)
+#utility.delete()
