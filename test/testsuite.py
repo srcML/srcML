38854e791cf7b56d59aa545c9b893d40d9b08ce8
Michael L. Collard, Ph.D <collard@uakron.edu>
2013-08-01 20:17:46 +0000

    Sorted walk of files
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/trunk@15263 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- test/testsuite.py
+++ test/testsuite.py
@@ -32,6 +32,33 @@ srcmlutility = os.environ.get("SRCML2SRC")
 if srcmlutility == "" or srcmlutility == None:
 	srcmlutility = "../bin/srcml2src"
 
+# Walk into directories in filesystem
+# Ripped from os module and slightly modified
+# for alphabetical sorting
+#
+def sortedWalk(top, topdown=True, onerror=None):
+    from os.path import join, isdir, islink
+ 
+    names = os.listdir(top)
+    names.sort()
+    dirs, nondirs = [], []
+ 
+    for name in names:
+        if isdir(os.path.join(top, name)):
+            dirs.append(name)
+        else:
+            nondirs.append(name)
+ 
+    if topdown:
+        yield top, dirs, nondirs
+    for name in dirs:
+        path = join(top, name)
+        if not os.path.islink(path):
+            for x in sortedWalk(path, topdown, onerror):
+                yield x
+    if not topdown:
+        yield top, dirs, nondirs
+
 # srcExpr translator
 srcexprtranslator = '/home/collard/srcML/trunk/srcexpr/src/srcexpr'
 
@@ -400,7 +427,7 @@ total_count = 0
 try:
 			
 	# process all files		
-	for root, dirs, files in os.walk(source_dir, topdown=True):		
+	for root, dirs, files in sortedWalk(source_dir, topdown=True):		
 
 		# process all files
 		for name in files:
