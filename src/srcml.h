5beffe7f9f24fae407ff385164261b88475cbc3a
Michael L. Collard, Ph.D <collard@uakron.edu>
2013-08-01 02:51:36 +0000

    Refine names and functions for C API
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/trunk@15254 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcml.h
+++ src/srcml.h
@@ -46,20 +46,41 @@
   NOTE:  "converted"? "transformed"? Other term?
 */
 
+typedef struct srcMLData srcMLData;
+
 /* source code file converted to an individual srcML file */
-int src2srcml(char* src_filename, char* srcml_filename, int options, char* language, char* src_encoding, char* attributes[][2]);
+int src2srcml_filename_filename(char* src_filename, char* srcml_filename, int options, char* language, char* src_encoding, char* attributes[][2]);
 
 /* list of source code files converted to a srcML archive file */
-int src2srcml(char* src_filenames[], char* srcmlfilename, char* language, char* src_encoding, int options);
+int src2srcml_filelist_filename(char* src_filenames[], int size, char* srcmlfilename, char* language, char* src_encoding, int options);
 
 /* source code file converted to an individual srcML buffer */
-int src2srcml(char* src_filename, char** srcml_buffer, char* language, char* src_encoding, int options);
+int src2srcml_filename_buffer(char* src_filename, char** srcml_buffer, char* language, char* src_encoding, int options);
 
 /* source code buffer converted to a srcML file */
-int src2srcml(char* src_buffer, char* srcml_filename, char* language, char* src_encoding, int options);
+int src2srcml_buffer_filename(char* src_buffer, char* srcml_filename, char* language, char* src_encoding, int options);
 
 /* source code buffer to the individual srcML format */
-int src2srcml(char* src_buffer, char** srcml_buffer, char* language, char* src_encoding, int options);
+int src2srcml_buffer_buffer(char* src_buffer, char** srcml_buffer, char* language, char* src_encoding, int options);
+
+/*
+  Write a srcML archive file by file
+*/
+
+/* open a srcML archive for output */
+srcMLData* src2srcml_open_file(char* srcml_filename, int options, char* src_encoding, char* attributes[][2]);
+
+/* write a src file to the srcML archive after translation */
+int src2srcml_write_unit_file(srcMLData* arcdata, char* src_filename, char* language, char* attributes[][2]);
+
+/* write a buffer to the srcML archive after translation */
+int src2srcml_write_unit_buffer(srcMLData* arcdata, char* buffer, int buffer_size, char* language, char* attributes[][2]);
+
+/* close the srcML archive */
+void src2srcml_close(srcMLData* arcdata);
+
+/* free the srcML archive data */
+void src2srcml_free(srcMLData*);
 
 /*
   srcml2src - translate from the srcML format back to source code
@@ -73,7 +94,7 @@ int srcml2src(char* srcml_filename, char* src_filename, int options, char* src_e
 /* srcml utility functions NOTE:  need memory forms of these */
 
 /* srcML attributes with namespaces */
-char* [][2] srcml_info(char* srcml_filename);
+char* [2][2] srcml_info(char* srcml_filename);
 
 /* srcML attributes with namespaces and number of units */
 char* [][2] srcml_longinfo(char* srcml_filename);
