630568c8a8f7ff1267066b69103ab2928232d92f
Michael L. Collard, Ph.D <collard@uakron.edu>
2013-08-05 20:41:07 +0000

    Start of example programs for libsrcml
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/branches/libsrcml@15305 f70b1346-9fe7-0310-9bf3-95a0de396e6b

---
+++ src/srcml_direct_language.c
@@ -0,0 +1,41 @@
+/*
+  srcml_direct_language.c
+
+  Copyright (C) 2013  SDML (www.sdml.info)
+
+  The srcML translator is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  The srcML translator is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with the srcML translator; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+/*
+  Example program of the use of the C API for srcML.
+
+  A straightforward translation of source code to the srcML format.
+  Translates the file "a.cpp" to the srcML format in "a.cpp.xml", however
+  in this case the language is specified:
+
+  * This creates a single-unit srcML file, i.e., a non-archive srcML
+  * The srcML attribute filename will be the name of the file passed as the first
+    parameter.
+*/
+
+#include "srcml.h"
+
+int main(int argc, char* argv[]) {
+
+    /* Translate from a source-code file to a srcML file */
+    srcml("a.cpp", "a.cpp.xml", SRCML_LANGUAGE_CXX);
+
+    return 0;
+}
