67d4cb8f47ab2efc4f2c03842a4275d77491e744
Michael L. Collard, Ph.D <collard@uakron.edu>
2013-08-06 02:16:18 +0000

    Revise null language
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/trunk@15316 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcml.h
+++ src/srcml.h
@@ -64,10 +64,10 @@
 #define SRCML_OPTION_APPLY_ROOT
 
 /* Set of languages */
-#define SRCML_LANGUAGE_NONE ""
-#define SRCML_LANGUAGE_C "C"
-#define SRCML_LANGUAGE_CXX "C++"
-#define SRCML_LANGUAGE_JAVA "Java"
+#define SRCML_LANGUAGE_NONE   0
+#define SRCML_LANGUAGE_C      "C"
+#define SRCML_LANGUAGE_CXX    "C++"
+#define SRCML_LANGUAGE_JAVA   "Java"
 #define SRCML_LANGUAGE_CSHARP "C#"
 
 /*
