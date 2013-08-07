a27ab4b21afd3400d6b53528f3ce228fbc62281e
Michael L. Collard, Ph.D <collard@uakron.edu>
2013-08-07 01:06:47 +0000

    Fix examples and cleanup api
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/trunk@15330 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcml.h
+++ src/srcml.h
@@ -20,13 +20,6 @@
 
 /*
   srcml functions form the namespace srcml_*
-
-  NOTE:  Perhaps change "unit" to "entry", e.g., "write_unit_*" -> "write_entry_*"
-  NOTE:  Went with camel case as it is c, and libarchive uses camelcase, and I like the way that API feels
-  NOTE:  We will probably not support all of this all at once.  Just want to work out the names so we don't
-  have to change any later
-
-  NOTE:  considered using "file" instead of "filename", but libarchive did that, and deprecated it later
 */
 
 #ifndef INCLUDED_SRCML_H
@@ -39,9 +32,9 @@
 //extern "C" {
 #endif
 
-/* srcml error messages */
-#define SRCML_OK 0
-#define SRCML_ERROR 1
+/* srcml status messages */
+#define SRCML_STATUS_OK 0
+#define SRCML_STATUS_ERROR 1
 
 /* srcml options */
 #define SRCML_OPTION_LITERAL  1<<0
@@ -143,7 +136,7 @@ int srcml_register_namespace(struct srcml_archive*, const char* prefix, const ch
 
 /* open a srcML archive for output */
 int srcml_write_open_filename(struct srcml_archive*, const char* srcml_filename);
-int srcml_write_open_memory  (struct srcml_archive*, const char* buffer, size_t buffer_size);
+int srcml_write_open_memory  (struct srcml_archive*, char* buffer, size_t buffer_size);
 int srcml_write_open_FILE    (struct srcml_archive*, FILE* srcml_file);
 int srcml_write_open_fd      (struct srcml_archive*, int srcml_fd);
 
@@ -154,25 +147,26 @@ int srcml_read_open_FILE    (struct srcml_archive*, FILE* srcml_file);
 int srcml_read_open_fd      (struct srcml_archive*, int srcml_fd);
 
 /* setup options for srcml unit */
-int srcml_entry_set_language (struct srcml_archive*, const char* language);
-int srcml_entry_set_filename (struct srcml_archive*, const char* filename);
-int srcml_entry_set_directory(struct srcml_archive*, const char* directory);
-int srcml_entry_set_version (struct srcml_archive*, const char* version);
+int srcml_entry_set_language (struct srcml_entry*, const char* language);
+int srcml_entry_set_filename (struct srcml_entry*, const char* filename);
+int srcml_entry_set_directory(struct srcml_entry*, const char* directory);
+int srcml_entry_set_version  (struct srcml_entry*, const char* version);
 
-const char* srcml_entry_get_language (struct srcml_archive*);
-const char* srcml_entry_get_filename (struct srcml_archive*);
-const char* srcml_entry_get_directory(struct srcml_archive*);
-const char* srcml_entry_get_version (struct srcml_archive*);
+const char* srcml_entry_get_language (const struct srcml_entry*);
+const char* srcml_entry_get_filename (const struct srcml_entry*);
+const char* srcml_entry_get_directory(const struct srcml_entry*);
+const char* srcml_entry_get_version  (const struct srcml_entry*);
 
 /* Convert to srcml and append to the archive */
-int srcml_write_entry_archive(struct srcml_archive*, struct srcml_entry*);
+int srcml_write_entry_archive (struct srcml_archive*, struct srcml_entry*);
 int srcml_write_entry_filename(struct srcml_archive*, const char* src_filename);
-int srcml_write_entry_memory  (struct srcml_archive*, const char* src_buffer, size_t buffer_size);
-int srcml_write_entry_FILE    (struct srcml_archive*, FILE* srcml_file);
-int srcml_write_entry_fd      (struct srcml_archive*, int srcml_fd);
+int srcml_write_entry_memory  (struct srcml_archive*, char* src_buffer, size_t buffer_size);
+int srcml_write_entry_FILE    (struct srcml_archive*, FILE* src_file);
+int srcml_write_entry_fd      (struct srcml_archive*, int src_fd);
 
 /* Read the next entry from the archive */
-struct srcml_entry* srcml_read_entry_archive (struct srcml_archive*);
+const struct srcml_entry* srcml_read_entry_archive (struct srcml_archive*);
+const struct srcml_entry* srcml_read_archive_current_entry(const struct srcml_archive*);
 int srcml_read_entry_filename(struct srcml_archive*, const char* src_filename);
 int srcml_read_entry_memory  (struct srcml_archive*, const char* src_buffer, size_t buffer_size);
 int srcml_read_entry_FILE    (struct srcml_archive*, FILE* srcml_file);
@@ -180,11 +174,11 @@ int srcml_read_entry_fd      (struct srcml_archive*, int srcml_fd);
 
 /* close the srcML archive */
 void srcml_write_close(struct srcml_archive*);
-void srcml_read_close(struct srcml_archive*);
+void srcml_read_close (struct srcml_archive*);
 
 /* free the srcML archive data */
 void srcml_write_free(struct srcml_archive*);
-void srcml_read_free(struct srcml_archive*);
+void srcml_read_free (struct srcml_archive*);
 
 void srcml_free_archive_entry(struct srcml_entry*);
 
@@ -196,12 +190,10 @@ const char* srcml_get_version  (const struct srcml_archive*);
 int         srcml_get_options  (const struct srcml_archive*);
 int         srcml_get_tabstop  (const struct srcml_archive*);
 
-/* srcml utility functions NOTE:  need memory forms of these */
-
-/* srcML attributes with namespaces */
+/* srcML attributes with namespaces (header read only)*/
 const char** srcml_info(const char* srcml_filename);
 
-/* srcML attributes with namespaces and number of units */
+/* srcML attributes with namespaces and number of units (complete file read) */
 const char** srcml_longinfo(const char* srcml_filename);
 
 /* srcML attributes with namespaces of a particular unit in an archive */
@@ -211,26 +203,10 @@ const char** srcml_info_unit(const char* srcml_filename, int unit);
 const char** srcml_list(const char* srcml_filename);
 
 /* srcML XPath query and XSLT transform functions */
-
 int srcml_add_transform_xpath(struct srcml_archive*, const char* xpath_string);
 int srcml_add_transform_xslt(struct srcml_archive*, const char* xlst_filename);
-
-/* XPath (as a string) is applied to the input srcml with the query result stored in the output srcml */
-int srcml_xpath_filename_filename(const char* input_srcml_filename, const char* xpath_string, const char* output_srcml_filename, int options);
-int srcml_xpath_filename_memory(const char* input_srcml_filename, const char* xpath_string, char** output_buffer, size_t* size, int options);
-int srcml_xpath_memory_filename(char* input_srcml_buffer, size_t input_size, const char* xpath_string, const char* output_srcml_filename, int options);
-int srcml_xpath_memory_memory(char* input_srcml_buffer, size_t input_size, const char* xpath_string, char** output_buffer, size_t* size, int options);
-
-/* XSLT file is applied to the input srcml with the transformed result stored in the output srcml */
-int srcml_xslt_filename_filename(const char* input_srcml_filename, const char* xslt_filename, const char* output_srcml_filename, int options, const char* xsltparams[][2]);
-
-/* RelaxNG file is applied to the input srcml with the transformed (???) result stored in the output srcml */
-int srcml_relaxng_filename_filename(const char* input_srcml_filename, const char* relaxng_filename, const char* output_srcml_filename, int options, const char* xsltparams[][2]);
-
-/* Pipeline of XPath, XSLT, and RelaxNG transformations on the input srcml filename with the transformed result
-   stored in the output srcml filename. XPath is a string, while XSLT and RelaxNG are filenames.  Transformations are
-   applied in the order given */
-int srcml_transform_filename_filename(const char* input_srcml_filename, const char* transformation[], size_t transformation_size, const char* output_srcml_filename, int options);
+int srcml_add_transform_relaxng(struct srcml_archive*, const char* xlst_filename);
+int srcml_apply_transforms(struct srcml_archive* iarchive, struct srcml_archive* oarchive);
 
 #ifdef __cplusplus
 //}
