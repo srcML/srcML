2654bd4b400c638b4a0133e5307640e06e19aaec
Michael John Decker <mdecker6@kent.edu>
2013-08-28 20:17:11 +0000

    Now storing the archive in memory in srcML
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/trunk@15764 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/libsrcml.cpp
+++ src/libsrcml.cpp
@@ -884,9 +884,9 @@ const srcml_unit* srcml_read_unit_archive (srcml_archive* archive) { return 0; }
 const srcml_unit* srcml_read_archive_current_unit(const srcml_archive* archive) { return 0; }
 int srcml_unparse_unit_filename(srcml_unit* unit, const char* src_filename) { 
 
-  xmlOutputBufferPtr output_buffer = xmlOutputBufferCreateFilename(src_filename, xmlFindCharEncodingHandler(unit->archive->encoding ? unit->archive->encoding->c_str() : "UTF-8"), unit->archive->options & SRCML_OPTION_COMPRESS);
-  unit->archive->reader->read(output_buffer);
-  xmlOutputBufferClose(output_buffer);
+  //xmlOutputBufferPtr output_buffer = xmlOutputBufferCreateFilename(src_filename, xmlFindCharEncodingHandler(unit->archive->encoding ? unit->archive->encoding->c_str() : "UTF-8"), unit->archive->options & SRCML_OPTION_COMPRESS);
+  //unit->archive->reader->read(output_buffer);
+  //xmlOutputBufferClose(output_buffer);
 
   return SRCML_STATUS_OK;
 
@@ -931,13 +931,13 @@ int srcml_unparse_unit_fd      (srcml_unit* unit, int srcml_fd) {
 srcml_unit* srcml_read_unit(srcml_archive* archive) {
 
   std::string * language = 0, * filename = 0, * directory = 0, * version = 0;
-  bool done = !archive->reader->readUnitAttributes(&language, &filename, &directory, &version); 
-  std::string * read_unit = archive->reader->read(); 
+  archive->reader->readUnitAttributes(&language, &filename, &directory, &version); 
+  std::string * read_unit = archive->reader->readsrcML(); 
 
   srcml_unit * unit = 0;
-  if(!done) {
+  if(read_unit) {
     unit = srcml_create_unit(archive);
-    //unit->unit = read_unit;
+    unit->unit = read_unit;
     unit->language = language;
     unit->filename = filename;
     unit->directory = directory;
