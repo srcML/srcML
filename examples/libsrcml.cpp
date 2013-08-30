baf16af4adab998d8b55058efba8d82f5eced3fe
Michael John Decker <mdecker6@kent.edu>
2013-08-30 16:51:03 +0000

    Update libsrcml.cpp and srcml.h in branch
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/branches/libsrcml@15868 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- examples/libsrcml.cpp
+++ examples/libsrcml.cpp
@@ -45,10 +45,12 @@ std::string srcml_error;
 
 enum SRCML_ARCHIVE_TYPE { SRCML_ARCHIVE_RW, SRCML_ARCHIVE_READ, SRCML_ARCHIVE_WRITE };
 
-struct registered_language {
+enum SRCML_TRANSFORM_TYPE { SRCML_XPATH, SRCML_XSLT, SRCML_RELAXNG };
 
-  const char * extension;
-  int language;
+struct transform {
+
+  SRCML_TRANSFORM_TYPE type;
+  std::string transformation;
 
 };
 
@@ -85,6 +87,8 @@ struct srcml_archive {
   // utility
   srcMLReader * reader;
   xmlParserInputBufferPtr input;
+
+  std::vector<transform> transformations;
 };
 
 struct srcml_unit {
@@ -1224,11 +1228,17 @@ const char** srcml_list(const char* srcml_filename) {
 /* srcML XPath query and XSLT transform functions */
 int srcml_add_transform_xpath(srcml_archive* archive, const char* xpath_string) {
 
+  transform tran = { SRCML_XPATH, xpath_string };
+  archive->transformations.push_back(tran);
+
   return SRCML_STATUS_OK;
 
 }
 
-int srcml_add_transform_xslt(srcml_archive* archive, const char* xlst_filename) {
+int srcml_add_transform_xslt(srcml_archive* archive, const char* xslt_filename) {
+
+  transform tran = { SRCML_XSLT, xslt_filename };
+  archive->transformations.push_back(tran);
 
   return SRCML_STATUS_OK;
 
@@ -1236,11 +1246,18 @@ int srcml_add_transform_xslt(srcml_archive* archive, const char* xlst_filename) 
 
 int srcml_add_transform_relaxng(srcml_archive* archive, const char* relaxng_filename) {
 
+  transform tran = { SRCML_RELAXNG, relaxng_filename };
+  archive->transformations.push_back(tran);
+
   return SRCML_STATUS_OK;
 
 }
 
+
+// TODO finish.  what happends to intermediate results?
 int srcml_apply_transforms(srcml_archive* iarchive, srcml_archive* oarchive) {
+
+  iarchive->transformations.clear();
 
   return SRCML_STATUS_OK;
 
