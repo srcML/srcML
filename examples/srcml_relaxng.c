25f4c389cab62b7215f9dbf21b70f858e5aae938
Michael John Decker <mdecker6@kent.edu>
2013-08-31 20:32:11 +0000

    Adding a relaxng example and test xslt and relaxng input files
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/branches/libsrcml@15911 f70b1346-9fe7-0310-9bf3-95a0de396e6b

---
+++ examples/srcml_relaxng.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "srcml.h"
+
+int main(int argc, char * argv[]) {
+
+  struct srcml_archive * iarchive = srcml_create_archive();
+  struct srcml_archive * oarchive = srcml_create_archive();
+  srcml_write_open_filename(oarchive, "relaxng.xml");
+
+  srcml_archive_set_filename(iarchive, "project.xml");
+  
+  srcml_append_transform_xslt(iarchive, "schema.rng");
+
+  srcml_apply_transforms(iarchive, oarchive);
+
+  srcml_close_archive(oarchive);
+  srcml_free_archive(oarchive);
+
+  return 0;
+}
