21c35a3cf3c1bb2457e37a4e0abc8803e7928cc5
Michael John Decker <mdecker6@kent.edu>
2013-09-03 14:39:47 +0000

    Adding code for to create ctxt for memory so can copy for others not supplied by libxml or at least can not find
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/trunk@15920 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcMLUtility.cpp
+++ src/srcMLUtility.cpp
@@ -898,6 +898,45 @@ static xmlParserCtxtPtr srcMLCreateMemoryParserCtxt(const char * buffer, int siz
 
   return ctxt;
 }
+#if 0
+xmlParserCtxtPtr
+xmlCreateMemoryParserCtxt(const char *buffer, int size) {
+  xmlParserCtxtPtr ctxt;
+  xmlParserInputPtr input;
+  xmlParserInputBufferPtr buf;
+
+  if (buffer == NULL)
+    return(NULL);
+  if (size <= 0)
+    return(NULL);
+
+  ctxt = xmlNewParserCtxt();
+  if (ctxt == NULL)
+    return(NULL);
+
+  /* TODO: xmlParserInputBufferCreateStatic, requires some serious changes */
+  buf = xmlParserInputBufferCreateMem(buffer, size, XML_CHAR_ENCODING_NONE);
+  if (buf == NULL) {
+    xmlFreeParserCtxt(ctxt);
+    return(NULL);
+  }
+
+  input = xmlNewInputStream(ctxt);
+  if (input == NULL) {
+    xmlFreeParserInputBuffer(buf);
+    xmlFreeParserCtxt(ctxt);
+    return(NULL);
+  }
+
+  input->filename = NULL;
+  input->buf = buf;
+  xmlBufResetInput(input->buf->buffer, input);
+
+  inputPush(ctxt, input);
+  return(ctxt);
+}
+
+#endif
 
 extern "C" {
 
