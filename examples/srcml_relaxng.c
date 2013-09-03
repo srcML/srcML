197cedb4d7d4c713c161637c9456c04fe45e79e5
Michael John Decker <mdecker6@kent.edu>
2013-09-03 15:49:51 +0000

    Changing to use read open for relaxng and xpath
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/branches/libsrcml@15932 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- examples/srcml_relaxng.c
+++ examples/srcml_relaxng.c
@@ -7,7 +7,7 @@ int main(int argc, char * argv[]) {
 
   struct srcml_archive * iarchive = srcml_create_archive();
   struct srcml_archive * oarchive = srcml_create_archive();
-  srcml_read_open_filename(oarchive, "project.xml");
+  srcml_read_open_filename(iarchive, "project.xml");
   srcml_write_open_filename(oarchive, "relaxng.xml");
 
   srcml_append_transform_relaxng(iarchive, "schema.rng");
