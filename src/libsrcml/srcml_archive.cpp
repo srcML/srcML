4494c15a04e278a4f92a13a28c826217a9a986a8
Michael John Decker <mdecker6@kent.edu>
2013-09-05 15:12:54 +0000

    More new catch
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/trunk@16023 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/libsrcml/srcml_archive.cpp
+++ src/libsrcml/srcml_archive.cpp
@@ -407,7 +407,17 @@ void srcml_read_internal(srcml_archive * archive) {
 int srcml_read_open_filename(srcml_archive* archive, const char* srcml_filename) {
 
   archive->input = xmlParserInputBufferCreateFilename(srcml_filename, xmlParseCharEncoding(0));
-  archive->reader = new srcMLReader(srcml_filename);
+  try {
+
+    archive->reader = new srcMLReader(srcml_filename);
+
+  } catch(...) {
+
+    xmlFreeParserInputBuffer(archive->input);
+    return SRCML_STATUS_ERROR;
+
+  }
+
   srcml_read_internal(archive);
 
   return SRCML_STATUS_OK;
@@ -417,7 +427,17 @@ int srcml_read_open_filename(srcml_archive* archive, const char* srcml_filename)
 int srcml_read_open_memory(srcml_archive* archive, const char* buffer, size_t buffer_size) {
 
   archive->input = xmlParserInputBufferCreateMem(buffer, buffer_size, xmlParseCharEncoding(0));
-  archive->reader = new srcMLReader(archive->input);
+  try {
+
+    archive->reader = new srcMLReader(archive->input);
+
+  } catch(...) {
+
+    xmlFreeParserInputBuffer(archive->input);
+    return SRCML_STATUS_ERROR;
+
+  }
+
   srcml_read_internal(archive);
 
   return SRCML_STATUS_OK;
@@ -427,7 +447,17 @@ int srcml_read_open_memory(srcml_archive* archive, const char* buffer, size_t bu
 int srcml_read_open_FILE(srcml_archive* archive, FILE* srcml_file) {
 
   archive->input = xmlParserInputBufferCreateFile(srcml_file, xmlParseCharEncoding(0));
-  archive->reader = new srcMLReader(archive->input);
+  try {
+
+    archive->reader = new srcMLReader(archive->input);
+
+  } catch(...) {
+
+    xmlFreeParserInputBuffer(archive->input);
+    return SRCML_STATUS_ERROR;
+
+  }
+
   srcml_read_internal(archive);
 
   return SRCML_STATUS_OK;
@@ -437,7 +467,17 @@ int srcml_read_open_FILE(srcml_archive* archive, FILE* srcml_file) {
 int srcml_read_open_fd(srcml_archive* archive, int srcml_fd) {
 
   archive->input = xmlParserInputBufferCreateFd(srcml_fd, xmlParseCharEncoding(0));
-  archive->reader = new srcMLReader(archive->input);
+  try {
+
+    archive->reader = new srcMLReader(archive->input);
+
+  } catch(...) {
+
+    xmlFreeParserInputBuffer(archive->input);
+    return SRCML_STATUS_ERROR;
+
+  }
+
   srcml_read_internal(archive);
 
   return SRCML_STATUS_OK;
