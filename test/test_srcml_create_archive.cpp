3b8a3b9e664347fd6751bcf93ca20634d645bdd7
Michael John Decker <mdecker6@kent.edu>
2013-09-10 14:07:07 +0000

    Adding a testing file
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/branches/libsrcml@16133 f70b1346-9fe7-0310-9bf3-95a0de396e6b

---
+++ test/test_srcml_create_archive.cpp
@@ -0,0 +1,16 @@
+/*
+
+  Test cases for srcml_create_archive.
+ */
+#include <stdio.h>
+#include <string.h>
+
+#include <srcml.h>
+#include <srcml_types.hpp>
+
+int main(int argc, char * argv[]) {
+
+  srcml_archive * archive = srcml_create_archive();
+
+  return 0;
+}
