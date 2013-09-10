0f8a93005251e8a957edc8c2508ae38fa287d217
Michael John Decker <mdecker6@kent.edu>
2013-09-10 14:15:05 +0000

    Asserting more defaults on create
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/branches/libsrcml@16135 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- test/test_srcml_create_archive.cpp
+++ test/test_srcml_create_archive.cpp
@@ -21,7 +21,20 @@ int main(int argc, char * argv[]) {
     assert(archive->language == 0);
     assert(archive->directory == 0);
     assert(archive->version == 0);
-    assert(archive.attributes.size() == 0);
+    assert(archive->attributes.size() == 0);
+
+    //assert(archive->options == 0);
+
+    int tabstop;
+    std::vector<std::string> prefixes;
+    std::vector<std::string> namespaces;
+    std::vector<pair> registered_languages;
+
+    assert(archive->translator == 0);
+    assert(archive->reader == 0);
+    assert(archive->input == 0);
+
+    assert(archive->transformations.size() = 0);
   }
 
   return 0;
