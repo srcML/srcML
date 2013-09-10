442d98fc8fc1cfaa41d4666e534c20d503e684d9
Michael John Decker <mdecker6@kent.edu>
2013-09-10 14:12:18 +0000

    Adding asserts
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/branches/libsrcml@16134 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- test/test_srcml_create_archive.cpp
+++ test/test_srcml_create_archive.cpp
@@ -4,13 +4,25 @@
  */
 #include <stdio.h>
 #include <string.h>
+#include <cassert>
 
 #include <srcml.h>
 #include <srcml_types.hpp>
 
 int main(int argc, char * argv[]) {
 
-  srcml_archive * archive = srcml_create_archive();
+  {
+
+    srcml_archive * archive = srcml_create_archive();
+    assert(archive->type == SRCML_ARCHIVE_INVALID);
+    assert(archive->filename == 0);
+    assert(archive->encoding == 0);
+    assert(archive->xml_encoding == 0);
+    assert(archive->language == 0);
+    assert(archive->directory == 0);
+    assert(archive->version == 0);
+    assert(archive.attributes.size() == 0);
+  }
 
   return 0;
 }
