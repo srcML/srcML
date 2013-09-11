3684c9a9b9669eb3ce760d563a73749405c3d5cf
Michael John Decker <mdecker6@kent.edu>
2013-09-11 03:38:42 +0000

    Adding global tests
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/branches/libsrcml@16181 f70b1346-9fe7-0310-9bf3-95a0de396e6b

---
+++ test/test_srcml_global.cpp
@@ -0,0 +1,155 @@
+/*
+
+  Test cases for srcml_archive_set_*.
+*/
+#include <stdio.h>
+#include <string.h>
+#include <cassert>
+
+#include <srcml.h>
+#include <srcml_types.hpp>
+#include <srcmlns.hpp>
+
+int main(int argc, char * argv[]) {
+
+  /* 
+     srcml_get_encoding
+   */
+
+  {
+
+    srcml_archive * archive = srcml_create_archive();
+    archive->encoding = 0;
+    assert(srcml_archive_get_encoding(archive) == 0);
+    srcml_free_archive(archive);
+  }
+
+
+  {
+
+    srcml_archive * archive = srcml_create_archive();
+    archive->encoding = new std::string("foo");
+    assert(srcml_archive_get_encoding(archive) == std::string("foo"));
+    srcml_free_archive(archive);
+  }
+
+  /* 
+     srcml_get_language
+   */
+
+  {
+
+    srcml_archive * archive = srcml_create_archive();
+    archive->language = 0;
+    assert(srcml_archive_get_language(archive) == 0);
+    srcml_free_archive(archive);
+  }
+
+  {
+
+    srcml_archive * archive = srcml_create_archive();
+    archive->language = new std::string("foo");
+    assert(srcml_archive_get_language(archive) == std::string("foo"));
+    srcml_free_archive(archive);
+  }
+
+  /* 
+     srcml_get_filename
+   */
+
+  {
+
+    srcml_archive * archive = srcml_create_archive();
+    archive->filename = 0;
+    assert(srcml_archive_get_filename(archive) == 0);
+    srcml_free_archive(archive);
+  }
+
+  {
+
+    srcml_archive * archive = srcml_create_archive();
+    archive->filename = new std::string("foo");
+    assert(srcml_archive_get_filename(archive) == std::string("foo"));
+    srcml_free_archive(archive);
+  }
+
+  /* 
+     srcml_get_directory
+   */
+
+  {
+
+    srcml_archive * archive = srcml_create_archive();
+    archive->directory = 0;
+    assert(srcml_archive_get_directory(archive) == 0);
+    srcml_free_archive(archive);
+  }
+
+  {
+
+    srcml_archive * archive = srcml_create_archive();
+    archive->directory = new std::string("foo");
+    assert(srcml_archive_get_directory(archive) == std::string("foo"));
+    srcml_free_archive(archive);
+  }
+
+  /* 
+     srcml_get_version
+   */
+
+  {
+
+    srcml_archive * archive = srcml_create_archive();
+    archive->version = 0;
+    assert(srcml_archive_get_version(archive) == 0);
+    srcml_free_archive(archive);
+  }
+
+  {
+
+    srcml_archive * archive = srcml_create_archive();
+    archive->version = new std::string("foo");
+    assert(srcml_archive_get_version(archive) == std::string("foo"));
+    srcml_free_archive(archive);
+  }
+
+  /*
+    srcml_archive_get_option
+   */
+
+  {
+
+    srcml_archive * archive = srcml_create_archive();
+
+    archive->options = 1;
+
+    assert(srcml_archive_get_options(archive) == 1);
+    srcml_free_archive(archive);
+  }
+
+  {
+
+    srcml_archive * archive = srcml_create_archive();
+
+    archive->options = 1 | 2;
+
+    assert(srcml_archive_get_options(archive) == (1 | 2));
+    srcml_free_archive(archive);
+  }
+
+  /*
+    srcml_archive_get_tabstop
+  */
+
+  {
+
+    srcml_archive * archive = srcml_create_archive();
+
+    archive->tabstop = 4;
+
+    assert(srcml_archive_get_tabstop(archive) == 4);
+    srcml_free_archive(archive);
+  }
+
+  return 0;
+}
