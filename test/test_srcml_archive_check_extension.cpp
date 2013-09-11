9d2ff30bac2058a9e0e2731bf40087f1127d3030
Michael John Decker <mdecker6@kent.edu>
2013-09-11 17:44:10 +0000

    archive check extension tests
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/branches/libsrcml@16248 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- test/test_srcml_archive_check_extension.cpp
+++ test/test_srcml_archive_check_extension.cpp
@@ -10,122 +10,32 @@
 #include <srcml_types.hpp>
 #include <srcmlns.hpp>
 
-extern srcml_archive global_archive;
-
 int main(int argc, char * argv[]) {
-
-  /* 
-     srcml_check_language
-   */
-
-  {
-    assert(srcml_check_language("C") == 1);
-  }
-
-  {
-    assert(srcml_check_language("C++") == 2);
-  }
-
-  {
-    assert(srcml_check_language("Java") == 8);
-  }
-
-  {
-    assert(srcml_check_language("C#") == 32);
-  }
-
-  /* 
-     srcml_language_list
-   */
-
-  {
-    assert(srcml_language_list()[0] == std::string("C"));
-  }
-
-  {
-    assert(srcml_language_list()[1] == std::string("C++"));
-  }
-
-  {
-    assert(srcml_language_list()[2] == std::string("C#"));
-  }
-
-  {
-    assert(srcml_language_list()[3] == std::string("Java"));
-  }
-
-  {
-    assert(srcml_language_list()[4] == 0);
-  }
 
   /* 
      srcml_check_extension
    */
 
+  srcml_archive * archive = srcml_create_archive();
+  srcml_archive_register_file_extension(archive, "cpp", "C++");
+
   {
-    srcml_register_file_extension("cpp", "C++");
-    assert(srcml_check_extension("a.cpp") == std::string("C++"));
+    assert(srcml_archive_check_extension(archive, "a.cpp") == std::string("C++"));
   }
 
   {
-    assert(srcml_check_extension("a.cpp.gz") == std::string("C++"));
+    assert(srcml_archive_check_extension(archive, "a.cpp.gz") == std::string("C++"));
   }
 
   {
-    assert(srcml_check_extension("a.foo") == 0);
+    assert(srcml_archive_check_extension(archive, "a.foo") == 0);
   }
 
   {
-    assert(srcml_check_extension(0) == 0);
+    assert(srcml_archive_check_extension(archive, 0) == 0);
   }
 
-  /* 
-     srcml_check_format
-   */
-
-  {
-    assert(srcml_check_format("a.tar") != 0);
-  }
-
-  {
-    assert(srcml_check_format("a.tar.bz2") != 0);
-  }
-
-  {
-    assert(srcml_check_format("a.tar.gz") != 0);
-  }
-
-  {
-    assert(srcml_check_format("a.tar.xz") != 0);
-  }
-
-  {
-    assert(srcml_check_format("a.xz") != 0);
-  }
-
-  {
-    assert(srcml_check_format("a.foo") == 0);
-  }
-
-  {
-    assert(srcml_check_format(0) == 0);
-  }
-
-  /* 
-     srcml_check_encoding
-   */
-
-  {
-    assert(srcml_check_encoding("UTF-8") != 0);
-  }
-
-  {
-    assert(srcml_check_encoding("foo") == 0);
-  }
-
-  {
-    assert(srcml_check_encoding(0) == 0);
-  }
+  srcml_free_archive(archive);
 
   return 0;
 
