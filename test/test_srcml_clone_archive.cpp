60b9276269109d06cbb0ffce264d70d404f480ab
Michael John Decker <mdecker6@kent.edu>
2013-09-11 19:25:51 +0000

    Started testing
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/branches/libsrcml@16256 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- test/test_srcml_clone_archive.cpp
+++ test/test_srcml_clone_archive.cpp
@@ -35,52 +35,54 @@ int main(int argc, char * argv[]) {
     transform trans = {SRCML_XPATH, "//src:unit"};
     archive->transformations.push_back(trans);
 
+    srcml_archive * new_archive = srcml_clone_archive(archive);
 
-    assert(archive->type == SRCML_ARCHIVE_INVALID);
-    assert(*archive->filename == "f");
-    assert(*archive->encoding == "e");
-    assert(archive->xml_encoding == 0);
-    assert(*archive->language == "l");
-    assert(*archive->directory == "d");
-    assert(*archive->version == "v");
-    assert(archive->attributes.size() == 2);
-    assert(archive->attributes.at(0) == "a");
-    assert(archive->attributes.at(1) == "a");
+    assert(new_archive->type == SRCML_ARCHIVE_INVALID);
+    assert(*new_archive->filename == "f");
+    assert(*new_archive->encoding == "e");
+    assert(new_archive->xml_encoding == 0);
+    assert(*new_archive->language == "l");
+    assert(*new_archive->directory == "d");
+    assert(*new_archive->version == "v");
+    assert(new_archive->attributes.size() == 2);
+    assert(new_archive->attributes.at(0) == "a");
+    assert(new_archive->attributes.at(1) == "a");
 
-    assert(archive->options == (1 | 2));
+    assert(new_archive->options == (1 | 2));
 
-    assert(archive->tabstop == 4);
-    assert(archive->prefixes.size() == 8);
-    assert(archive->namespaces.size() == 8);
-    assert(archive->registered_languages.size() == archive->registered_languages.size());
+    assert(new_archive->tabstop == 4);
+    assert(new_archive->prefixes.size() == 8);
+    assert(new_archive->namespaces.size() == 8);
+    assert(new_archive->registered_languages.size() == archive->registered_languages.size());
 
-    assert(archive->prefixes.at(0) == SRCML_SRC_NS_PREFIX_DEFAULT);
-    assert(archive->namespaces.at(0) == SRCML_SRC_NS_URI);
-    assert(archive->prefixes.at(1) == "c");
-    assert(archive->namespaces.at(1) == SRCML_CPP_NS_URI);
-    assert(archive->prefixes.at(2) == SRCML_ERR_NS_PREFIX_DEFAULT);
-    assert(archive->namespaces.at(2) == SRCML_ERR_NS_URI);
-    assert(archive->prefixes.at(3) == SRCML_EXT_LITERAL_NS_PREFIX_DEFAULT);
-    assert(archive->namespaces.at(3) == SRCML_EXT_LITERAL_NS_URI);
-    assert(archive->prefixes.at(4) == SRCML_EXT_OPERATOR_NS_PREFIX_DEFAULT);
-    assert(archive->namespaces.at(4) == SRCML_EXT_OPERATOR_NS_URI);
-    assert(archive->prefixes.at(5) == SRCML_EXT_MODIFIER_NS_PREFIX_DEFAULT);
-    assert(archive->namespaces.at(5) == SRCML_EXT_MODIFIER_NS_URI);
-    assert(archive->prefixes.at(6) == SRCML_EXT_POSITION_NS_PREFIX_DEFAULT);
-    assert(archive->namespaces.at(6) == SRCML_EXT_POSITION_NS_URI);
-    assert(archive->prefixes.at(7) == "foo");
-    assert(archive->namespaces.at(7) == "bar");
+    assert(new_archive->prefixes.at(0) == SRCML_SRC_NS_PREFIX_DEFAULT);
+    assert(new_archive->namespaces.at(0) == SRCML_SRC_NS_URI);
+    assert(new_archive->prefixes.at(1) == "c");
+    assert(new_archive->namespaces.at(1) == SRCML_CPP_NS_URI);
+    assert(new_archive->prefixes.at(2) == SRCML_ERR_NS_PREFIX_DEFAULT);
+    assert(new_archive->namespaces.at(2) == SRCML_ERR_NS_URI);
+    assert(new_archive->prefixes.at(3) == SRCML_EXT_LITERAL_NS_PREFIX_DEFAULT);
+    assert(new_archive->namespaces.at(3) == SRCML_EXT_LITERAL_NS_URI);
+    assert(new_archive->prefixes.at(4) == SRCML_EXT_OPERATOR_NS_PREFIX_DEFAULT);
+    assert(new_archive->namespaces.at(4) == SRCML_EXT_OPERATOR_NS_URI);
+    assert(new_archive->prefixes.at(5) == SRCML_EXT_MODIFIER_NS_PREFIX_DEFAULT);
+    assert(new_archive->namespaces.at(5) == SRCML_EXT_MODIFIER_NS_URI);
+    assert(new_archive->prefixes.at(6) == SRCML_EXT_POSITION_NS_PREFIX_DEFAULT);
+    assert(new_archive->namespaces.at(6) == SRCML_EXT_POSITION_NS_URI);
+    assert(new_archive->prefixes.at(7) == "foo");
+    assert(new_archive->namespaces.at(7) == "bar");
 
-    assert(archive->translator == 0);
-    assert(archive->reader == 0);
-    assert(archive->input == 0);
+    assert(new_archive->translator == 0);
+    assert(new_archive->reader == 0);
+    assert(new_archive->input == 0);
 
-    assert(archive->transformations.size() == 0);
+    assert(new_archive->transformations.size() == 1);
 
     archive->translator = 0;
     archive->reader = 0;
     archive->input = 0;
     srcml_free_archive(archive);
+    srcml_free_archive(new_archive);
   }
 
   return 0;
