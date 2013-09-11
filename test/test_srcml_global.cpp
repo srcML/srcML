6010f08520267d2c0c9287772dd92f945256e9d6
Michael John Decker <mdecker6@kent.edu>
2013-09-11 15:28:40 +0000

    Adding more tests for global get/set
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/branches/libsrcml@16208 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- test/test_srcml_global.cpp
+++ test/test_srcml_global.cpp
@@ -26,6 +26,77 @@ int main(int argc, char * argv[]) {
     assert(srcml_get_encoding() == std::string("foo"));
   }
 
+  /* 
+     srcml_get_language
+   */
+
+  {
+    srcml_set_language(0);
+    assert(srcml_get_language() == 0);
+  }
+
+  {
+    srcml_set_language("foo");
+    assert(srcml_get_language() == std::string("foo"));
+  }
+
+  /* 
+     srcml_get_language
+   */
+
+  {
+    srcml_set_language(0);
+    assert(srcml_get_language() == 0);
+  }
+
+  {
+    srcml_set_language("foo");
+    assert(srcml_get_language() == std::string("foo"));
+  }
+
+  /* 
+     srcml_get_filename
+   */
+
+  {
+    srcml_set_filename(0);
+    assert(srcml_get_filename() == 0);
+  }
+
+  {
+    srcml_set_filename("foo");
+    assert(srcml_get_filename() == std::string("foo"));
+  }
+
+  /* 
+     srcml_get_directory
+   */
+
+  {
+    srcml_set_directory(0);
+    assert(srcml_get_directory() == 0);
+  }
+
+  {
+    srcml_set_directory("foo");
+    assert(srcml_get_directory() == std::string("foo"));
+  }
+
+  /* 
+     srcml_get_version
+   */
+
+  {
+    srcml_set_version(0);
+    assert(srcml_get_version() == 0);
+  }
+
+  {
+    srcml_set_version("foo");
+    assert(srcml_get_version() == std::string("foo"));
+  }
+
+
   return 0;
 
 }
