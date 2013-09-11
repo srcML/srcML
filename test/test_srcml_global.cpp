bdc69f378c2684b0a333463b2930b5a89638d785
Michael John Decker <mdecker6@kent.edu>
2013-09-11 15:34:35 +0000

    Switching to use global archive extern for testing
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/branches/libsrcml@16209 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- test/test_srcml_global.cpp
+++ test/test_srcml_global.cpp
@@ -10,7 +10,93 @@
 #include <srcml_types.hpp>
 #include <srcmlns.hpp>
 
+extern srcml_archive global_archive;
+
 int main(int argc, char * argv[]) {
+
+  /* 
+     srcml_get_encoding
+   */
+
+  {
+    global_archive.encoding = 0;
+    assert(srcml_get_encoding() == 0);
+  }
+
+  {
+    global_archive.encoding = "foo";
+    assert(srcml_get_encoding() == std::string("foo"));
+  }
+
+  /* 
+     srcml_get_language
+   */
+
+  {
+    global_archive.language = 0;
+    assert(srcml_get_language() == 0);
+  }
+
+  {
+    global_archive.language = "foo";
+    assert(srcml_get_language() == std::string("foo"));
+  }
+
+  /* 
+     srcml_get_language
+   */
+
+  {
+    global_archive.language = 0;
+    assert(srcml_get_language() == 0);
+  }
+
+  {
+    global_archive.language = "foo";
+    assert(srcml_get_language() == std::string("foo"));
+  }
+
+  /* 
+     srcml_get_filename
+   */
+
+  {
+    global_archive.filename = 0;
+    assert(srcml_get_filename() == 0);
+  }
+
+  {
+    global_archive.filename = "foo";
+    assert(srcml_get_filename() == std::string("foo"));
+  }
+
+  /* 
+     srcml_get_directory
+   */
+
+  {
+    global_archive.directory = 0;
+    assert(srcml_get_directory() == 0);
+  }
+
+  {
+    global_archive.directory = "foo";
+    assert(srcml_get_directory() == std::string("foo"));
+  }
+
+  /* 
+     srcml_get_version
+   */
+
+  {
+    global_archive.version = 0;
+    assert(srcml_get_version() == 0);
+  }
+
+  {
+    global_archive.version = "foo";
+    assert(srcml_get_version() == std::string("foo"));
+  }
 
   /* 
      srcml_get_encoding
