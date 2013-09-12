74a9e51f3692762a917ff91a71786762b7bfbf5b
Michael John Decker <mdecker6@kent.edu>
2013-09-12 20:14:24 +0000

    Creating a set unit tests
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/branches/libsrcml@16304 f70b1346-9fe7-0310-9bf3-95a0de396e6b

---
+++ test/test_srcml_unit_set.cpp
@@ -0,0 +1,114 @@
+/*
+
+  Test cases for srcml_archive_set_*.
+*/
+#include <stdio.h>
+#include <string.h>
+#include <cassert>
+
+#include <srcml.h>
+#include <srcml_types.hpp>
+#include <srcmlns.hpp>
+
+int main(int argc, char * argv[]) {
+
+  srcml_archive * archive;
+
+  /* 
+     srcml_set_language
+   */
+
+  {
+
+    srcml_unit * unit = srcml_create_unit(archive);
+    srcml_unit_set_language(unit, 0);
+    assert(unit->language == 0);
+    srcml_free_unit(unit);
+  }
+
+  {
+
+    srcml_unit * unit = srcml_create_unit(archive);
+    srcml_unit_set_language(unit, "foo");
+    assert(*unit->language == "foo");
+    srcml_free_unit(unit);
+  }
+
+  {
+    assert(srcml_unit_set_language(0, "foo") == SRCML_STATUS_ERROR);
+  }
+
+  /* 
+     srcml_set_filename
+   */
+
+  {
+
+    srcml_unit * unit = srcml_create_unit(archive);
+    srcml_unit_set_filename(unit, 0);
+    assert(unit->filename == 0);
+    srcml_free_unit(unit);
+  }
+
+  {
+
+    srcml_unit * unit = srcml_create_unit(archive);
+    srcml_unit_set_filename(unit, "foo");
+    assert(*unit->filename == "foo");
+    srcml_free_unit(unit);
+  }
+
+  {
+    assert(srcml_unit_set_filename(0, "foo") == SRCML_STATUS_ERROR);
+  }
+
+  /* 
+     srcml_set_directory
+   */
+
+  {
+
+    srcml_unit * unit = srcml_create_unit(archive);
+    srcml_unit_set_directory(unit, 0);
+    assert(unit->directory == 0);
+    srcml_free_unit(unit);
+  }
+
+  {
+
+    srcml_unit * unit = srcml_create_unit(archive);
+    srcml_unit_set_directory(unit, "foo");
+    assert(*unit->directory == "foo");
+    srcml_free_unit(unit);
+  }
+
+  {
+    assert(srcml_unit_set_directory(0, "foo") == SRCML_STATUS_ERROR);
+  }
+
+  /* 
+     srcml_set_version
+   */
+
+  {
+
+    srcml_unit * unit = srcml_create_unit(archive);
+    srcml_unit_set_version(unit, 0);
+    assert(unit->version == 0);
+    srcml_free_unit(unit);
+  }
+
+  {
+
+    srcml_unit * unit = srcml_create_unit(archive);
+    srcml_unit_set_version(unit, "foo");
+    assert(*unit->version == "foo");
+    srcml_free_unit(unit);
+  }
+
+  {
+    assert(srcml_unit_set_version(0, "foo") == SRCML_STATUS_ERROR);
+  }
+
+  return 0;
+}
