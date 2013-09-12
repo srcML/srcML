e67a1ac75d2ff494b9d5be936ea32c1b5461f285
Michael John Decker <mdecker6@kent.edu>
2013-09-12 20:30:17 +0000

    Test srcml create unit
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/branches/libsrcml@16311 f70b1346-9fe7-0310-9bf3-95a0de396e6b

---
+++ test/test_srcml_create_unit.cpp
@@ -0,0 +1,38 @@
+/*
+
+  Test cases for srcml_create_archive.
+*/
+#include <stdio.h>
+#include <string.h>
+#include <cassert>
+
+#include <srcml.h>
+#include <srcml_types.hpp>
+#include <srcmlns.hpp>
+
+int main(int argc, char * argv[]) {
+
+  {
+
+    srcml_archive * archive = srcml_create_archive();
+    srcml_unit * unit = srcml_create_unit(archive);
+   
+    assert(unit->archive == archive);
+    assert(unit->language == 0);
+    assert(unit->filename == 0);
+    assert(unit->directory == 0);
+    assert(unit->version == 0);
+    assert(unit->unit == 0);
+
+    srcml_free_unit(unit);
+    srcml_free_archive(archive);
+  }
+
+  {
+
+    assert(srcml_create_unit(0) == 0);
+   
+  }
+
+  return 0;
+}
