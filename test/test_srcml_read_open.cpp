cf313758c7f58eeef2b0c250ac2d28367788fcd7
Michael John Decker <mdecker6@kent.edu>
2013-09-12 19:10:57 +0000

    Testing some reads
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/branches/libsrcml@16288 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- test/test_srcml_read_open.cpp
+++ test/test_srcml_read_open.cpp
@@ -6,6 +6,7 @@
 #include <string.h>
 #include <cassert>
 #include <fstream>
+#include <unistd.h>
 
 #include <srcml.h>
 #include <srcml_types.hpp>
@@ -13,13 +14,15 @@
 
 int main(int argc, char * argv[]) {
 
+  const std::string srcml = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n<unit xmlns=\"http://www.sdml.info/srcML/src\">\n\n<unit xmlns:cpp=\"http://www.sdml.info/srcML/cpp\" language=\"C++\" filename=\"a.cpp\"><expr_stmt><expr><name>a</name></expr>;</expr_stmt>\n</unit>\n\n<unit xmlns:cpp=\"http://www.sdml.info/srcML/cpp\" language=\"C++\" filename=\"b.cpp\"><expr_stmt><expr><name>b</name></expr>;</expr_stmt>\n</unit>\n\n</unit>";
+
+  std::ofstream srcml_file("project.xml");
+  srcml_file << srcml;
+  srcml_file.close();
+
   /* 
      srcml_read_open_filename
   */
-  const std::string srcml = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n<unit xmlns=\"http://www.sdml.info/srcML/src\">\n\n<unit xmlns:cpp=\"http://www.sdml.info/srcML/cpp\" language=\"C++\" filename=\"a.cpp\"><expr_stmt><expr><name>a</name></expr>;</expr_stmt>\n</unit>\n\n<unit xmlns:cpp=\"http://www.sdml.info/srcML/cpp\" language=\"C++\" filename=\"b.cpp\"><expr_stmt><expr><name>b</name></expr>;</expr_stmt>\n
-</unit>\n\n</unit>";
-
-  std::ofstream("projectxml");
 
   {
 
@@ -34,6 +37,15 @@ int main(int argc, char * argv[]) {
   {
 
   srcml_archive * archive = srcml_create_archive();
+  assert(srcml_read_open_filename(archive, "foobar.xml") == SRCML_STATUS_ERROR);
+
+  srcml_free_archive(archive);
+
+  }
+
+  {
+
+  srcml_archive * archive = srcml_create_archive();
   assert(srcml_read_open_filename(archive, 0) == SRCML_STATUS_ERROR);
 
   srcml_free_archive(archive);
@@ -41,7 +53,43 @@ int main(int argc, char * argv[]) {
   }
 
   {
-  assert(srcml_read_open_filename(0, "a.cpp.xml") == SRCML_STATUS_ERROR);
+  assert(srcml_read_open_filename(0, "project.xml") == SRCML_STATUS_ERROR);
+  }
+
+  /* 
+     srcml_read_open_memory
+  */
+
+  {
+
+  srcml_archive * archive = srcml_create_archive();
+  srcml_read_open_memory(archive, srcml.c_str(), srcml.size());
+
+  srcml_close_archive(archive);
+  srcml_free_archive(archive);
+
+  }
+
+  {
+
+  srcml_archive * archive = srcml_create_archive();
+  assert(srcml_read_open_memory(archive, 0, 1) == SRCML_STATUS_ERROR);
+
+  srcml_free_archive(archive);
+
+  }
+
+  {
+
+  srcml_archive * archive = srcml_create_archive();
+  assert(srcml_read_open_memory(archive, srcml.c_str(), 0) == SRCML_STATUS_ERROR);
+
+  srcml_free_archive(archive);
+
+  }
+
+  {
+    assert(srcml_read_open_memory(0, srcml.c_str(), srcml.size()) == SRCML_STATUS_ERROR);
   }
 
   return 0;
