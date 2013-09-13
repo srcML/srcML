ccbd2d20d4ef97989bc46d02845b973e82034f81
Michael John Decker <mdecker6@kent.edu>
2013-09-13 19:13:09 +0000

    Testing xslt and relaxNG append
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/branches/libsrcml@16360 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- test/test_srcml_append_transform.cpp
+++ test/test_srcml_append_transform.cpp
@@ -37,6 +37,55 @@ int main(int argc, char * argv[]) {
     assert(srcml_append_transform_xpath(0, "//src:unit") == SRCML_STATUS_ERROR);
   }
 
+  /* 
+     srcml_append_transform_xslt
+   */
+
+  {
+    srcml_archive * archive = srcml_create_archive();
+    srcml_append_transform_xslt(archive, "copy.xsl");
+
+    assert(archive->transformations.back().type == SRCML_XSLT);
+    assert(archive->transformations.back().transformation == "copy.xsl");
+
+    srcml_free_archive(archive);  
+  }
+
+  {
+    srcml_archive * archive = srcml_create_archive();
+    assert(srcml_append_transform_xslt(archive, 0) == SRCML_STATUS_ERROR);
+
+    srcml_free_archive(archive);  
+  }
+
+  {
+    assert(srcml_append_transform_xslt(0, "copy.xsl") == SRCML_STATUS_ERROR);
+  }
+
+  /* 
+     srcml_append_transform_xslt
+   */
+
+  {
+    srcml_archive * archive = srcml_create_archive();
+    srcml_append_transform_relaxng(archive, "schema.rng");
+
+    assert(archive->transformations.back().type == SRCML_RELAXNG);
+    assert(archive->transformations.back().transformation == "schema.rng");
+
+    srcml_free_archive(archive);  
+  }
+
+  {
+    srcml_archive * archive = srcml_create_archive();
+    assert(srcml_append_transform_relaxng(archive, 0) == SRCML_STATUS_ERROR);
+
+    srcml_free_archive(archive);  
+  }
+
+  {
+    assert(srcml_append_transform_relaxng(0, "schema.rng") == SRCML_STATUS_ERROR);
+  }
 
   return 0;
 
