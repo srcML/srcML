642723866b4794979b4ab9d3d4624dfb9623f71f
Michael John Decker <mdecker6@kent.edu>
2013-09-13 16:02:42 +0000

    Making read unit
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/branches/libsrcml@16341 f70b1346-9fe7-0310-9bf3-95a0de396e6b

---
+++ test/test_srcml_read_unit.cpp
@@ -0,0 +1,77 @@
+/*
+
+  Test cases for srcml_read_unit
+*/
+#include <stdio.h>
+#include <string.h>
+#include <cassert>
+#include <fstream>
+#include <unistd.h>
+#include <fcntl.h>
+
+#include <srcml.h>
+#include <srcml_types.hpp>
+#include <srcmlns.hpp>
+
+int main(int argc, char * argv[]) {
+  const std::string srcml_a = "<unit xmlns=\"http://www.s\
+dml.info/srcML/src\">\n\n<unit xmlns:cpp=\"http://www.sdml.info/srcML/cpp\" language=\"C\" filename=\"project.c\"><expr_stmt><expr><name>a</name></expr>;</expr_stmt>\n</unit>";
+
+  const std::string srcml_b = "<s:unit xmlns:s=\"h\
+ttp://www.sdml.info/srcML/src\" xmlns:cpp=\"http://www.sdml.info/srcML/cpp\" language=\"C++\" dir=\"test\" filename=\"project\" version=\"1\"><s:expr_stmt><s:expr><s:name>b</s:name></s:expr>;</s:expr_stmt>\n</s:unit>";
+
+  const std::string srcml = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n<unit xmlns=\"http://www.sdml.info/srcML/src\">\n\n<unit xmlns:cpp=\"http://www.sdml.info/srcML/cpp\" language=\"C\" filename=\"project.c\"><expr_stmt><expr><name>a</name></expr>;</expr_stmt>\n</unit>\n\n</unit>\n";
+  const std::string srcml_full = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n<s:unit xmlns:s=\"http://www.sdml.info/srcML/src\">\n\n<s:unit xmlns:cpp=\"http://www.sdml.info/srcML/cpp\" language=\"C++\" dir=\"test\" filename=\"project\" version=\"1\"><s:expr_stmt><s:expr><s:name>b</s:name></s:expr>;</s:expr_stmt>\n</s:unit>\n\n</s:unit>\n";
+  const std::string srcml_single = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n<s:unit xmlns:s=\"http://www.sdml.info/srcML/src\" xmlns:cpp=\"http://www.sdml.info/srcML/cpp\" language=\"C++\" dir=\"test\" filename=\"project\" version=\"1\"><s:expr_stmt><s:expr><s:name>b</s:name></s:expr>;</s:expr_stmt>\n</s:unit>\n";
+
+  /*
+    srcml_read_unit
+   */
+
+  {
+
+    char * s;
+    srcml_archive * archive = srcml_create_archive();
+    srcml_read_open_memory(archive, srcml.c_str(), srcml.size());
+    srcml_unit * unit = srcml_read_unit(archive);
+    assert(s == srcml_a);
+   
+    srcml_free_unit(unit);
+    srcml_close_archive(archive);
+    srcml_free_archive(archive);
+  }
+
+  {
+
+    char * s;
+    srcml_archive * archive = srcml_create_archive();
+    srcml_read_open_memory(archive, srcml_full.c_str(), srcml_full.size());
+    srcml_unit * unit = srcml_read_unit(archive);
+    assert(srcml_unit_get_language(unit) == std::string("C++"));
+    assert(srcml_unit_get_filename(unit) == std::string("project"));
+    assert(srcml_unit_get_directory(unit) == std::string("test"));
+    assert(srcml_unit_get_version(unit) == std::string("1"));
+   
+    srcml_free_unit(unit);
+    srcml_close_archive(archive);
+    srcml_free_archive(archive);
+  }
+
+  {
+
+    char * s;
+    srcml_archive * archive = srcml_create_archive();
+    srcml_read_open_memory(archive, srcml_single.c_str(), srcml_single.size());
+    srcml_unit * unit = srcml_read_unit(archive);
+    assert(srcml_unit_get_language(unit) == std::string("C++"));
+    assert(srcml_unit_get_filename(unit) == std::string("project"));
+    assert(srcml_unit_get_directory(unit) == std::string("test"));
+    assert(srcml_unit_get_version(unit) == std::string("1"));
+   
+    srcml_free_unit(unit);
+    srcml_close_archive(archive);
+    srcml_free_archive(archive);
+  }
+
+  return 0;
+}
