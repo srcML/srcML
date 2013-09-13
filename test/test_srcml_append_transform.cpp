25433c9520f5000827518fe424fec6ef4bc03b25
Michael John Decker <mdecker6@kent.edu>
2013-09-13 22:22:57 +0000

    Testing all appends at once
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/branches/libsrcml@16365 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- test/test_srcml_append_transform.cpp
+++ test/test_srcml_append_transform.cpp
@@ -122,6 +122,28 @@ int main(int argc, char * argv[]) {
     assert(srcml_append_transform_relaxng(0, "schema.rng") == SRCML_STATUS_ERROR);
   }
 
+  /* 
+     srcml_append_transform_*
+   */
+
+  {
+    srcml_archive * archive = srcml_create_archive();
+    srcml_read_open_memory(archive, s.c_str(), s.size());
+    srcml_append_transform_xpath(archive, "//src:unit");
+    srcml_append_transform_xslt(archive, "copy.xsl");
+    srcml_append_transform_relaxng(archive, "schema.rng");
+
+    assert(archive->transformations.at(0).type == SRCML_XPATH);
+    assert(archive->transformations.at(0).transformation == "//src:unit");
+    assert(archive->transformations.at(1).type == SRCML_XSLT);
+    assert(archive->transformations.at(1).transformation == "copy.xsl");
+    assert(archive->transformations.back().type == SRCML_RELAXNG);
+    assert(archive->transformations.back().transformation == "schema.rng");
+
+    srcml_close_archive(archive);
+    srcml_free_archive(archive);  
+  }
+
   return 0;
 
 }
