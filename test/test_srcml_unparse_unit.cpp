0ef22a00eaad2454e4c14cb37ec9a0460d303bef
Michael John Decker <mdecker6@kent.edu>
2013-09-13 16:38:52 +0000

    Remove unparse tests that are not really testing unparse but read unit
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/branches/libsrcml@16348 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- test/test_srcml_unparse_unit.cpp
+++ test/test_srcml_unparse_unit.cpp
@@ -17,6 +17,7 @@ int main(int argc, char * argv[]) {
 
   const std::string src = "a;\n";
   const std::string srcml = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n<unit xmlns=\"http://www.sdml.info/srcML/src\">\n\n<unit xmlns:cpp=\"http://www.sdml.info/srcML/cpp\" language=\"C\" filename=\"project.c\"><expr_stmt><expr><name>a</name></expr>;</expr_stmt>\n</unit>\n\n</unit>\n";
+
   std::ofstream srcml_file("project.xml");
   srcml_file << srcml;
   srcml_file.close();
