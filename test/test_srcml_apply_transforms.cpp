21666e0a41682c3d69470db58983e86e14ab6a91
Michael John Decker <mdecker6@kent.edu>
2013-09-14 02:53:15 +0000

    More xslt tests
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/branches/libsrcml@16368 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- test/test_srcml_apply_transforms.cpp
+++ test/test_srcml_apply_transforms.cpp
@@ -17,7 +17,7 @@ int main(int argc, char * argv[]) {
   const std::string srcml_b = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n<s:unit xmlns:s=\"http://www.sdml.info/srcML/src\" xmlns:cpp=\"http://www.sdml.info/srcML/cpp\" language=\"C++\" dir=\"test\" filename=\"project\" version=\"1\"><s:expr_stmt><s:expr><s:name>b</s:name></s:expr>;</s:expr_stmt>\n</s:unit>\n";
   const std::string srcml = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n<unit xmlns=\"http://www.sdml.info/srcML/src\">\n\n<unit xmlns:cpp=\"http://www.sdml.info/srcML/cpp\" language=\"C++\" filename=\"a.cpp\"><expr_stmt><expr><name>a</name></expr>;</expr_stmt>\n</unit>\n\n</unit>\n";
   const std::string srcml_a_after = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n<unit xmlns=\"http://www.sdml.info/srcML/src\" filename=\"a.cpp\">\n\n<unit xmlns=\"http://www.sdml.info/srcML/src\" xmlns:cpp=\"http://www.sdml.info/srcML/cpp\" language=\"C++\" filename=\"a.cpp\"><expr_stmt><expr><name>a</name></expr>;</expr_stmt>\n</unit>\n\n</unit>\n";
-  const std::string srcml_b_after = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n<s:unit xmlns:s=\"http://www.sdml.info/srcML/src\">\n\n<s:unit xmlns:s=\"http://www.sdml.info/srcML/src\" xmlns:cpp=\"http://www.sdml.info/srcML/cpp\" language=\"C++\" dir=\"test\" filename=\"project\" version=\"1\"><s:expr_stmt><s:expr><s:name>b</s:name></s:expr>;</s:expr_stmt>\n</s:unit>\n\n</s:unit>\n";
+  const std::string srcml_b_after = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n<s:unit xmlns:s=\"http://www.sdml.info/srcML/src\" dir=\"test\" filename=\"project\" version=\"1\">\n\n<s:unit xmlns:s=\"http://www.sdml.info/srcML/src\" xmlns:cpp=\"http://www.sdml.info/srcML/cpp\" language=\"C++\" dir=\"test\" filename=\"project\" version=\"1\"><s:expr_stmt><s:expr><s:name>b</s:name></s:expr>;</s:expr_stmt>\n</s:unit>\n\n</s:unit>\n";
   const std::string srcml_full = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n<s:unit xmlns:s=\"http://www.sdml.info/srcML/src\">\n\n<s:unit xmlns:cpp=\"http://www.sdml.info/srcML/cpp\" language=\"C++\" dir=\"test\" filename=\"project\" version=\"1\"><s:expr_stmt><s:expr><s:name>b</s:name></s:expr>;</s:expr_stmt>\n</s:unit>\n\n</s:unit>\n";
 
   /* 
@@ -87,6 +87,23 @@ int main(int argc, char * argv[]) {
     srcml_close_archive(iarchive);
     srcml_free_archive(iarchive);  
     assert(s == srcml_a_after);
+  }
+
+  {
+    char * s;
+    srcml_archive * iarchive = srcml_create_archive();
+    srcml_read_open_memory(iarchive, srcml_b.c_str(), srcml_b.size());
+    srcml_append_transform_xslt(iarchive, "copy.xsl");
+    srcml_archive * oarchive = srcml_clone_archive(iarchive);
+    srcml_write_open_memory(oarchive, &s);
+
+    srcml_apply_transforms(iarchive, oarchive);
+
+    srcml_close_archive(oarchive);
+    srcml_free_archive(oarchive);  
+    srcml_close_archive(iarchive);
+    srcml_free_archive(iarchive);  
+    assert(s == srcml_b_after);
   }
 
   return 0;
