bdc5d7392831db0f1302a512fc0e53ef90d6b931
Michael John Decker <mdecker6@kent.edu>
2013-09-14 17:48:34 +0000

    Xslt tests
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/branches/libsrcml@16396 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- test/test_srcml_xslt.cpp
+++ test/test_srcml_xslt.cpp
@@ -5,7 +5,11 @@
 #include <stdio.h>
 #include <string.h>
 #include <cassert>
+#include <fstream>
+#include <unistd.h>
+#include <fcntl.h>
 
+#include <srcml_sax2_utilities.hpp>
 #include <srcml.h>
 #include <srcml_types.hpp>
 #include <srcmlns.hpp>
@@ -15,6 +19,99 @@ int main(int argc, char * argv[]) {
   /* 
      srcml_xslt
    */
+
+  {
+    const char * s = "<unit>a;</unit>";
+    std::ofstream file("input.xml");
+    file << s;
+    file.close();
+    xmlParserInputBufferPtr buffer_input = xmlParserInputBufferCreateFilename("input.xml", xmlParseCharEncoding(0));
+    const char * xslts[2] = {"copy.xsl", 0 };
+    int fd = open("project.xml", O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+    assert(srcml_xslt(buffer_input, "src:unit", xslts, 0, 0, fd, 0) == SRCML_STATUS_OK);
+    std::ifstream in("project.xml");
+    std::string output;
+    std::string temp;
+    while(in >> temp)
+      output += temp;
+    assert(output == "<?xmlversion=\"1.0\"encoding=\"\"standalone=\"yes\"?><unit>a;</unit>");
+    unlink("input.xml");
+    unlink("project.xml");
+  }
+
+  {
+    const char * xslts[2] = {"copy.xsl", 0 };
+    int fd = open("project.xml", O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+    assert(srcml_xslt(0, "src:unit", xslts, 0, 0, fd, 0) == SRCML_STATUS_ERROR);
+    unlink("project.xml");
+  }
+
+  {
+    const char * s = "<unit>a;</unit>";
+    std::ofstream file("input.xml");
+    file << s;
+    file.close();
+    xmlParserInputBufferPtr buffer_input = xmlParserInputBufferCreateFilename("input.xml", xmlParseCharEncoding(0));
+    const char * xslts[2] = {"copy.xsl", 0 };
+    int fd = open("project.xml", O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+    assert(srcml_xslt(buffer_input, 0, xslts, 0, 0, fd, 0) == SRCML_STATUS_OK);
+    std::ifstream in("project.xml");
+    std::string output;
+    std::string temp;
+    while(in >> temp)
+      output += temp;
+    assert(output == "<?xmlversion=\"1.0\"encoding=\"\"standalone=\"yes\"?><unit>a;</unit>");
+    unlink("input.xml");
+    unlink("project.xml");
+  }
+
+  {
+    const char * s = "<unit>a;</unit>";
+    std::ofstream file("input.xml");
+    file << s;
+    file.close();
+    xmlParserInputBufferPtr buffer_input = xmlParserInputBufferCreateFilename("input.xml", xmlParseCharEncoding(0));
+    int fd = open("project.xml", O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+    assert(srcml_xslt(buffer_input, "src:unit", 0, 0, 0, fd, 0) == SRCML_STATUS_OK);
+    std::ifstream in("project.xml");
+    std::string output;
+    std::string temp;
+    while(in >> temp)
+      output += temp;
+    assert(output == "<?xmlversion=\"1.0\"encoding=\"\"standalone=\"yes\"?><unit>a;</unit>");
+    unlink("input.xml");
+    unlink("project.xml");
+  }
+
+  {
+    const char * s = "<unit>a;</unit>";
+    std::ofstream file("input.xml");
+    file << s;
+    file.close();
+    xmlParserInputBufferPtr buffer_input = xmlParserInputBufferCreateFilename("input.xml", xmlParseCharEncoding(0));
+    const char * xslts[2] = {0, 0 };
+    int fd = open("project.xml", O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+    assert(srcml_xslt(buffer_input, "src:unit", xslts, 0, 0, fd, 0) == SRCML_STATUS_OK);
+    std::ifstream in("project.xml");
+    std::string output;
+    std::string temp;
+    while(in >> temp)
+      output += temp;
+    assert(output == "<?xmlversion=\"1.0\"encoding=\"\"standalone=\"yes\"?><unit>a;</unit>");
+    unlink("input.xml");
+    unlink("project.xml");
+  }
+
+  {
+    const char * s = "<unit>a;</unit>";
+    std::ofstream file("input.xml");
+    file << s;
+    file.close();
+    xmlParserInputBufferPtr buffer_input = xmlParserInputBufferCreateFilename("input.xml", xmlParseCharEncoding(0));
+    const char * xslts[2] = {"copy.xsl", 0 };
+    assert(srcml_xslt(buffer_input, "src:unit", xslts, 0, 0, -1, 0) == SRCML_STATUS_OK);
+    unlink("input.xml");
+  }
 
   return 0;
 
