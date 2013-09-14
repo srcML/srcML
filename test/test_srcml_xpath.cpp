b138e9bbb78fec2f2686a6ca5b1f101cb205d100
Michael John Decker <mdecker6@kent.edu>
2013-09-14 16:30:00 +0000

    More tests for sax2
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/branches/libsrcml@16383 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- test/test_srcml_xpath.cpp
+++ test/test_srcml_xpath.cpp
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <cassert>
+#include <srcml_sax2_utilities.hpp>
 
 #include <srcml.h>
 #include <srcml_types.hpp>
@@ -15,6 +16,27 @@ int main(int argc, char * argv[]) {
   /* 
      srcml_xpath
    */
+
+  {
+    const char * s = "<unit/>";
+    xmlParserCtxtPtr ctxt = srcMLCreateMemoryParserCtxt(s, strlen(s));
+    assert(ctxt != NULL);
+    xmlFreeParserCtxt(ctxt);
+  }
+
+  {
+    const char * s = "<unit/>";
+    xmlParserCtxtPtr ctxt = srcMLCreateMemoryParserCtxt(s, 0);
+    assert(ctxt == NULL);
+  }
+
+  {
+    xmlParserCtxtPtr ctxt = srcMLCreateMemoryParserCtxt(0, 1);
+    assert(ctxt == NULL);
+  }
+
+  //xmlParserCtxtPtr  srcMLCreateParserCtxt(xmlParserInputBufferPtr buffer_input)
+  //srcMLParseDocument(xmlParserCtxtPtr ctxt, bool allowendearly)
 
   return 0;
 
