4c6205ad72a44bc463aa3d3a5ec538f9c6d0d5d0
Michael John Decker <mdecker6@kent.edu>
2013-09-14 19:39:04 +0000

    Testing read root
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/branches/libsrcml@16417 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- test/test_srcMLReader.cpp
+++ test/test_srcMLReader.cpp
@@ -17,9 +17,9 @@ int main(int argc, char * argv[]) {
 
   const std::string srcml = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n<unit xmlns=\"http://www.sdml.info/srcML/src\" xmlns:pos=\"http://www.sdml.info/srcML/position\" language=\"C++\" dir=\"test\" filename=\"project\" version=\"1\" pos:tabs=\"4\" foo=\"bar\">\n\n<unit xmlns:cpp=\"http://www.sdml.info/srcML/cpp\" language=\"C++\" dir=\"test\" filename=\"a.cpp\" version=\"1\"><expr_stmt><expr><name>a</name></expr>;</expr_stmt>\n</unit>\n\n<unit xmlns:cpp=\"http://www.sdml.info/srcML/cpp\" language=\"C++\" filename=\"b.cpp\"><expr_stmt><expr><name>b</name></expr>;</expr_stmt>\n</unit>\n\n</unit>\n";
 
-  const std::string srcml_single = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n<unit xmlns=\"http://www.sdml.info/srcML/src\" xmlns:cpp=\"http://www.sdml.info/srcML/cpp\" language=\"C++\" dir=\"test\" filename=\"project\" version=\"1\"><expr_stmt><expr><name>a</name></expr>;</expr_stmt>\n</unit>\n";
+  const std::string srcml_single = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n<unit xmlns=\"http://www.sdml.info/srcML/src\" xmlns:cpp=\"http://www.sdml.info/srcML/cpp\" language=\"C++\" dir=\"test\" filename=\"project\" version=\"1\" tabs=\"4\" foo=\"bar\"><expr_stmt><expr><name>a</name></expr>;</expr_stmt>\n</unit>\n";
 
-  const std::string srcml_ns = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n<s:unit xmlns:s=\"http://www.sdml.info/srcML/src\">\n\n<s:unit xmlns:cpp=\"http://www.sdml.info/srcML/cpp\" language=\"C++\" filename=\"a.cpp\"><s:expr_stmt><s:expr><s:name>a</s:name></s:expr>;</s:expr_stmt>\n</s:unit>\n\n<s:unit xmlns:cpp=\"http://www.sdml.info/srcML/cpp\" language=\"C++\" filename=\"b.cpp\"><s:expr_stmt><s:expr><s:name>b</s:name></s:expr>;</s:expr_stmt>\n</s:unit>\n\n</s:unit>\n";
+  const std::string srcml_ns = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n<s:unit xmlns:s=\"http://www.sdml.info/srcML/src\" xmlns:pos=\"http://www.sdml.info/srcML/position\" language=\"C++\" dir=\"test\" filename=\"project\" version=\"1\" pos:tabs=\"4\" foo=\"bar\">\n\n<s:unit xmlns:cpp=\"http://www.sdml.info/srcML/cpp\" language=\"C++\" filename=\"a.cpp\"><s:expr_stmt><s:expr><s:name>a</s:name></s:expr>;</s:expr_stmt>\n</s:unit>\n\n<s:unit xmlns:cpp=\"http://www.sdml.info/srcML/cpp\" language=\"C++\" filename=\"b.cpp\"><s:expr_stmt><s:expr><s:name>b</s:name></s:expr>;</s:expr_stmt>\n</s:unit>\n\n</s:unit>\n";
 
   std::ofstream srcml_file("project.xml");
   srcml_file << srcml;
@@ -113,6 +113,59 @@ int main(int argc, char * argv[]) {
     assert(namespaces.at(0) == "http://www.sdml.info/srcML/src");
     assert(namespaces.at(1) == "http://www.sdml.info/srcML/position");
     assert(options == SRCML_OPTION_POSITION);
+    assert(tabstop == 4);
+  }
+
+  {
+    srcMLReader reader("project_ns.xml");
+    std::string * language = 0, * filename = 0, * directory = 0, * version = 0;
+    std::vector<std::string> attributes;
+    std::vector<std::string> prefixes;
+    std::vector<std::string> namespaces;
+    OPTION_TYPE options = 0;
+    int tabstop = 0;
+    reader.readRootUnitAttributes(&language, &filename, &directory, &version, attributes, prefixes, namespaces, options, tabstop);
+    assert(*language == "C++");
+    assert(*filename == "project");
+    assert(*directory == "test");
+    assert(*version == "1");
+    assert(attributes.size() == 2);
+    assert(attributes.at(0) == "foo");
+    assert(attributes.at(1) == "bar");
+    assert(prefixes.size() == 2);
+    assert(prefixes.at(0) == "s");
+    assert(prefixes.at(1) == "pos");
+    assert(namespaces.size() == 2);
+    assert(namespaces.at(0) == "http://www.sdml.info/srcML/src");
+    assert(namespaces.at(1) == "http://www.sdml.info/srcML/position");
+    assert(options == SRCML_OPTION_POSITION);
+    assert(tabstop == 4);
+  }
+
+  {
+    srcMLReader reader("project_single.xml");
+    std::string * language = 0, * filename = 0, * directory = 0, * version = 0;
+    std::vector<std::string> attributes;
+    std::vector<std::string> prefixes;
+    std::vector<std::string> namespaces;
+    OPTION_TYPE options = 0;
+    int tabstop = 0;
+    reader.readRootUnitAttributes(&language, &filename, &directory, &version, attributes, prefixes, namespaces, options, tabstop);
+    assert(*language == "C++");
+    assert(*filename == "project");
+    assert(*directory == "test");
+    assert(*version == "1");
+    assert(attributes.size() == 2);
+    assert(attributes.at(0) == "foo");
+    assert(attributes.at(1) == "bar");
+    assert(prefixes.size() == 2);
+    assert(prefixes.at(0) == "");
+    assert(prefixes.at(1) == "cpp");
+    assert(namespaces.size() == 2);
+    assert(namespaces.at(0) == "http://www.sdml.info/srcML/src");
+    assert(namespaces.at(1) == "http://www.sdml.info/srcML/cpp");
+    assert(options == SRCML_OPTION_CPP);
+    assert(tabstop == 4);
   }
 
 
