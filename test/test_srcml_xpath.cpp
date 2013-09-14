0de8efb52605dde959db79135434c263021c32ee
Michael John Decker <mdecker6@kent.edu>
2013-09-14 17:30:40 +0000

    More xpath tests
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/branches/libsrcml@16394 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- test/test_srcml_xpath.cpp
+++ test/test_srcml_xpath.cpp
@@ -73,35 +73,106 @@ int main(int argc, char * argv[]) {
   }
 
   {
-    const char * s = "<unit/>";
+    const char * s = "<unit>a;</unit>";
     assert(srcml_extract_text(s, strlen(s), xmlOutputBufferCreateFilename("project.xml", xmlFindCharEncodingHandler("ISO-8859-1"), 0), 0, 0) == SRCML_STATUS_OK);
+    std::ifstream in("project.xml");
+    std::string output;
+    std::string temp;
+    while(in >> temp)
+      output += temp;
+    assert(output == "a;");
     unlink("project.xml");
   }
 
   {
-    const char * s = "<unit/>";
+    const char * s = "<unit>a;</unit>";
     assert(srcml_extract_text(0, strlen(s), xmlOutputBufferCreateFilename("project.xml", xmlFindCharEncodingHandler("ISO-8859-1"), 0), 0, 0) == SRCML_STATUS_ERROR);
   }
 
   {
-    const char * s = "<unit/>";
+    const char * s = "<unit>a;</unit>";
     assert(srcml_extract_text(s, 0, xmlOutputBufferCreateFilename("project.xml", xmlFindCharEncodingHandler("ISO-8859-1"), 0), 0, 0) == SRCML_STATUS_ERROR);
   }
 
   {
-    const char * s = "<unit/>";
+    const char * s = "<unit>a;</unit>";
     assert(srcml_extract_text(s, strlen(s), 0, 0, 0) == SRCML_STATUS_ERROR);
   }
 
   {
-    const char * s = "<unit/>";
+    const char * s = "<unit>a;</unit>";
     std::ofstream file("input.xml");
     file << s;
     file.close();
-    xmlParserInputBufferPtr buffer_input = xmlParserInputBufferCreateFilename(s, xmlParseCharEncoding(0));
+    xmlParserInputBufferPtr buffer_input = xmlParserInputBufferCreateFilename("input.xml", xmlParseCharEncoding(0));
     const char * xpaths[2] = {"//src:unit", 0 };
     int fd = open("project.xml", O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
     assert(srcml_xpath(buffer_input, "src:unit", xpaths, fd, 0) == SRCML_STATUS_OK);
+    std::ifstream in("project.xml");
+    std::string output;
+    std::string temp;
+    while(in >> temp)
+      output += temp;
+    assert(output == "<?xmlversion=\"1.0\"encoding=\"\"standalone=\"yes\"?><unit/>");
+    unlink("input.xml");
+    unlink("project.xml");
+  }
+
+  {
+    const char * xpaths[2] = {"//src:unit", 0 };
+    int fd = open("project.xml", O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+    assert(srcml_xpath(0, "src:unit", xpaths, fd, 0) == SRCML_STATUS_ERROR);
+    unlink("project.xml");
+  }
+
+  {
+    const char * s = "<unit>a;</unit>";
+    std::ofstream file("input.xml");
+    file << s;
+    file.close();
+    xmlParserInputBufferPtr buffer_input = xmlParserInputBufferCreateFilename("input.xml", xmlParseCharEncoding(0));
+    const char * xpaths[2] = {"//src:unit", 0 };
+    int fd = open("project.xml", O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+    assert(srcml_xpath(buffer_input, 0, xpaths, fd, 0) == SRCML_STATUS_ERROR);
+    unlink("input.xml");
+    unlink("project.xml");
+  }
+
+  {
+    const char * s = "<unit>a;</unit>";
+    std::ofstream file("input.xml");
+    file << s;
+    file.close();
+    xmlParserInputBufferPtr buffer_input = xmlParserInputBufferCreateFilename("input.xml", xmlParseCharEncoding(0));
+    int fd = open("project.xml", O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+    assert(srcml_xpath(buffer_input, "src:unit", 0, fd, 0) == SRCML_STATUS_ERROR);
+    unlink("input.xml");
+    unlink("project.xml");
+  }
+
+  {
+    const char * s = "<unit>a;</unit>";
+    std::ofstream file("input.xml");
+    file << s;
+    file.close();
+    xmlParserInputBufferPtr buffer_input = xmlParserInputBufferCreateFilename("input.xml", xmlParseCharEncoding(0));
+    const char * xpaths[2] = {0, 0 };
+    int fd = open("project.xml", O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+    assert(srcml_xpath(buffer_input, "src:unit", xpaths, fd, 0) == SRCML_STATUS_ERROR);
+    unlink("input.xml");
+    unlink("project.xml");
+  }
+
+  {
+    const char * s = "<unit>a;</unit>";
+    std::ofstream file("input.xml");
+    file << s;
+    file.close();
+    xmlParserInputBufferPtr buffer_input = xmlParserInputBufferCreateFilename("input.xml", xmlParseCharEncoding(0));
+    const char * xpaths[2] = {"//src:unit", 0 };
+    int fd = open("project.xml", O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+    assert(srcml_xpath(buffer_input, "src:unit", xpaths, -1, 0) == SRCML_STATUS_ERROR);
+    unlink("input.xml");
   }
 
   return 0;
