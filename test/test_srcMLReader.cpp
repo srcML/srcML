76f6ed315062c9dadad76fa7c8aaf022e7b5f2b3
Michael John Decker <mdecker6@kent.edu>
2013-09-14 18:56:45 +0000

    Constructor tests
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/branches/libsrcml@16409 f70b1346-9fe7-0310-9bf3-95a0de396e6b

---
+++ test/test_srcMLReader.cpp
@@ -0,0 +1,47 @@
+/*
+
+  Test cases for srcMLReader
+*/
+#include <stdio.h>
+#include <string.h>
+#include <cassert>
+#include <fstream>
+#include <unistd.h>
+#include <fcntl.h>
+
+#include <srcml.h>
+#include <srcml_types.hpp>
+#include <srcmlns.hpp>
+
+int main(int argc, char * argv[]) {
+
+  const std::string srcml = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n<unit xmlns=\"http://www.sdml.info/srcML/src\" dir=\"test\" filename=\"project\" version=\"1\">\n\n<unit xmlns:cpp=\"http://www.sdml.info/srcML/cpp\" language=\"C++\" filename=\"a.cpp\"><expr_stmt><expr><name>a</name></expr>;</expr_stmt>\n</unit>\n\n<unit xmlns:cpp=\"http://www.sdml.info/srcML/cpp\" language=\"C++\" filename=\"b.cpp\"><expr_stmt><expr><name>b</name></expr>;</expr_stmt>\n</unit>\n\n</unit>\n";
+
+  const std::string srcml_single = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n<unit xmlns=\"http://www.sdml.info/srcML/src\" xmlns:cpp=\"http://www.sdml.info/srcML/cpp\" language=\"C++\" dir=\"test\" filename=\"project\" version=\"1\"><expr_stmt><expr><name>a</name></expr>;</expr_stmt>\n</unit>\n";
+
+  const std::string srcml_ns = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n<s:unit xmlns:s=\"http://www.sdml.info/srcML/src\">\n\n<s:unit xmlns:cpp=\"http://www.sdml.info/srcML/cpp\" language=\"C++\" filename=\"a.cpp\"><s:expr_stmt><s:expr><s:name>a</s:name></s:expr>;</s:expr_stmt>\n</s:unit>\n\n<s:unit xmlns:cpp=\"http://www.sdml.info/srcML/cpp\" language=\"C++\" filename=\"b.cpp\"><s:expr_stmt><s:expr><s:name>b</s:name></s:expr>;</s:expr_stmt>\n</s:unit>\n\n</s:unit>\n";
+
+  std::ofstream srcml_file("project.xml");
+  srcml_file << srcml;
+  srcml_file.close();
+
+  std::ofstream srcml_file_single("project_single.xml");
+  srcml_file_single << srcml_single;
+  srcml_file_single.close();
+
+  std::ofstream srcml_file_ns("project_ns.xml");
+  srcml_file_ns << srcml_ns;
+  srcml_file_ns.close();
+
+  /* 
+     srcMLReader(const char * filename)
+  */
+
+
+  unlink("project.xml");
+  unlink("project_single.xml");
+  unlink("project_ns.xml");
+
+  return 0;
+
+}
--- test/test_srcml_list.cpp
+++ test/test_srcml_list.cpp
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <cassert>
+#include <fstream>
 
 #include <srcml.h>
 #include <srcml_types.hpp>
@@ -17,7 +18,7 @@ int main(int argc, char * argv[]) {
    */
 
   const char * srcml_one = "<unit filename=\"a.cpp\"></unit>";
-  const char * srcml_one = "<unit><unit filename=\"a.cpp\"></unit><unit filename=\"b.cpp\"></unit><unit filename=\"c.cpp\"></unit></unit>";
+  const char * srcml_two = "<unit><unit filename=\"a.cpp\"></unit><unit filename=\"b.cpp\"></unit><unit filename=\"c.cpp\"></unit></unit>";
 
   std::ofstream out_one("project_one.xml");
   out_one << srcml_one;
@@ -28,7 +29,28 @@ int main(int argc, char * argv[]) {
 
   {
 
-    srcml_list("project_one.xml");
+    const char ** list = srcml_list("project_one.xml");
+    assert(list != 0);
+    assert(list[0] == std::string("a.cpp"));
+    assert(list[1] == 0);
+
+  }
+
+  {
+
+    const char ** list = srcml_list("project_two.xml");
+    assert(list != 0);
+    assert(list[0] == std::string("a.cpp"));
+    assert(list[1] == std::string("b.cpp"));
+    assert(list[2] == std::string("c.cpp"));
+    assert(list[3] == 0);
+
+  }
+
+  {
+
+    const char ** list = srcml_list(0);
+    assert(list == 0);
 
   }
 
