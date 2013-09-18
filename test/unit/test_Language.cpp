e097aa1587b855e4bc68440a1920acca1a04a846
Michael John Decker <mdecker6@kent.edu>
2013-09-18 14:17:34 +0000

    More language tests
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/trunk@16481 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- test/unit/test_Language.cpp
+++ test/unit/test_Language.cpp
@@ -12,9 +12,42 @@
 
 int main(int argc, char * argv[]) {
 
+  /*
+    inLanguage()
+   */ 
+
+  {
+    Language l(Language::LANGUAGE_CXX);
+    assert(!l.inLanguage(Language::LANGUAGE_C));
+  }
+
+  {
+    Language l(Language::LANGUAGE_CXX);
+    assert(l.inLanguage(Language::LANGUAGE_CXX));
+  }
+
+  /*
+    getLanguage()
+   */ 
+
   {
     Language l(Language::LANGUAGE_CXX);
     assert(l.getLanguage() == Language::LANGUAGE_CXX);
+  }
+
+  /*
+    getLanguage(const char *)
+   */ 
+  {
+    assert(Language::getLanguage("C++") == Language::LANGUAGE_CXX);
+  }
+
+  /*
+    getLanguageString()
+   */ 
+  {
+    Language l(Language::LANGUAGE_CXX);
+    assert(Language::getLanguage("C++") == Language::LANGUAGE_CXX);
   }
 
   return 0;
