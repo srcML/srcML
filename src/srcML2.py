37c5940ef8105f2a1ea418941ba9f0617dd0848c
Michael John Decker <mdecker6@kent.edu>
2013-09-18 13:10:44 +0000

    Filling out new python wrapper
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/trunk@16477 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcML2.py
+++ src/srcML2.py
@@ -25,11 +25,11 @@ libsrcml.srcml_write_open_memory.restype = c_int
 libsrcml.srcml_write_open_memory.argtypes = [c_void_p, c_void_p]
 
 # srcml_unit
-libsrcml.srcml_create_archive.restype = c_void_p
-libsrcml.srcml_create_archive.argtypes = [c_void_p]
+libsrcml.srcml_create_unit.restype = c_void_p
+libsrcml.srcml_create_unit.argtypes = [c_void_p]
 
-libsrcml.srcml_free_archive.restype = None
-libsrcml.srcml_free_archive.argtypes = [c_void_p]
+libsrcml.srcml_free_unit.restype = None
+libsrcml.srcml_free_unit.argtypes = [c_void_p]
 
 
 # free functions
@@ -41,13 +41,14 @@ def srcml_version_string() :
 
 class srcMLTranslator(object):
 
-    def __init__(self, language, op):
+    def __init__(self):
         self.archive = libsrcml.srcml_create_archive()
         self.buffer = c_char_p()
         libsrcml.srcml_write_open_memory(self.archive, pointer(self.buffer))
 
     def translate(self, path, unit_directory, unit_filename, unit_version, language):
-        libsrcml.srcml_translate(self.translator, path, unit_directory, unit_filename, unit_version, language)
+        self.unit = libsrcml.srcml_create_unit(self.archive)
+        libsrcml.srcml_parse_unit_memory(self.translator, path, unit_directory, unit_filename, unit_version, language)
 
     def getsrcML(self) :
         return libsrcml.srcml_get_srcml(self.translator)
@@ -55,41 +56,14 @@ class srcMLTranslator(object):
     def delete(self) :
         libsrcml.srcml_free_archive(self.archive)
 
-
-class srcMLUtility(object):
-
-    def __init__(self, filename, src_encoding, options, diff_version) :
-        self.utility = c_void_p(libsrcml.srcml_utility_file_new(filename, src_encoding, options, diff_version))
-
-    def __init__(self, buffer, size, src_encoding, options, diff_version) :
-        self.utility = c_void_p(libsrcml.srcml_utility_memory_new(buffer, size, src_encoding, options, diff_version))
-
-    def extract_text(self, to_dir, ofilename, unit) :
-        libsrcml.srcml_extract_text_file(self.utility, to_dir, ofilename, unit) 
-
-    def extract_text(self, unit) :
-        return libsrcml.srcml_extract_text_buffer(self.utility, unit) 
-
-    def unit_count(self, file) :
-        return libsrcml.srcml_unit_count(self.utility, file) 
-
-    def extract_xml(self, unit) :
-        return libsrcml.srcml_extract_xml_buffer(self.utility, unit) 
-
-    def long_info(self) :
-        return libsrcml.srcml_long_info(self.utility) 
-
-    def delete(self) :
-        libsrcml.srcml_utility_delete(self.utility)
-
 # test
 #from srcMLapps import *
-#translator = srcMLTranslator(2, "ISO-8859-1", "ISO-8859-1", 0, "directory", "file", "version", URI_PREFIX, 8)
+translator = srcMLTranslator()
 #translator.setInputString("a;")
 #translator.translate("a", "b", "c", "d", 2)
 #translator.close()
 #print translator.getsrcML()
-#translator.delete()
+translator.delete()
 
 #srcml = """<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 #<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" language="C++"><expr_stmt><expr><name>a</name></expr>;</expr_stmt>
