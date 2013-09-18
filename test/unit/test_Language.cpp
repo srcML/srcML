30f003c49eb89dbb08d0af2c13b1095906cc4415
Michael John Decker <mdecker6@kent.edu>
2013-09-18 14:31:24 +0000

    Testing more language
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/trunk@16483 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- test/unit/test_Language.cpp
+++ test/unit/test_Language.cpp
@@ -53,11 +53,63 @@ int main(int argc, char * argv[]) {
   /*
     registerUserExt()
    */ 
+
   {
     std::vector<pair> reg_ext;
     assert(Language::registerUserExt("cpp", "C++", reg_ext));
+    assert(reg_ext.size() == 1);
     assert(reg_ext.at(0).n == Language::LANGUAGE_CXX);
     assert(reg_ext.at(0).s == "cpp");
+  }
+
+  {
+    std::vector<pair> reg_ext;
+    assert(!Language::registerUserExt("cpp", "Foo", reg_ext));
+    assert(reg_ext.size() == 0);
+  }
+
+  /*
+    getLanguageFromFilename
+   */
+
+  {
+    std::vector<pair> reg_ext;
+    Language::registerUserExt("cpp", "C++", reg_ext);
+    assert(Language::getLanguageFromFilename("a.cpp", reg_ext) == Language::LANGUAGE_CXX);
+  }
+
+  {
+    std::vector<pair> reg_ext;
+    Language::registerUserExt("cpp", "C++", reg_ext);
+    assert(Language::getLanguageFromFilename("a.cpp.gz", reg_ext) == Language::LANGUAGE_CXX);
+  }
+
+  {
+    std::vector<pair> reg_ext;
+    Language::registerUserExt("cpp", "C++", reg_ext);
+    assert(Language::getLanguageFromFilename("a.cpp.bz2", reg_ext) == Language::LANGUAGE_CXX);
+  }
+
+  {
+    std::vector<pair> reg_ext;
+    Language::registerUserExt("cpp", "C++", reg_ext);
+    assert(Language::getLanguageFromFilename("a.cpp.xz", reg_ext) == Language::LANGUAGE_CXX);
+  }
+
+  {
+    std::vector<pair> reg_ext;
+    Language::registerUserExt("cpp", "C++", reg_ext);
+    assert(Language::getLanguageFromFilename("a.foo", reg_ext) == 0);
+  }
+
+  /*
+    register_standard_file_extensions()
+   */ 
+
+  {
+    std::vector<pair> reg_ext;
+    assert(Language::register_standar_file_extensions(reg_ext));
+    assert(reg_ext.size() != 0);
   }
 
   return 0;
