01917152c1dc5196c57ede288c297775345a4526
Michael John Decker <mdecker6@kent.edu>
2013-09-18 14:58:53 +0000

    Started to test translator core
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/trunk@16485 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- test/unit/test_srcMLTranslatorCore.cpp
+++ test/unit/test_srcMLTranslatorCore.cpp
@@ -6,8 +6,33 @@
  */
 #include <stdio.h>
 #include <string.h>
+#include <srcMLTranslatorCore.hpp>
+#include <Language.hpp>
 
 int main(int argc, char * argv[]) {
+
+  /*
+
+    srcMLTranslatorCore
+
+   */
+
+  {
+
+    xmlBufferPtr buffer = xmlBufferCreate();
+    xmlOutputBufferPtr output_buffer = xmlOutputBufferCreateBuffer(buffer, xmlFindCharEncodingHandler(0));
+    xmlTextWriterPtr writer = xmlNewTextWriter(output_buffer);
+    OPTION_TYPE op;
+
+    {
+      srcMLTranslatorCore translator(Language::getLanguage("C++"), "ISO-8859-1", "ISO-8859-1", 
+                                 writer, op, "", "", "", 0, 4);
+
+    }
+
+    xmlBufferFree(buffer);
+    
+  }
 
   return 0;
 }
