fed226a8de9e4cc276ff699cab511294fb466a65
Michael John Decker <mdecker6@kent.edu>
2013-09-18 15:17:56 +0000

    Testing translate separate
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/trunk@16487 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- test/unit/test_srcMLTranslatorCore.cpp
+++ test/unit/test_srcMLTranslatorCore.cpp
@@ -8,8 +8,20 @@
 #include <string.h>
 #include <srcMLTranslatorCore.hpp>
 #include <Language.hpp>
+#include <srcmlns.hpp>
 
 int main(int argc, char * argv[]) {
+
+  const char* urisprefix[] = {
+
+    SRCML_SRC_NS_PREFIX_DEFAULT,
+    SRCML_CPP_NS_PREFIX_DEFAULT,
+    SRCML_ERR_NS_PREFIX_DEFAULT,
+    SRCML_EXT_LITERAL_NS_PREFIX_DEFAULT,
+    SRCML_EXT_OPERATOR_NS_PREFIX_DEFAULT,
+    SRCML_EXT_MODIFIER_NS_PREFIX_DEFAULT,
+    SRCML_EXT_POSITION_NS_PREFIX_DEFAULT,
+  };
 
   /*
 
@@ -25,7 +37,7 @@ int main(int argc, char * argv[]) {
     OPTION_TYPE op;
 
     srcMLTranslatorCore translator(Language::getLanguage("C++"), "ISO-8859-1", "ISO-8859-1", 
-                                 writer, op, "", "", "", 0, 4);
+                                 writer, op, "", "", "", urisprefix, 4);
 
     xmlBufferFree(buffer);
     
@@ -45,7 +57,8 @@ int main(int argc, char * argv[]) {
     OPTION_TYPE op;
 
     srcMLTranslatorCore translator(Language::getLanguage("C++"), "ISO-8859-1", "ISO-8859-1", 
-                                 writer, op, "", "", "", 0, 4);
+                                 writer, op, "", "", "", urisprefix, 4);
+
 
     xmlBufferFree(buffer);
     
@@ -65,7 +78,7 @@ int main(int argc, char * argv[]) {
     OPTION_TYPE op;
 
     srcMLTranslatorCore translator(Language::getLanguage("C++"), "ISO-8859-1", "ISO-8859-1", 
-                                 writer, op, "", "", "", 0, 4);
+                                 writer, op, "", "", "", urisprefix, 4);
 
     xmlParserInputBufferPtr input = xmlParserInputBufferCreateMem("a;", 2, xmlParseCharEncoding(0));
     try {
@@ -84,13 +97,38 @@ int main(int argc, char * argv[]) {
     OPTION_TYPE op;
 
     srcMLTranslatorCore translator(Language::getLanguage("C++"), "ISO-8859-1", "ISO-8859-1", 
-                                 writer, op, "", "", "", 0, 4);
+                                 writer, op, "", "", "", urisprefix, 4);
 
     try {
       translator.setInput((xmlParserInputBufferPtr)NULL);
       assert(false);
     } catch(...) {}
 
+    xmlBufferFree(buffer);
+    
+  }
+
+  /*
+
+    translate_separate
+
+   */
+
+  {
+
+    xmlBufferPtr buffer = xmlBufferCreate();
+    xmlOutputBufferPtr output_buffer = xmlOutputBufferCreateBuffer(buffer, xmlFindCharEncodingHandler(0));
+    xmlTextWriterPtr writer = xmlNewTextWriter(output_buffer);
+    OPTION_TYPE op;
+
+    srcMLTranslatorCore translator(Language::LANGUAGE_CXX, "ISO-8859-1", "ISO-8859-1", 
+                                 writer, op, 0, 0, 0, urisprefix, 4);
+
+    xmlParserInputBufferPtr input = xmlParserInputBufferCreateMem("a;", 2, xmlParseCharEncoding(0));
+    translator.setInput(input);
+    xmlBufferPtr output = xmlBufferCreate();
+    translator.translate_separate("", "",  "", "", Language::LANGUAGE_CXX, output);
+    xmlFreeParserInputBuffer(input);
     xmlBufferFree(buffer);
     
   }
