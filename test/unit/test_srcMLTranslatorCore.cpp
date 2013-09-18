18c3604d89fb925d442e379c5186872bc98b0a3d
Michael John Decker <mdecker6@kent.edu>
2013-09-18 15:07:43 +0000

    Testing setInput
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/trunk@16486 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- test/unit/test_srcMLTranslatorCore.cpp
+++ test/unit/test_srcMLTranslatorCore.cpp
@@ -24,11 +24,72 @@ int main(int argc, char * argv[]) {
     xmlTextWriterPtr writer = xmlNewTextWriter(output_buffer);
     OPTION_TYPE op;
 
-    {
-      srcMLTranslatorCore translator(Language::getLanguage("C++"), "ISO-8859-1", "ISO-8859-1", 
+    srcMLTranslatorCore translator(Language::getLanguage("C++"), "ISO-8859-1", "ISO-8859-1", 
                                  writer, op, "", "", "", 0, 4);
 
-    }
+    xmlBufferFree(buffer);
+    
+  }
+
+  /*
+
+    srcMLTranslatorCore
+
+   */
+
+  {
+
+    xmlBufferPtr buffer = xmlBufferCreate();
+    xmlOutputBufferPtr output_buffer = xmlOutputBufferCreateBuffer(buffer, xmlFindCharEncodingHandler(0));
+    xmlTextWriterPtr writer = xmlNewTextWriter(output_buffer);
+    OPTION_TYPE op;
+
+    srcMLTranslatorCore translator(Language::getLanguage("C++"), "ISO-8859-1", "ISO-8859-1", 
+                                 writer, op, "", "", "", 0, 4);
+
+    xmlBufferFree(buffer);
+    
+  }
+
+  /*
+
+    setInput
+
+   */
+
+  {
+
+    xmlBufferPtr buffer = xmlBufferCreate();
+    xmlOutputBufferPtr output_buffer = xmlOutputBufferCreateBuffer(buffer, xmlFindCharEncodingHandler(0));
+    xmlTextWriterPtr writer = xmlNewTextWriter(output_buffer);
+    OPTION_TYPE op;
+
+    srcMLTranslatorCore translator(Language::getLanguage("C++"), "ISO-8859-1", "ISO-8859-1", 
+                                 writer, op, "", "", "", 0, 4);
+
+    xmlParserInputBufferPtr input = xmlParserInputBufferCreateMem("a;", 2, xmlParseCharEncoding(0));
+    try {
+      translator.setInput(input);
+    } catch(...) { assert(false); }
+    xmlFreeParserInputBuffer(input);
+    xmlBufferFree(buffer);
+    
+  }
+
+  {
+
+    xmlBufferPtr buffer = xmlBufferCreate();
+    xmlOutputBufferPtr output_buffer = xmlOutputBufferCreateBuffer(buffer, xmlFindCharEncodingHandler(0));
+    xmlTextWriterPtr writer = xmlNewTextWriter(output_buffer);
+    OPTION_TYPE op;
+
+    srcMLTranslatorCore translator(Language::getLanguage("C++"), "ISO-8859-1", "ISO-8859-1", 
+                                 writer, op, "", "", "", 0, 4);
+
+    try {
+      translator.setInput((xmlParserInputBufferPtr)NULL);
+      assert(false);
+    } catch(...) {}
 
     xmlBufferFree(buffer);
     
