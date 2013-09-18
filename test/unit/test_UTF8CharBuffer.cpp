29246d9048d7d8a6e11a128d7cfbac71510c174b
Michael John Decker <mdecker6@kent.edu>
2013-09-18 19:42:11 +0000

    Started to test UTF8CharBuffer
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/trunk@16501 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- test/unit/test_UTF8CharBuffer.cpp
+++ test/unit/test_UTF8CharBuffer.cpp
@@ -4,10 +4,46 @@
 #include <stdio.h>
 #include <string.h>
 #include <cassert>
+#include <UTF8CharBuffer.hpp>
 
 int main(int argc, char * argv[]) {
 
-  
+  /*
+
+    UTF8CharBuffer(const char* ifilename, const char* encoding)
+
+   */
+
+  {
+
+    UTF8CharBuffer utf8("test_UTF8CharBuffer.cpp", "UTF-8");
+
+  }
+
+  /*
+
+    UTF8CharBuffer(const char* source, int asize, const char * encoding)
+
+   */
+
+  {
+
+    UTF8CharBuffer("abc", 3, "UTF-8");
+
+  }
+
+  /*
+
+    UTF8CharBuffer(xmlParserInputBufferPtr pinput, const char * encoding)
+
+   */
+
+  {
+
+    //UTF8CharBuffer(, "UTF-8");
+
+  }
+
 
   return 0;
 }
