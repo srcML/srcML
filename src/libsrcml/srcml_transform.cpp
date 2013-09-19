120c71359206952886e08607566b1e236c1463ed
Michael John Decker <mdecker6@kent.edu>
2013-09-19 02:21:09 +0000

    Add related tag so transform is related to function is srcml_transform
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/trunk@16530 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/libsrcml/srcml_transform.cpp
+++ src/libsrcml/srcml_transform.cpp
@@ -32,6 +32,7 @@
 
 /** 
  * srcml_append_transform_xpath
+ * @related transform
  * @param archive a srcml archive
  * @param xpath_string an XPath expression
  *
@@ -54,6 +55,7 @@ int srcml_append_transform_xpath(srcml_archive* archive, const char* xpath_strin
 
 /**
  * srcml_append_transform_xslt
+ * @related transform
  * @param archive a srcml_archive
  * @param xslt_filename an XSLT program filename path
  *
@@ -76,6 +78,7 @@ int srcml_append_transform_xslt(srcml_archive* archive, const char* xslt_filenam
 
 /**
  * srcml_append_transform_relaxng
+ * @related transform
  * @param archive a srcml archive
  * @param relaxng_filename a RelaxNG schema filename path
  *
@@ -98,6 +101,7 @@ int srcml_append_transform_xslt(srcml_archive* archive, const char* xslt_filenam
 
 /**
  * srcml_apply_transforms
+ * @related transform
  * @param iarchive an input srcml archive
  * @param oarchvie and output srcml archive
  *
