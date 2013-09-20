118e274abad7d3c3482cbb2ce782ab28c81769b9
Michael John Decker <mdecker6@kent.edu>
2013-09-20 01:38:11 +0000

    Use Toolkit instead of translator, since srcML is much more than just a translator
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcML/trunk@16588 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/RelaxNGUnits.hpp
+++ src/RelaxNGUnits.hpp
@@ -3,20 +3,20 @@
 
   Copyright (C) 2008-2011  SDML (www.srcML.org)
 
-  This file is part of the srcML translator.
+  This file is part of the srcML Toolkit.
 
-  The srcML translator is free software; you can redistribute it and/or modify
+  The srcML Toolkit is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation; either version 2 of the License, or
   (at your option) any later version.
 
-  The srcML translator is distributed in the hope that it will be useful,
+  The srcML Toolkit is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
 
   You should have received a copy of the GNU General Public License
-  along with the srcML translator; if not, write to the Free Software
+  along with the srcML Toolkit; if not, write to the Free Software
   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 */
