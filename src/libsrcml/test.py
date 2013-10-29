fc70bfe76211186c0f012a39cf578886d73acc46
Michael John Decker <mdecker6@kent.edu>
2013-10-29 18:13:14 -0400

    Added some tests of unit set

--- src/libsrcml/test.py
+++ src/libsrcml/test.py
@@ -3,7 +3,7 @@ import libsrcml
 # test api
 archive = libsrcml.srcml_archive()
 archive.set_filename("project")
-archive.set_language("a.cpp")
+archive.set_language("C++")
 archive.set_directory("dir")
 archive.set_version("1.0")
 archive.write_open_memory()
@@ -13,6 +13,10 @@ unit.parse_filename("a.cpp")
 archive.write_unit(unit)
 
 unit = libsrcml.srcml_unit(archive)
+unit.set_filename("b.cpp")
+unit.set_language("C++")
+unit.set_directory("dir")
+unit.set_version("1.0")
 unit.parse_memory("b;")
 archive.write_unit(unit)
 
