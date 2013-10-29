47f08d3c7d0875a3a5e42ce0390ca58416ad11b8
Michael John Decker <mdecker6@kent.edu>
2013-10-29 15:11:50 -0400

    Starting to add functions from libsrcml

--- src/libsrcml/libsrcml.py
+++ src/libsrcml/libsrcml.py
@@ -10,3 +10,12 @@ elif os.path.exists('../../bin/libsrcml.dll') :
     LIBSRCML_PATH="../../bin/libsrcml.dll"
 
 libsrcml = cdll.LoadLibrary(LIBSRCML_PATH)
+
+# struct srcml_archive* srcml_create_archive();
+libsrcml.srcml_new.restype = c_void_p
+libsrcml.srcml_new.argtypes = []
+
+# int srcml_write_open_filename(struct srcml_archive*, const char* srcml_filename);
+libsrcml.srcml_new.restype = c_int
+libsrcml.srcml_new.argtypes = [c_void_p, c_char_p]
+
