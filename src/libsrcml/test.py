e02055f2b5484f3336574687794e2575a09ed101
Michael John Decker <mdecker6@kent.edu>
2013-10-29 17:55:18 -0400

    Adding a usage of get and set

--- src/libsrcml/test.py
+++ src/libsrcml/test.py
@@ -2,6 +2,10 @@ import libsrcml
 
 # test api
 archive = libsrcml.srcml_archive()
+archive.set_filename("project")
+archive.set_language("a.cpp")
+archive.set_directory("dir")
+archive.set_version("1.0")
 archive.write_open_memory()
 
 unit = libsrcml.srcml_unit(archive)
@@ -13,5 +17,10 @@ unit.parse_memory("b;")
 archive.write_unit(unit)
 
 archive.close()
+
+print archive.get_filename()
+print archive.get_language()
+print archive.get_directory()
+print archive.get_version()
 
 print archive.srcML()
