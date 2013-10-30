88866fe6d64d0e96a14545f0a19968327d36c3b6
Michael John Decker <mdecker6@kent.edu>
2013-10-30 11:10:21 -0400

    Correct library path for new directory

--- src/libsrcml/python/globals.py
+++ src/libsrcml/python/globals.py
@@ -2,12 +2,12 @@ import os
 from ctypes import cdll
 
 LIBSRCML_PATH=""
-if os.path.exists('../../bin/libsrcml.dylib') :
-    LIBSRCML_PATH="../../bin/libsrcml.dylib"
-elif os.path.exists('../../bin/libsrcml.so') :
-    LIBSRCML_PATH="../../bin/libsrcml.so"
-elif os.path.exists('../../bin/libsrcml.dll') :
-    LIBSRCML_PATH="../../bin/libsrcml.dll"
+if os.path.exists('../../../bin/libsrcml.dylib') :
+    LIBSRCML_PATH="../../../bin/libsrcml.dylib"
+elif os.path.exists('../../../bin/libsrcml.so') :
+    LIBSRCML_PATH="../../../bin/libsrcml.so"
+elif os.path.exists('../../../bin/libsrcml.dll') :
+    LIBSRCML_PATH="../../../bin/libsrcml.dll"
 
 libsrcml = cdll.LoadLibrary(LIBSRCML_PATH)
 
