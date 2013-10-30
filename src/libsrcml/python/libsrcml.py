1967fa172f55f331639ba26224ead6dd2d48b2af
Michael John Decker <mdecker6@kent.edu>
2013-10-30 11:50:40 -0400

    Adding options for srcml in separate file

--- src/libsrcml/python/libsrcml.py
+++ src/libsrcml/python/libsrcml.py
@@ -1,6 +1,7 @@
 import srcml_archive
 import srcml_unit
 from globals import cleanup_globals, version_number, version_string
+from options import *
 
 srcml_archive = srcml_archive.srcml_archive
 srcml_unit = srcml_unit.srcml_unit
