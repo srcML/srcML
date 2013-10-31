203e4c985a106985f8e28761fec600cd07965d22
Michael John Decker <mdecker6@kent.edu>
2013-10-31 16:27:27 -0400

    Adding more tests mainly write/parse

--- src/libsrcml/python/cli_test.py
+++ src/libsrcml/python/cli_test.py
@@ -1,4 +1,5 @@
 import libsrcml
+import difflib
 
 test_count = 0
 error_count = 0
@@ -8,8 +9,8 @@ def verify_test(correct, output) :
     globals()['test_count'] += 1
 
     if correct != output :
-        print "correct|" + correct + "|"
-        print "output|" + output + "|"
+        for line in difflib.unified_diff(correct.split("\n"), output.split("\n")) :
+            print line
         globals()['error_count'] += 1
 
 # test language
@@ -27,11 +28,46 @@ verify_test("project", archive.get_filename())
 verify_test("C++", archive.get_language())
 verify_test("dir", archive.get_directory())
 verify_test("1.0", archive.get_version())
+archive.close()
 
-archive.write_open_memory()
+# write/parse tests
+srcml = """<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<unit xmlns="http://www.sdml.info/srcML/src">
 
+<unit xmlns:cpp="http://www.sdml.info/srcML/cpp" language="C++"><expr_stmt><expr><name>a</name></expr>;</expr_stmt>
+</unit>
+
+</unit>
+"""
+
+# filename
+archive = libsrcml.srcml_archive()
+archive.write_open_filename("project.xml")
 unit = libsrcml.srcml_unit(archive)
+unit.parse_filename("a.cpp")
+archive.write_unit(unit)
+archive.close()
+
+file = open("project.xml", "r")
+gen = file.read()
+file.close()
+verify_test(srcml, gen)
+
+# memory
+archive = libsrcml.srcml_archive()
+archive.write_open_memory()
+unit = libsrcml.srcml_unit(archive)
+unit.set_language("C++")
+unit.parse_memory("a;\n")
+archive.write_unit(unit)
+archive.close()
+
+verify_test(srcml, archive.srcML())
+
+archive = libsrcml.srcml_archive()
+archive.write_open_memory()
 try :
+    unit = libsrcml.srcml_unit(archive)
     unit.parse_filename("a.cpp")
     archive.write_unit(unit)
 except libsrcml.srcMLException as e :
