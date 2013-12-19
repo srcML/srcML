839e8793fef7bd38b8f9a0075f1e3ad949827f74
Drew Guarnera <dtg3@zips.uakron.edu>
2013-12-19 11:52:20 -0500

    Allow CMake to build new srcml client

--- CMake/config.cmake
+++ CMake/config.cmake
@@ -36,7 +36,7 @@ option(LIBSRCML_SAX2_ENABLED "Build with SAX2Framework for srcML" OFF)
 set_property(GLOBAL PROPERTY SAX2_ENABLED ${LIBSRCML_SAX2_ENABLED})
 
 # Adding build option for srcml executable.
-option(ENABLE_NEW_SRCML_EXEC_BUILD "Build the newer version of the srcML executable." OFF)
+option(ENABLE_NEW_SRCML_EXEC_BUILD "Build the newer version of the srcML executable." ON)
 set_property(GLOBAL PROPERTY ENABLE_NEW_SRCML_EXEC_BUILD ${ENABLE_NEW_SRCML_EXEC_BUILD})
 
 # Locating packages.
