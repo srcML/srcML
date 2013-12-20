46f6a04cdc530eea58ad1a1cec66f99fb825d0a1
Michael John Decker <mdecker6@kent.edu>
2013-12-19 19:30:44 -0500

    Fill in some of the dependencies.

--- CMake/debian.cmake
+++ CMake/debian.cmake
@@ -1,5 +1,5 @@
 # set Debian package name
 set(CPACK_DEBIAN_PACKAGE_NAME "srcML")
 set(CPACK_DEBIAN_PACKAGE_ARCHITECTURE "i386")
-set(CPACK_DEBIAN_PACKAGE_DEPENDS "")
+set(CPACK_DEBIAN_PACKAGE_DEPENDS "libxml2 (>= 2.7.8), libxslt1.1 (>= 1.1.26), libarchive12 (>= 3.0.3")
 
