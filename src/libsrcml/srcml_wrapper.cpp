061ab3441ecd2bb3f6d50e3715a75b37fa6dbbcb
Michael John Decker <mdecker6@kent.edu>
2014-01-08 16:56:30 -0500

    Adding strdup wrapper.

--- src/libsrcml/srcml_wrapper.cpp
+++ src/libsrcml/srcml_wrapper.cpp
@@ -1,6 +1,7 @@
 #include <srcml_wrapper.hpp>
 #include <stdio.h>
 #include <srcmlapps.hpp>
+#include <string.h>
 
 void * srcml_malloc(size_t size) {
 
@@ -9,6 +10,21 @@ void * srcml_malloc(size_t size) {
   if(!ret && !size) {
 
     fprintf(stderr, "Error allocating (%lu bytes), exiting.", size);
+    exit(STATUS_ERROR);
+
+  }
+
+  return ret;
+
+}
+
+char * srcml_strdup(const char * s1) {
+
+  char * ret = strdup(s1);
+
+  if(!ret && !s1) {
+
+    fprintf(stderr, "Error duplicating (%s), exiting.", s1);
     exit(STATUS_ERROR);
 
   }
