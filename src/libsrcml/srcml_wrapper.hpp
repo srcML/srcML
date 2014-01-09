08649a472098c09c1b3aba82a52c91ab392dd30c
Michael John Decker <mdecker6@kent.edu>
2014-01-09 17:24:10 -0500

    New functions that stop parsing but do not stop program.

--- src/libsrcml/srcml_wrapper.hpp
+++ src/libsrcml/srcml_wrapper.hpp
@@ -9,12 +9,16 @@
 
 #include <sys/types.h>
 #include <stdlib.h>
+#include <libxml/parser.h>
+#include <SAX2ExtractUnitsSrc.hpp>
 
 #ifndef INCLUDED_SRCML_WRAPPER_HPP
 #define INCLUDED_SRCML_WRAPPER_HPP
 
 void * srcml_malloc(size_t size);
+void * srcml_malloc_sax(size_t size, xmlParserCtxtPtr ctxt);
 
 char * srcml_strdup(const char * s1);
+char * srcml_strdup_sax(const char * s1, xmlParserCtxtPtr ctxt);
 
 #endif
