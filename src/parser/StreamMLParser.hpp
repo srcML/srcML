90b22af6a2995b72e261e29a25351245ee2e2505
Michael John Decker <mdecker6@kent.edu>
2014-01-08 19:55:16 -0500

    Correct preprocessor output for Java when cpp not enabled.

--- src/parser/StreamMLParser.hpp
+++ src/parser/StreamMLParser.hpp
@@ -33,6 +33,7 @@
 #include <cassert>
 
 #include "srcMLToken.hpp"
+#include "Options.hpp"
 
 template <typename Base>
 class StreamMLParser : public Base, public TokenStream {
@@ -178,7 +179,7 @@ private:
     bool consumeSkippedToken() {
 
         // preprocessor (unless we already are in one)
-        if (!inskip && Base::LA(1) == Base::PREPROC) {
+      if (isoption(options, OPTION_CPP) && !inskip && Base::LA(1) == Base::PREPROC) {
 
             // start preprocessor handling
             inskip = true;
