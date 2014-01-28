9a86dbb3ed149a66c71bd32896c9e6c0cb46a4b5
Michael John Decker <mdecker6@kent.edu>
2014-01-28 09:32:13 -0500

    Make sure wrote nothing.

--- test/libsrcml/test/test_srcml_write_open.cpp
+++ test/libsrcml/test/test_srcml_write_open.cpp
@@ -55,6 +55,8 @@ int main() {
   srcml_close_archive(archive);
   srcml_free_archive(archive);
 
+  // check file either does not exist or empty
+
   }
 
   {
@@ -88,6 +90,8 @@ int main() {
   srcml_close_archive(archive);
   srcml_free_archive(archive);
   free(s);
+
+  dassert(strlen(s), 0);
   }
 
   {
@@ -110,9 +114,9 @@ int main() {
   }
 
   {
-    char * s = 0;
-    int size;
-    dassert(srcml_write_open_memory(0, &s, &size), SRCML_STATUS_ERROR);
+  char * s = 0;
+  int size;
+  dassert(srcml_write_open_memory(0, &s, &size), SRCML_STATUS_ERROR);
   free(s);
   }
 
@@ -133,6 +137,12 @@ int main() {
   srcml_close_archive(archive);
   srcml_free_archive(archive);
   fclose(file);
+
+  char buf[1];
+  file = fopen("project.xml", "r");
+  size_t num_read = fread(buf, 1, 1, file);
+  fclose(file);
+  dassert(num_read, 0);
 
   }
 
@@ -169,6 +179,11 @@ int main() {
   srcml_free_archive(archive);
   close(fd);
 
+  char buf[1];
+  fd = open("project.xml", O_RDONLY);
+  size_t num_read = read(fd, buf, 1);
+  close(fd);
+  dassert(num_read, 0);
   }
 
   {
