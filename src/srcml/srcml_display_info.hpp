5571e644bb0de1b34a81428b2656213473af1183
Drew Guarnera <dtg3@zips.uakron.edu>
2014-02-11 16:50:20 -0500

    Split out display info

---
+++ src/srcml/srcml_display_info.hpp
@@ -0,0 +1,34 @@
+/*
+  srcml_display_info.hpp
+
+  Copyright (C) 2014  SDML (www.srcML.org)
+
+  This file is part of the srcML Toolkit.
+
+  The srcML Toolkit is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  The srcML Toolkit is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with the srcML Toolkit; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+/*
+  srcml_display_info.hpp display info about a given srcml input file.
+*/
+
+#ifndef SRCML_DISPLAY_INFO_HPP
+#define SRCML_DISPLAY_INFO_HPP
+
+#include <string>
+
+void srcml_display_info(const std::string& srcmlInput);
+
+#endif
