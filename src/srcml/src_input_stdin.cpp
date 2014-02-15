d97f35b62d5150c9c7864ddac04bb30228da950e
Drew Guarnera <dtg3@zips.uakron.edu>
2014-02-14 20:17:18 -0500

    Add header comment

--- src/srcml/src_input_stdin.cpp
+++ src/srcml/src_input_stdin.cpp
@@ -1,5 +1,30 @@
-#include <src_input_stdin.hpp>
+/*
+  src_input_stdin.cpp
 
+  Copyright (C) 2004-2014  SDML (www.srcML.org)
+
+  This file is part of the srcML Toolkit.
+
+  The srcML Toolkit is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  The srcML Toolkit is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with the srcML Toolkit; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+/*
+  src_input_stdin helper functions for dealing with stdin as an input source
+*/
+
+#include <src_input_stdin.hpp>
 #include <stdio.h>
 #include <unistd.h>
 #include <iostream>
