e49ec2a31e404301fb301b9dfadf02c6e0562600
Michael John Decker <mdecker6@kent.edu>
2014-02-17 10:03:58 -0500

    Read multiple unit/header

--- test/libsrcml/test/test_srcml_read_unit.cpp
+++ test/libsrcml/test/test_srcml_read_unit.cpp
@@ -100,6 +100,30 @@ int main() {
   {
 
     srcml_archive * archive = srcml_create_archive();
+    srcml_read_open_memory(archive, srcml_two.c_str(), srcml_two.size());
+    srcml_unit * unit = srcml_read_unit(archive);
+    dassert(srcml_unit_get_language(unit), std::string("C"));
+    dassert(srcml_unit_get_filename(unit), std::string("project.c"));
+    dassert(srcml_unit_get_directory(unit), 0);
+    dassert(srcml_unit_get_version(unit), 0);
+    dassert(*unit->unit, srcml_a);
+    unit = srcml_read_unit(archive);
+    dassert(srcml_unit_get_language(unit), std::string("C"));
+    dassert(srcml_unit_get_filename(unit), std::string("project.c"));
+    dassert(srcml_unit_get_directory(unit), 0);
+    dassert(srcml_unit_get_version(unit), 0);
+    dassert(*unit->unit, srcml_b_two);
+    unit = srcml_read_unit(archive);
+    dassert(unit, 0);
+
+    srcml_free_unit(unit);
+    srcml_close_archive(archive);
+    srcml_free_archive(archive);
+  }
+
+  {
+
+    srcml_archive * archive = srcml_create_archive();
     dassert(srcml_read_unit(archive), 0);
     srcml_free_archive(archive);
   }
@@ -405,6 +429,30 @@ int main() {
     dassert(srcml_unit_get_directory(unit), std::string("test"));
     dassert(srcml_unit_get_version(unit), std::string("1"));
     dassert(unit->unit, 0);
+
+    srcml_free_unit(unit);
+    srcml_close_archive(archive);
+    srcml_free_archive(archive);
+  }
+
+  {
+
+    srcml_archive * archive = srcml_create_archive();
+    srcml_read_open_memory(archive, srcml_two.c_str(), srcml_two.size());
+    srcml_unit * unit = srcml_read_unit_header(archive);
+    dassert(srcml_unit_get_language(unit), std::string("C"));
+    dassert(srcml_unit_get_filename(unit), std::string("project.c"));
+    dassert(srcml_unit_get_directory(unit), 0);
+    dassert(srcml_unit_get_version(unit), 0);
+    dassert(unit->unit, 0);
+    unit = srcml_read_unit_header(archive);
+    dassert(srcml_unit_get_language(unit), std::string("C"));
+    dassert(srcml_unit_get_filename(unit), std::string("project.c"));
+    dassert(srcml_unit_get_directory(unit), 0);
+    dassert(srcml_unit_get_version(unit), 0);
+    dassert(unit->unit, 0);
+    unit = srcml_read_unit_header(archive);
+    dassert(unit, 0);
 
     srcml_free_unit(unit);
     srcml_close_archive(archive);
