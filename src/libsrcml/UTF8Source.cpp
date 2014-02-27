6fa8e622521683a1bc087b37e338ba3dd8a6c43d
Michael John Decker <mdecker6@kent.edu>
2014-02-27 07:34:36 -0500

    Filling out something to convert output encodings.

---
+++ src/libsrcml/UTF8Source.cpp
@@ -0,0 +1,107 @@
+/**
+ * @file UTF8CharBuffer.cpp
+ *
+ * @copyright Copyright (C) 2008-2014  SDML (www.srcML.org)
+ *
+ * This file is part of the srcML Toolkit.
+ *
+ * The srcML Toolkit is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * The srcML Toolkit is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the srcML Toolkit; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include <iostream>
+#include <UTF8Source.hpp>
+
+
+UTF8Source::UTF8Source(const char * filename, const char * encoding) 
+    : output(0), cd(0) {
+
+    if(!filename) throw UTF8SourceError();
+
+    output = fopen(filename, "w");
+
+    if(!output) throw UTF8SourceError();
+
+    processEncoding(encoding);
+
+}
+
+/*
+UTF8Source::UTF8Source(const char * raw_buffer, size_t buffer_size, const char * encoding) 
+    : output(0), raw_buffer(raw_buffer), pos(0), size(buffer_size), output_buffer(0), cd(0) {
+
+    if(!raw_buffer) throw UTF8SourceError();
+
+    processEncoding(encoding);
+
+}
+*/
+
+void UTF8Source::processEncoding(const char * encoding) {
+
+    if(encoding && strcmp("UTF-8", encoding) != 0) {
+
+	cd = iconv_open(encoding, "UTF-8");
+
+    }
+
+}
+
+size_t UTF8Source::growBuffer(const char * & raw_buffer, size_t size) {
+
+
+    size_t num_in_convert = size;
+    size_t num_out_convert = 4 * size;
+
+    // iconv incements buffers need temporary since static and get refilled
+    unsigned char * output_buffer = iconv_buffer;
+    iconv(cd, (char **)&raw_buffer, &num_in_convert, (char **)&output_buffer, &num_out_convert);
+
+    return (4 * size) - num_out_convert;
+}
+
+/*
+
+*/
+int UTF8Source::write_string(const char * input, size_t input_size) {
+
+    if(!cd) {
+
+	fwrite(input, 1, input_size, output);
+	return 1;
+
+    }
+
+    size_t pos = 0; 
+
+    while(pos < input_size) {
+
+	size_t size = input_size - pos;
+	size_t num_to_convert = size < SRCBUFSIZE ? size : SRCBUFSIZE;
+	size_t num_converted = growBuffer(input, num_to_convert);
+
+	fwrite(iconv_buffer, 1, num_converted, output);
+
+	pos += num_to_convert;
+
+    }
+
+    return 1;
+}
+
+UTF8Source::~UTF8Source() {
+
+    if(cd) iconv_close(cd);
+
+}
---
+++ src/libsrcml/UTF8Source.hpp
@@ -0,0 +1,69 @@
+/**
+ * UTF8Source.hpp
+ *
+ * Copyright (C) 2008-2014  SDML (www.srcML.org)
+ *
+ * This file is part of the srcML Toolkit.
+ *
+ * The srcML Toolkit is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * The srcML Toolkit is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the srcML Toolkit; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+/**
+ * CharBuffer for antlr that uses a iconv input routines.
+ * This provides for any necessary encoding conversion to UTF8,
+ * so using libxml for XML output is direct.
+ *
+ * libxml stores data internally in UTF*
+ */
+
+#ifndef INCLUDE_UTF8Source_HPP
+#define INCLUDE_UTF8Source_HPP
+
+#include <cstring>
+
+#include <stdio.h>
+#include <iconv.h>
+
+class UTF8SourceError {};
+
+
+class UTF8Source {
+public:
+
+    // size of the original character buffer
+    static const size_t SRCBUFSIZE = 1024;
+
+    // Create a character buffer
+    UTF8Source(const char * filename, const char * encoding);
+    UTF8Source(const char* raw_buffer, size_t buffer_size, const char * encoding);
+
+    int write_string(const char * input, size_t input_size);
+
+    ~UTF8Source();
+
+private:
+
+    size_t growBuffer(const char * & raw_buffer, size_t size);
+    void processEncoding(const char * encoding);
+
+    FILE * output;
+
+    char * raw_buffer;
+
+    iconv_t cd;
+    unsigned char iconv_buffer[4 * SRCBUFSIZE];
+
+};
+#endif
