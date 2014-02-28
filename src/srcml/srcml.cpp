b21ec8a135af43092409dc48cdf8ffa314fe1258
Michael John Decker <mdecker6@kent.edu>
2014-02-28 16:03:42 -0500

    Correct file number defines for windows

--- src/srcml/srcml.cpp
+++ src/srcml/srcml.cpp
@@ -43,6 +43,9 @@
 
 #ifdef WIN32
 #include <io.h>
+#define  STDIN_FILENO   0       /* standard input file descriptor */
+#define STDOUT_FILENO   1       /* standard output file descriptor */
+#define STDERR_FILENO   2       /* standard error file descriptor */
 #endif
 
 int main(int argc, char * argv[]) {
