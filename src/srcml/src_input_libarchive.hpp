4d8fa4b134aa6bc88c0fe2a9a59028cb70895966
Michael L. Collard, Ph.D <collard@uakron.edu>
2014-03-03 07:46:14 -0500

    Cleanup header comment

--- src/srcml/src_input_libarchive.hpp
+++ src/srcml/src_input_libarchive.hpp
@@ -18,11 +18,8 @@
  * You should have received a copy of the GNU General Public License
  * along with the srcML Toolkit; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- */
-
-/*
-  src_input_libarchive assigns local files, stdin, and archival input to the
-  srcml parsing queue
+ *
+ * Source input from local files, stdin, and source archives for srcml parsing queue
 */
 
 #ifndef SRC_INPUT_LIBARCHIVE_HPP
