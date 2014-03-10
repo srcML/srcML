9ff32dcb7c49c1482676b6bd36e28dbe1bc8daaa
Michael L. Collard, Ph.D <collard@uakron.edu>
2014-03-10 14:54:23 -0400

    Start of SHA1 encoding for OS X

--- src/srcml/srcml_consume.cpp
+++ src/srcml/srcml_consume.cpp
@@ -24,6 +24,9 @@
   srcml_consume calls appropriate libsrcml functions for processing source file data
 */
 
+#ifdef __MACH__
+#include <CommonCrypto/CommonDigest.h>
+#endif
 #include <srcml_consume.hpp>
 #include <srcml.h>
 #include <thread_queue.hpp>
@@ -33,6 +36,7 @@
 #include <parse_queue.hpp>
 #include <write_request.hpp>
 #include <write_queue.hpp>
+#include <iomanip>
 
 // Public consumption thread function
 void srcml_consume(ParseQueue* queue, WriteQueue* wqueue) {
@@ -57,6 +61,31 @@ void srcml_consume(ParseQueue* queue, WriteQueue* wqueue) {
             if (pr.version)
                 srcml_unit_set_version(unit, pr.version->c_str());
             srcml_unit_set_language(unit, pr.language.c_str());
+
+#ifdef __MACH__
+            if (pr.buffer.size()) {
+
+                // compute the SHA1 has for this unit
+                // based on the code as encoding in the original file
+                unsigned char md[CC_SHA1_DIGEST_LENGTH];
+                CC_SHA1(&pr.buffer[0], pr.buffer.size(), md);
+
+                char outmdo[CC_SHA1_DIGEST_LENGTH * 2 + 1];
+                for (int i = 0; i < CC_SHA1_DIGEST_LENGTH; ++i)
+                    snprintf(outmdo + i * 2, 3, "%02x", md[i]);
+
+                // convert to hex ascii string
+                static const char hexchar[] = { '0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f' };
+                char outmd[CC_SHA1_DIGEST_LENGTH * 2 + 1];
+                for (int i = 0; i < CC_SHA1_DIGEST_LENGTH; ++i) {
+                    outmd[i * 2]     = hexchar[md[i] >> 4];
+                    outmd[i * 2 + 1] = hexchar[md[i] & 0x0F];
+                }
+                outmd[CC_SHA1_DIGEST_LENGTH * 2] = '\0';
+
+//                srcml_unit_set_version(unit, outmd);
+            }
+#endif
 
             if (pr.disk_filename.empty()) {
                 pr.status = srcml_parse_unit_memory(unit, &pr.buffer.front(), pr.buffer.size());
