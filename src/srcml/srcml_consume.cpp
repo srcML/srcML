e745f5d804e114f8f11040751c8e1a2a5ab30bcc
Michael L. Collard, Ph.D <collard@uakron.edu>
2014-03-10 20:36:37 -0400

    Cleanup of sha1 code for OS X

--- src/srcml/srcml_consume.cpp
+++ src/srcml/srcml_consume.cpp
@@ -50,7 +50,7 @@ void srcml_consume(ParseQueue* queue, WriteQueue* wqueue) {
             break;
 
         // build and parse
-        srcml_unit * unit = 0;
+        srcml_unit* unit = 0;
 
         if (!pr.status) {
             unit = srcml_create_unit(pr.srcml_arch);
@@ -63,30 +63,38 @@ void srcml_consume(ParseQueue* queue, WriteQueue* wqueue) {
             srcml_unit_set_language(unit, pr.language.c_str());
 
 #ifdef __MACH__
-            if (pr.buffer.size()) {
+            // compute the SHA1 has for this unit
+            // based on the code as encoding in the original file
+            unsigned char md[CC_SHA1_DIGEST_LENGTH];
+            CC_SHA1(&pr.buffer.front(), (CC_LONG)pr.buffer.size(), md);
 
-                // compute the SHA1 has for this unit
-                // based on the code as encoding in the original file
-                unsigned char md[CC_SHA1_DIGEST_LENGTH];
-                CC_SHA1(&pr.buffer[0], pr.buffer.size(), md);
-
-                char outmdo[CC_SHA1_DIGEST_LENGTH * 2 + 1];
-                for (int i = 0; i < CC_SHA1_DIGEST_LENGTH; ++i)
-                    snprintf(outmdo + i * 2, 3, "%02x", md[i]);
-
-                // convert to hex ascii string
-                static const char hexchar[] = { '0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f' };
-                char outmd[CC_SHA1_DIGEST_LENGTH * 2 + 1];
-                for (int i = 0; i < CC_SHA1_DIGEST_LENGTH; ++i) {
-                    outmd[i * 2]     = hexchar[md[i] >> 4];
-                    outmd[i * 2 + 1] = hexchar[md[i] & 0x0F];
-                }
-                outmd[CC_SHA1_DIGEST_LENGTH * 2] = '\0';
-
-//                srcml_unit_set_version(unit, outmd);
-            }
+            // convert to hex ascii string
+            static const char hexchar[] = { '0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f' };
+            const char outmd[] = {
+                hexchar[md[0] >> 4], hexchar[md[0] & 0x0F],
+                hexchar[md[1] >> 4], hexchar[md[1] & 0x0F],
+                hexchar[md[2] >> 4], hexchar[md[2] & 0x0F],
+                hexchar[md[3] >> 4], hexchar[md[3] & 0x0F],
+                hexchar[md[4] >> 4], hexchar[md[4] & 0x0F],
+                hexchar[md[5] >> 4], hexchar[md[5] & 0x0F],
+                hexchar[md[6] >> 4], hexchar[md[6] & 0x0F],
+                hexchar[md[7] >> 4], hexchar[md[7] & 0x0F],
+                hexchar[md[8] >> 4], hexchar[md[8] & 0x0F],
+                hexchar[md[9] >> 4], hexchar[md[9] & 0x0F],
+                hexchar[md[10] >> 4], hexchar[md[10] & 0x0F],
+                hexchar[md[11] >> 4], hexchar[md[11] & 0x0F],
+                hexchar[md[12] >> 4], hexchar[md[12] & 0x0F],
+                hexchar[md[13] >> 4], hexchar[md[13] & 0x0F],
+                hexchar[md[14] >> 4], hexchar[md[14] & 0x0F],
+                hexchar[md[15] >> 4], hexchar[md[15] & 0x0F],
+                hexchar[md[16] >> 4], hexchar[md[16] & 0x0F],
+                hexchar[md[17] >> 4], hexchar[md[17] & 0x0F],
+                hexchar[md[18] >> 4], hexchar[md[18] & 0x0F],
+                hexchar[md[19] >> 4], hexchar[md[19] & 0x0F],
+                '\0'
+            };
+            srcml_unit_set_version(unit, outmd);
 #endif
-
             if (pr.disk_filename.empty()) {
                 pr.status = srcml_parse_unit_memory(unit, &pr.buffer.front(), pr.buffer.size());
             } else {
