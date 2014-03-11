648574c96c65011d2a66d190e0146c532c4ebe87
Michael John Decker <mdecker6@kent.edu>
2014-03-11 11:10:41 -0400

    Read in a timestamp

--- src/libsrcml/srcMLReaderHandler.hpp
+++ src/libsrcml/srcMLReaderHandler.hpp
@@ -407,9 +407,10 @@ public :
             std::string value = "";
             value.append((const char *)attributes[pos + 3], attributes[pos + 4] - attributes[pos + 3]);
 
-            if(attribute == "timestamp")
+            if(attribute == "timestamp") {
                 srcml_archive_enable_option(archive, SRCML_OPTION_TIMESTAMP);
-            else if(attribute == "language")
+		srcml_unit_set_timestamp(unit, value.c_str());
+            } else if(attribute == "language")
                 srcml_unit_set_language(unit, value.c_str());
             else if(attribute == "filename")
                 srcml_unit_set_filename(unit, value.c_str());
