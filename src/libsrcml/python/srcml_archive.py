2e813bd8459f41a26d2a1e76fa228b549232badd
Michael John Decker <mdecker6@kent.edu>
2014-03-13 10:54:29 -0400

    Add calls

--- src/libsrcml/python/srcml_archive.py
+++ src/libsrcml/python/srcml_archive.py
@@ -414,11 +414,29 @@ class srcml_archive :
     def append_transform_xpath(self, xpath_string) :
         check_return(libsrcml.srcml_append_transform_xpath(self.archive, xpath_string))
 
-    def append_transform_xslt(self, xslt_filename) :
-        check_return(libsrcml.srcml_append_transform_xslt(self.archive, xslt_filename))
+    def append_transform_xslt_filename(self, xslt_filename) :
+        check_return(libsrcml.srcml_append_transform_xslt_filename(self.archive, xslt_filename))
 
-    def append_transform_relaxng(self, relaxng_filename) :
-        check_return(libsrcml.srcml_append_transform_relaxng(self.archive, relaxng_filename))
+    def append_transform_xslt_memory(self, xslt_buffer) :
+        check_return(libsrcml.srcml_append_transform_xsl_memory(self.archive, xslt_buffer, len(xslt_buffer)))
+
+    def append_transform_xslt_FILE(self, xslt_file) :
+        check_return(libsrcml.srcml_append_transform_xslt_FILE(self.archive, xslt_file))
+
+    def append_transform_xslt_fd(self, xslt_fd) :
+        check_return(libsrcml.srcml_append_transform_xslt_fd(self.archive, xslt_fd))
+
+    def append_transform_relaxng_filename(self, relaxng_filename) :
+        check_return(libsrcml.srcml_append_transform_relaxng_filename(self.archive, relaxng_filename))
+
+    def append_transform_relaxng_memory(self, relaxng_buffer) :
+        check_return(libsrcml.srcml_append_transform_relaxng_memory(self.archive, relaxng_buffer, len(relaxng_buffer)))
+
+    def append_transform_relaxng_FILE(self, relaxng_file) :
+        check_return(libsrcml.srcml_append_transform_relaxng_FILE(self.archive, relaxng_file))
+
+    def append_transform_relaxng_fd(self, relaxng_fd) :
+        check_return(libsrcml.srcml_append_transform_relaxng_fd(self.archive, relaxng_fd))
 
     def apply_transforms(self, oarchive) :
         check_return(libsrcml.srcml_apply_transforms(self.archive, oarchive.archive))
