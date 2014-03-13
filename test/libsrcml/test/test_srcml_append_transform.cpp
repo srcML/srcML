2c3a5980acc6f51313023634600de92f419ed36f
Michael John Decker <mdecker6@kent.edu>
2014-03-13 07:36:41 -0400

    Started to test new append transformations

--- test/libsrcml/test/test_srcml_append_transform.cpp
+++ test/libsrcml/test/test_srcml_append_transform.cpp
@@ -30,9 +30,36 @@
 #include <srcml_types.hpp>
 #include <srcmlns.hpp>
 
+#include <string>
+#include <fstream>
+#if defined(__GNUC__) && !defined(__MINGW32__)
+#include <unistd.h>
+#else
+#include <io.h>
+#endif
+#include <fcntl.h>
+
 #include "dassert.hpp"
 
 int main() {
+
+    std::string copy;
+    {
+	std::ifstream in("copy.xsl");
+	char c = 0;
+        while(in.get(c)) {
+            copy += c;
+        }
+    }
+
+    std::string schema;
+    {
+	std::ifstream in("schema.rng");
+	char c = 0;
+        while(in.get(c)) {
+            schema += c;
+        }
+    }
 
     std::string s = "<unit/>";
 
@@ -106,6 +133,135 @@ int main() {
 
     {
         dassert(srcml_append_transform_xslt_filename(0, "copy.xsl"), SRCML_STATUS_INVALID_ARGUMENT);
+    }
+
+    /*
+      srcml_append_transform_xslt_memory
+    */
+
+    {
+        srcml_archive * archive = srcml_create_archive();
+        srcml_read_open_memory(archive, s.c_str(), s.size());
+        srcml_append_transform_xslt_memory(archive, copy.c_str(), copy.size());
+
+        dassert(archive->transformations.back().type, SRCML_XSLT);
+        assert(archive->transformations.back().transformation.doc != 0);
+
+        srcml_close_archive(archive);
+        srcml_free_archive(archive);
+    }
+
+    {
+        srcml_archive * archive = srcml_create_archive();
+        dassert(srcml_append_transform_xslt_memory(archive, copy.c_str(), copy.size()), SRCML_STATUS_INVALID_IO_OPERATION);
+
+        srcml_free_archive(archive);
+    }
+
+    {
+        srcml_archive * archive = srcml_create_archive();
+        srcml_read_open_memory(archive, s.c_str(), s.size());
+        dassert(srcml_append_transform_xslt_memory(archive, 0, copy.size()), SRCML_STATUS_INVALID_ARGUMENT);
+
+        srcml_close_archive(archive);
+        srcml_free_archive(archive);
+    }
+
+    {
+        srcml_archive * archive = srcml_create_archive();
+        srcml_read_open_memory(archive, s.c_str(), s.size());
+        dassert(srcml_append_transform_xslt_memory(archive, copy.c_str(), 0), SRCML_STATUS_INVALID_ARGUMENT);
+
+        srcml_close_archive(archive);
+        srcml_free_archive(archive);
+    }
+
+    {
+        dassert(srcml_append_transform_xslt_memory(0, copy.c_str(), copy.size()), SRCML_STATUS_INVALID_ARGUMENT);
+    }
+
+    /*
+      srcml_append_transform_xslt_FILE
+    */
+
+    {
+        srcml_archive * archive = srcml_create_archive();
+        srcml_read_open_memory(archive, s.c_str(), s.size());
+	FILE * f = fopen("copy.xsl", "r");
+        srcml_append_transform_xslt_FILE(archive, f);
+	fclose(f);
+
+        dassert(archive->transformations.back().type, SRCML_XSLT);
+        assert(archive->transformations.back().transformation.doc != 0);
+
+        srcml_close_archive(archive);
+        srcml_free_archive(archive);
+    }
+
+    {
+        srcml_archive * archive = srcml_create_archive();
+	FILE * f = fopen("copy.xsl", "r");
+        dassert(srcml_append_transform_xslt_FILE(archive, f), SRCML_STATUS_INVALID_IO_OPERATION);
+        fclose(f);
+
+        srcml_free_archive(archive);
+    }
+
+    {
+        srcml_archive * archive = srcml_create_archive();
+        srcml_read_open_memory(archive, s.c_str(), s.size());
+        dassert(srcml_append_transform_xslt_FILE(archive, 0), SRCML_STATUS_INVALID_ARGUMENT);
+
+        srcml_close_archive(archive);
+        srcml_free_archive(archive);
+    }
+
+    {
+	FILE * f = fopen("copy.xsl", "r");
+        dassert(srcml_append_transform_xslt_FILE(0, f), SRCML_STATUS_INVALID_ARGUMENT);
+	fclose(f);
+    }
+
+    /*
+      srcml_append_transform_xslt_fd
+    */
+
+    {
+        srcml_archive * archive = srcml_create_archive();
+        srcml_read_open_memory(archive, s.c_str(), s.size());
+        int fd = open("copy.xsl", O_RDONLY);
+        srcml_append_transform_xslt_fd(archive, fd);
+	close(fd);
+
+        dassert(archive->transformations.back().type, SRCML_XSLT);
+        assert(archive->transformations.back().transformation.doc != 0);
+
+        srcml_close_archive(archive);
+        srcml_free_archive(archive);
+    }
+
+    {
+        srcml_archive * archive = srcml_create_archive();
+        int fd = open("copy.xsl", O_RDONLY);
+        dassert(srcml_append_transform_xslt_fd(archive, fd), SRCML_STATUS_INVALID_IO_OPERATION);
+	close(fd);
+
+        srcml_free_archive(archive);
+    }
+
+    {
+        srcml_archive * archive = srcml_create_archive();
+        srcml_read_open_memory(archive, s.c_str(), s.size());
+        dassert(srcml_append_transform_xslt_fd(archive, 0), SRCML_STATUS_INVALID_ARGUMENT);
+
+        srcml_close_archive(archive);
+        srcml_free_archive(archive);
+    }
+
+    {
+        int fd = open("copy.xsl", O_RDONLY);
+        dassert(srcml_append_transform_xslt_fd(0, fd), SRCML_STATUS_INVALID_ARGUMENT);
+	close(fd);
     }
 
     /*
