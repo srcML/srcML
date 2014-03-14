fa18a08d5244afeaec17143cf3b02245a0af02ec
Michael John Decker <mdecker6@kent.edu>
2014-03-14 07:41:50 -0400

    Tests should be in bin directory

--- CMake/testing.cmake
+++ CMake/testing.cmake
@@ -36,6 +36,7 @@ macro(addUnitTest TEST_NAME FILE_NAME)
     add_executable(${TEST_NAME} ${FILE_NAME})
     target_link_libraries(${TEST_NAME} srcml_static ${ARGN})
     add_test(${TEST_NAME} ${TEST_NAME})
+    set_target_properties(${TEST_NAME} PROPERTIES RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin)
 endmacro()
 
 
