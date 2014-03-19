9badffbe2b61d0b1b3f7c7cd8b3f093dfc817126
Michael John Decker <mdecker6@kent.edu>
2014-03-19 14:01:20 -0400

    Created macros for read/write and open/close.  Starting to use them.

---
+++ src/libsrcml/srcml_macros.hpp
@@ -0,0 +1,16 @@
+#ifndef INCLUDED_SRCML_MACROS_HPP
+#define INCLUDED_SRCML_MACROS_HPP
+
+#ifndef _MSC_BUILD
+#define OPEN(FILE, ACCESS, PERM) open(FILE, ACCESS, PERM)
+#define CLOSE(FILE) close(FILE)
+#define READ(FILE, BUF, NUM) read(FILE, BUF, NUM)
+#define WRITE(FILE, BUF, NUM) write(FILE, BUF, NUM)
+#else
+#define OPEN(FILE, ACCESS, PERM) _open(FILE, ACCESS)
+#define close(FILE) _close(FILE)
+#define READ(FILE, BUF, NUM) _read(FILE, BUF, NUM)
+#define WRITE(FILE, BUF, NUM) _write(FILE, BUF, NUM)
+#endif
+
+#endif
