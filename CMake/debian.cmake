7d7d79d8a7fcb8a120de43f3260f8596a61b7bfe
Michael John Decker <mdecker6@kent.edu>
2014-03-25 10:55:10 -0400

    Use the correct architecture for rpm and debian

--- CMake/debian.cmake
+++ CMake/debian.cmake
@@ -21,7 +21,7 @@
 set(CPACK_DEBIAN_PACKAGE_NAME "srcML")
 
 # set architecture
-set(CPACK_DEBIAN_PACKAGE_ARCHITECTURE "i386")
+set(CPACK_DEBIAN_PACKAGE_ARCHITECTURE ${CMAKE_SYSTEM_PROCESSOR})
 
 # set dependencies
 set(CPACK_DEBIAN_PACKAGE_DEPENDS "libxml2 (>= 2.7.8), libxslt1.1 (>= 1.1.26), libarchive12 (>= 3.0.3)")
