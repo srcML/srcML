94d3b01b42487c5f98dd15f2cac6cd8bf1f790df
Michael L. Collard, Ph.D <collard@uakron.edu>
2014-04-07 13:33:55 -0400

    Fixing header

--- src/srcml/src_language.cpp
+++ src/srcml/src_language.cpp
@@ -20,10 +20,6 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-/*
-  src_language.cpp functions for determining the language of a given input file
-*/
-
 #include <src_language.hpp>
 #include <boost/filesystem.hpp>
 
