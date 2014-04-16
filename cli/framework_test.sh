0a16772ee1cd461accf2cb636250cb344b71cb1c
Michael L. Collard, Ph.D <collard@uakron.edu>
2014-04-16 15:03:43 -0400

    Combine cases into one check

--- cli/framework_test.sh
+++ cli/framework_test.sh
@@ -7,31 +7,19 @@ STDOUT=.stdout_$(BASENAME $0)
 # log stdout and stderr to files
 exec 6>&1 7>&2 1>$STDOUT 2>$STDERR
 
-check_stdout() {
+check() {
 
    # log stdout and stderr to standard streams
-   exec 1>&6 2>&7
+   exec 1>&6 #2>&7
 
    # verify stdout of command (stdin to this check) agrees with STDOUT
-   diff $STDOUT <(cat <&3)
-   [ "$?" != "0" ] && exit 1
-
-   # verify stderr of command agrees with caught stderr
-   diff $STDERR <(cat <&4)
-   [ "$?" != "0" ] && exit 1
-
-   # log stdout and stderr back to files
-   exec 6>&- 7>&- 6>&1 7>&2 1>$STDOUT 2>$STDERR
-}
-
-check_file() {
-
-   # log stdout and stderr to standard streams
-   exec 1>&6 2>&7
-
-   # verify output file of command
-   diff $1 <(cat <&3)
-   [ "$?" != "0" ] && exit 1
+   if [ $# -eq 1 ]; then
+       diff $1 <(cat <&3)
+       [ "$?" != "0" ] && exit 1
+   else
+       diff $STDOUT <(cat <&3)
+       [ "$?" != "0" ] && exit 1
+   fi
 
    # verify stderr of command agrees with caught stderr
    diff $STDERR <(cat <&4)
