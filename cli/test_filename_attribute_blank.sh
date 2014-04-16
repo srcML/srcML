39015d5cea6a257df584cc7f0a27c3d2d12d1ab2
Michael L. Collard, Ph.D <collard@uakron.edu>
2014-04-16 15:55:05 -0400

    Change around the test files

--- cli/test_filename_attribute_blank.sh
+++ cli/test_filename_attribute_blank.sh
@@ -5,28 +5,6 @@
 # test framework
 source $(dirname "$0")/framework_test.sh
 
-srcml -l C++ << 'STDIN'
- 
-STDIN
-
-check 3<< 'STDOUT' 4<< 'STDERR'
-<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" language="C++"> 
-</unit>
-STDOUT
-STDERR
-
-srcml -l C++ -o foo.xml << 'STDIN'
- 
-STDIN
-
-check foo.xml 3<< 'FILECONTENTS' 4<< 'STDERR'
-<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" language="C++"> 
-</unit>
-FILECONTENTS
-STDERR
-
 rm -f a.cpp
 echo "" > a.cpp
 srcml -l C++ --filename="foo.cpp" a.cpp
