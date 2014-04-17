224997bfe17e666ad67d82ec1c6da729d32ecca2
Michael John Decker <mdecker6@kent.edu>
2014-04-17 11:26:00 -0400

    More work on adding by element to python

--- python/src/srcml_archive.py
+++ python/src/srcml_archive.py
@@ -411,6 +411,27 @@ class srcml_archive :
     def write_unit(self, unit) :
         check_return(libsrcml.srcml_write_unit(self.archive, unit.unit))
 
+    def write_start_unit(self, unit) :
+        check_return(libsrcml.srcml_write_start_unit(self.archive, unit.unit))
+
+    def write_end_unit(self) :
+        check_return(libsrcml.srcml_write_end_unit(self.archive))
+
+    def write_start_element(self, prefix, name, uri) :
+        check_return(libsrcml.srcml_write_start_element(self.archive, prefix, name, uri))
+
+    def write_end_element(self) :
+        check_return(libsrcml.srcml_write_end_element(self.archive))
+
+    def write_namespace(self, prefix, uri) :
+        check_return(libsrcml.srcml_write_namespace(self.archive, prefix, uri))
+
+    def write_attribute(self, prefix, name, uri, content) :
+        check_return(libsrcml.srcml_write_attribute(self.archive, prefix, name, uri, content))
+
+    def write_string(self, content) :
+        check_return(libsrcml.srcml_write_string(self.archive, content))
+
     def read_unit(self) :
         unit = libsrcml.srcml_read_unit(self.archive)
 
