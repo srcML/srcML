426b66a251c25c2a28866eb996e8f2cae20da7ab
Drew Guarnera <dtg3@zips.uakron.edu>
2014-05-11 12:35:33 -0400

    Open input for transformations

--- src/srcml/transform_srcml.cpp
+++ src/srcml/transform_srcml.cpp
@@ -27,11 +27,26 @@
 
 // transform srcml with query or transformation
 void transform_srcml(const srcml_request_t& srcml_request,
-                     const srcml_input_t& /*input_sources*/,
+                     const srcml_input_t& input_sources,
                      const srcml_output_dest& /* output*/) {
 
-	// iterate through all transformations added during cli parsing
-	BOOST_FOREACH(const std::string& trans, srcml_request.transformations) {
-		std::cerr << trans << "\n"; // output the transformations for debugging
+	BOOST_FOREACH(const srcml_input_src& input, input_sources) {
+		srcml_archive* arch = srcml_create_archive();
+		int status;
+        if (contains<int>(input))
+            status = srcml_read_open_fd(arch, input);
+        else if (contains<FILE*>(input))
+            status = srcml_read_open_FILE(arch, input);
+        else
+            status = srcml_read_open_filename(arch, input.c_str());
+        if (status != SRCML_STATUS_OK)
+            throw status;
+
+		// iterate through all transformations added during cli parsing
+		BOOST_FOREACH(const std::string& trans, srcml_request.transformations) {
+			std::cerr << trans << "\n"; // output the transformations for debugging
+		}
+
+		srcml_free_archive(arch);
 	}
 }
