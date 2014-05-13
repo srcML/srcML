153f4a5209df8181efa03e30846e5d51dc71c3b0
Michael John Decker <mdecker6@kent.edu>
2014-05-13 08:35:39 -0400

    map based template does not work on Visual Studio.  Use multi-pass until figure out.

--- src/parser/bitset_bucket_sorter.hpp
+++ src/parser/bitset_bucket_sorter.hpp
@@ -46,6 +46,8 @@
 
 #define create_token_set(NAME, ...) typedef boost::mpl::vector_c<unsigned long, __VA_ARGS__> NAME
 
+#ifndef _MSC_BUILD
+
 template<typename seq, typename key, typename item>
 struct replace_pos {
 
@@ -54,42 +56,6 @@ struct replace_pos {
     typedef typename boost::mpl::insert<temp, boost::mpl::pair<key, item> >::type type;
 
 };
-
-// mutli-pass bucket sort
-template<typename token_set, int bucket_number, int limit>
-struct bitset_bucket {
-
-    typedef typename boost::mpl::if_<boost::mpl::less<boost::mpl::times<boost::mpl::long_<bucket_number>, boost::mpl::int_<32> >, boost::mpl::long_<limit> >,
-            typename boost::mpl::accumulate<token_set, boost::mpl::long_<0>,
-                boost::mpl::if_<
-                    boost::mpl::equal_to<boost::mpl::shift_right<boost::mpl::_2, boost::mpl::long_<5> >, boost::mpl::long_<bucket_number> >,
-                    boost::mpl::bitor_<boost::mpl::_1, boost::mpl::shift_left<boost::mpl::long_<1>, boost::mpl::modulus<boost::mpl::_2, boost::mpl::long_<32> > > >,
-                    boost::mpl::_1 >
-                >::type,
-            boost::mpl::long_<0> >::type type;
-
-};
-
-/*
-template<typename token_set>
-struct bitset_buckets {
-
-    static const int num_token_longs = 24;
-    static const unsigned long data[num_token_longs];
-
-};
-
-
-template<typename token_set>
-const unsigned long bitset_buckets<token_set>::data[bitset_buckets<token_set>::num_token_longs] = 
-        { bitset_bucket<token_set, 0>::type::value, bitset_bucket<token_set, 1>::type::value, bitset_bucket<token_set, 2>::type::value, bitset_bucket<token_set, 3>::type::value
-        , bitset_bucket<token_set, 4>::type::value, bitset_bucket<token_set, 5>::type::value, bitset_bucket<token_set, 6>::type::value, bitset_bucket<token_set, 7>::type::value
-        , bitset_bucket<token_set, 8>::type::value, bitset_bucket<token_set, 9>::type::value, bitset_bucket<token_set, 10>::type::value, bitset_bucket<token_set, 11>::type::value
-        , bitset_bucket<token_set, 12>::type::value, bitset_bucket<token_set, 13>::type::value, bitset_bucket<token_set, 14>::type::value, bitset_bucket<token_set, 15>::type::value
-        , bitset_bucket<token_set, 16>::type::value, bitset_bucket<token_set, 17>::type::value, bitset_bucket<token_set, 18>::type::value, bitset_bucket<token_set, 19>::type::value
-        , bitset_bucket<token_set, 20>::type::value, bitset_bucket<token_set, 21>::type::value, bitset_bucket<token_set, 22>::type::value, bitset_bucket<token_set, 23>::type::value
-};
-*/
 
 template<typename token_set>
 struct bitset_bucket_sorter {
@@ -171,5 +137,42 @@ const unsigned long bitset_buckets<token_set>::data[bitset_buckets<token_set>::n
     boost::mpl::at<bitset_buckets<token_set>::buckets, boost::mpl::integral_c<unsigned long, 23> >::type::value
 
 };
+
+#else
+
+// mutli-pass bucket sort
+template<typename token_set, int bucket_number, int limit = srcMLParser::START_ELEMENT_TOKEN>
+struct bitset_bucket {
+
+    typedef typename boost::mpl::if_<boost::mpl::less<boost::mpl::times<boost::mpl::long_<bucket_number>, boost::mpl::int_<32> >, boost::mpl::long_<limit> >,
+            typename boost::mpl::accumulate<token_set, boost::mpl::long_<0>,
+                boost::mpl::if_<
+                    boost::mpl::equal_to<boost::mpl::shift_right<boost::mpl::_2, boost::mpl::long_<5> >, boost::mpl::long_<bucket_number> >,
+                    boost::mpl::bitor_<boost::mpl::_1, boost::mpl::shift_left<boost::mpl::long_<1>, boost::mpl::modulus<boost::mpl::_2, boost::mpl::long_<32> > > >,
+                    boost::mpl::_1 >
+                >::type,
+            boost::mpl::long_<0> >::type type;
+
+};
+
+template<typename token_set>
+struct bitset_buckets {
+
+    static const int num_token_longs = 24;
+    static const unsigned long data[num_token_longs];
+
+};
+
+template<typename token_set>
+const unsigned long bitset_buckets<token_set>::data[bitset_buckets<token_set>::num_token_longs] = 
+        { bitset_bucket<token_set, 0>::type::value, bitset_bucket<token_set, 1>::type::value, bitset_bucket<token_set, 2>::type::value, bitset_bucket<token_set, 3>::type::value
+        , bitset_bucket<token_set, 4>::type::value, bitset_bucket<token_set, 5>::type::value, bitset_bucket<token_set, 6>::type::value, bitset_bucket<token_set, 7>::type::value
+        , bitset_bucket<token_set, 8>::type::value, bitset_bucket<token_set, 9>::type::value, bitset_bucket<token_set, 10>::type::value, bitset_bucket<token_set, 11>::type::value
+        , bitset_bucket<token_set, 12>::type::value, bitset_bucket<token_set, 13>::type::value, bitset_bucket<token_set, 14>::type::value, bitset_bucket<token_set, 15>::type::value
+        , bitset_bucket<token_set, 16>::type::value, bitset_bucket<token_set, 17>::type::value, bitset_bucket<token_set, 18>::type::value, bitset_bucket<token_set, 19>::type::value
+        , bitset_bucket<token_set, 20>::type::value, bitset_bucket<token_set, 21>::type::value, bitset_bucket<token_set, 22>::type::value, bitset_bucket<token_set, 23>::type::value
+};
+
+#endif
 
 #endif
