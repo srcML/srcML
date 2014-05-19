af65213ba2486338ae2bad653d9b19a9368222c6
Michael L. Collard, Ph.D <collard@uakron.edu>
2014-05-19 08:53:21 -0400

    Final fix for curl segmentation fault

--- src/srcml/src_input_libarchive.cpp
+++ src/srcml/src_input_libarchive.cpp
@@ -118,80 +118,76 @@ void src_input_libarchive(ParseQueue& queue,
         exit(1);
     }
 
-    if (status == ARCHIVE_OK) {
+    /* In general, go through this once for each time the header can be read
+       Exception: if empty, go through the loop exactly once */
+    int count = 0;
+    archive_entry *entry;
+    while (status == ARCHIVE_OK &&
+           (((status = archive_read_next_header(arch, &entry)) == ARCHIVE_OK) ||
+            (status == ARCHIVE_EOF && !count))) {
 
-        /* In general, go through this once for each time the header can be read
-           Exception: if empty, go through the loop exactly once */
-        int count = 0;
-        int status = ARCHIVE_OK;
-        archive_entry *entry;
-        while (status == ARCHIVE_OK &&
-               (((status = archive_read_next_header(arch, &entry)) == ARCHIVE_OK) ||
-                (status == ARCHIVE_EOF && !count))) {
+        // skip any directories
+        if (status == ARCHIVE_OK && archive_entry_filetype(entry) == AE_IFDIR)
+            continue;
 
-            // skip any directories
-            if (status == ARCHIVE_OK && archive_entry_filetype(entry) == AE_IFDIR)
-                continue;
+        // default is filename from archive entry (if not empty)
+        std::string filename = status == ARCHIVE_OK ? archive_entry_pathname(entry) : "";
 
-            // default is filename from archive entry (if not empty)
-            std::string filename = status == ARCHIVE_OK ? archive_entry_pathname(entry) : "";
+        if (count == 0 && filename != "data" && status != ARCHIVE_EOF)
+            srcml_archive_enable_option(srcml_arch, SRCML_OPTION_ARCHIVE);
 
-            if (count == 0 && filename != "data" && status != ARCHIVE_EOF)
-                srcml_archive_enable_option(srcml_arch, SRCML_OPTION_ARCHIVE);
+        // archive entry filename for non-archive input is "data"
+        if (filename.empty() || filename == "data")
+            filename = input_file.resource;
 
-            // archive entry filename for non-archive input is "data"
-            if (filename.empty() || filename == "data")
-                filename = input_file.resource;
+        if (srcml_request.att_filename && !(srcml_archive_get_options(srcml_arch) & SRCML_OPTION_ARCHIVE))
+            filename = *srcml_request.att_filename;
 
-            if (srcml_request.att_filename && !(srcml_archive_get_options(srcml_arch) & SRCML_OPTION_ARCHIVE))
-                filename = *srcml_request.att_filename;
+        // language may have been explicitly set
+        std::string language;
 
-            // language may have been explicitly set
-            std::string language;
+        if (srcml_request.att_language)
+            language = *srcml_request.att_language;
 
-            if (srcml_request.att_language)
-                language = *srcml_request.att_language;
+        // if not explicitly set, language comes from extension
+        // we have to do this ourselves, since libsrcml can't for memory
+        if (language.empty())
+            if (const char* l = srcml_archive_check_extension(srcml_arch, filename.c_str()))
+                language = l;
 
-            // if not explicitly set, language comes from extension
-            // we have to do this ourselves, since libsrcml can't for memory
-            if (language.empty())
-                if (const char* l = srcml_archive_check_extension(srcml_arch, filename.c_str()))
-                    language = l;
+        // form the parsing request
+        ParseRequest* prequest = new ParseRequest;
+        if (srcml_request.att_filename || (filename != "-"))
+            prequest->filename = filename;
+        prequest->directory = srcml_request.att_directory;
+        prequest->version = srcml_request.att_version;
+        prequest->srcml_arch = srcml_arch;
+        prequest->language = language;
+        prequest->status = !language.empty() ? 0 : SRCML_STATUS_UNSET_LANGUAGE;
 
-            // form the parsing request
-            ParseRequest* prequest = new ParseRequest;
-            if (srcml_request.att_filename || (filename != "-"))
-                prequest->filename = filename;
-            prequest->directory = srcml_request.att_directory;
-            prequest->version = srcml_request.att_version;
-            prequest->srcml_arch = srcml_arch;
-            prequest->language = language;
-            prequest->status = !language.empty() ? 0 : SRCML_STATUS_UNSET_LANGUAGE;
+        // fill up the parse request buffer
+        if (!status) {
+            // if we know the size, create the right sized data_buffer
+            if (archive_entry_size_is_set(entry))
+                prequest->buffer.reserve(archive_entry_size(entry));
 
-            // fill up the parse request buffer
-            if (!status) {
-                // if we know the size, create the right sized data_buffer
-                if (archive_entry_size_is_set(entry))
-                    prequest->buffer.reserve(archive_entry_size(entry));
-
-                const char* buffer;
-                size_t size;
+            const char* buffer;
+            size_t size;
 #if ARCHIVE_VERSION_NUMBER < 3000000
-                off_t offset;
+            off_t offset;
 #else
-                int64_t offset;
+            int64_t offset;
 #endif
-                while (status == ARCHIVE_OK && archive_read_data_block(arch, (const void**) &buffer, &size, &offset) == ARCHIVE_OK)
-                    prequest->buffer.insert(prequest->buffer.end(), buffer, buffer + size);
-            }
-
-            // schedule for parsing
-            queue.schedule(prequest);
-
-            ++count;
+            while (status == ARCHIVE_OK && archive_read_data_block(arch, (const void**) &buffer, &size, &offset) == ARCHIVE_OK)
+                prequest->buffer.insert(prequest->buffer.end(), buffer, buffer + size);
         }
-        archive_read_finish(arch);
+
+        // schedule for parsing
+        queue.schedule(prequest);
+
+        ++count;
     }
+    archive_read_finish(arch);
 }
 
 namespace {
