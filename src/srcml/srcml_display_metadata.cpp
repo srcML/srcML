296e54986501146b1850498f9b821c4ab39cbe0a
Michael L. Collard, Ph.D <collard@uakron.edu>
2014-05-29 09:40:56 -0400

    Fix which encoding is being specified

--- src/srcml/srcml_display_metadata.cpp
+++ src/srcml/srcml_display_metadata.cpp
@@ -172,9 +172,10 @@ void srcml_display_metadata(const srcml_request_t& srcml_request, const srcml_in
         }
         // srcml->src encoding
         if (srcml_request.command & SRCML_COMMAND_DISPLAY_SRCML_ENCODING){
-            const char* archive_info = srcml_archive_get_src_encoding(srcml_arch);
+            const char* archive_info = srcml_archive_get_encoding(srcml_arch);
+
             if (archive_info)
-                std::cout << "src-encoding=\"" << archive_info << "\"\n";
+                std::cout << "encoding=\"" << archive_info << "\"\n";
         }
         // srcml long info
         if (srcml_request.command & SRCML_COMMAND_LONGINFO) {
