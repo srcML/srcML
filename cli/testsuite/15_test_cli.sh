292e7bf231caecd4194fd24f3e97127c96ce0f1a
Drew Guarnera <dtg3@zips.uakron.edu>
2014-06-11 13:47:01 -0400

    change --get-version to --get-src-version

--- cli/testsuite/15_test_cli.sh
+++ cli/testsuite/15_test_cli.sh
@@ -46,7 +46,7 @@ srcml2src --get-filename <<< "$input"
 
 check 3<<< ""
 
-srcml2src --get-version <<< "$input"
+srcml2src --get-src-version <<< "$input"
 
 check 3<<< ""
 
@@ -67,7 +67,7 @@ srcml2src --get-filename <<< "$input"
 
 check 3<<< ""
 
-srcml2src --get-version <<< "$input"
+srcml2src --get-src-version <<< "$input"
 
 check 3<<< ""
 
