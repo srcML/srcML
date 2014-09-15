4cfe493113b5ea5243d7854354d27baf839ffb58
Michael John Decker <mdecker6@kent.edu>
2014-09-15 13:51:41 -0400

    Test generation of attribute.  Currently, src namespace seems to be output twice.

--- test/unit/libsrcml/test_srcml_xpath.cpp
+++ test/unit/libsrcml/test_srcml_xpath.cpp
@@ -215,7 +215,6 @@ int main() {
         srcml_xpath inserting attribute
     */
 
-
     {
         const char * s = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n"
         "<unit xmlns=\"http://www.sdml.info/srcML/src\" xmlns:cpp=\"http://www.sdml.info/srcML/cpp\" language=\"C++\" filename=\"a.cpp\"><function_decl><type><name>void</name></type> <name>f</name><parameter_list>()</parameter_list>;</function_decl>\n\n"
@@ -245,6 +244,83 @@ int main() {
                         "<function_decl><type><name>void</name></type> <name>g</name><parameter_list>(<param><decl><type><name>int</name></type> <name>a</name></decl></param>, <param><decl><type><name>double</name></type> <name>b</name></decl></param>)</parameter_list>;</function_decl>\n\n"
 
                         "<function_decl><type><name>void</name></type> <name>h</name><parameter_list>()</parameter_list>;</function_decl>\n\n"
+
+                        "</unit>\n\n"
+                        "</unit>\n");
+        xmlFreeParserInputBuffer(buffer_input);
+        UNLINK("input.xml");
+        UNLINK("project.xml");
+    }
+
+    {
+        const char * s = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n"
+        "<unit xmlns=\"http://www.sdml.info/srcML/src\">\n\n"
+
+        "<unit xmlns:cpp=\"http://www.sdml.info/srcML/cpp\" language=\"C++\" filename=\"a.cpp\"><function_decl><type><name>void</name></type> <name>f</name><parameter_list>()</parameter_list>;</function_decl>\n\n"
+
+        "<function_decl><type><name>void</name></type> <name>g</name><parameter_list>(<param><decl><type><name>int</name></type> <name>a</name></decl></param>, <param><decl><type><name>double</name></type> <name>b</name></decl></param>)</parameter_list>;</function_decl>\n\n"
+
+        "<function_decl><type><name>void</name></type> <name>h</name><parameter_list>()</parameter_list>;</function_decl>\n\n"
+
+        "</unit>\n\n"
+        "</unit>\n";
+        
+        std::ofstream file("input.xml");
+        file << s;
+        file.close();
+        xmlParserInputBufferPtr buffer_input = xmlParserInputBufferCreateFilename("input.xml", xmlParseCharEncoding(0));
+        int fd = OPEN("project.xml", O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+        dassert(srcml_xpath(buffer_input, "src:unit", "//src:unit", "foo", "bar", 0, "name", "value", fd, SRCML_OPTION_XML_DECL | SRCML_OPTION_NAMESPACE_DECL), SRCML_STATUS_OK);
+        std::ifstream in("project.xml");
+        std::string output;
+        char temp;
+        while(in.get(temp))
+            output += temp;
+
+        dassert(output, "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n"
+                        "<unit xmlns=\"http://www.sdml.info/srcML/src\" xmlns:foo=\"bar\">\n\n"
+                        "<unit xmlns:cpp=\"http://www.sdml.info/srcML/cpp\" language=\"C++\" filename=\"a.cpp\" foo:name=\"value\"><function_decl><type><name>void</name></type> <name>f</name><parameter_list>()</parameter_list>;</function_decl>\n\n"
+
+                        "<function_decl><type><name>void</name></type> <name>g</name><parameter_list>(<param><decl><type><name>int</name></type> <name>a</name></decl></param>, <param><decl><type><name>double</name></type> <name>b</name></decl></param>)</parameter_list>;</function_decl>\n\n"
+
+                        "<function_decl><type><name>void</name></type> <name>h</name><parameter_list>()</parameter_list>;</function_decl>\n\n"
+
+                        "</unit>\n\n"
+                        "</unit>\n");
+        xmlFreeParserInputBuffer(buffer_input);
+        UNLINK("input.xml");
+        UNLINK("project.xml");
+    }
+
+    {
+        const char * s = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n"
+        "<unit xmlns=\"http://www.sdml.info/srcML/src\" xmlns:cpp=\"http://www.sdml.info/srcML/cpp\" language=\"C++\" filename=\"a.cpp\"><function_decl><type><name>void</name></type> <name>f</name><parameter_list>()</parameter_list>;</function_decl>\n\n"
+
+        "<function_decl><type><name>void</name></type> <name>g</name><parameter_list>(<param><decl><type><name>int</name></type> <name>a</name></decl></param>, <param><decl><type><name>double</name></type> <name>b</name></decl></param>)</parameter_list>;</function_decl>\n\n"
+
+        "<function_decl><type><name>void</name></type> <name>h</name><parameter_list>()</parameter_list>;</function_decl>\n\n"
+
+        "</unit>";
+        
+        std::ofstream file("input.xml");
+        file << s;
+        file.close();
+        xmlParserInputBufferPtr buffer_input = xmlParserInputBufferCreateFilename("input.xml", xmlParseCharEncoding(0));
+        int fd = OPEN("project.xml", O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+        dassert(srcml_xpath(buffer_input, "src:unit", "//src:function_decl", "foo", "bar", 0, "name", "value", fd, SRCML_OPTION_XML_DECL | SRCML_OPTION_NAMESPACE_DECL), SRCML_STATUS_OK);
+        std::ifstream in("project.xml");
+        std::string output;
+        char temp;
+        while(in.get(temp))
+            output += temp;
+
+        dassert(output, "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n"
+                        "<unit xmlns=\"http://www.sdml.info/srcML/src\" xmlns:foo=\"bar\">\n\n"
+                        "<unit xmlns:cpp=\"http://www.sdml.info/srcML/cpp\" language=\"C++\" filename=\"a.cpp\"><function_decl foo:name=\"value\"><type><name>void</name></type> <name>f</name><parameter_list>()</parameter_list>;</function_decl>\n\n"
+
+                        "<function_decl foo:name=\"value\"><type><name>void</name></type> <name>g</name><parameter_list>(<param><decl><type><name>int</name></type> <name>a</name></decl></param>, <param><decl><type><name>double</name></type> <name>b</name></decl></param>)</parameter_list>;</function_decl>\n\n"
+
+                        "<function_decl foo:name=\"value\"><type><name>void</name></type> <name>h</name><parameter_list>()</parameter_list>;</function_decl>\n\n"
 
                         "</unit>\n\n"
                         "</unit>\n");
