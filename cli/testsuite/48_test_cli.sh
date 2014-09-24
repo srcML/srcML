f34137a739d8a156c580e3238771d330991b3ab9
Michael L. Collard, Ph.D <collard@uakron.edu>
2014-09-24 11:02:13 -0400

    Fix test case 48

--- cli/testsuite/48_test_cli.sh
+++ cli/testsuite/48_test_cli.sh
@@ -4,19 +4,20 @@
 source $(dirname "$0")/framework_test.sh
 
 # test
-srcml = <unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" language="C++"/>
-	STDOUT
+define srcml <<- 'STDOUT'
+<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" language="C++"/>
+STDOUT
 
-fsrcml  <unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" language="C++" filename=sub/a.cpp>
-	STDOUT
+define fsrcml <<- 'STDOUT'
+<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" language="C++" filename="sub/a.cpp"/>
+STDOUT
 
-src2srcml --no-xml-declaration
-src2srcml -l C++ --no-xml-declaration
-validate(open(sub/a.cpp.xml 'r').read() srcml)
-src2srcml --no-xml-declaration
-validate(open(sub/a.cpp.xml 'r').read() fsrcml)
+echo -n "" | src2srcml -l C++ --no-xml-declaration
 
+check 3<<< "$srcml"
 
-##
+echo -n "" | src2srcml -l C++ --no-xml-declaration -o sub/a.cpp.xml
 
-# no namespace declaration
+check sub/a.cpp.xml 3<<< "$srcml"
+
+exit 0
