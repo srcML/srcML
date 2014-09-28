9496e9e48db1e4ea8631423ea90de841ab41b02d
Michael L. Collard, Ph.D <collard@uakron.edu>
2014-09-28 14:27:10 -0400

    Fix test case 34

--- cli/testsuite/34_test_cli.sh
+++ cli/testsuite/34_test_cli.sh
@@ -3,33 +3,44 @@
 # test framework
 source $(dirname "$0")/framework_test.sh
 
+
 # test
-# srcml2src
+define foutput <<- STDOUT
+	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" language="C++"><expr_stmt><expr><name>a</name></expr>;</expr_stmt>
+	</unit>
+	STDOUT
+
+createfile sub/a.cpp.xml "$foutput"
 
 srcml2src sub/a.cpp.xml --output sub/a.cpp
 
-check sub/a.cpp "$sfile"
+check sub/a.cpp 3<<< "a;"
 
 srcml2src sub/a.cpp.xml --output=sub/a.cpp
 
-check sub/a.cpp "$sfile"
+check sub/a.cpp 3<<< "a;"
 
 srcml2src sub/a.cpp.xml -o sub/a.cpp
 
-check sub/a.cpp "$sfile"
+check sub/a.cpp 3<<< "a;"
 
-srcml2src '-' -o sub/a.cpp <<< "$sxmlfile"
+srcml2src - -o sub/a.cpp <<< "$foutput"
 
-check sub/a.cpp "$sfile"
+check sub/a.cpp 3<<< "a;"
 
-srcml2src -o sub/a.cpp <<< "$sxmlfile"
+srcml2src -o sub/a.cpp <<< "$foutput"
 
-check sub/a.cpp "$sfile"
+check sub/a.cpp 3<<< "a;"
 
+srcml2src - --output /dev/stdout <<< "$foutput"
 
-# non-windows
-if platform.system() != "Windows" :
-	srcml2src '-' '--output' '/dev/stdout' sxmlfile sfile)
-	srcml2src '-' '--output=/dev/stdout' sxmlfile sfile)
-	srcml2src '-' -o '/dev/stdout' sxmlfile sfile)
+check 3<<< "a;"
 
+srcml2src - --output=/dev/stdout <<< "$foutput"
+
+check 3<<< "a;"
+
+srcml2src - -o /dev/stdout <<< "$foutput"
+
+check 3<<< "a;"
