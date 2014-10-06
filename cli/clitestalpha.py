e949e17fbc69a1466df1fe9b5c31230752c8a6f6
Michael L. Collard, Ph.D <collard@uakron.edu>
2014-10-06 08:21:25 -0400

    Fix test case

--- cli/clitestalpha.py
+++ cli/clitestalpha.py
@@ -2314,7 +2314,7 @@ xpath_error = """srcml2src: Start tag expected, '<' not found in '-'
 xpath = xml_declaration + """
 <unit xmlns="http://www.sdml.info/srcML/src">
 
-<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" language="C++"/>
+<unit xmlns:cpp="http://www.sdml.info/srcML/cpp" language="C++"/>
 
 </unit>
 """
