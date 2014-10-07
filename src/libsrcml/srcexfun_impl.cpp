2a9b55603df67fb7db5aed4e86aa2c3f0d0398b5
Brian Bartman <bbartman@kent.edu>
2014-10-07 15:23:38 -0400

    Working on stubbing out the necessary extension functions.

--- src/libsrcml/srcexfun_impl.cpp
+++ src/libsrcml/srcexfun_impl.cpp
@@ -18,3 +18,78 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 #include <srcexfun_impl.hpp>
+#include <libxml/xpathInternals.h>
+#include <libxml/dict.h>
+
+/*
+VectorNodeSet forALLDescendentsAndSelfOptimized(xmlNodePtr input, char const* ns, char const* nodeName) {
+    xmlNodePtr currentNode = input;
+    xmlNodePtr temp;
+    VectorNodeSet ret;
+    xmlDocPtr docPtr = input->doc;
+    xmlChar const* expectedNodeNamePtr = nullptr;
+    xmlNsPtr expectedNS = nullptr;
+
+    xmlNsPtr currentNs = docPtr->oldNs;
+    while(currentNs) {
+        if(xmlStrEqual(currentNs->href, BAD_CAST ns) == 0){
+            expectedNS = currentNs;
+            break;
+        }
+        currentNs = currentNs->next;
+    }
+    typedef const xmlChar* ConstCharPtr;
+    expectedNodeNamePtr = xmlDictExists(docPtr->dict, ConstCharPtr(nodeName), -1);
+    if(expectedNodeNamePtr == nullptr) {
+        throw std::runtime_error("Unable to locate tag name within dictionary.");
+    }
+
+    START:
+        if(currentNode == nullptr) {
+            goto EXIT;
+        }
+    VISIT:
+        if(currentNode->type == XML_ELEMENT_NODE) {
+            if(currentNode->ns == expectedNS) {
+                if(currentNode->name == expectedNodeNamePtr) {
+                    ret.push_back(currentNode);
+                }
+            }
+        }
+    DESCENDING:
+        temp = currentNode->children;
+        if (temp != nullptr) {
+            currentNode = temp;
+            goto VISIT;
+        } 
+    STRAIF_SIBLINGS:
+        temp = currentNode->next;
+        if (temp != nullptr && currentNode != input) {
+            currentNode = temp;
+            goto VISIT;
+        }
+    ASCENDING:
+        if (currentNode->parent != input) {
+            currentNode = currentNode->parent;
+            goto STRAIF_SIBLINGS;
+        }
+    EXIT:
+        return ret;
+}
+*/
+void xpath_exfun_has_return(xmlXPathParserContextPtr ctxt, int nargs) {
+    
+}
+
+
+void xpath_exfun_is_nested(xmlXPathParserContextPtr ctxt, int nargs) {
+
+}
+
+void xpath_exfun_is_partial_template_specialization(xmlXPathParserContextPtr ctxt, int nargs) {
+
+}
+
+void xpath_exfun_is_local(xmlXPathParserContextPtr ctxt, int nargs) {
+    
+}
--- src/libsrcml/srcexfun_impl.hpp
+++ src/libsrcml/srcexfun_impl.hpp
@@ -20,6 +20,165 @@
 #ifndef INCLUDED_SRCEXFUN_IMPL_HPP
 #define INCLUDED_SRCEXFUN_IMPL_HPP
 #include <libxml/xpath.h>
+
+/**
+ *  XPath Signature: bool has_return()
+ *
+ *  Languages: C, C++, C#, Java
+ *
+ *  Checks all blocks children and all subsequent children for a return statement.
+ *  This is scope respecting, in that it doesn't search within closures functions
+ *  or local classes.
+ *
+ *  Works with srcml elements: 
+ *      block
+ *      function
+ *      while
+ *      if
+ *      elseif
+ *      else
+ *      try
+ *      catch
+ *      finally
+ *      do
+ *      for
+ *      foreach
+ *      switch
+ *      lambda
+ *      delegate
+ *      using_stmt
+ *      fixed
+ *      lock
+ *      synchronized
+ *      fixed
+ *      unsafe
+ *      constructor
+ *      destructor
+ *      static
+ *      checked
+ *      unchecked
+ *  
+ */
 void xpath_exfun_has_return(xmlXPathParserContextPtr ctxt, int nargs);
 
+/**
+ *  XPath Signature: bool is_nested()
+ *
+ *  Checks to see if a statement is inside another statement (for all statements),
+ *  for type declarations it checks if the type inside another type declaration,
+ *  for namespaces it checks if it is inside another namespace (or package for Java).
+ *  For constructors, functions and destructors it is nested if it's within a class
+ *  or namespace.
+ *
+ *  Languages: C, C++, C#, Java
+ *
+ *  Works on srcml elements:
+ *      decl_stmt
+ *      empty_stmt
+ *      if
+ *      switch
+ *      else
+ *      elseif
+ *      for
+ *      do
+ *      while
+ *      foreach
+ *      break
+ *      continue
+ *      return
+ *      default
+ *      block
+ *      label
+ *      goto
+ *      asm
+ *      synchronized
+ *      assert
+ *      fixed
+ *      using_stmt
+ *      unsafe
+ *      lock
+ *      try
+ *      catch
+ *      finally
+ *      typedef
+ *      throw
+ *      function
+ *      function_decl
+ *      constructor
+ *      constructor_decl
+ *      destructor
+ *      destructor_decl
+ *      property
+ *      event
+ *      union
+ *      union_decl
+ *      class
+ *      class_decl
+ *      struct
+ *      struct_decl
+ *      enum
+ *      namespace
+ *      package
+ *
+ */
+void xpath_exfun_is_nested(xmlXPathParserContextPtr ctxt, int nargs);
+
+/** 
+ *  XPath Signature: bool is_partial_template_specialization()
+ *
+ *  Checks to see if a class template is a partial specialization. This is done by 
+ *  checking for a template arguments and a template within the name of the class,
+ *  struct or union.
+ *
+ *  Language: C++
+ *
+ *  Works with srcML elements:
+ *     union
+ *     union_decl
+ *     class
+ *     class_decl
+ *     struct
+ *     struct_decl
+ */
+void xpath_exfun_is_partial_template_specialization(xmlXPathParserContextPtr ctxt, int nargs);
+
+/**
+ *  XPath Signature: bool is_local()
+ *
+ *  Checks variable declarations to see if they are local to a function. For, classes
+ *  and other type declarations occur within a function.
+ *
+ *  Languages: C, C++, C#, Java
+ *
+ *  Works with srcML elements:
+ *     class
+ *     class_decl
+ *     union
+ *     union_decl
+ *     struct
+ *     struct_decl
+ *     decl
+ *     decl_stmt
+ *     type
+ *     param
+ *     argument
+
+ */
+void xpath_exfun_is_local(xmlXPathParserContextPtr ctxt, int nargs);
+
+/**
+ *  XPath Signature: bool has_init()
+ *
+ *  Checks for an initialization within a declaration statement or the init
+ *  that's part of other statements such as a for, using_stmt, or synchronize.
+ *
+ *  Languages: C, C++, C#, Java
+ *
+ *  Works with srcML elements:
+ *      decl
+ *      decl_stmt
+ *      param
+ *      argument
+ */
+void xpath_exfun_has_init(xmlXPathParserContextPtr ctxt, int nargs);
 #endif
