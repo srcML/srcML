81259ee600663b20f92e84aa2a2b09b3710cef98
Drew Guarnera <dtg3@zips.uakron.edu>
2014-10-09 17:50:40 -0400

    Timeout support for curl

---
+++ src/srcml/timer.hpp
@@ -0,0 +1,59 @@
+/**
+ * @timer.hpp
+ *
+ * @copyright Copyright (C) 2014 SDML (www.srcML.org)
+ *
+ * This file is part of the srcml command-line client.
+ *
+ * The srcML Toolkit is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * The srcML Toolkit is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the srcml command-line client; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef TIMER_HPP
+#define TIMER_HPP
+
+#include <time.h>
+
+class Timer {
+public:
+	Timer() : time_limit(0) {}
+	Timer(double limit) : time_limit(limit) {}
+
+	inline void start() {
+		start_time = clock();
+	}
+
+	// time in seconds
+	inline double elapsed() {
+		return (double)((clock() - start_time)/CLOCKS_PER_SEC);
+	}
+
+	inline bool is_expired() {
+		return ((((clock() - start_time)/CLOCKS_PER_SEC) >= time_limit) && time_limit != 0);
+	}
+
+	//inline void reset() {
+	//	start_time = clock();
+	//}
+
+	inline void set_limit(double limit) {
+		time_limit = limit;
+	}
+
+private:
+    double time_limit; // in seconds
+    clock_t start_time; // start_timeing time
+};
+
+#endif
