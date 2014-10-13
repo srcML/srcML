68f0df04b06ea5b0b87eaa057d4088a99efe3ce1
Brian Bartman <bbartman@kent.edu>
2014-10-13 11:34:47 -0400

    Starting setting up initial support for is_class_template_partial_specialization

--- src/libsrcml/srcexfun.cpp
+++ src/libsrcml/srcexfun.cpp
@@ -298,6 +298,12 @@ void xpathsrcMLRegister(xmlXPathContextPtr context) {
     xmlXPathRegisterFuncNS(context, (const xmlChar *)"is_nested",
                            BAD_CAST SRCML_SRC_NS_URI,
                            xpath_exfun_is_nested);
+                           
+    xmlXPathRegisterFuncNS(context, (const xmlChar *)"is_class_template_partial_specialization",
+                           BAD_CAST SRCML_SRC_NS_URI,
+                           xpath_exfun_is_class_template_partial_specialization);
+
+
 
     // register all the xpath extension functions
     for (std::vector<struct xpath_ext_function>::size_type i = 0; i < MACROS.size(); ++i) {
