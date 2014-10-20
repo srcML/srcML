1c271132be0cfe630cbd1c3efd33a94d82848d7c
Brian Bartman <bbartman@kent.edu>
2014-10-20 09:09:12 -0400

    Updated unit test for is_protected

--- test/unit/libsrcml/xpath_test_data/is_protected/is_protected.java
+++ test/unit/libsrcml/xpath_test_data/is_protected/is_protected.java
@@ -1,14 +1,14 @@
-protected class Employee2
+protected class Employee2 // 1
 {
-    protected string name = "FirstName, LastName";
+    protected string name = "FirstName, LastName"; //2
 
-    protected string GetName()
+    protected string GetName() //3
     {
         return name;
     }
     
     
-    protected class FooCls{ } 
+    protected class FooCls{ }  //4
     
     class FooCls2 { } 
 }
