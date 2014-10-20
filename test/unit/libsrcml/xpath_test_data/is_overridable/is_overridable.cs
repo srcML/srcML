3015885a6068a249bc02cea449a4250f7a6b2451
Brian Bartman <bbartman@kent.edu>
2014-10-20 11:10:39 -0400

    Completed is_overriddable's re-integration.

--- test/unit/libsrcml/xpath_test_data/is_overridable/is_overridable.cs
+++ test/unit/libsrcml/xpath_test_data/is_overridable/is_overridable.cs
@@ -9,9 +9,6 @@ class foo {
     override void bar3(); // 3
     override void bar4(){} // 4
     
-    final void bar5();
-    final void bar6(){}
-    
     sealed void bar7();
     sealed void bar8() { } 
     
@@ -31,11 +28,6 @@ class foo {
         get /* 14 */;
     }
 
-    final string bar {
-        set; 
-        get;
-    }
-    
     abstract string bar15 { // 15
         set /* 16 */; 
         get /* 17 */;
@@ -55,7 +47,6 @@ class foo {
     override event EventType Bar18;
     abstract event EventType Bar19;
     virtual event EventType Bar20;
-    final event EventType Bar;
     sealed event EventType Bar;
     event EventType Bar;
 
@@ -70,131 +61,55 @@ interface IFoo {
     void bar32(); // 24
     event EventType Bar33; // 25
 }
-/*
-final class foo {
-    
-    void bar();
-    void bar(){}
-    
-    virtual void bar1(); // 1
-    virtual void bar2(){} // 2
-    
-    override void bar3(); // 3
-    override void bar4(){} // 4
-    
-    final void bar5();
-    final void bar6(){}
-    
-    sealed void bar7();
-    sealed void bar8() { } 
-    
-    override void bar5(); // 5
-    override void bar6() { } // 6
-    
-    abstract void bar7(); // 7
-    abstract void bar8() { } // 8
-    
-    virtual string bar13 { // 9
-        set; 
-        get;
-    }
-    
-    override string bar16 { // 11
-        set; 
-        get;
-    }
-
-    final string bar15 { // 14
-        set; 
-        get;
-    }
-    
-    abstract string bar18 { // 17
-        set; 
-        get;
-    }
-    
-    sealed string bar21 { // 21
-        set; 
-        get;
-    }
-    
-    string bar {
-        set; 
-        get;
-    }
-    
-    
-    override event EventType Bar24;
-    abstract event EventType Bar25;
-    virtual event EventType Bar26;
-    final event EventType Bar27;
-    sealed event EventType Bar28;
-    event EventType Bar;
-
-}
-*/
 
 sealed class foo {
     
     void bar();
     void bar(){}
     
-    virtual void bar1(); // 1
-    virtual void bar2(){} // 2
+    virtual void bar1();
+    virtual void bar2(){}
     
-    override void bar3(); // 3
-    override void bar4(){} // 4
-    
-    final void bar5();
-    final void bar6(){}
+    override void bar3();
+    override void bar4(){}
     
     sealed void bar7();
     sealed void bar8() { } 
     
-    override void bar5(); // 5
-    override void bar6() { } // 6
+    override void bar5();
+    override void bar6() { }
     
-    abstract void bar7(); // 7
-    abstract void bar8() { } // 8
+    abstract void bar7();
+    abstract void bar8() { }
     
-    virtual string bar13 { // 9
-        set /* 10 */; 
-        get /* 11 */;
+    virtual string bar13 {
+        set; 
+        get;
     }
     
-    override string bar16 { // 11
-        set /* 12 */; 
-        get /* 13 */;
-    }
-
-    final string bar15 { // 14
-        set /* 15 */; 
-        get /* 16 */;
+    override string bar16 {
+        set; 
+        get;
     }
     
-    abstract string bar18 { // 17
-        set /* 18 */; 
-        get /* 19 */;
+    abstract string bar18 {
+        set; 
+        get;
     }
     
-    sealed string bar21 { // 21
-        set /* 22 */; 
-        get /* 23 */;
+    sealed string bar21 {
+        set; 
+        get;
     }
     
     string bar {
         set; 
         get;
     }
-    
-    
     override event EventType Bar24;
     abstract event EventType Bar25;
     virtual event EventType Bar26;
-    final event EventType Bar27;
     sealed event EventType Bar28;
     event EventType Bar;
-
 }
 
