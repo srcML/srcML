1d4cf8f25ee1be2b943184c69019b02a21cfe3da
Michael L. Collard, Ph.D <collard@uakron.edu>
2014-10-23 17:08:38 -0400

    Fix status of non-source code files

--- src/srcml/src_input_libarchive.cpp
+++ src/srcml/src_input_libarchive.cpp
@@ -174,7 +174,7 @@ void src_input_libarchive(ParseQueue& queue,
         prequest->status = !language.empty() ? 0 : SRCML_STATUS_UNSET_LANGUAGE;
 
         // fill up the parse request buffer
-        if (!status) {
+        if (!status && !prequest->status) {
             // if we know the size, create the right sized data_buffer
             if (archive_entry_size_is_set(entry))
                 prequest->buffer.reserve(archive_entry_size(entry));
