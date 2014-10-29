dc76aefba64327826c3b0de215fbc68a0664f97d
Brian Bartman <bbartman@kent.edu>
2014-10-29 08:07:45 -0400

    Creating initial binding construct and configuration for it.

---
+++ bindings/python/config.py
@@ -0,0 +1,9 @@
+
+srcmlHPath = "${SRCML_H_HEADER_PATH}"
+generatorPath = "${GENERATOR_LIB_PATH}"
+pythonBindingsLocation = "${SRCML_PY_BINDINGS_LOCATION}"
+srcmlLibLocation = "${DYNAMIC_LIBSRCML_LOCATION}"
+
+
+
+
