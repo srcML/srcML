b625bef5263fbdf0205e09f3ee937b20a68a5e59
Brian Bartman <bbartman@kent.edu>
2014-10-29 09:13:09 -0400

    Created initial library for XML Generation, and language generation. Added a file to be used to generate srcml.h.xml.

---
+++ bindings/generator/__init__.py
@@ -0,0 +1,1 @@
+from MinimalLbsrcMLBindings import *
--- bindings/python/config.py
+++
@@ -1,9 +0,0 @@
-
-srcmlHPath = "${SRCML_H_HEADER_PATH}"
-generatorPath = "${GENERATOR_LIB_PATH}"
-pythonBindingsLocation = "${SRCML_PY_BINDINGS_LOCATION}"
-srcmlLibLocation = "${DYNAMIC_LIBSRCML_LOCATION}"
-
-
-
-
