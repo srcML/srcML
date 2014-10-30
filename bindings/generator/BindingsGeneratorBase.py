be7dc1b4293bfc553deccbf07ba5d9c370c5e2c6
Brian Bartman <bbartman@kent.edu>
2014-10-30 15:05:27 -0400

    Completed generation of python bindings. Moving on to example conversion.

--- bindings/generator/BindingsGeneratorBase.py
+++ bindings/generator/BindingsGeneratorBase.py
@@ -18,13 +18,22 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 import lxml.etree as ET
+import cStringIO, string
 
 XPathNamespaces = {
     "src": "http://www.sdml.info/srcML/src",
     "cpp": "http://www.sdml.info/srcML/cpp"
 }
 
-class FunctionInfo:
+class ParamTypeInfo:
+    def __init__(self):
+        self.type = ""
+        self.name = None
+
+    def __eq__(self, other):
+        return self.type == other.type
+
+class FunctionPtrInfo:
     def __init__(self):
         self.returnType = None
         self.parameterTypes = []
@@ -35,6 +44,63 @@ class FunctionInfo:
         self.parameterTypes = []
         self.functionName = ""
 
+    def  __hash__(self):
+        return self.returnType.__hash__() + sum([x.type.__hash__() for x in self.parameterTypes]) + self.functionName.__hash__()
+
+    def __eq__(self, other):
+        return (self.returnType == other.returnType
+            and len(self.parameterTypes) == len(other.parameterTypes)
+            and cmp(self.parameterTypes, other.parameterTypes) == 0
+            and self.functionName == other.functionName)
+
+    def __repr__(self):
+        return str(self)
+
+    def __str__(self):
+        return """{0.returnType} {0.functionName}({1})""".format(self, ",".join([x.type for x in self.parameterTypes]))
+
+PARAM_IN = "in"
+PARAM_OUT = "out"
+PARAM_IN_OUT = "in/out"
+
+class ParamInfo:
+    def __init__(self):
+        self.name = ""
+        self.actualType = ""
+        self.userType = ""
+        self.callingConvention = PARAM_IN
+
+class FunctionDeclInfo:
+    def __init__(self):
+        self.name = ""
+        self.returnType = None
+        self.params = []
+
+    def __str__(self):
+        output = ""
+        if self.returnType != None:
+            output += "{0.returnType}".format(self)
+
+        return output + """ {0.name}({1})""".format(
+            self,
+            ",".join([x.actualType for x in self.params])
+        )
+
+def extractNormalizedTypeName(element):
+    out = cStringIO.StringIO()
+    if element.text != None and not all(c in string.whitespace for c in element.text):
+        out.write(element.text)
+    def extract(currentElement):
+        if currentElement.text != None and currentElement.text != "__LIBSRCML_DECL" and not all(c in string.whitespace for c in currentElement.text):
+            out.write(currentElement.text + " ")
+        for chld in currentElement.iterchildren():
+            extract(chld)
+
+    for chld in element.iterchildren():
+        extract(chld)
+
+    return out.getvalue()
+
 class BindingGenerator(object):
     def __init__(self):
         self.srcmlAPI = None
@@ -42,15 +108,40 @@ class BindingGenerator(object):
         self.functionPointerTypes = []
         self.functionDecls = []
 
+    def getTypeInfo(self, xmlType):
+        if xmlType.tag == "{http://www.sdml.info/srcML/src}function_decl":
+            # Function Pointer Type handled first
+            currentFuncPtrInfo = FunctionPtrInfo()
+            xmlReturnType = xmlType.xpath("src:type", namespaces=XPathNamespaces)[0]
+            currentFuncPtrInfo.returnType = self.getTypeInfo(xmlReturnType)
+            # Getting function pointer name.
+            nameNodeList = xmlType.xpath("*[not(self::src:type or self::src:parameter_list)]", namespaces=XPathNamespaces)
+            for nameNode in nameNodeList:
+                currentFuncPtrInfo.functionName += extractNormalizedTypeName(nameNode)
+
+            # Handling Parameter List
+            parameterDecls = xmlType.xpath("src:parameter_list/src:param/src:decl", namespaces=XPathNamespaces)
+            for paramDecl in parameterDecls:
+                paramType = paramDecl.xpath("src:type", namespaces=XPathNamespaces)[0]
+                paramInfo = ParamTypeInfo()
+                paramInfo.type = self.getTypeInfo(paramType)
+                nameNode = paramDecl.xpath("src:name", namespaces=XPathNamespaces)
+                if nameNode != None and len(nameNode) > 0:
+                    paramInfo.name = extractNormalizedTypeName(nameNode[0])
+                currentFuncPtrInfo.parameterTypes.append(paramInfo)
+
+            return currentFuncPtrInfo
+        else:
+            # Handling typical type name resolution hand off.
+            return self.resolveType(
+                extractNormalizedTypeName(xmlType).strip()
+            )
 
     def run(self, srcMLedHeaderFilePath, bindingOutputFilePath):
         # Loading XML File.
         self.srcmlAPI = ET.parse(srcMLedHeaderFilePath)
 
         # Gathering things to use during code generation.
-        self.gatherStaticConstantMacros()
-        self.gatherFunctionPointerTypes()
-        self.gatherFunctionDecls()
 
         # Open the stream to insert bindings into
         bindingsFile = open(bindingOutputFilePath, "w")
@@ -59,16 +150,22 @@ class BindingGenerator(object):
         # into the documentation.
         bindingsFile.write(self.startBindings() + "\n")
 
+        self.gatherStaticConstantMacros()
         for constant in self.constantMacros:
             bindingsFile.write(self.defineConstantFromMacro(constant[0], constant[1]) + "\n")
+        bindingsFile.write("\n\n")
+        bindingsFile.write(self.postStaticConstants() + "\n")
 
-        currentFuncInfo = FunctionInfo()
+        self.gatherFunctionPointerTypes()
+        bindingsFile.write("\n\n")
         for funcPtr in self.functionPointerTypes:
-            bindingsFile.write(self.defineFuncPtrType(currentFuncInfo) + "\n")
+            bindingsFile.write(self.defineFuncPtrType(funcPtr) + "\n")
+        bindingsFile.write(self.postFunctionPointers() + "\n")
+        
 
-        currentFuncInfo = FunctionInfo()
+        self.gatherFunctionDecls()
         for funcDecl in self.functionDecls:
-            bindingsFile.write(self.buildFunctionBinding(currentFuncInfo) + "\n")
+            bindingsFile.write(self.buildFunctionBinding(funcDecl) + "\n")
             pass
 
         bindingsFile.write(self.endBindings() + "\n")
@@ -85,11 +182,55 @@ class BindingGenerator(object):
         ]
 
     def gatherFunctionPointerTypes(self):
-        typicalFunctionPointerList = self.srcmlAPI.xpath("//src:function_decl[contains(.,'(')]", namespaces=XPathNamespaces)
-        erroniousFunctionPointerList = self.srcmlAPI.xpath("//src:argument/src:expr/src:call/src:call", namespaces=XPathNamespaces)
-        print typicalFunctionPointerList
-        print erroniousFunctionPointerList
+        typicalFunctionPointerList = self.srcmlAPI.xpath("//src:function_decl[src:modifier[.='*']]", namespaces=XPathNamespaces)
+        # Extracting function pointers and building a list of function pointers
+        functionPointerSet = set()
+        for funcPtr in typicalFunctionPointerList:
+            currentFuncPtrInfo = self.getTypeInfo(funcPtr)
+            functionPointerSet.add(currentFuncPtrInfo)
+        self.functionPointerTypes = list(functionPointerSet)
 
 
     def gatherFunctionDecls(self):
-        pass
+        """Suck in all of the function declarations."""
+        functionDecls = self.srcmlAPI.xpath(
+            "//src:function_decl[not(src:modifier[.='*'])]",
+            namespaces=XPathNamespaces
+        )
+        for func in functionDecls:
+            funcDeclInfo = FunctionDeclInfo()
+
+            # Get return type info.
+            xmlReturnType = func.xpath("src:type", namespaces=XPathNamespaces)[0]
+            funcDeclInfo.returnType = self.getTypeInfo(xmlReturnType)
+
+            # Getting function name.
+            functionName = func.xpath("src:name", namespaces=XPathNamespaces)[0]
+            funcDeclInfo.name = extractNormalizedTypeName(functionName)
+
+            # Handling Parameter List
+            parameters = func.xpath("src:parameter_list/src:param", namespaces=XPathNamespaces)
+            for param in parameters:
+                paramInfo = ParamInfo()
+                if param[0].tag == "{http://www.sdml.info/srcML/src}decl":
+                    # Extracting typical parameter info.
+                    nameNode = param[0].xpath("src:name", namespaces=XPathNamespaces)
+                    if nameNode != None and len(nameNode) > 0:
+                        paramInfo.name = extractNormalizedTypeName(nameNode[0]).strip()
+                    paramInfo.actualType = extractNormalizedTypeName(param[0][0]).strip()
+                    paramInfo.userType = self.resolveType(paramInfo.actualType)
+                    if paramInfo.actualType == "int *" or paramInfo.actualType == "char * *":
+                        paramInfo.callingConvention = PARAM_IN_OUT
+                    
+                elif param[0].tag == "{http://www.sdml.info/srcML/src}function_decl":
+                    paramInfo.actualType = extractNormalizedTypeName(param[0]).strip()
+                    paramInfo.fpType = self.getTypeInfo(param[0])
+                    nameNode = param[0].xpath("src:name", namespaces=XPathNamespaces)
+                    if nameNode != None and len(nameNode) > 0:
+                        paramInfo.name = extractNormalizedTypeName(nameNode[0]).strip()
+                    paramInfo.userType = self.resolveType(paramInfo.fpType)
+                else:
+                    raise Exception ("Unhandled tag: {0}:".format(param[0].tag))
+                funcDeclInfo.params.append(paramInfo)
+            self.functionDecls.append(funcDeclInfo)
+
--- bindings/python/configTemplate.py
+++ bindings/python/configTemplate.py
@@ -23,7 +23,7 @@ pythonBindingsLocation = "${SRCML_PY_BINDINGS_LOCATION}"
 srcmlXMLHeaderFile = "${SRCML_H_XML_OUTPUT_PATH}"
 outputFilePath = "${PY_BINDINGS_OUTPUT_FILE}"
 
-import sys
+import sys, cStringIO
 
 sys.path.append(generatorPath)
 from generator import *
@@ -31,26 +31,181 @@ from generator import *
 class GenPythonCode(BindingGenerator):
     def __init__(self):
         super(GenPythonCode, self).__init__()
+        self.typeStrLookup = dict({
+            "srcml_unit *": "c_void_p",
+            "const srcml_unit *": "c_void_p",
+            "int" : "c_int",
+            "char * *":"c_void_p",
+            "void" : "None",
+            "unsigned long long" :"c_ulonglong",
+            "srcml_archive *": "c_void_p",
+            "const srcml_archive *": "c_void_p",
+            "FILE *": "IMPOSSIBLE",
+            "int *" : "c_void_p",
+            "char *" : "c_char_p",
+            "void *" : "c_void_p",
+            "const char *": "c_char_p",
+            "size_t" : "c_int"
+        })
+        self.fpLookup = dict()
 
     def startBindings(self):
         """ This is called when the bindings started."""
-        return ""
+        # Setting imports etc...
+        return """##
+# @file bindings.py
+#
+# @copyright Copyright (C) 2013-2014 SDML (www.srcML.org)
+# 
+# The srcML Toolkit is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# The srcML Toolkit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with the srcML Toolkit; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+from ctypes import cdll, c_int, c_char_p, pointer, c_ulonglong, CFUNCTYPE, c_void_p
+
+libsrcml = cdll.LoadLibrary("libsrcml.so")
+"""
+
 
     def endBindings(self):
         """ This is called when the bindings end."""
         return ""
 
+    def postStaticConstants(self):
+        return"""
+def check_result(rc):
+    if rc != SRCML_STATUS_OK:
+        raise Exception("srcml has encountered an error. Error Code: {0}".format(rc))
+
+def check_created_result(res):
+    if res == None:
+        raise Exception("srcml allocation failed")
+    return res
+"""
+
+
+    def postFunctionPointers(self):
+        return ""
+
     def defineConstantFromMacro(self, name, valueString):
         return "{0} = {1}".format(name, valueString)
 
-    def resolveType(self, type):
+    def resolveType(self, typeInfo):
+        if isinstance(typeInfo, str):
+            return self.typeStrLookup[typeInfo]
+        elif isinstance(typeInfo, FunctionPtrInfo):
+            return self.fpLookup[typeInfo]
+        else:
+            raise Exception("Received unresolvable type. {0}".format(typeInfo.__class__.__name__))
         return ""
 
     def defineFuncPtrType(self, funcPtrInfo):
-        return ""
+        # Removing pointer from name
+        outputName = funcPtrInfo.functionName[1::]
+        output = outputName + " = CFUNCTYPE("
+        output += funcPtrInfo.returnType
+        if len(funcPtrInfo.parameterTypes) > 1:
+            output += ", "
+            output += ", ".join([p.type for p in funcPtrInfo.parameterTypes])
+        output += ")"
+        self.fpLookup.update(dict({funcPtrInfo : outputName}))
+        return output
 
     def buildFunctionBinding(self, functionDeclInfo):
-        return ""
+        # Checking for an impossible parameter
+        if "IMPOSSIBLE" in [x.userType for x in  functionDeclInfo.params]:
+            # return "# Skipped {0} because of FILE incompatibilities".format(functionDeclInfo.name)
+            return ""
+
+        # Outputting all info for function bindings.
+        outputStrm = cStringIO.StringIO()
+        # Building initial from library.
+
+        # Writing return type and Argument Types
+        outputStrm.write("libsrcml.{0.name}.restype = {0.returnType}\n".format(functionDeclInfo))
+        outputStrm.write("libsrcml.{0.name}.argtypes = [{1}]\n".format(functionDeclInfo, ", ".join([p.userType for p in functionDeclInfo.params])))
+
+        intResultTemplate = """
+def {pyName}({parameters}):
+    check_result(libsrcml.{nativeName}({invocationArguments}))
+"""
+        voidReturnTemplate = """
+def {pyName}({parameters}):
+    libsrcml.{nativeName}({invocationArguments})
+"""
+        factoryReturnTemplate = """
+def {pyName}({parameters}):
+    return check_created_result(libsrcml.{nativeName}({invocationArguments}))
+"""
+        simpleNoCheckReturnTemplate = """
+def {pyName}({parameters}):
+    libsrcml.{nativeName}({invocationArguments})
+"""
+
+        selectedTemplate = ""
+        if functionDeclInfo.returnType == "None":
+            selectedTemplate = voidReturnTemplate
+        elif functionDeclInfo.returnType == "c_void_p" or functionDeclInfo.returnType == "c_char_p":
+            selectedTemplate = factoryReturnTemplate
+        elif functionDeclInfo.returnType == "c_int":
+            selectedTemplate = intResultTemplate
+        elif functionDeclInfo.returnType == "c_ulonglong":
+            selectedTemplate = simpleNoCheckReturnTemplate
+        else:
+            raise Exception("Unhandled return type: {0}".format(functionDeclInfo.returnType))
+
+        # Gathering simply variables for template.
+        pyName = functionDeclInfo.name[6:]
+        nativeName = functionDeclInfo.name
+
+        # Computing parameters and invocation arguments
+        parameters = []
+        invocationArguments = []
+        archiveNameCount = 0
+        unitNameCount = 0
+        for param in functionDeclInfo.params:
+            paramName = ""
+            if param.name == "":
+                if param.actualType.find("srcml_archive") != -1:
+                    paramName = "archive"
+                    if archiveNameCount > 0:
+                        paramName += "_{0}".format(archiveNameCount)
+                    archiveNameCount += 1
+                elif param.actualType.find("srcml_unit") != -1:
+                    paramName = "unit"
+                    if unitNameCount > 0:
+                        paramName += "_{0}".format(unitNameCount)
+                    unitNameCount += 1
+                else:
+                    raise Exception("Unnamed argument of unhandled type. Native parameter representation: {0}".format(functionDeclInfo.actualType))
+            else:
+                paramName = param.name
+
+            parameters.append(paramName)
+            # Checking to see if a variable is an output
+            # in/out variable using pointer to handle
+            # that case.
+            if param.callingConvention == PARAM_IN_OUT:
+                invocationArguments.append("pointer({0})".format(paramName))
+            else:
+                invocationArguments.append(paramName)
+
+
+        outputStrm.write(selectedTemplate.format(pyName = pyName, nativeName = nativeName, parameters=", ".join(parameters), invocationArguments = ", ".join(invocationArguments) ) )
+
+        ret = outputStrm.getvalue()
+        outputStrm.close()
+        return ret
 
 genCode = GenPythonCode()
 genCode.run(srcmlXMLHeaderFile, outputFilePath)
