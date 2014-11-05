069ad25103f57659334e9869227cafe4df00bcb5
bbartman <bbartman@kent.edu>
2014-11-05 13:18:46 -0500

    Updating build system so that it works correctly with nsis and can handle checking for the existance of and dl-ing vc redistriubtable dependencies. Currently NSIS can detect the existance of the correct binaries, the next step is to get the key and check the version and initiate the dl.

--- CMake/nsis.cmake
+++ CMake/nsis.cmake
@@ -21,9 +21,49 @@
 set(CPACK_NSIS_MUI_ICON ${CMAKE_SOURCE_DIR}/CMake/srcml_icon.ico)
 set(CPACK_NSIS_MUI_UNIICON ${CMAKE_SOURCE_DIR}/CMake/srcml_icon.ico)
 set(CPACK_NSIS_INSTALLED_ICON_NAME srcml_icon.ico)
-
+# D:\ProgrammingProjects\srcMLGitHubRepo\srcML
 # set add to path variable and ask for shortcut
 set(CPACK_NSIS_MODIFY_PATH ON)
+if(WIN32)
+    set(CPACK_NSIS_EXTRA_PREINSTALL_COMMANDS "!include LogicLib.nsh
+!macro IfKeyExists ROOT MAIN_KEY KEY
+  Push $R0
+  Push $R1
+  Push $R2
+ 
+  StrCpy $R1 \\\"0\\\"
+  StrCpy $R2 \\\"0\\\"
+ 
+  \\\${Do}
+    EnumRegKey $R0 \\\${ROOT} \\\"\\\${MAIN_KEY}\\\" \\\"$R1\\\"
+    \\\${If} $R0 == \\\"\\\${KEY}\\\"
+      StrCpy $R2 \\\"1\\\"
+      \\\${Break}
+    \\\${EndIf}
+    IntOp $R1 $R1 + 1
+  \\\${LoopWhile} $R0 != \\\"\\\"
+ 
+  ClearErrors
+ 
+  Exch 2
+  Pop $R0
+  Pop $R1
+  Exch $R2
+!macroend")
+    set(CPACK_NSIS_EXTRA_INSTALL_COMMANDS "
 
+!insertmacro IfKeyExists \\\"HKEY_LOCAL_MACHINE\\\" \\\"SOFTWARE\\\\Wow6432Node\\\\Microsoft\\\\DevDiv\\\\vc\\\\Servicing\\\\12.0\\\\RuntimeMinimum\\\" \\\"Version\\\"
+Pop \$R0
+!include LogicLib.nsh
+\\\${If} \$R0 == 1
+  MessageBox MB_OK \\\"No Key!\\\"
+\\\${Else}
+  MessageBox MB_OK \\\"Found KEY!\\\"
+\\\${EndIf}
+
+")
+# 
+    message(STATUS "${CPACK_NSIS_EXTRA_INSTALL_COMMANDS}")
+endif()
 # set contact in add/remove programs
 set(CPACK_NSIS_CONTACT "Software Developement Laboratories <bugs@srcML.org>")
