70231fe0dcf942bd0838c3425302d7924ea21411
Michael L. Collard, Ph.D <collard@uakron.edu>
2014-12-26 13:22:36 -0500

    Fix test cases

--- cli/testsuite/18_test_cli.sh
+++ cli/testsuite/18_test_cli.sh
@@ -16,7 +16,7 @@ rm -f sub/a.cpp
 
 
 #remove
-rm -f sub/a.cpp sub/b.cpp;
+rm -f sub/a.cpp sub/b.cpp
 
 # if platform.system() != "Windows" and sys.platform != 'cygwin' :
 #	 checkNoOutput([srcml2sr --to-dir + '=.' <<< "$nestedfile)
@@ -42,16 +42,18 @@ define nestedfileextra <<- 'STDIN'
 	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 	<unit xmlns="http://www.sdml.info/srcML/src" revision="0.8.0">
 
-	<unit xmlns:cpp="http://www.sdml.info/srcML/cpp" hash="1a2c5d67e6f651ae10b7673c53e8c502c97316d6" revision="0.8.0" language="C" dir="sub" filename="a.cpp">
+	<unit xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="C" dir="sub" filename="a.cpp" hash="1a2c5d67e6f651ae10b7673c53e8c502c97316d6">
 	<expr_stmt><expr><name>a</name></expr>;</expr_stmt>
 	</unit>
 
-	<unit xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="Java" dir="sub" filename="b.cpp">
+	<unit xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="Java" dir="sub" filename="b.cpp" hash="aecf18b52d520ab280119febd8ff6c803135ddfc">
 	<expr_stmt><expr><name>b</name></expr>;</expr_stmt>
 	</unit>
 
 	</unit>
 	STDIN
+
+message "$nestedfileextra"
 
 define file1 <<- 'STDOUT'
 	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
@@ -68,26 +70,26 @@ srcml2src --xml --unit "1" <<< "$nestedfileextra"
 
 check 3<<< "$file1"
 
-srcml2src --xml --unit "1" --get-filename <<< "$nestedfileextra"
+srcml2src --unit "1" --get-filename <<< "$nestedfileextra"
 
 check 3<<< "a.cpp"
 
-srcml2src --xml --unit "2" --get-filename <<< "$nestedfileextra"
+srcml2src --unit "2" --get-filename <<< "$nestedfileextra"
 
 check 3<<< "b.cpp"
 
-srcml2src --xml --unit "1" --get-directory <<< "$nestedfileextra"
+srcml2src --unit "1" --get-directory <<< "$nestedfileextra"
 
 check 3<<< "sub"
 
-srcml2src --xml --unit "2" --get-directory <<< "$nestedfileextra"
+srcml2src --unit "2" --get-directory <<< "$nestedfileextra"
 
 check 3<<< "sub"
 
-srcml2src --xml --unit "1" --get-language <<< "$nestedfileextra"
+srcml2src --unit "1" --get-language <<< "$nestedfileextra"
 
 check 3<<< "C"
 	
-srcml2src --xml --unit "2" --get-language <<< "$nestedfileextra"
+srcml2src --unit "2" --get-language <<< "$nestedfileextra"
 
 check 3<<< "Java"
