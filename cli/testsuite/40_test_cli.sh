57923768fb78e93f2efadf83b2af70f7355784eb
Heather Michaud <hmm34@zips.uakron.edu>
2014-12-31 01:56:12 -0500

    Fix CLI test 40

--- cli/testsuite/40_test_cli.sh
+++ cli/testsuite/40_test_cli.sh
@@ -8,23 +8,28 @@ source $(dirname "$0")/framework_test.sh
 ##
 # filename flag
 define output <<- 'STDOUT'
-	<?xml version="1.0" encoding="UTF-8" standalone="yes">
+	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="C++" filename="foo.cpp"/>
-	INPUT
+	STDOUT
 
-define output <<- 'STDOUT'
-	f<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="C++" filename="foo.cpp"/>
-	INPUT
+createfile sub/a.cpp ""
 
 src2srcml sub/a.cpp -f "foo.cpp"
+
+check 3<<< "$output"
+
 src2srcml sub/a.cpp --filename "foo.cpp"
+
+check 3<<< "$output"
+
 src2srcml sub/a.cpp --filename="foo.cpp"
-src2srcml -l C++ -f 'foo.cpp' -o sub/a.cpp.xml sfile1
-validate(open(sub/a.cpp.xml 'r').read() srcml)
-src2srcml -f 'foo.cpp' sub/a.cpp-o sub/a.cpp.xml
 
-validate(open(sub/a.cpp.xml 'r').read() fsrcml)
+check 3<<< "$output"
 
+src2srcml -l C++ -f 'foo.cpp' -o sub/a.cpp.xml sub/a.cpp
 
+check sub/a.cpp.xml 3<<< "$output"
 
+src2srcml -f 'foo.cpp' sub/a.cpp -o sub/a.cpp.xml
+
+check sub/a.cpp.xml 3<<< "$output"
