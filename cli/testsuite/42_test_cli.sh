8e8766a9e37ff49353ee8dba2d9c159259df6c9d
Heather Michaud <hmm34@zips.uakron.edu>
2014-12-31 02:13:12 -0500

    Fix CLI test 42

--- cli/testsuite/42_test_cli.sh
+++ cli/testsuite/42_test_cli.sh
@@ -7,27 +7,40 @@ source $(dirname "$0")/framework_test.sh
 
 ##
 # version flag
-define output <<- 'STDOUT'
-	<?xml version="1.0" encoding="UTF-8" standalone="yes">
+define srcml <<- 'STDOUT'
+	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="C++" version="1.0"/>
 	STDOUT
 
-define output <<- 'STDOUT'
+# file input
+define fsrcml <<- 'STDOUT'
 	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="C++" filename=sub/a.cpp version="1.0"/>
-	INPUT
+	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="C++" filename="sub/a.cpp" version="1.0"/>
+	STDOUT
+
+createfile sub/a.cpp ""
+
 src2srcml sub/a.cpp -s "1.0"
+
+check 3<<< "$fsrcml"
+
 src2srcml sub/a.cpp --src-version "1.0"
+
+check 3<<< "$fsrcml"
+
 src2srcml sub/a.cpp --src-version="1.0"
 
-src2srcml -l C++ -s '1.0' -o sub/a.cpp.xml sfile1
-validate(open(sub/a.cpp.xml 'r').read() srcml)
-src2srcml -s '1.0' sub/a.cpp-o sub/a.cpp.xml
+check 3<<< "$fsrcml"
 
-validate(open(sub/a.cpp.xml 'r').read() fsrcml)
+src2srcml -l C++ -s '1.0' -o sub/a.cpp.xml sub/a.cpp
 
+check sub/a.cpp.xml 3<<< "$fsrcml"
 
+src2srcml -s '1.0' sub/a.cpp -o sub/a.cpp.xml
 
-##
+check sub/a.cpp.xml 3<<< "$fsrcml"
 
-# xml encoding flag
+# standard input
+src2srcml -l C++ -s '1.0' -o sub/a.cpp.xml < sub/a.cpp
+
+check sub/a.cpp.xml 3<<< "$srcml"
