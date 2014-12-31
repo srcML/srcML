a973970776a1c79e74a61633bffc754b9010154b
Heather Michaud <hmm34@zips.uakron.edu>
2014-12-30 22:13:10 -0500

    Fix CLI test 32
    Also copy over test directory used, ../emptysrc/

--- cli/testsuite/32_test_cli.sh
+++ cli/testsuite/32_test_cli.sh
@@ -25,7 +25,7 @@ src2srcml emptysrc/empty.c.gz
 
 check 3<<- 'STDOUT'
 	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="C" filename="emptysrc/empty.c"/>
+	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="C" filename="emptysrc/empty.c.gz"/>
 	STDOUT
 
 
@@ -33,7 +33,7 @@ src2srcml emptysrc/empty.h.gz
 
 check 3<<- STDOUT
 	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="C" filename="emptysrc/empty.h"/>
+	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="C" filename="emptysrc/empty.h.gz"/>
 	STDOUT
 
 # check c++ file extensions
@@ -42,148 +42,148 @@ src2srcml emptysrc/empty.cpp
 
 check 3<<- 'STDOUT'
 	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="C" filename="emptysrc/empty.c"/>
+	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="C++" filename="emptysrc/empty.cpp"/>
 	STDOUT
 
 src2srcml emptysrc/empty.cc
 
 check 3<<- 'STDOUT'
 	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="C" filename="emptysrc/empty.c"/>
+	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="C++" filename="emptysrc/empty.cc"/>
 	STDOUT
 
 src2srcml emptysrc/empty.cxx
 
 check 3<<- 'STDOUT'
 	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="C" filename="emptysrc/empty.c"
+	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="C++" filename="emptysrc/empty.cxx"/>
 	STDOUT
 
 src2srcml emptysrc/empty.c++
 
 check 3<<- 'STDOUT'
 	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="C" filename="emptysrc/empty.c"
+	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="C++" filename="emptysrc/empty.c++"/>
 	STDOUT
 
 src2srcml emptysrc/empty.hpp
 
 check 3<<- 'STDOUT'
 	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="C" filename="emptysrc/empty.h"
+	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="C++" filename="emptysrc/empty.hpp"/>
 	STDOUT
 
 src2srcml emptysrc/empty.hh
 
 check 3<<- 'STDOUT'
 	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="C" filename="emptysrc/empty.h"/>
+	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="C++" filename="emptysrc/empty.hh"/>
 	STDOUT
 
 src2srcml emptysrc/empty.hxx
 
 check 3<<- 'STDOUT'
 	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="C" filename="emptysrc/empty.h"
+	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="C++" filename="emptysrc/empty.hxx"/>
 	STDOUT
 
 src2srcml emptysrc/empty.h++
 
 check 3<<- 'STDOUT'
 	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="C" filename="emptysrc/empty.h"
+	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="C++" filename="emptysrc/empty.h++"/>
 	STDOUT
 
 src2srcml emptysrc/empty.tcc
 
 check 3<<- 'STDOUT'
 	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="C" filename="emptysrc/empty.t"/>
+	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="C++" filename="emptysrc/empty.tcc"/>
 	STDOUT
 
 src2srcml emptysrc/empty.cpp.gz
 
 check 3<<- 'STDOUT'
 	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="C" filename="emptysrc/empty.c"/>
+	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="C++" filename="emptysrc/empty.cpp.gz"/>
 	STDOUT
 
 
 src2srcml emptysrc/empty.cc.gz
 
-check 3<<<- 'STDOUT'
+check 3<<- 'STDOUT'
 	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="C" filename="emptysrc/empty.c"/>
+	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="C++" filename="emptysrc/empty.cc.gz"/>
 	STDOUT
 
 src2srcml emptysrc/empty.cxx.gz
 
-check 3<<<- 'STDOUT'
+check 3<<- 'STDOUT'
 	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="C" filename="emptysrc/empty.c"/>
+	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="C++" filename="emptysrc/empty.cxx.gz"/>
 	STDOUT
 
 src2srcml emptysrc/empty.c++.gz
 
-check 3<<<- 'STDOUT'
+check 3<<- 'STDOUT'
 	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="C" filename="emptysrc/empty.c"/>
+	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="C++" filename="emptysrc/empty.c++.gz"/>
 	STDOUT
 
 src2srcml emptysrc/empty.hpp.gz
 
-check 3<<<- 'STDOUT'
+check 3<<- 'STDOUT'
 	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="C" filename="emptysrc/empty.h"/>
+	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="C++" filename="emptysrc/empty.hpp.gz"/>
 	STDOUT
 
 src2srcml emptysrc/empty.hh.gz
 
-check 3<<<- 'STDOUT'
+check 3<<- 'STDOUT'
 	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="C" filename="emptysrc/empty.h"/>
-	STDOUT
-
-define emptyh 3<<- 'STDOUT'
-	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="C" filename="emptysrc/empty.h"/>
+	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="C++" filename="emptysrc/empty.hh.gz"/>
 	STDOUT
 
 src2srcml emptysrc/empty.hxx.gz
 
-check 3<<<- "$emptyh"
+check 3<<- 'STDOUT'
+	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="C++" filename="emptysrc/empty.hxx.gz"/>
+	STDOUT
 
 src2srcml emptysrc/empty.h++.gz
 
-check 3<<<- "$emptyh"
+check 3<<- 'STDOUT'
+	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="C++" filename="emptysrc/empty.h++.gz"/>
+	STDOUT
 
 src2srcml emptysrc/empty.tcc.gz
 
-check 3<<<- 'STDOUT'
+check 3<<- 'STDOUT'
 	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="C" filename="emptysrc/empty.t"/>
+	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="C++" filename="emptysrc/empty.tcc.gz"/>
 	STDOUT
 
-define emptyjava 3<<- 'STDOUT'
-	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-	<unit xmlns="http://www.sdml.info/srcML/src" revision="0.8.0" language="C" filename="emptysrc/empty.java"/>
-	STDOUT
 
 # check java file extension
 src2srcml emptysrc/empty.java
 
-check 3<<< "$emptyjava"
+check 3<<- 'STDOUT'
+	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+	<unit xmlns="http://www.sdml.info/srcML/src" revision="0.8.0" language="Java" filename="emptysrc/empty.java"/>
+	STDOUT
 
 src2srcml emptysrc/empty.java.gz
 
-check 3<<< "$emptyjava"
-
-define emptyaj 3<<- 'STDOUT'
+check 3<<- 'STDOUT'
 	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-	<unit xmlns="http://www.sdml.info/srcML/src" revision="0.8.0" language="C" filename="emptysrc/empty.aj"/>
+	<unit xmlns="http://www.sdml.info/srcML/src" revision="0.8.0" language="Java" filename="emptysrc/empty.java.gz"/>
 	STDOUT
 
 src2srcml emptysrc/empty.aj.gz
 
-check 3<<< "$emptyaj"
-
+check 3<<- 'STDOUT'
+	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+	<unit xmlns="http://www.sdml.info/srcML/src" revision="0.8.0" language="AspectJ" filename="emptysrc/empty.aj.gz"/>
+	STDOUT
