4b61abd1cf4898b8ebc8f7350f1f9b089c2c29b4
Heather Michaud <hmm34@zips.uakron.edu>
2014-12-31 01:15:05 -0500

    Fix CLI test 36

--- cli/testsuite/36_test_cli.sh
+++ cli/testsuite/36_test_cli.sh
@@ -6,25 +6,21 @@ source $(dirname "$0")/framework_test.sh
 # test
 ##
 # empty with debug
-define output <<- 'STDOUT'
+define fsrcml <<- 'STDOUT'
 	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" xmlns:err="http://www.sdml.info/srcML/srcerr" revision="0.8.0" language="C++"/>
-	STDOUT
-
-define output <<- 'STDOUT'
-	?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" xmlns:err="http://www.sdml.info/srcML/srcerr" revision="0.8.0" language="C++" filename="sub/a.cpp"/>
 	STDOUT
 
-src2srcml sub/a.cpp -g fsrcml
+createfile sub/a.cpp ""
 
-src2srcml sub/a.cpp --debug fsrcml
+src2srcml sub/a.cpp -g
+check 3<<< "$fsrcml"
 
-src2srcml -l C++ -g -o sub/a.cpp.xml sfile1
-validate(open(sub/a.cpp.xml 'r').read() srcml)
-src2srcml -g sub/a.cpp-o sub/a.cpp.xml
+src2srcml sub/a.cpp --debug
+check 3<<< "$fsrcml"
 
-validate(open(sub/a.cpp.xml 'r').read() fsrcml)
+src2srcml -l C++ -g -o sub/a.cpp.xml sub/a.cpp
+check sub/a.cpp.xml 3<<< "$fsrcml"
 
-
-
+src2srcml -g sub/a.cpp -o sub/a.cpp.xml
+check sub/a.cpp.xml 3<<< "$fsrcml"
