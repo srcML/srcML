a53234b02d3e04b50d2415786816fb57c576e26c
Heather Michaud <hmm34@zips.uakron.edu>
2014-12-30 21:02:09 -0500

    Fix CLI test 18

--- cli/testsuite/18_test_cli.sh
+++ cli/testsuite/18_test_cli.sh
@@ -42,12 +42,10 @@ define nestedfileextra <<- 'STDIN'
 	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 	<unit xmlns="http://www.sdml.info/srcML/src" revision="0.8.0">
 
-	<unit xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="C" dir="sub" filename="a.cpp" hash="1a2c5d67e6f651ae10b7673c53e8c502c97316d6">
-	<expr_stmt><expr><name>a</name></expr>;</expr_stmt>
+	<unit xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="C" dir="sub" filename="a.cpp" hash="1a2c5d67e6f651ae10b7673c53e8c502c97316d6"><expr_stmt><expr><name>a</name></expr>;</expr_stmt>
 	</unit>
 
-	<unit xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="Java" dir="sub" filename="b.cpp" hash="aecf18b52d520ab280119febd8ff6c803135ddfc">
-	<expr_stmt><expr><name>b</name></expr>;</expr_stmt>
+	<unit xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="Java" dir="sub" filename="b.cpp" hash="aecf18b52d520ab280119febd8ff6c803135ddfc"><expr_stmt><expr><name>b</name></expr>;</expr_stmt>
 	</unit>
 
 	</unit>
@@ -57,8 +55,7 @@ message "$nestedfileextra"
 
 define file1 <<- 'STDOUT'
 	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="C" dir="sub" filename="a.cpp" hash="1a2c5d67e6f651ae10b7673c53e8c502c97316d6">
-	<expr_stmt><expr><name>a</name></expr>;</expr_stmt>
+	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="C" dir="sub" filename="a.cpp" hash="1a2c5d67e6f651ae10b7673c53e8c502c97316d6"><expr_stmt><expr><name>a</name></expr>;</expr_stmt>
 	</unit>
 	STDOUT
 
@@ -70,26 +67,26 @@ srcml2src --xml --unit "1" <<< "$nestedfileextra"
 
 check 3<<< "$file1"
 
-srcml2src --unit "1" --get-filename <<< "$nestedfileextra"
+srcml2src --xml --unit "1" --get-filename <<< "$nestedfileextra"
 
 check 3<<< "a.cpp"
 
-srcml2src --unit "2" --get-filename <<< "$nestedfileextra"
+srcml2src --xml --unit "2" --get-filename <<< "$nestedfileextra"
 
 check 3<<< "b.cpp"
 
-srcml2src --unit "1" --get-directory <<< "$nestedfileextra"
+srcml2src --xml --unit "1" --get-directory <<< "$nestedfileextra"
 
 check 3<<< "sub"
 
-srcml2src --unit "2" --get-directory <<< "$nestedfileextra"
+srcml2src --xml --unit "2" --get-directory <<< "$nestedfileextra"
 
 check 3<<< "sub"
 
-srcml2src --unit "1" --get-language <<< "$nestedfileextra"
+srcml2src --xml --unit "1" --get-language <<< "$nestedfileextra"
 
 check 3<<< "C"
 	
-srcml2src --unit "2" --get-language <<< "$nestedfileextra"
+srcml2src --xml --unit "2" --get-language <<< "$nestedfileextra"
 
 check 3<<< "Java"
