444561c9f5edea0301a900a4979ae5378f312d36
Heather Michaud <hmm34@zips.uakron.edu>
2014-12-30 21:05:30 -0500

    Fix CLI test 19

--- cli/testsuite/19_test_cli.sh
+++ cli/testsuite/19_test_cli.sh
@@ -9,16 +9,13 @@ define input <<- 'INPUT'
 	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:lc="http://www.sdml.info/srcML/linecol" revision="0.8.0" language="C++">
 
-	<unit xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="C" dir="sub" mytag="foo" filename="a.cpp">
-	<expr_stmt lc:line="1"><expr><name>a</name></expr>;</expr_stmt>
+	<unit xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="C" dir="sub" mytag="foo" filename="a.cpp"><expr_stmt lc:line="1"><expr><name>a</name></expr>;</expr_stmt>
 	</unit>
 
-	<unit xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="Java" dir="sub" mytag="foo" filename="b.cpp">
-	<expr_stmt lc:line="1"><expr><name>b</name></expr>;</expr_stmt>
+	<unit xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="Java" dir="sub" mytag="foo" filename="b.cpp"><expr_stmt lc:line="1"><expr><name>b</name></expr>;</expr_stmt>
 	</unit>
 
-	<unit xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="C++" dir="sub" mytag="foo" filename="c.cpp">
-	<expr_stmt lc:line="1"><expr><name>c</name></expr>;</expr_stmt>
+	<unit xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="C++" dir="sub" mytag="foo" filename="c.cpp"><expr_stmt lc:line="1"><expr><name>c</name></expr>;</expr_stmt>
 	</unit>
 
 	</unit>
@@ -26,22 +23,19 @@ define input <<- 'INPUT'
 
 define file1 <<- 'STDOUT'
 	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" xmlns:lc="http://www.sdml.info/srcML/linecol" revision="0.8.0" language="C" dir="sub" filename="a.cpp" mytag="foo">
-	<expr_stmt lc:line="1"><expr><name>a</name></expr>;</expr_stmt>
+	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" xmlns:lc="http://www.sdml.info/srcML/linecol" revision="0.8.0" language="C" dir="sub" filename="a.cpp" mytag="foo"><expr_stmt lc:line="1"><expr><name>a</name></expr>;</expr_stmt>
 	</unit>
 	STDOUT
 
 define file2 <<- 'STDOUT'
 	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" xmlns:lc="http://www.sdml.info/srcML/linecol" revision="0.8.0" language="Java" dir="sub" filename="b.cpp" mytag="foo">
-	<expr_stmt lc:line="1"><expr><name>b</name></expr>;</expr_stmt>
+	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" xmlns:lc="http://www.sdml.info/srcML/linecol" revision="0.8.0" language="Java" dir="sub" filename="b.cpp" mytag="foo"><expr_stmt lc:line="1"><expr><name>b</name></expr>;</expr_stmt>
 	</unit>
 	STDOUT
 
 define file3 <<- 'STDOUT'
 	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" xmlns:lc="http://www.sdml.info/srcML/linecol" revision="0.8.0" language="C++" dir="sub" filename="c.cpp" mytag="foo">
-	<expr_stmt lc:line="1"><expr><name>c</name></expr>;</expr_stmt>
+	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" xmlns:lc="http://www.sdml.info/srcML/linecol" revision="0.8.0" language="C++" dir="sub" filename="c.cpp" mytag="foo"><expr_stmt lc:line="1"><expr><name>c</name></expr>;</expr_stmt>
 	</unit>
 	STDOUT
 
@@ -69,26 +63,26 @@ srcml2src --xml --unit "3" <<< "$input"
 
 check 3<<< "$file3"
 
-srcml2src --unit "1" --get-filename <<< "$input"
+srcml2src --xml --unit "1" --get-filename <<< "$input"
 
 check 3<<< "a.cpp"
 
-srcml2src --unit "2" --get-filename <<< "$input"
+srcml2src --xml --unit "2" --get-filename <<< "$input"
 
 check 3<<< "b.cpp"
 
-srcml2src --unit "1" --get-directory <<< "$input"
+srcml2src --xml --unit "1" --get-directory <<< "$input"
 
 check 3<<< "sub"
 
-srcml2src --unit "2" --get-directory <<< "$input"
+srcml2src --xml --unit "2" --get-directory <<< "$input"
 
 check 3<<< "sub"
 
-srcml2src --unit "1" --get-language <<< "$input"
+srcml2src --xml --unit "1" --get-language <<< "$input"
 
 check 3<<< "C"
 
-srcml2src --unit "2" --get-language <<< "$input"
+srcml2src --xml --unit "2" --get-language <<< "$input"
 
 check 3<<< "Java"
