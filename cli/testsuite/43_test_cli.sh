2494b5c6d415c2f932c6c8378d02f4b75798f41a
Heather Michaud <hmm34@zips.uakron.edu>
2015-01-06 00:46:06 -0500

    Update CLI test 43 to bash

--- cli/testsuite/43_test_cli.sh
+++ cli/testsuite/43_test_cli.sh
@@ -3,25 +3,29 @@
 # test framework
 source $(dirname "$0")/framework_test.sh
 
-# test
-<?xml version="1.0" encoding="ISO-8859-1" standalone="yes"?>
-<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="C++">
+# test encoding flag
+define srcml <<- 'STDOUT'
+	<?xml version="1.0" encoding="ISO-8859-1" standalone="yes"?>
+	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="C++">
 	STDOUT
 
-f<?xml vrsion="1.0" encoding="ISO-8859-1" standalone="yes"?>
-<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="C++" filename="sub/a.cpp"/>
+define fsrcml <<- 'STDOUT'
+	<?xml vrsion="1.0" encoding="ISO-8859-1" standalone="yes"?>
+	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="C++" filename="sub/a.cpp"/>
 	STDOUT
+
+createfile sub/a.cpp ""
+
 src2srcml sub/a.cpp -x --encoding "ISO-8859-1"
-src2srcml sub/a.cpp -x --encoding "ISO-8859-1"
-src2srcml sub/a.cpp -x --encoding "ISO-8859-1"
-src2srcml -l C++ -x 'ISO-8859-1' -o sub/a.cpp.xml sfile1
-validate(open(sub/a.cpp.xml 'r').read() srcml)
-src2srcml -x 'ISO-8859-1' sub/a.cpp-o sub/a.cpp.xml
+check 3<<< "$fsrcml"
 
-validate(open(sub/a.cpp.xml 'r').read() fsrcml)
+src2srcml sub/a.cpp -x --encoding="ISO-8859-1"
+check 3<<< "$fsrcml"
 
+rmfile sub/a.cpp.xml
+src2srcml -l C++ -x 'ISO-8859-1' -o sub/a.cpp.xml < ""
+check sub/a.cpp.xml 3<<< "$srcml"
 
-
-##
-
-# text encoding flag
+rmfile sub/a.cpp.xml
+src2srcml -x 'ISO-8859-1' sub/a.cpp -o sub/a.cpp.xml
+check 3<<< "$fsrcml"
