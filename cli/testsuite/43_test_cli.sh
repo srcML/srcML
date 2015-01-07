38924a6683663bcc275a9a3291245ef5064eeeed
Michael L. Collard, Ph.D <collard@uakron.edu>
2015-01-07 15:43:25 -0500

    Fix test case 43

--- cli/testsuite/43_test_cli.sh
+++ cli/testsuite/43_test_cli.sh
@@ -16,16 +16,24 @@ define fsrcml <<- 'STDOUT'
 
 createfile sub/a.cpp ""
 
-src2srcml sub/a.cpp -x --encoding "ISO-8859-1"
+src2srcml sub/a.cpp -x "ISO-8859-1"
+
 check 3<<< "$fsrcml"
 
-src2srcml sub/a.cpp -x --encoding="ISO-8859-1"
+src2srcml sub/a.cpp --encoding="ISO-8859-1"
+
 check 3<<< "$fsrcml"
 
-rmfile sub/a.cpp.xml
+src2srcml sub/a.cpp --encoding "ISO-8859-1"
+
+check 3<<< "$fsrcml"
+
 src2srcml -l C++ -x 'ISO-8859-1' -o sub/a.cpp.xml < ""
+
 check sub/a.cpp.xml 3<<< "$srcml"
 
-rmfile sub/a.cpp.xml
 src2srcml -x 'ISO-8859-1' sub/a.cpp -o sub/a.cpp.xml
+
 check 3<<< "$fsrcml"
+
+rmfile sub/a.cpp.xml
