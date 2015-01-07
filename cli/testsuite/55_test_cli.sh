0a410799aff86309ee4645124390bccd8db965f0
Heather Michaud <hmm34@zips.uakron.edu>
2015-01-06 22:16:20 -0500

    Update python to bash

--- cli/testsuite/55_test_cli.sh
+++ cli/testsuite/55_test_cli.sh
@@ -3,17 +3,19 @@
 # test framework
 source $(dirname "$0")/framework_test.sh
 
-# test
+# test literal and operator combinations
 
 define srcml <<- 'STDOUT'
 	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" xmlns:lit="http://www.sdml.info/srcML/literal" xmlns:op="http://www.sdml.info/srcML/operator" revision="0.8.0" language="C++">
 	STDOUT
 
-define output <<- 'STDOUT'
+define sfile <<- 'STDOUT'
 	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" xmlns:lit="http://www.sdml.info/srcML/literal" xmlns:op="http://www.sdml.info/srcML/operator" revision="0.8.0" language="C++" filename="sub/a.cpp"/>
 	STDOUT
+
+createfile sub/a.cpp ""
 
 echo -n "" | src2srcml -l C++ --literal --operator
 
@@ -26,20 +28,23 @@ check 3<<< "$srcml"
 echo -n "" | src2srcml -l C++ --literal --operator -o sub/a.cpp.xml
 
 check sub/a.cpp.xml 3<<< "$sfile"
+rmfile sub/a.cpp.xml
 
 src2srcml --literal --operator sub/a.cpp -o sub/a.cpp.xml
 
 check sub/a.cpp.xml 3<<< "$sfile"
+rmfile sub/a.cpp.xml
 
-src2srcml -l C++ --operator --literal sfile srcml)
+src2srcml -l C++ --operator --literal < sub/a.cpp
+
+check 3<<< "$srcml"
 
 src2srcml --operator --literal sub/a.cpp
-if sys.platform != 'cygwin' :
-	src2srcml -l C++ --operator --literal -o sub/a.cpp.xml sfie
-	validate(open(sub/a.cpp.xml 'r').read() srcml)
-src2srcml --operator --literal sub/a.cpp -o sub/a.cpp.xml
 
-validate(open(sub/a.cpp.xml 'r').read() fsrcml)
+check 3<<< "$sfile"
 
+src2srcml -l C++ --operator --literal -o sub/a.cpp.xml < sub/a.cpp
+check 3<<< "$srcml"
+rmfile sub/a.cpp.xml
 
-
+rmfile sub/a.cpp
