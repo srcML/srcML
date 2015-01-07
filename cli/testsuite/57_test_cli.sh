096c28cacfa7ecfb3276de6fa44e4a4db8e60259
Heather Michaud <hmm34@zips.uakron.edu>
2015-01-06 22:21:26 -0500

    Update python to bash

--- cli/testsuite/57_test_cli.sh
+++ cli/testsuite/57_test_cli.sh
@@ -3,38 +3,46 @@
 # test framework
 source $(dirname "$0")/framework_test.sh
 
-# test
+# test operator and modifier combinations
 
 define output <<- 'STDOUT'
 	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" xmlns:op="http://www.sdml.info/srcML/operator" xmlns:type="http://www.sdml.info/srcML/modifier" revision="0.8.0" language="C++"/>
-	INPUT
+	STDOUT
 
-define output <<- 'STDOUT'
+define foutput <<- 'STDOUT'
 	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" xmlns:op="http://www.sdml.info/srcML/operator" xmlns:type="http://www.sdml.info/srcML/modifier" revision="0.8.0" language="C++" filename="sub/a.cpp"/>
-	INPUT
+	STDOUT
 
-src2srcml -l C++ --operator --modifier sfile srcml)
+createfile sub/a.cpp ""
+
+src2srcml -l C++ --operator --modifier < sub/a.cpp
+check 3<<< "$output"
+
 src2srcml --operator --modifier sub/a.cpp
-if sys.platform != 'cygwin' :
-	src2srcml -l C++ --operator --modifier -o sub/a.cpp.xml sfile
-	validate(open(sub/a.cpp.xml 'r').read() srcml)
+check 3<<< "$foutput"
+
+src2srcml -l C++ --operator --modifier -o sub/a.cpp.xml < sub/a.cpp
+check sub/a.cpp.xml 3<<< "$output"
+rmfile sub/a.cpp.xml
+
 src2srcml --operator --modifier sub/a.cpp -o sub/a.cpp.xml
+check sub/a.cpp.xml 3<<< "$foutput"
+rmfile sub/a.cpp.xml
 
-validate(open(sub/a.cpp.xml 'r').read() fsrcml)
-
-
-
-src2srcml -l C++ --modifier --operator sfile srcml)
+src2srcml -l C++ --modifier --operator < sub/a.cpp
+check 3<<< "$output"
 
 src2srcml --modifier --operator sub/a.cpp
-if sys.platform != 'cygwin' :
-	src2srcml -l C++ --modifier --operator -o sub/a.cpp.xml sfie
-	validate(open(sub/a.cpp.xml 'r').read() srcml)
+check 3<<< "$foutput"
+
+src2srcml -l C++ --modifier --operator -o sub/a.cpp.xml < sub/a.cpp
+check sub/a.cpp.xml 3<<< "$output"
+rmfile sub/a.cpp.xml
+
 src2srcml --modifier --operator sub/a.cpp -o sub/a.cpp.xml
+check sub/a.cpp.xml 3<<< "$foutput"
+rmfile sub/a.cpp.xml
 
-validate(open(sub/a.cpp.xml 'r').read() fsrcml)
-
-
-
+rmfile sub/a.cpp
