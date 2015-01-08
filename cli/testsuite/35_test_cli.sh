114cbf66811a3a9eea30be8f849f804af4829e57
Heather Michaud <hmm34@zips.uakron.edu>
2015-01-07 21:50:28 -0500

    Remove no op tests

--- cli/testsuite/35_test_cli.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# test framework
-source $(dirname "$0")/framework_test.sh
-
-exit 0
-
--- cli/testsuite/87_test_cli.sh
+++ cli/testsuite/87_test_cli.sh
@@ -4,6 +4,27 @@
 source $(dirname "$0")/framework_test.sh
 
 # test
+srcmlend = STDOUT
+</unit>
+	STDOUT
+
+if platform.system() != "Windows" :
+	dir = execute(['ls' 'dir'.split("\n")
+else :
+	dir = os.listdir('dir')
+
+srcml = srcmlstart
+
+for file in dir :
+	if file == 'file.aj' :
+	srcml += aj
+	if file == 'file.c' :
+	srcml += c
+	if file == 'file.cpp' :
+	srcml += cpp
+	if file == 'file.java' :
+	srcml += java
+
 srcml += srcmlend
 
 if platform.system() == "Windows" or sys.platform == 'cygwin' :
