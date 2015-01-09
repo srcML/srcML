6de21e3c13d95aa35131d0ffcc77a3cf4552eea9
Michael L. Collard, Ph.D <collard@uakron.edu>
2015-01-09 15:28:07 -0500

    Partial fix for cli test 90

--- cli/testsuite/90_utf8_bom_input.sh
+++ cli/testsuite/90_utf8_bom_input.sh
@@ -12,12 +12,22 @@ define output <<- 'STDOUT'
 	</unit>
 	STDOUT
 
+define foutput <<- 'STDOUT'
+	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="C" filename="bomfile.c"><expr_stmt><expr><name>a</name></expr>;</expr_stmt></unit>
+	STDOUT
+
 src2srcml --language 'C' <<< "a;"
 
 check 3<<< "$output"
 
-message "\xef\xbb\xbfa;"
+createfile bomfile.c "\xef\xbb\xbfa;"
 
-src2srcml --language 'C' <<< "\xef\xbb\xbfa;"
+src2srcml bomfile.c
+
+check 3<<< "$foutput"
+
+# NOTE: This is broken in libsrcml
+src2srcml --language 'C' < bomfile.c
 
 check 3<<< "$output"
