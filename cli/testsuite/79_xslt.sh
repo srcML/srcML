94bf7331c20f90b3c5f0f320b67f620e3bd4d2da
Heather Michaud <hmm34@zips.uakron.edu>
2015-01-14 18:35:33 -0500

    Fix cli xpath xslt test 79

--- cli/testsuite/79_xslt.sh
+++ cli/testsuite/79_xslt.sh
@@ -3,141 +3,115 @@
 # test framework
 source $(dirname "$0")/framework_test.sh
 
-# test
-define output <<- 'STDOUT'
+# test xslt empty input
+define srcml <<- 'STDOUT'
 	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="C++"/>
 	STDOUT
-INPUT
-xslt_error = STDOUTsrcml2src: Start tag expected '<' not found in '-'
+
+define copyxslt <<- 'STDOUT'
+	<xsl:stylesheet
+	xmlns="http://www.sdml.info/srcML/src"
+	xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+	xmlns:cpp="http://www.sdml.info/srcML/cpp"
+	version="1.0">
+
+	<!--
+	copy.xsl
+
+	Identity transformation.
+
+	Michael L. Collard
+	collard@uakron.edu
+	-->
+
+	<xsl:output method="xml" version="1.0" encoding="UTF-8" standalone="yes"/>
+
+	<xsl:template match="@*|node()"><xsl:copy><xsl:apply-templates select="@*|node()"/></xsl:copy></xsl:template>
+
+	</xsl:stylesheet>
 	STDOUT
 
-file = open(sub/a.cpp.xml 'w')
-file.write(srcml)
-file.close()
+define xpath_err1 <<- 'STDERR'
+	xslt : copy.xsl
+	STDERR
+
+define xpath_err2 <<- 'STDERR'
+	xslt : copy.xsl
+	xpathparam : NAME=VALUE
+	STDERR
+
+define xpath_err3 <<- 'STDERR'
+	xslt : copy.xsl
+	xpathparam : name="a"
+	STDERR
+
+createfile sub/a.cpp.xml "$srcml"
+createfile copy.xsl "$copyxslt"
+
+# simple copy
+srcml --xslt=copy.xsl sub/a.cpp.xml
+check 3<<< "$srcml" 4<<< "$xpath_err1"
+
+srcml --xslt=copy.xsl < sub/a.cpp.xml
+check 3<<< "$srcml" 4<<< "$xpath_err1"
+
+srcml --xslt=copy.xsl sub/a.cpp.xml -o sub/b.cpp.xml
+check sub/b.cpp.xml 3<<< "$srcml" 4<<< "$xpath_err1"
+
+srcml --xslt=copy.xsl -o sub/b.cpp.xml sub/a.cpp.xml
+check sub/b.cpp.xml 3<<< "$srcml" 4<<< "$xpath_err1"
+
+srcml --xslt=copy.xsl -o sub/b.cpp.xml < sub/a.cpp.xml
+check sub/b.cpp.xml 3<<< "$srcml" 4<<< "$xpath_err1"
+
+# xslt apply root copy
+srcml --apply-root --xslt=copy.xsl sub/a.cpp.xml
+check 3<<< "$srcml" 4<<< "$xpath_err1"
+
+srcml --apply-root --xslt=copy.xsl < sub/a.cpp.xml
+check 3<<< "$srcml" 4<<< "$xpath_err1"
+
+srcml --apply-root --xslt=copy.xsl sub/a.cpp.xml -o sub/b.cpp.xml
+check sub/b.cpp.xml 3<<< "$srcml" 4<<< "$xpath_err1"
+
+srcml --apply-root --xslt=copy.xsl -o sub/b.cpp.xml sub/a.cpp.xml
+check sub/b.cpp.xml 3<<< "$srcml" 4<<< "$xpath_err1"
+
+srcml --apply-root --xslt=copy.xsl -o sub/b.cpp.xml < sub/a.cpp.xml
+check sub/b.cpp.xml 3<<< "$srcml" 4<<< "$xpath_err1"
 
 
-# xslt
+# xslt copy xpathparam NAME=VALUE
+srcml --apply-root --xslt=copy.xsl --xpathparam 'NAME=VALUE' sub/a.cpp.xml
+check 3<<< "$srcml" 4<<< "$xpath_err2"
 
-if sys.platform != 'cygwin' :
-	checkError([srcml2src --xslt + '=copy.xsl' "" xslt_error)
-	srcml2src --xslt + '=copy.xsl
+srcml --apply-root --xslt=copy.xsl --xpathparam 'NAME=VALUE' < sub/a.cpp.xml
+check 3<<< "$srcml" 4<<< "$xpath_err2"
 
-	check 4<<< "2"
+srcml --apply-root --xslt=copy.xsl --xpathparam 'NAME=VALUE' sub/a.cpp.xml -o sub/b.cpp.xml
+check sub/b.cpp.xml 3<<< "$srcml" 4<<< "$xpath_err2"
 
-srcml2src --xslt + '=copy.xsl' srcml srcml)
-echo -n "" | srcml2src --xslt + '=copy.xsl' 'sub/a.cpp.xml
-if sys.platform != 'cygwin' :
-	srcml2src --xslt + '=copy.xsl' -o sub/b.cpp.xml srcml
-	validate(open(sub/b.cpp.xml 'r').read() srcml)
-srcml2src --xslt + '=copy.xsl' sub/a.cpp.xml -o sub/b.cpp.xml ""
-validate(open(sub/b.cpp.xml 'r').read() srcml)
+srcml --apply-root --xslt=copy.xsl --xpathparam 'NAME=VALUE' -o sub/b.cpp.xml sub/a.cpp.xml
+check sub/b.cpp.xml 3<<< "$srcml" 4<<< "$xpath_err2"
 
-
-srcml2src --xslt srcml
-
-check 4<<< "1"
-srcml2src --xslt + '=' srcml
-
-check 4<<< "1"
-
-# xslt apply root
-
-if sys.platform != 'cygwin' :
-	checkError([srcml2src --apply-root --xslt + '=copy.xsl' "" xslt_error)
-	srcml2src --apply-root --xslt + '=copy.xsl
-
-	check 4<<< "2"
-
-srcml2src --apply-root --xslt + '=copy.xsl' srcml srcml)
-echo -n "" | srcml2src --apply-root --xslt + '=copy.xsl' 'sub/a.cpp.xml
-if sys.platform != 'cygwin' :
-	srcml2src --apply-root --xslt + '=copy.xsl' -o sub/b.cpp.xml srcml
-	validate(open(sub/b.cpp.xml 'r').read() srcml)
-srcml2src --apply-root --xslt + '=copy.xsl' sub/a.cpp.xml -o sub/b.cpp.xml ""
-validate(open(sub/b.cpp.xml 'r').read() srcml)
+srcml --apply-root --xslt=copy.xsl --xpathparam 'NAME=VALUE' -o sub/b.cpp.xml < sub/a.cpp.xml
+check sub/b.cpp.xml 3<<< "$srcml" 4<<< "$xpath_err2"
 
 
-srcml2src --apply-root --xslt srcml
+# xslt copy xpathparam name="a"
+srcml --apply-root --xslt=copy.xsl --xpathparam 'name="a"' sub/a.cpp.xml
+check 3<<< "$srcml" 4<<< "$xpath_err3"
 
-check 4<<< "1"
-srcml2src --apply-root --xslt + '=' srcml
+srcml --apply-root --xslt=copy.xsl --xpathparam 'name="a"' < sub/a.cpp.xml
+check 3<<< "$srcml" 4<<< "$xpath_err3"
 
-check 4<<< "1"
+srcml --apply-root --xslt=copy.xsl --xpathparam 'name="a"' sub/a.cpp.xml -o sub/b.cpp.xml
+check sub/b.cpp.xml 3<<< "$srcml" 4<<< "$xpath_err3"
 
-# param
+srcml --apply-root --xslt=copy.xsl --xpathparam 'name="a"' -o sub/b.cpp.xml sub/a.cpp.xml
+check sub/b.cpp.xml 3<<< "$srcml" 4<<< "$xpath_err3"
 
-srcml2src --xslt + '=copy.xsl' --xpathparam 'NAME=VALUE' srcml srcml)
-echo -n "" | srcml2src --xslt + '=copy.xsl' --xpathparam 'NAME=VALUE' 'sub/a.cpp.xml
-if sys.platform != 'cygwin' :
-	srcml2src --xslt + '=copy.xsl' --xpathparam 'NAME=VALUE' -o sub/b.cpp.xml srcml
-	validate(open(sub/b.cpp.xml 'r').read() srcml)
-srcml2src --xslt + '=copy.xsl' --xpathparam 'NAME=VALUE' sub/a.cpp.xml -o sub/b.cpp.xml ""
-validate(open(sub/b.cpp.xml 'r').read() srcml)
-
-
-srcml2src --xslt + '=copy.xsl' --xpathparam srcml
-
-check 4<<< "1"
-srcml2src --xslt + '=copy.xsl' --xpathparam "NAME" srcml
-
-check 4<<< "1"
-
-xslt = STDOUTa
-	STDOUT
-
-srcml2src --xslt + '=param.xsl' --xpathparam 'name="a"' srcml xslt)
-srcml2src --xslt + '=param.xsl' --xpathparam 'name="a"' sub/a.cpp.xml xslt)
-if sys.platform != 'cygwin' :
-	srcml2src --xslt + '=param.xsl' --xpathparam 'name="a"' -o sub/b.cpp.xml srcml
-
-	validate(open(sub/b.cpp.xml 'r').read() xslt)
-srcml2src --xslt + '=param.xsl' --xpathparam 'name="a"' sub/a.cpp.xml -o sub/b.cpp.xml ""
-validate(open(sub/b.cpp.xml 'r').read() xslt)
-
-
-srcml2src --xslt + '=copy.xsl' --xpathparam srcml
-
-check 4<<< "1"
-srcml2src --xslt + '=copy.xsl' --xpathparam "name" srcml
-
-check 4<<< "1"
-
-# stringparam
-
-srcml2src --xslt + '=copy.xsl' --param 'NAME=VALUE' srcml srcml)
-echo -n "" | srcml2src --xslt + '=copy.xsl' --param 'NAME=VALUE' 'sub/a.cpp.xml
-if sys.platform != 'cygwin' :
-	srcml2src --xslt + '=copy.xsl' --param 'NAME=VALUE' -o sub/b.cpp.xml srcml
-	validate(open(sub/b.cpp.xml 'r').read() srcml)
-srcml2src --xslt + '=copy.xsl' --param 'NAME=VALUE' sub/a.cpp.xml -o sub/b.cpp.xml ""
-validate(open(sub/b.cpp.xml 'r').read() srcml)
-
-
-srcml2src --xslt + '=copy.xsl' --param srcml
-
-check 4<<< "1"
-srcml2src --xslt + '=copy.xsl' --param "NAME" srcml
-
-check 4<<< "1"
-
-xslt = STDOUTa
-	STDOUT
-
-srcml2src --xslt + '=param.xsl' --param 'name=a' srcml xslt)
-srcml2src --xslt + '=param.xsl' --param 'name=a' sub/a.cpp.xml xslt)
-if sys.platform != 'cygwin' :
-	srcml2src --xslt + '=param.xsl' --param 'name=a' -o sub/b.cpp.xml srcml
-
-	validate(open(sub/b.cpp.xml 'r').read() xslt)
-srcml2src --xslt + '=param.xsl' --param 'name=a' sub/a.cpp.xml -o sub/b.cpp.xml ""
-validate(open(sub/b.cpp.xml 'r').read() xslt)
-
-
-srcml2src --xslt + '=copy.xsl' --param srcml
-
-check 4<<< "1"
-srcml2src --xslt + '=copy.xsl' --param "name" srcml
-
-check 4<<< "1"
+srcml --apply-root --xslt=copy.xsl --xpathparam 'name="a"' -o sub/b.cpp.xml < sub/a.cpp.xml
+check sub/b.cpp.xml 3<<< "$srcml" 4<<< "$xpath_err3"
 
