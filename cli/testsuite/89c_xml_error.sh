e3f1c751a4777cc84abdc821bd39fd6cc5e29a21
Heather Michaud <hmm34@zips.uakron.edu>
2015-01-13 22:07:06 -0500

    Partial fix for invalid input XML 89c

--- cli/testsuite/89c_xml_error.sh
+++ cli/testsuite/89c_xml_error.sh
@@ -3,29 +3,73 @@
 # test framework
 source $(dirname "$0")/framework_test.sh
 
-# test
-
-# xml error test
-info_single = STDOUTxmlns="http://www.sdml.info/srcML/src"
-xmlns:cpp="http://www.sdml.info/srcML/cpp"
-encoding="UTF-8"
-language="C++"
+# xml error on invalid input test
+define info_single <<- 'STDOUT'
+	xmlns="http://www.sdml.info/srcML/src"
+	xmlns:cpp="http://www.sdml.info/srcML/cpp"
+	encoding="UTF-8"
+	language="C++"
 	STDOUT
 
-info_archive = STDOUTxmlns="http://www.sdml.info/srcML/src"
-encoding="UTF-8"
+define info_archive <<- 'STDOUT'
+	xmlns="http://www.sdml.info/srcML/src"
+	encoding="UTF-8"
 	STDOUT
+
+define illformed <<- 'INPUT'
+	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" language="C++">
+	<expr_stmt><expr><name>a</name></expr>;</expr_stmt>
+	</unit
+	INPUT
+
+define illformed_archive <<- 'INPUT'
+	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+	<unit xmlns="http://www.sdml.info/srcML/src">
+
+	<unit xmlns:cpp="http://www.sdml.info/srcML/cpp" language="C++">
+	<expr_stmt><expr><name>a</name></expr>;</expr_stmt>
+	</unit>
+
+	<unit xmlns:cpp="http://www.sdml.info/srcML/cpp" language="C++">
+	<expr_stmt><expr><name>a</name></expr>;</expr_stmt>
+	</unit>
+
+	<unit xmlns:cpp="http://www.sdml.info/srcML/cpp" language="C++">
+	<expr_stmt><expr><name>a</name></expr>;</expr_stmt>
+	</unit>
+	</unit>
+
+	</unit>
+	INPUT
+
+createfile xml_error/illformed.xml "$illformed"
+createfile xml_error/illformedarchive.xml "$illformed_archive"
+
 # ok
-srcml2src --info 'xml_error/illformed.xml' '' info_single)
-srcml2src --info 'xml_error/illformedarchive.xml' '' info_archive)
+srcml2src --info xml_error/illformed.xml
+check 3<<< "$info_single"
 
-xml_error = STDOUTsrcml2src: expected '>' in 'xml_error/illformed.xml'
+srcml2src --info xml_error/illformedarchive.xml
+check 3<<< "$info_archive"
+
+define output <<- 'STDOUT'
+	
+	a;
 	STDOUT
 
-xml_archive_error = STDOUTsrcml2src: Extra content at the end of the document in 'xml_error/illformedarchive.xml'
-	STDOUT
+define xml_error <<- 'STDERR'
+	Error Parsing: expected '>'
+	STDERR
+
+define xml_archive_error <<- 'STDERR'
+	srcml: Extra content at the end of the document in 'xml_error/illformedarchive.xml'
+	STDERR
 
 # bad
-checkError([srcml2src 'xml_error/illformed.xml' '' xml_error)
-checkError([srcml2src --units 'xml_error/illformedarchive.xml' '' xml_archive_error)
+srcml2src xml_error/illformed.xml
+check 3<<< "$output" 4<<< "$xml_error"
+
+srcml2src --units xml_error/illformedarchive.xml
+check 3<<< "$output" 4<<< "$xml_archive_error"
 
