f483bf4ff46a31d9d509c592d2dd1837b54d38f6
Heather Michaud <hmm34@zips.uakron.edu>
2015-01-13 20:07:18 -0500

    Partial fix for xpath attribute

--- cli/testsuite/92_xpath_attribute.sh
+++ cli/testsuite/92_xpath_attribute.sh
@@ -4,55 +4,41 @@
 source $(dirname "$0")/framework_test.sh
 
 # test
-define output <<- 'STDOUT'
-	srcml_nested = STDOUT<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+define srcml_nested <<- 'STDOUT'
+	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 	<unit xmlns="http://www.sdml.info/srcML/src" revision="0.8.0">
-	
-INPUT
-</unit>
 
-<unit xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="C++" filename="b.cpp"><expr_stmt><expr><name>b</name></expr>;</expr_stmt>
-</unit>
+	<unit xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="C++" filename="b.cpp"><expr_stmt><expr><name>b</name></expr>;</expr_stmt>
+	</unit>
 
-</unit>
+	</unit>
 	STDOUT
 
-xpath_attribute = "//src:unit/@filename"
-
-xpath_attribute_string = "string(//src:unit/@filename)"
-
-define output <<- 'STDOUT'
-	xpath_attribute_output = STDOUT<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+define attr_out <<- 'STDOUT'
+	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 	<unit xmlns="http://www.sdml.info/srcML/src" revision="0.8.0">
+
+	<unit xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="C++" filename="b.cpp" item="1" location="/src:filename[1]">b.cpp</unit>
+
+	</unit>
+	STDOUT
+
+createfile sub/a.cpp.xml "$srcml_nested"
+
+srcml2src --xpath "//src:unit/@filename" sub/a.cpp.xml
+check 3<<< "$attr_out"
+
+srcml2src --xpath "//src:unit/@filename" <<< "$srcml_nested"
+check 3<<< "$attr_out"
+
+# the following tests hang the test suite due to:
+# Error Parsing: Start tag expected, '<' not found
+exit 1
+
+srcml2src --xpath "string(//src:unit/@filename)" sub/a.cpp.xml
+check 3<<< "$attr_out"
+
+srcml2src --xpath "string(//src:unit/@filename)" <<< "$srcml_nested"
+check 3<<< "$attr_out"
+
 	
-INPUT
-
-</unit>
-	STDOUT
-
-xpath_attribute_string_output = STDOUTa.cpp
-	STDOUT
-
-define output <<- 'STDOUT'
-	xpath_attribute_nested_output = STDOUT<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-	<unit xmlns="http://www.sdml.info/srcML/src" revision="0.8.0">
-	
-INPUT
-
-<unit xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="C++" filename="b.cpp" item="1">b.cpp</unit>
-
-</unit>
-	STDOUT
-
-xpath_attribute_string_nested_output = STDOUTa.cpp
-b.cpp
-	STDOUT
-
-srcml2src --xpath xpath_attribute srcml xpath_attribute_output)
-srcml2src --xpath xpath_attribute_string srcml xpath_attribute_string_output)
-
-srcml2src --xpath xpath_attribute srcml_nested xpath_attribute_nested_output)
-srcml2src --xpath xpath_attribute_string srcml_nested xpath_attribute_string_nested_output)
-
-# comment
-
