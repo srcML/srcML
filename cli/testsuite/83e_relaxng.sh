af7cd3d590c9b0e1dee08f4dfc6350aae5ffda6d
Heather Michaud <hmm34@zips.uakron.edu>
2015-01-15 16:06:45 -0500

    Fix relaxng test

--- cli/testsuite/83e_relaxng.sh
+++ cli/testsuite/83e_relaxng.sh
@@ -4,65 +4,80 @@
 source $(dirname "$0")/framework_test.sh
 
 # test
-define output <<- 'STDOUT'
+define srcml <<- 'STDOUT'
 	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="C++"/>
 	STDOUT
-INPUT
-relaxng_error = STDOUTsrcml2src: Start tag expected '<' not found in '-'
+
+define schema <<- 'STDOUT'
+	<grammar xmlns="http://relaxng.org/ns/structure/1.0">
+	  <start><ref name="anyElement"/></start>
+	  <define name="anyElement">
+	    <element><anyName/>
+	      <zeroOrMore>
+	      <choice>
+	        <attribute><anyName/></attribute>
+		    <text/>
+			<ref name="anyElement"/>
+		  </choice>
+	      </zeroOrMore>
+	    </element>
+	  </define>
+	</grammar>
 	STDOUT
 
-file = open(sub/a.cpp.xml 'w')
-file.write(srcml)
-file.close()
+createfile sub/a.cpp.xml "$srcml"
+
+# apply schema
+createfile schema.rng "$schema"
+
+srcml2src --relaxng=schema.rng sub/a.cpp.xml
+check 3<<< "$srcml" 4<<< "relaxng : schema.rng"
+
+srcml2src --relaxng=schema.rng < sub/a.cpp.xml
+check 3<<< "$srcml" 4<<< "relaxng : schema.rng"
+
+srcml2src --relaxng=schema.rng sub/a.cpp.xml -o sub/b.cpp.xml
+check sub/b.cpp.xml 3<<< "$srcml" 4<<< "relaxng : schema.rng"
+
+srcml2src --relaxng=schema.rng -o sub/b.cpp.xml sub/a.cpp.xml
+check sub/b.cpp.xml 3<<< "$srcml" 4<<< "relaxng : schema.rng"
+
+srcml2src --relaxng=schema.rng -o sub/b.cpp.xml < sub/a.cpp.xml
+check sub/b.cpp.xml 3<<< "$srcml" 4<<< "relaxng : schema.rng"
+
+# apply root and apply schema
+srcml2src --apply-root --relaxng=schema.rng sub/a.cpp.xml
+check 3<<< "$srcml" 4<<< "relaxng : schema.rng"
+
+srcml2src --apply-root --relaxng=schema.rng < sub/a.cpp.xml
+check 3<<< "$srcml" 4<<< "relaxng : schema.rng"
+
+srcml2src --apply-root --relaxng=schema.rng sub/a.cpp.xml -o sub/b.cpp.xml
+check sub/b.cpp.xml 3<<< "$srcml" 4<<< "relaxng : schema.rng"
+
+srcml2src --apply-root --relaxng=schema.rng -o sub/b.cpp.xml sub/a.cpp.xml
+check sub/b.cpp.xml 3<<< "$srcml" 4<<< "relaxng : schema.rng"
+
+srcml2src --apply-root --relaxng=schema.rng -o sub/b.cpp.xml < sub/a.cpp.xml
+check sub/b.cpp.xml 3<<< "$srcml" 4<<< "relaxng : schema.rng"
 
 
-if sys.platform != 'cygwin' :
-	checkError([srcml2src --relaxng + '=schema.rng' "" relaxng_error)
-	srcml2src --relaxng + '=schema.rng
+## TODO really make sure this is ok to return ok when no schema supplied.
+#srcml2src --relaxng srcml
+#
+#check 4<<< "0"
+#if sys.platform != 'cygwin' :
+#	srcml2src --relaxng + '=' srcml
+#
+#	check 4<<< "0"
+#
 
-	check 4<<< "2"
-
-srcml2src --relaxng + '=schema.rng' srcml srcml)
-echo -n "" | srcml2src --relaxng + '=schema.rng' 'sub/a.cpp.xml
-if sys.platform != 'cygwin' :
-	srcml2src --relaxng + '=schema.rng' -o sub/b.cpp.xml srcml
-	validate(open(sub/b.cpp.xml 'r').read() srcml)
-srcml2src --relaxng + '=schema.rng' sub/a.cpp.xml -o sub/b.cpp.xml ""
-validate(open(sub/b.cpp.xml 'r').read() srcml)
-
-
-# TODO really make sure this is ok to return ok when no schema supplied.
-srcml2src --relaxng srcml
-
-check 4<<< "0"
-if sys.platform != 'cygwin' :
-	srcml2src --relaxng + '=' srcml
-
-	check 4<<< "0"
-
-# relaxng apply root
-
-if sys.platform != 'cygwin' :
-	checkError([srcml2src --apply-root --relaxng + '=schema.rng' "" relaxng_error)
-	srcml2src --apply-root --relaxng + '=schema.rng
-
-	check 4<<< "2"
-
-srcml2src --apply-root --relaxng + '=schema.rng' srcml srcml)
-echo -n "" | srcml2src --apply-root --relaxng + '=schema.rng' 'sub/a.cpp.xml
-if sys.platform != 'cygwin' :
-	srcml2src --apply-root --relaxng + '=schema.rng' -o sub/b.cpp.xml srcml
-	validate(open(sub/b.cpp.xml 'r').read() srcml)
-srcml2src --apply-root --relaxng + '=schema.rng' sub/a.cpp.xml -o sub/b.cpp.xml ""
-validate(open(sub/b.cpp.xml 'r').read() srcml)
-
-
-# TODO really make sure this is ok to return ok when no schema supplied.
-srcml2src --apply-root --relaxng srcml
-
-check 4<<< "0"
-if sys.platform != 'cygwin' :
-	srcml2src --apply-root --relaxng + '=' srcml
-
-	check 4<<< "0"
+## TODO really make sure this is ok to return ok when no schema supplied.
+#srcml2src --apply-root --relaxng srcml
+#
+#check 4<<< "0"
+#if sys.platform != 'cygwin' :
+#	srcml2src --apply-root --relaxng + '=' srcml
+#
+#	check 4<<< "0"
