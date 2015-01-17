37eb85324bff49af29d38b03ab53d01e7e64e666
Heather Michaud <hmm34@zips.uakron.edu>
2015-01-17 01:53:45 -0500

    Fix src version test 96

--- cli/testsuite/96_src-version.sh
+++ cli/testsuite/96_src-version.sh
@@ -6,12 +6,12 @@ source $(dirname "$0")/framework_test.sh
 # test src version
 define rev1 <<- 'STDIN'
 	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-	<unit xmlns="http://www.sdml.info/srcML/src" revision="0.8.0"/>
+	<unit xmlns="http://www.sdml.info/srcML/src" revision="0.8.0" version="0.8.0"/>
 	STDIN
 
 define rev2 <<- 'STDIN'
 	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-	<unit xmlns="http://www.sdml.info/srcML/src" revision="0.9.0"/>
+	<unit xmlns="http://www.sdml.info/srcML/src" revision="0.8.0" version="0.9.0"/>
 	STDIN
 
 createfile sub/a.cpp.xml "$rev1"
