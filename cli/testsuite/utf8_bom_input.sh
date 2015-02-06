aa8832d4ca4ab9b97164be10e8d770278876207c
Heather Michaud <hmm34@zips.uakron.edu>
2015-02-05 19:37:46 -0500

    Fix utf8 test

--- cli/testsuite/utf8_bom_input.sh
+++ cli/testsuite/utf8_bom_input.sh
@@ -14,14 +14,16 @@ define output <<- 'STDOUT'
 
 define foutput <<- 'STDOUT'
 	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="C" filename="bomfile.c"><expr_stmt><expr><name>a</name></expr>;</expr_stmt></unit>
+	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="C" filename="bomfile.c"><expr_stmt><expr><name>a</name></expr>;</expr_stmt>
+	</unit>
 	STDOUT
 
 src2srcml --language 'C' <<< "a;"
 
 check 3<<< "$output"
 
-createfile bomfile.c "\xef\xbb\xbfa;"
+createfile bomfile.c "\xef\xbb\xbfa;
+"
 
 src2srcml bomfile.c
 
