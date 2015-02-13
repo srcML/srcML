9c33b9ab8dece0bc5c61778d1948c189e284ddac
Heather Michaud <hmm34@zips.uakron.edu>
2015-02-12 23:15:16 -0500

    Only use curl or tar when file doesn't exist.
    Use the same log for all systems. Don't remove compressed file and
    uncompressed directory after the test to make consecutive tests faster.

--- cli/large_systems_test/large_systems.sh
+++ cli/large_systems_test/large_systems.sh
@@ -2,6 +2,17 @@
 
 mkdir logs
 
+# timestamp log files
+TIMESTAMP=$(date +"%Y%m%d-%H%M%S")
+
+TLOG="logs/timelog-${TIMESTAMP}.txt"
+DLOG="logs/difflog-${TIMESTAMP}.txt"
+SLOG="logs/sizelog-${TIMESTAMP}.txt"
+
+echo "Timed log for this session at: ${TLOG}"
+echo "Diff log for this session at: ${DLOG}"
+echo "Size log for this session at: ${SLOG}"
+
 # use CSV input to find big system download locations
 (cat large_systems_list.csv ; echo) | while IFS=',' read -r location
 do
@@ -9,45 +20,41 @@ do
     if [[ $location != "" ]] ; then
         echo "--------------------------------------"
 
-        # timestamp all files
-        TSZNAME="logs/$(date +"%Y%m%d-%H%M%S")-${location##*/}"
-        TSNAME="${TSZNAME/.tar.*z/}"   # without the .tar.gz or .tar.xz extensions
-        TLOG="${TSNAME}-timelog"
-        DLOG="${TSNAME}-difflog"
-        SLOG="${TSNAME}-sizelog"
+        ZNAME="logs/${location##*/}"
+        NAME="${ZNAME/.tar.*z/}"   # without the .tar.gz or .tar.xz extensions
 
-        echo "Timed log for this session at: ${TLOG}"
-        echo "Diff log for this session at: ${DLOG}"
-        echo "Size log for this session at: ${SLOG}"
+        # get the compressed file, if it doesn't already exist
+        if [ ! -f "${ZNAME}" ] ; then
+            echo "Grabbing ${location} into ${ZNAME}"
+            curl -o "${ZNAME}" "${location}"
+        fi
 
-        # get the compressed file
-        echo "Grabbing ${location} into ${TSZNAME}"
-        curl -o "${TSZNAME}" "${location}"
-
-        echo "Testing srcML on compressed format..."
+        echo "Testing srcML on compressed ${ZNAME} ..."
 
         # redirect stderr to stdout to store the time, but keep the srcml output
         # for other tests.
-        XZOUTPUT="${TSZNAME}-compressed-output.xml"
-        TIME="$(time ( srcml ${TSZNAME} -o ${XZOUTPUT} ) 2>&1 1>/dev/null )"
+        XZOUTPUT="${ZNAME}-compressed-output.xml"
+        TIME="$(time ( srcml ${ZNAME} -o ${XZOUTPUT} ) 2>&1 1>/dev/null )"
 
         # store timed output
-        echo "srcml ${TSZNAME} -o ${XZOUTPUT}" > "${TLOG}"
+        echo "srcml ${ZNAME} -o ${XZOUTPUT}" > "${TLOG}"
         echo "${TIME}" >> "${TLOG}"
 
-        # uncompress
-        mkdir "${TSNAME}"
-        echo "Uncompressing ${TSZNAME} to ${TSNAME}..."
-        tar -xf "${TSZNAME}" -C "${TSNAME}" --strip-components 1
+        # uncompress, if the unzipped version doesn't already exist
+        if [ ! -d "${NAME}" ] ; then
+            mkdir "${NAME}"
+            echo "Uncompressing ${ZNAME} to ${NAME} ..."
+            tar -xf "${ZNAME}" -C "${NAME}" --strip-components 1
+        fi
 
         # redirect stderr to stdout to store time
-        echo "Testing srcML on uncompressed format..."
-        UNZOUTPUT="${TSNAME}-output.xml" # output from
-        TIME="$(time ( srcml ${TSNAME} -o ${UNZOUTPUT} ) 2>&1 1>/dev/null )"
+        echo "Testing srcML on uncompressed ${NAME} ..."
+        UNZOUTPUT="${NAME}-output.xml" # output from
+        TIME="$(time ( srcml ${NAME} -o ${UNZOUTPUT} ) 2>&1 1>/dev/null )"
 
         # store timed output
         echo "" >> "${TLOG}"
-        echo "srcml ${TSNAME} -o ${UNZOUTPUT}" >> "${TLOG}"
+        echo "srcml ${NAME} -o ${UNZOUTPUT}" >> "${TLOG}"
         echo "${TIME}" >> "${TLOG}"
 
         # diff srcml output from the compressed and uncompressed input
@@ -57,9 +64,8 @@ do
         #echo "diff ${XZOUTPUT} ${UNZOUTPUT}" > "${DLOG}"
         #echo "${DIFF}" >> "${DLOG}"
 
-        # cleanup everything but the logs
-        rm "${TSZNAME}"    # compressed file that was downloaded using curl
-        rm -rf "${TSNAME}" # directory after compressed file was uncompressed
+        # keep the logs and the big system (test runs faster consecutively), but
+        # cleanup srcml outputs
         rm "${XZOUTPUT}"   # srcml output from running on compressed file
         rm "${UNZOUTPUT}"  # srcml output from running on directory
     fi
