edf79d0f433079b49d358f296ed7569eb436e60a
Heather Michaud <hmm34@zips.uakron.edu>
2015-02-21 23:27:33 -0500

    Add tests for input.cpio, input.cpio.gz, input.tar.bz2

---
+++ cli/testsuite/input_cpio.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# test framework
+source $(dirname "$0")/framework_test.sh
+
+# test on compressed files with .cpio extension
+define src <<- 'STDOUT'
+
+	a;
+	STDOUT
+
+define foutput <<- 'STDOUT'
+	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+	<unit xmlns="http://www.sdml.info/srcML/src" revision="REVISION" filename="file://archive/a.cpp.cpio">
+
+	<unit xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="REVISION" language="C++" filename="archive/a.cpp">
+	<expr_stmt><expr><name>a</name></expr>;</expr_stmt>
+	</unit>
+
+	</unit>
+	STDOUT
+
+define output <<- 'STDOUT'
+	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+	<unit xmlns="http://www.sdml.info/srcML/src" revision="REVISION">
+
+	<unit xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="REVISION" language="C++" filename="archive/a.cpp">
+	<expr_stmt><expr><name>a</name></expr>;</expr_stmt>
+	</unit>
+
+	</unit>
+	STDOUT
+
+
+createfile archive/a.cpp "$src"
+echo archive/a.cpp | tr " " "\n" | cpio -o > archive/a.cpp.cpio
+
+src2srcml archive/a.cpp.cpio -o archive/a.cpp.xml
+check archive/a.cpp.xml 3<<< "$foutput" 4<<< "1 block"
+
+srcml archive/a.cpp.cpio
+check 3<<< "$foutput"
+
+srcml -l C++ < archive/a.cpp.cpio
+check 3<<< "$output"
+
+srcml -l C++ -o archive/a.cpp.xml < archive/a.cpp.cpio
+check archive/a.cpp.xml 3<<< "$output"
---
+++ cli/testsuite/input_tar_bz2.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# test framework
+source $(dirname "$0")/framework_test.sh
+
+# test on compressed files with .tar.bz2 extension
+define src <<- 'STDOUT'
+
+	a;
+	STDOUT
+
+define foutput <<- 'STDOUT'
+	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+	<unit xmlns="http://www.sdml.info/srcML/src" revision="REVISION" filename="file://archive/a.cpp.tar.bz2">
+
+	<unit xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="REVISION" language="C++" filename="archive/a.cpp">
+	<expr_stmt><expr><name>a</name></expr>;</expr_stmt>
+	</unit>
+
+	</unit>
+	STDOUT
+
+define output <<- 'STDOUT'
+	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+	<unit xmlns="http://www.sdml.info/srcML/src" revision="REVISION">
+
+	<unit xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="REVISION" language="C++" filename="archive/a.cpp">
+	<expr_stmt><expr><name>a</name></expr>;</expr_stmt>
+	</unit>
+
+	</unit>
+	STDOUT
+
+
+createfile archive/a.cpp "$src"
+tar -cjf archive/a.cpp.tar.bz2 archive/a.cpp
+
+src2srcml archive/a.cpp.tar.bz2 -o archive/a.cpp.xml
+check archive/a.cpp.xml 3<<< "$foutput"
+
+srcml archive/a.cpp.tar.bz2
+check 3<<< "$foutput"
+
+srcml -l C++ < archive/a.cpp.tar.bz2
+check 3<<< "$output"
+
+srcml -l C++ -o archive/a.cpp.xml < archive/a.cpp.tar.bz2
+check archive/a.cpp.xml 3<<< "$output"
