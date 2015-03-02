fef7f2cd1e3fc9ce977e1c96614ce53f1e91c8d0
Michael L. Collard, Ph.D <collard@uakron.edu>
2015-03-02 10:41:11 -0500

    Merge from client

--- src/srcSAX/sax2_srcsax_handler.cpp
+++ src/srcSAX/sax2_srcsax_handler.cpp
@@ -456,10 +456,6 @@ void start_unit(void * ctx, const xmlChar * localname, const xmlChar * prefix, c
                int nb_namespaces, const xmlChar ** namespaces, int nb_attributes, int nb_defaulted,
                const xmlChar ** attributes) {
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 12e8e0e21dad14a1d250ef5f38b0b40fc57ec91d
 #ifdef SRCSAX_DEBUG
     fprintf(stderr, "HERE: %s %s %d '%s'\n", __FILE__, __FUNCTION__, __LINE__, (const char *)localname);
 #endif
@@ -507,12 +503,10 @@ void start_unit(void * ctx, const xmlChar * localname, const xmlChar * prefix, c
 
     }
 
-<<<<<<< HEAD
-=======
-    free_srcsax_namespaces(nb_namespaces, srcsax_namespaces);
-    free_srcsax_attributes(nb_attributes, srcsax_attributes);
 
->>>>>>> 12e8e0e21dad14a1d250ef5f38b0b40fc57ec91d
+    //free_srcsax_namespaces(nb_namespaces, srcsax_namespaces);
+//    free_srcsax_attributes(nb_attributes, srcsax_attributes);
+
 #ifdef SRCSAX_DEBUG
     fprintf(stderr, "HERE: %s %s %d '%s'\n", __FILE__, __FUNCTION__, __LINE__, (const char *)localname);
 #endif
