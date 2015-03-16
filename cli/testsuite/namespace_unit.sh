4a25f668be2cffc6c8dc13f7ac436f383242292c
Heather Michaud <hmm34@zips.uakron.edu>
2015-03-16 15:39:58 -0400

    Combine namespace tests

--- cli/testsuite/namespace_unit.sh
+++ cli/testsuite/namespace_unit.sh
@@ -18,39 +18,38 @@ createfile sub/a.cpp ""
 
 # separate
 src2srcml --xmlns="http://www.sdml.info/srcML/src" sub/a.cpp
-
 check 3<<< "$foutput"
 
-echo -n "" | src2srcml -l C++ --xmlns="http://www.sdml.info/srcML/src" -o sub/a.cpp.xml
+echo -n "" | src2srcml -l C++ --xmlns="http://www.sdml.info/srcML/src"
+check 3<<< "$output"
 
+echo -n "" | src2srcml -l C++ --xmlns:cpp="http://www.sdml.info/srcML/cpp"
+check 3<<< "$output"
+
+echo -n "" | src2srcml -l C++ --xmlns="http://www.sdml.info/srcML/src" -o sub/a.cpp.xml
 check sub/a.cpp.xml 3<<< "$output"
 
 src2srcml --xmlns="http://www.sdml.info/srcML/src" sub/a.cpp -o sub/a.cpp.xml
-
 check sub/a.cpp.xml 3<<< "$foutput"
 
 src2srcml --xmlns:cpp="http://www.sdml.info/srcML/cpp" sub/a.cpp
-
 check 3<<< "$foutput"
 
 echo -n "" | src2srcml -l C++ --xmlns:cpp="http://www.sdml.info/srcML/cpp" -o sub/a.cpp.xml
-
 check sub/a.cpp.xml 3<<< "$output"
 
 src2srcml --xmlns:cpp="http://www.sdml.info/srcML/cpp" sub/a.cpp -o sub/a.cpp.xml
-
 check sub/a.cpp.xml 3<<< "$foutput"
 
 # multiple
-echo -n "" | src2srcml --xmlns="http://www.sdml.info/srcML/src" --xmlns:cpp="http://www.sdml.info/srcML/cpp" sub/a.cpp
+echo -n "" | src2srcml -l C++ --xmlns="http://www.sdml.info/srcML/src" --xmlns:cpp="http://www.sdml.info/srcML/cpp"
+check 3<<< "$output"
 
+echo -n "" | src2srcml --xmlns="http://www.sdml.info/srcML/src" --xmlns:cpp="http://www.sdml.info/srcML/cpp" sub/a.cpp
 check 3<<< "$foutput"
 
 echo -n "" | src2srcml -l C++ --xmlns="http://www.sdml.info/srcML/src" --xmlns:cpp="http://www.sdml.info/srcML/cpp" -o sub/a.cpp.xml
-
 check sub/a.cpp.xml 3<<< "$output"
 
 src2srcml --xmlns="http://www.sdml.info/srcML/src" --xmlns:cpp="http://www.sdml.info/srcML/cpp" sub/a.cpp -o sub/a.cpp.xml
-
 check sub/a.cpp.xml 3<<< "$foutput"
-
