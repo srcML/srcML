54d2ff5e34a62af11b3a88aa36912b3d865936da
Michael L. Collard, Ph.D <collard@uakron.edu>
2015-03-24 22:32:15 -0400

    Fix spacing in test case

--- cli/testsuite/apply_root2.sh
+++ cli/testsuite/apply_root2.sh
@@ -46,13 +46,13 @@ define nested_exprstmt_out <<- 'STDOUT'
 	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 	<unit xmlns="http://www.sdml.info/srcML/src" revision="REVISION">
 
-	<unit xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="REVISION" language="C++" filename="a.cpp"item="1"><expr_stmt><expr><name>a</name></expr>;</expr_stmt></unit>
+	<unit xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="REVISION" language="C++" filename="a.cpp" item="1"><expr_stmt><expr><name>a</name></expr>;</expr_stmt></unit>
 
-	<unit xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="REVISION" language="C++" filename="b.cpp"item="1"><expr_stmt><expr><name>b</name></expr>;</expr_stmt></unit>
+	<unit xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="REVISION" language="C++" filename="b.cpp" item="1"><expr_stmt><expr><name>b</name></expr>;</expr_stmt></unit>
 
-	<unit xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="REVISION" language="C++" filename="a2.cpp"item="1"><expr_stmt><expr><name>a</name></expr>;</expr_stmt></unit>
+	<unit xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="REVISION" language="C++" filename="a2.cpp" item="1"><expr_stmt><expr><name>a</name></expr>;</expr_stmt></unit>
 
-	<unit xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="REVISION" language="C++" filename="b2.cpp"item="1"><expr_stmt><expr><name>b</name></expr>;</expr_stmt></unit>
+	<unit xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="REVISION" language="C++" filename="b2.cpp" item="1"><expr_stmt><expr><name>b</name></expr>;</expr_stmt></unit>
 
 	</unit>
 	STDOUT
