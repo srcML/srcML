2a36495c884e3eba4bf40b36adecbff8b5dcae52
Drew Guarnera <dtg3@zips.uakron.edu>
2015-04-06 16:18:22 -0400

    Change raw-text option to --text, -t

--- src/srcml/srcml_cli.cpp
+++ src/srcml/srcml_cli.cpp
@@ -313,7 +313,7 @@ srcml_request_t parseCLI(int argc, char* argv[]) {
             ("no-xml-declaration", prog_opts::bool_switch()->notifier(&option_markup<SRCML_OPTION_XML_DECL>), "do not output the XML declaration")
             ("output,o", prog_opts::value<std::string>()->notifier(&option_field<&srcml_request_t::output_filename>)->default_value("stdout://-"), "write result ouput to arg which is a FILE or URI")
             ("quiet,q", prog_opts::bool_switch()->notifier(&option_command<SRCML_COMMAND_QUIET>), "suppresses status messages")
-            ("src-encoding,t", prog_opts::value<std::string>()->notifier(&option_field<&srcml_request_t::src_encoding>), "set the input source encoding to arg")
+            ("src-encoding", prog_opts::value<std::string>()->notifier(&option_field<&srcml_request_t::src_encoding>), "set the input source encoding to arg")
             ("max-threads", prog_opts::value<int>()->notifier(&option_field<&srcml_request_t::max_threads>)->default_value(4), "set the maximum number of threads srcml can spawn")
             ("verbose,v", prog_opts::bool_switch()->notifier(&option_command<SRCML_COMMAND_VERBOSE>), "conversion and status information to stderr")
             ("version,V", prog_opts::bool_switch()->notifier(&option_command<SRCML_COMMAND_VERSION>), "display version number and exit")
@@ -321,7 +321,7 @@ srcml_request_t parseCLI(int argc, char* argv[]) {
             ("update", prog_opts::bool_switch()->notifier(&option_command<SRCML_COMMAND_UPDATE>), "output and update existing srcml")
             ("line-ending", prog_opts::value<std::string>()->notifier(&option_field<&srcml_request_t::line_ending>), "set the line endings for a desired environment \"Windows\" or \"Unix\"")
             ("external", prog_opts::value<std::string>()->notifier(&option_field<&srcml_request_t::external>), "run a user defined external script or application on srcml client output")
-            ("raw-text", prog_opts::value<std::vector<std::string> >()->notifier(&option_field<&srcml_request_t::raw_text>), "raw string text to be processed")
+            ("text,t", prog_opts::value<std::vector<std::string> >()->notifier(&option_field<&srcml_request_t::raw_text>), "raw string text to be processed")
             ;
 
         src2srcml_options.add_options()
