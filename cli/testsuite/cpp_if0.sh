831bbf1193f87f35bbb98b228ac8b52f7e96b240
Drew Guarnera <dtg3@zips.uakron.edu>
2015-04-12 13:05:23 -0400

    Fix ramaining tests for #1117

--- cli/testsuite/cpp_if0.sh
+++ cli/testsuite/cpp_if0.sh
@@ -2,12 +2,12 @@
 
 # test framework
 source $(dirname "$0")/framework_test.sh
-set +e
 # test
 ##
 # cpp markup if0
 
 define input <<- 'INPUT'
+
 	#if 0
 	break;
 	#endif
@@ -16,9 +16,10 @@ define input <<- 'INPUT'
 define output <<- 'STDOUT'
 	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="REVISION" language="C++">
-	<cpp:if>#<cpp:directive>if</cpp:directive> <expr>0</expr></cpp:if>
+	<cpp:if>#<cpp:directive>if</cpp:directive> <expr><literal type="number">0</literal></expr></cpp:if>
 	break;
 	<cpp:endif>#<cpp:directive>endif</cpp:directive></cpp:endif>
+
 	</unit>
 	STDOUT
 
@@ -27,28 +28,20 @@ src2srcml -l C++ <<< "$input"
 
 check 3<<< "$output"
 
-src2srcml -l C++ --cpp-text-if0 <<< "$input"
+#src2srcml -l C++ --cpp-text-if0 <<< "$input"
 
-check 3<<< "$output"
+#check 3<<< "$output"
 
 define output2 <<- 'STDOUT'
 	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="REVISION" language="C++">
-	<cpp:if>#<cpp:directive>if</cpp:directive> <expr>0</expr></cpp:if>
+	<cpp:if>#<cpp:directive>if</cpp:directive> <expr><literal type="number">0</literal></expr></cpp:if>
 	<break>break;</break>
 	<cpp:endif>#<cpp:directive>endif</cpp:directive></cpp:endif>
+	
 	</unit>
 	STDOUT
 
 src2srcml -l C++ --cpp-markup-if0 <<< "$input"
 
 check 3<<< "$output2"
-
-src2srcml -l C++ --cpp-markup-if0 --cpp-text-if0 foobar
-
-check_exit 15
-
-src2srcml -l C++ --cpp-text-if0 --cpp-markup-if0 foobar
-
-check_exit 15
-
--- cli/testsuite/cpp_if02.sh
+++ cli/testsuite/cpp_if02.sh
@@ -38,16 +38,6 @@ check sub/a.cpp.xml 3<<< "$output"
 src2srcml sub/a.cpp -o sub/a.cpp.xml
 check sub/a.cpp.xml 3<<< "$foutput"
 
-src2srcml --cpp-text-if0 sub/a.cpp
-check 3<<< "$foutput"
-
-src2srcml -l C++ --cpp-text-if0 -o sub/a.cpp.xml < sub/a.cpp
-check sub/a.cpp.xml 3<<< "$output"
-
-src2srcml --cpp-text-if0 sub/a.cpp -o sub/a.cpp.xml
-check sub/a.cpp.xml 3<<< "$foutput"
-
-
 define output <<- 'STDOUT'
 	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="REVISION" language="C++"><cpp:if>#<cpp:directive>if</cpp:directive> <expr><literal type="number">0</literal></expr></cpp:if>
