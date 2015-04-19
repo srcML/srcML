b8cd4d7613303b06d87cd4b3236d0ba8afe616ad
Michael L. Collard, Ph.D <collard@uakron.edu>
2015-04-19 08:10:15 -0400

    Fix for non-archive input for empty xpath results

--- src/srcml/transform_srcml.cpp
+++ src/srcml/transform_srcml.cpp
@@ -151,13 +151,17 @@ void transform_srcml(const srcml_request_t& srcml_request,
         }
 
         // copy input xml namespaces
-        for (int i = 0; i < (int)srcml_archive_get_namespace_size(in_arch); ++i) {
+        // TODO: This assumes namespaces on first input. Need to open all, figure out
+        // output namespaces, then process
+        if (srcml_archive_get_options(in_arch) & SRCML_OPTION_ARCHIVE) {
+            for (int i = 0; i < (int)srcml_archive_get_namespace_size(in_arch); ++i) {
 
-            // do not register the srcML namespace, unless the prefix is different
-            if (std::string(srcml_archive_get_namespace_uri(in_arch, i)) == "http://www.sdml.info/srcML/src")
-                continue;
+                // do not register the srcML namespace, unless the prefix is different
+                if (std::string(srcml_archive_get_namespace_uri(in_arch, i)) == "http://www.sdml.info/srcML/src")
+                    continue;
 
-            srcml_archive_register_namespace(out_arch, srcml_archive_get_namespace_uri(in_arch, i), srcml_archive_get_namespace_prefix(in_arch, i));
+                srcml_archive_register_namespace(out_arch, srcml_archive_get_namespace_uri(in_arch, i), srcml_archive_get_namespace_prefix(in_arch, i));
+            }
         }
 
 		// iterate through all transformations added during cli parsing
