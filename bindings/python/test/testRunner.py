85f286017ed8ab27c799ea942738d38193548d7f
Brian Bartman <bbartman@kent.edu>
2015-04-23 09:49:05 -0400

    Removed some extra code from file.

--- bindings/python/test/testRunner.py
+++ bindings/python/test/testRunner.py
@@ -19,22 +19,7 @@
 
 import unittest
 import sys, pdb
-import signal, os
 
-# def handler(signum, frame):
-#     # print 'Signal handler called with signal', signum
-#     raise Exception("FUCK!")
-
-# Set the signal handler and a 5-second alarm
-# signal.signal(signal.SIGTERM, handler)
-# signal.signal(signal.SIGABRT, handler)
-# signal.signal(signal.SIGKILL, handler)
-# signal.alarm(5)
-
-# # This open() may hang indefinitely
-# fd = os.open('/dev/ttyS0', os.O_RDWR)
-
-# signal.alarm(0)          # Disable the alarm
 sys.path.append("../")
 from srcml_test import *
 
