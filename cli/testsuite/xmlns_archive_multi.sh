623bdf9466c934143f34f2f5807d412c3bef86e4
Heather Michaud <hmm34@zips.uakron.edu>
2015-05-11 15:40:53 -0400

    Add XMLNS with prefix tests

--- cli/testsuite/xmlns_archive_multi.sh
+++ cli/testsuite/xmlns_archive_multi.sh
@@ -3,14 +3,14 @@
 # test framework
 source $(dirname "$0")/framework_test.sh
 
-# test
+# default
 define foosrcml <<- 'STDOUT'
 	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-	<unit xmlns="http://www.sdml.info/srcML/src" xmlns="foo.com" revision="0.8.0">
+	<unit xmlns="http://www.sdml.info/srcML/src" xmlns="foo.com" revision="REVISION">
 
-	<unit xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="C++" filename="sub/a.cpp" hash="a301d91aac4aa1ab4e69cbc59cde4b4fff32f2b8"><expr_stmt><expr><name>a</name></expr>;</expr_stmt></unit>
+	<unit xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="REVISION" language="C++" filename="sub/a.cpp" hash="a301d91aac4aa1ab4e69cbc59cde4b4fff32f2b8"><expr_stmt><expr><name>a</name></expr>;</expr_stmt></unit>
 
-	<unit xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="C++" filename="sub/b.cpp" hash="9a1e1d3d0e27715d29bcfbf72b891b3ece985b36"><expr_stmt><expr><name>b</name></expr>;</expr_stmt></unit>
+	<unit xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="REVISION" language="C++" filename="sub/b.cpp" hash="9a1e1d3d0e27715d29bcfbf72b891b3ece985b36"><expr_stmt><expr><name>b</name></expr>;</expr_stmt></unit>
 
 	</unit>
 	STDOUT
@@ -80,3 +80,79 @@ check sub/a.xml 3<<< "$foosrcml"
 
 srcml -o sub/a.xml --in-order sub/a.cpp sub/b.cpp --xmlns=foo.com
 check sub/a.xml 3<<< "$foosrcml"
+
+# with prefix
+define fooprefixsrcml <<- 'STDOUT'
+	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:foo="foo.com" revision="REVISION">
+
+	<unit xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="REVISION" language="C++" filename="sub/a.cpp" hash="a301d91aac4aa1ab4e69cbc59cde4b4fff32f2b8"><expr_stmt><expr><name>a</name></expr>;</expr_stmt></unit>
+
+	<unit xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="REVISION" language="C++" filename="sub/b.cpp" hash="9a1e1d3d0e27715d29bcfbf72b891b3ece985b36"><expr_stmt><expr><name>b</name></expr>;</expr_stmt></unit>
+
+	</unit>
+	STDOUT
+
+srcml --xmlns:foo=foo.com sub/a.cpp sub/b.cpp --in-order
+check 3<<< "$fooprefixsrcml"
+
+srcml sub/a.cpp sub/b.cpp --xmlns:foo=foo.com --in-order
+check 3<<< "$fooprefixsrcml"
+
+srcml sub/a.cpp sub/b.cpp --in-order --xmlns:foo=foo.com
+check 3<<< "$fooprefixsrcml"
+
+srcml --in-order --xmlns:foo=foo.com sub/a.cpp sub/b.cpp
+check 3<<< "$fooprefixsrcml"
+
+srcml --in-order sub/a.cpp sub/b.cpp --xmlns:foo=foo.com
+check 3<<< "$fooprefixsrcml"
+
+srcml --xmlns:foo=foo.com sub/a.cpp sub/b.cpp --in-order -o sub/a.xml
+check sub/a.xml 3<<< "$fooprefixsrcml"
+
+srcml --xmlns:foo=foo.com sub/a.cpp sub/b.cpp -o sub/a.xml --in-order
+check sub/a.xml 3<<< "$fooprefixsrcml"
+
+srcml --xmlns:foo=foo.com -o sub/a.xml sub/a.cpp sub/b.cpp --in-order
+check sub/a.xml 3<<< "$fooprefixsrcml"
+
+srcml -o sub/a.xml --xmlns:foo=foo.com sub/a.cpp sub/b.cpp --in-order
+check sub/a.xml 3<<< "$fooprefixsrcml"
+
+srcml sub/a.cpp sub/b.cpp --xmlns:foo=foo.com --in-order -o sub/a.xml
+check sub/a.xml 3<<< "$fooprefixsrcml"
+
+srcml sub/a.cpp sub/b.cpp --in-order -o sub/a.xml --xmlns:foo=foo.com
+check sub/a.xml 3<<< "$fooprefixsrcml"
+
+srcml sub/a.cpp sub/b.cpp -o sub/a.xml --in-order --xmlns:foo=foo.com
+check sub/a.xml 3<<< "$fooprefixsrcml"
+
+srcml -o sub/a.xml sub/a.cpp sub/b.cpp --in-order --xmlns:foo=foo.com
+check sub/a.xml 3<<< "$fooprefixsrcml"
+
+srcml --in-order --xmlns:foo=foo.com sub/a.cpp sub/b.cpp -o sub/a.xml
+check sub/a.xml 3<<< "$fooprefixsrcml"
+
+srcml --in-order --xmlns:foo=foo.com -o sub/a.xml sub/a.cpp sub/b.cpp
+check sub/a.xml 3<<< "$fooprefixsrcml"
+
+srcml --in-order -o sub/a.xml --xmlns:foo=foo.com sub/a.cpp sub/b.cpp
+check sub/a.xml 3<<< "$fooprefixsrcml"
+
+srcml -o sub/a.xml --in-order --xmlns:foo=foo.com sub/a.cpp sub/b.cpp
+check sub/a.xml 3<<< "$fooprefixsrcml"
+
+srcml --in-order sub/a.cpp sub/b.cpp --xmlns:foo=foo.com -o sub/a.xml
+check sub/a.xml 3<<< "$fooprefixsrcml"
+
+srcml --in-order sub/a.cpp sub/b.cpp -o sub/a.xml --xmlns:foo=foo.com
+check sub/a.xml 3<<< "$fooprefixsrcml"
+
+srcml --in-order -o sub/a.xml sub/a.cpp sub/b.cpp --xmlns:foo=foo.com
+check sub/a.xml 3<<< "$fooprefixsrcml"
+
+srcml -o sub/a.xml --in-order sub/a.cpp sub/b.cpp --xmlns:foo=foo.com
+check sub/a.xml 3<<< "$fooprefixsrcml"
+
--- cli/testsuite/xmlns_archive_single.sh
+++ cli/testsuite/xmlns_archive_single.sh
@@ -3,12 +3,12 @@
 # test framework
 source $(dirname "$0")/framework_test.sh
 
-# test
+# default xmlns
 define foosrcml <<- 'STDOUT'
 	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-	<unit xmlns="http://www.sdml.info/srcML/src" xmlns="foo.com" revision="0.8.0">
+	<unit xmlns="http://www.sdml.info/srcML/src" xmlns="foo.com" revision="REVISION">
 
-	<unit xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="0.8.0" language="C++" filename="sub/a.cpp" hash="a301d91aac4aa1ab4e69cbc59cde4b4fff32f2b8"><expr_stmt><expr><name>a</name></expr>;</expr_stmt></unit>
+	<unit xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="REVISION" language="C++" filename="sub/a.cpp" hash="a301d91aac4aa1ab4e69cbc59cde4b4fff32f2b8"><expr_stmt><expr><name>a</name></expr>;</expr_stmt></unit>
 
 	</unit>
 	STDOUT
@@ -77,3 +77,80 @@ check sub/a.xml 3<<< "$foosrcml"
 
 srcml -o sub/a.xml --archive sub/a.cpp --xmlns=foo.com
 check sub/a.xml 3<<< "$foosrcml"
+
+
+# with prefix
+define fooprefixsrcml <<- 'STDOUT'
+	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:foo="foo.com" revision="REVISION">
+
+	<unit xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="REVISION" language="C++" filename="sub/a.cpp" hash="a301d91aac4aa1ab4e69cbc59cde4b4fff32f2b8"><expr_stmt><expr><name>a</name></expr>;</expr_stmt></unit>
+
+	</unit>
+	STDOUT
+
+
+srcml --xmlns:foo=foo.com sub/a.cpp --archive
+check 3<<< "$fooprefixsrcml"
+
+srcml sub/a.cpp --xmlns:foo=foo.com --archive
+check 3<<< "$fooprefixsrcml"
+
+srcml sub/a.cpp --archive --xmlns:foo=foo.com
+check 3<<< "$fooprefixsrcml"
+
+srcml --archive --xmlns:foo=foo.com sub/a.cpp
+check 3<<< "$fooprefixsrcml"
+
+srcml --archive sub/a.cpp --xmlns:foo=foo.com
+check 3<<< "$fooprefixsrcml"
+
+srcml --xmlns:foo=foo.com sub/a.cpp --archive -o sub/a.xml
+check sub/a.xml 3<<< "$fooprefixsrcml"
+
+srcml --xmlns:foo=foo.com sub/a.cpp -o sub/a.xml --archive
+check sub/a.xml 3<<< "$fooprefixsrcml"
+
+srcml --xmlns:foo=foo.com -o sub/a.xml sub/a.cpp --archive
+check sub/a.xml 3<<< "$fooprefixsrcml"
+
+srcml -o sub/a.xml --xmlns:foo=foo.com sub/a.cpp --archive
+check sub/a.xml 3<<< "$fooprefixsrcml"
+
+srcml sub/a.cpp --xmlns:foo=foo.com --archive -o sub/a.xml
+check sub/a.xml 3<<< "$fooprefixsrcml"
+
+srcml sub/a.cpp --archive -o sub/a.xml --xmlns:foo=foo.com
+check sub/a.xml 3<<< "$fooprefixsrcml"
+
+srcml sub/a.cpp -o sub/a.xml --archive --xmlns:foo=foo.com
+check sub/a.xml 3<<< "$fooprefixsrcml"
+
+srcml -o sub/a.xml sub/a.cpp --archive --xmlns:foo=foo.com
+check sub/a.xml 3<<< "$fooprefixsrcml"
+
+srcml --archive --xmlns:foo=foo.com sub/a.cpp -o sub/a.xml
+check sub/a.xml 3<<< "$fooprefixsrcml"
+
+srcml --archive --xmlns:foo=foo.com -o sub/a.xml sub/a.cpp
+check sub/a.xml 3<<< "$fooprefixsrcml"
+
+srcml --archive -o sub/a.xml --xmlns:foo=foo.com sub/a.cpp
+check sub/a.xml 3<<< "$fooprefixsrcml"
+
+srcml -o sub/a.xml --archive --xmlns:foo=foo.com sub/a.cpp
+check sub/a.xml 3<<< "$fooprefixsrcml"
+
+srcml --archive sub/a.cpp --xmlns:foo=foo.com -o sub/a.xml
+check sub/a.xml 3<<< "$fooprefixsrcml"
+
+srcml --archive sub/a.cpp -o sub/a.xml --xmlns:foo=foo.com
+check sub/a.xml 3<<< "$fooprefixsrcml"
+
+srcml --archive -o sub/a.xml sub/a.cpp --xmlns:foo=foo.com
+check sub/a.xml 3<<< "$fooprefixsrcml"
+
+srcml -o sub/a.xml --archive sub/a.cpp --xmlns:foo=foo.com
+check sub/a.xml 3<<< "$fooprefixsrcml"
+
+
