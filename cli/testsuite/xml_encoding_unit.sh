cf68dacd5accb94a513e418604938d6e7e6baf9f
Heather Michaud <hmm34@zips.uakron.edu>
2015-05-11 12:21:52 -0400

    Add xml_encoding tests

--- cli/testsuite/xml_encoding_unit.sh
+++ cli/testsuite/xml_encoding_unit.sh
@@ -3,7 +3,7 @@
 # test framework
 source $(dirname "$0")/framework_test.sh
 
-# test encoding flag
+# test encoding flag on a unit
 define srcml <<- 'STDOUT'
 	<?xml version="1.0" encoding="ISO-8859-1" standalone="yes"?>
 	<unit xmlns="http://www.sdml.info/srcML/src" xmlns:cpp="http://www.sdml.info/srcML/cpp" revision="REVISION" language="C++"/>
@@ -16,22 +16,26 @@ define fsrcml <<- 'STDOUT'
 
 createfile sub/a.cpp ""
 
-src2srcml sub/a.cpp -x "ISO-8859-1"
+echo -n "" | src2srcml -l C++ -x "ISO-8859-1"
+check 3<<< "$srcml"
 
+echo -n "" | src2srcml -l C++ --xml-encoding "ISO-8859-1"
+check 3<<< "$srcml"
+
+echo -n "" | src2srcml -l C++ --xml-encoding="ISO-8859-1"
+check 3<<< "$srcml"
+
+src2srcml sub/a.cpp -x "ISO-8859-1"
 check 3<<< "$fsrcml"
 
 src2srcml sub/a.cpp --xml-encoding="ISO-8859-1"
-
 check 3<<< "$fsrcml"
 
 src2srcml sub/a.cpp --xml-encoding "ISO-8859-1"
-
 check 3<<< "$fsrcml"
 
 src2srcml -l C++ -x "ISO-8859-1" -o sub/a.cpp.xml < sub/a.cpp
-
 check sub/a.cpp.xml 3<<< "$srcml"
 
 src2srcml -x "ISO-8859-1" sub/a.cpp -o sub/a.cpp.xml
-
 check sub/a.cpp.xml 3<<< "$fsrcml"
