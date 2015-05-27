0b386fd5e61b02c39491e5d76fafd9a2f9c8a822
Heather Michaud <hmm34@zips.uakron.edu>
2015-05-27 15:33:57 -0400

    CLI Tests: Spaces to tabs for bash definitions

--- cli/testsuite/xpathparam.sh
+++ cli/testsuite/xpathparam.sh
@@ -3,5 +3,29 @@
 # test framework
 source $(dirname "$0")/framework_test.sh
 
+# test xslt empty input
+define srcml <<- 'STDOUT'
+	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+	<unit xmlns="http://www.srcML.org/srcML/src" xmlns:cpp="http://www.srcML.org/srcML/cpp" revision="REVISION" language="C++"/>
+	STDOUT
+
+define paramxslt <<- 'STDOUT'
+	<xsl:stylesheet xmlns="http://www.srcML.org/srcML/src" xmlns:xsl="http://www.w3.org/1999/XSL/Transform" xmlns:cpp="http://www.srcML.org/srcML/cpp" version="1.0">
+	<xsl:output method="text"/>
+	<xsl:param name="paramname"/>
+	<xsl:template match="/"><xsl:text select="$paramname"/></xsl:template>
+	</xsl:stylesheet>
+	STDOUT
+
+createfile sub/a.cpp.xml "$srcml"
+#cp sub/a.cpp.xml a.cpp.xml
+createfile param.xsl "$paramxslt"
+#cp param.xsl myparam.xsl
+
+message "HELLOO2"
+
+srcml --xslt=param.xsl --stringparam paramname='paramvalue' sub/a.cpp.xml
+check 3<<< "stuff"
+
+message "HELLOO3"
 # functionality missing
-exit 1
