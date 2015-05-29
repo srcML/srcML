31388eff0317cae79af2d6d9c4a3bc0e6b33641d
Michael L. Collard, Ph.D <collard@uakron.edu>
2015-05-29 04:52:39 -0400

    CLI: Partical fix for test

--- cli/testsuite/xpath_element_unit.sh
+++ cli/testsuite/xpath_element_unit.sh
@@ -4,6 +4,17 @@
 source $(dirname "$0")/framework_test.sh
 
 # test setting the attribute on xpath query results
+define resultstdin <<- 'STDOUT'
+	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+	<unit xmlns="http://www.srcML.org/srcML/src" xmlns:foo="foo.com" revision="REVISION">
+
+	<unit xmlns:cpp="http://www.srcML.org/srcML/cpp" revision="REVISION" language="C++"><expr_stmt><expr><foo:foo><name>a</name></foo:foo></expr>;</expr_stmt>
+	</unit>
+
+	</unit>
+	STDOUT
+
+# test setting the attribute on xpath query results
 define result <<- 'STDOUT'
 	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 	<unit xmlns="http://www.srcML.org/srcML/src" xmlns:foo="foo.com" revision="REVISION">
@@ -28,7 +39,7 @@ check 3<<< "$result"
 
 # from standard input
 echo "a;" | srcml -l C++ --xpath="//src:name" --element="foo:foo"
-check 3<<< "$result"
+check 3<<< "$resultstdin"
 
 # output to a file
 srcml sub/a.xml --xpath="//src:name" --element="foo:foo" -o result.xml
@@ -38,4 +49,4 @@ srcml --xpath="//src:name" sub/a.xml --element="foo:foo" -o result.xml
 check result.xml <<< "$result"
 
 echo "a;" | srcml -l C++ --xpath="//src:name" --element="foo:foo" -o result.xml
-check result.xml <<< "$result"
+check result.xml <<< "$resultstdin"
