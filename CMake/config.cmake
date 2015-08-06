99ea48bc6fe3687a91b9a1eeac54f97ddcea9681
Brian Bartman <bbartman@kent.edu>
2015-08-06 09:13:54 -0400

    Added build system stuff to build the doxygen documentation. correctly.

--- CMake/config.cmake
+++ CMake/config.cmake
@@ -17,7 +17,6 @@
 # along with the srcML Toolkit; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-
 # 
 # It is expected that the windows dependency directoy will contain a folder
 # named dep which will contain all of the dependencies. For windows.
--- CMake/doc.cmake
+++ CMake/doc.cmake
@@ -16,3 +16,33 @@
 # You should have received a copy of the GNU General Public License
 # along with the srcML Toolkit; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+option(GENERATE_DOCUMENTATION "Enables the generation of documentation during the build and installation process." off)
+if(${GENERATE_DOCUMENTATION})
+    find_program(RONN_EXE REQUIRED NAMES ronn ronn.exe PATHS /bin /usr/bin ${WINDOWS_DEP_PATH}/bin)
+    find_package(Doxygen REQUIRED)
+    configure_file("${CMAKE_SOURCE_DIR}/doc/config" "${CMAKE_BINARY_DIR}/config")
+    SET(DOXYGEN_INPUT "${CMAKE_BINARY_DIR}/config")
+    SET(DOXYGEN_OUTPUT_DIR "${CMAKE_BINARY_DIR}")
+    ADD_CUSTOM_COMMAND(
+        OUTPUT ${DOXYGEN_OUTPUT_DIR}/html ${DOXYGEN_OUTPUT_DIR}/latex
+        COMMAND ${CMAKE_COMMAND} -E echo_append "Building API Documentation..."
+        COMMAND ${DOXYGEN_EXECUTABLE} ${DOXYGEN_INPUT}
+        COMMAND ${CMAKE_COMMAND} -E echo "Done."
+        WORKING_DIRECTORY ${DOXYGEN_OUTPUT_DIR}
+        DEPENDS ${DOXYGEN_INPUT} ${CMAKE_SOURCE_DIR}/src/libsrcml/srcml.h
+    )
+    # Custom commands for creating things using the ronn executable.
+    # add_custom_command(OUTPUT srcml.md
+    #     COMMAND 
+    #     DEPENDS srcml_input.md)
+    # add_custom_command(OUTPUT srcml srcml.html 
+
+    #     RONN_EXE 
+
+    ADD_CUSTOM_TARGET(doc ALL DEPENDS ${DOXYGEN_OUTPUT_DIR}/html ${DOXYGEN_OUTPUT_DIR}/latex)
+    install(FILES ${CMAKE_BINARY_DIR}/doc/srcml DESTINATION man)
+    install(DIRECTORY ${DOXYGEN_OUTPUT_DIR}/html ${DOXYGEN_OUTPUT_DIR}/latex DESTINATION doc)
+
+endif()
+
