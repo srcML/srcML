f5e02c41ab750a8811bbf4365a5369683061a7e0
Michael John Decker <mdecker6@kent.edu>
2016-05-26 11:44:59 -0400

    Replace macro #define with const unsigned int.

--- src/libsrcml/srcml.h
+++ src/libsrcml/srcml.h
@@ -103,19 +103,19 @@ extern "C" {
 
 /**@{ @name Options */
 /** Issue an XML declaration */
-#define SRCML_OPTION_XML_DECL          1<<4
+const unsigned int SRCML_OPTION_XML_DECL          = 1<<4;
 /** Include line/column position attributes */
-#define SRCML_OPTION_POSITION          1<<1
+const unsigned int SRCML_OPTION_POSITION          = 1<<1;
 /** Markup preprocessor elements (default for C, C++) */
-#define SRCML_OPTION_CPP               1<<2 | 1<<3 
+const unsigned int SRCML_OPTION_CPP               = 1<<2 | 1<<3;
 
 /** Leave as text preprocessor else parts (default: markup) */
-#define SRCML_OPTION_CPP_TEXT_ELSE     1<<6
+const unsigned int SRCML_OPTION_CPP_TEXT_ELSE     = 1<<6;
 /** Markup preprocessor @code #if 0 @endcode sections (default: leave as text) */
-#define SRCML_OPTION_CPP_MARKUP_IF0    1<<7
+const unsigned int SRCML_OPTION_CPP_MARKUP_IF0    = 1<<7;
 
 /** Encode the original source encoding as an attribute */
-#define SRCML_OPTION_STORE_ENCODING    1<<26
+const unsigned int SRCML_OPTION_STORE_ENCODING    = 1<<26;
 /**@}*/
 
 
