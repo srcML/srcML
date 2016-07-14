8f611a75f06c4a4483d6d9188e614e7b4d61dd33
Heather M. Michaud <hmm34@zips.uakron.edu>
2016-07-14 12:14:34 -0400

    Test for non-existent remote (archived/compressed) file list #1382

--- cli/testsuite/files_from_exit.sh
+++ cli/testsuite/files_from_exit.sh
@@ -11,3 +11,54 @@ check_exit 1
 
 src2srcml --files-from nonexistent_list.txt -o nonexistent_list.xml
 check_exit 1
+
+src2srcml --files-from https://raw.githubusercontent.com/hmm34/massive-lana/master/nonexistent_list.txt
+check_exit 1
+
+src2srcml --files-from https://raw.githubusercontent.com/hmm34/massive-lana/master/nonexistent_list.txt -o nonexistent_list.xml
+check_exit 1
+
+src2srcml --files-from https://raw.githubusercontent.com/hmm34/massive-lana/master/nonexistent_list.txt.bz2.gz
+check_exit 1
+
+src2srcml --files-from https://raw.githubusercontent.com/hmm34/massive-lana/master/nonexistent_list.txt.bz2
+check_exit 1
+
+src2srcml --files-from https://raw.githubusercontent.com/hmm34/massive-lana/master/nonexistent_list.txt.cpio.bz2
+check_exit 1
+
+src2srcml --files-from https://raw.githubusercontent.com/hmm34/massive-lana/master/nonexistent_list.txt.cpio.gz
+check_exit 1
+
+src2srcml --files-from https://raw.githubusercontent.com/hmm34/massive-lana/master/nonexistent_list.txt.cpio
+check_exit 1
+
+src2srcml --files-from https://raw.githubusercontent.com/hmm34/massive-lana/master/nonexistent_list.txt.gz.bz2
+check_exit 1
+
+src2srcml --files-from https://raw.githubusercontent.com/hmm34/massive-lana/master/nonexistent_list.txt.gz
+check_exit 1
+
+src2srcml --files-from https://raw.githubusercontent.com/hmm34/massive-lana/master/nonexistent_list.txt.tar.bz2
+check_exit 1
+
+src2srcml --files-from https://raw.githubusercontent.com/hmm34/massive-lana/master/nonexistent_list.txt.tar.gz
+check_exit 1
+
+src2srcml --files-from https://raw.githubusercontent.com/hmm34/massive-lana/master/nonexistent_list.txt.tar
+check_exit 1
+
+src2srcml --files-from https://raw.githubusercontent.com/hmm34/massive-lana/master/nonexistent_list.txt.tbz2
+check_exit 1
+
+src2srcml --files-from https://raw.githubusercontent.com/hmm34/massive-lana/master/nonexistent_list.txt.tgz
+check_exit 1
+
+src2srcml --files-from https://raw.githubusercontent.com/hmm34/massive-lana/master/nonexistent_list.txt.zip.bz2
+check_exit 1
+
+src2srcml --files-from https://raw.githubusercontent.com/hmm34/massive-lana/master/nonexistent_list.txt.zip.gz
+check_exit 1
+
+src2srcml --files-from https://raw.githubusercontent.com/hmm34/massive-lana/master/nonexistent_list.txt.zip
+check_exit 1
