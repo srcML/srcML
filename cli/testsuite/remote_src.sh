1524eef5edfd36b7c2c0a15db9df5c680c88d672
Heather M. Michaud <hmm34@zips.uakron.edu>
2016-07-14 15:44:13 -0400

    Test direct input empty/valid compressed/archive remote source #1382

--- cli/testsuite/remote_src.sh
+++ cli/testsuite/remote_src.sh
@@ -5,7 +5,7 @@ source $(dirname "$0")/framework_test.sh
 
 define output <<- 'STDOUT'
 	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-	<unit xmlns="http://www.srcML.org/srcML/src" xmlns:cpp="http://www.srcML.org/srcML/cpp" revision="REVISION" language="C++" filename="raw.githubusercontent.com/hmm34/massive-lana/master/a.cpp"><return>return <expr><name>a</name></expr>;</return>
+	<unit xmlns="http://www.srcML.org/srcML/src" xmlns:cpp="http://www.srcML.org/srcML/cpp" revision="REVISION" language="C++" filename="raw.githubusercontent.com/hmm34/massive-lana/master/a.cpp"><expr_stmt><expr><name>a</name></expr>;</expr_stmt>
 	</unit>
 	STDOUT
 
@@ -13,7 +13,7 @@ define archive_output <<- 'STDOUT'
 	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 	<unit xmlns="http://www.srcML.org/srcML/src" revision="0.9.5">
 
-	<unit xmlns:cpp="http://www.srcML.org/srcML/cpp" revision="0.9.5" language="C++" filename="raw.githubusercontent.com/hmm34/massive-lana/master/a.cpp" hash="2b3e1887614fc8a67d9951595872e4381b4a429a"><return>return <expr><name>a</name></expr>;</return>
+	<unit xmlns:cpp="http://www.srcML.org/srcML/cpp" revision="0.9.5" language="C++" filename="raw.githubusercontent.com/hmm34/massive-lana/master/a.cpp" hash="aa2a72b26cf958d8718a2e9bc6b84679a81d54cb"><expr_stmt><expr><name>a</name></expr>;</expr_stmt>
 	</unit>
 
 	</unit>
@@ -44,3 +44,57 @@ check 3<<< "$archive_output"
 
 srcml --files-from list.txt -o remote_from_list.xml
 check remote_from_list.xml 3<<< "$archive_output"
+
+
+# direct input from a remote source, with various compressions
+define empty_srcml_with_url <<- 'STDOUT'
+	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+	<unit xmlns="http://www.srcML.org/srcML/src" xmlns:cpp="http://www.srcML.org/srcML/cpp" revision="REVISION" language="C++" url="test" filename="test"><expr_stmt><expr><name>a</name></expr>;</expr_stmt></unit>
+	STDOUT
+
+# compression only
+srcml https://github.com/hmm34/massive-lana/blob/master/a.cpp.bz2?raw=true --url="test" --filename="test"
+check 3<<< "$empty_srcml_with_url"
+
+srcml https://github.com/hmm34/massive-lana/blob/master/a.cpp.gz?raw=true --url="test" --filename="test"
+check 3<<< "$empty_srcml_with_url"
+
+srcml https://github.com/hmm34/massive-lana/blob/master/a.cpp.bz2.gz?raw=true --url="test" --filename="test"
+check 3<<< "$empty_srcml_with_url"
+
+srcml https://github.com/hmm34/massive-lana/blob/master/a.cpp.gz.bz2?raw=true --url="test" --filename="test"
+check 3<<< "$empty_srcml_with_url"
+
+# archive/compression mix
+srcml https://github.com/hmm34/massive-lana/raw/master/a.cpp.cpio --url="test"
+check 3<<< "$empty_srcml_with_url"
+
+srcml https://github.com/hmm34/massive-lana/blob/master/a.cpp.cpio.bz2?raw=true --url="test"
+check 3<<< "$empty_srcml_with_url"
+
+srcml https://github.com/hmm34/massive-lana/blob/master/a.cpp.cpio.gz?raw=true --url="test"
+check 3<<< "$empty_srcml_with_url"
+
+srcml https://github.com/hmm34/massive-lana/blob/master/a.cpp.tar.bz2?raw=true --url="test"
+check 3<<< "$empty_srcml_with_url"
+
+srcml https://github.com/hmm34/massive-lana/blob/master/a.cpp.tar.gz?raw=true --url="test"
+check 3<<< "$empty_srcml_with_url"
+
+srcml https://github.com/hmm34/massive-lana/raw/master/a.cpp.tar --url="test"
+check 3<<< "$empty_srcml_with_url"
+
+srcml https://github.com/hmm34/massive-lana/blob/master/a.cpp.tbz2?raw=true --url="test"
+check 3<<< "$empty_srcml_with_url"
+
+srcml https://github.com/hmm34/massive-lana/blob/master/a.cpp.tgz?raw=true --url="test"
+check 3<<< "$empty_srcml_with_url"
+
+srcml https://github.com/hmm34/massive-lana/blob/master/a.cpp.zip?raw=true --url="test"
+check 3<<< "$empty_srcml_with_url"
+
+srcml https://github.com/hmm34/massive-lana/blob/master/a.cpp.zip.bz2?raw=true --url="test"
+check 3<<< "$empty_srcml_with_url"
+
+srcml https://github.com/hmm34/massive-lana/blob/master/a.cpp.zip.gz?raw=true --url="test"
+check 3<<< "$empty_srcml_with_url"
