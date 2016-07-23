3ceee883ac0e75ccfe6c5b7f7db88b7b0735d2b5
Heather M. Michaud <hmm34@zips.uakron.edu>
2016-07-23 14:01:43 -0400

    Add compressed/archived remote srcml tests #1382

--- cli/testsuite/remote_srcml_archive_multi.sh
+++ cli/testsuite/remote_srcml_archive_multi.sh
@@ -18,3 +18,52 @@ check 3<<< "a;"
 
 srcml https://raw.githubusercontent.com/srcML/test-data/master/srcml/archive-multi.xml --unit 1 -o sub/archive_multi.cpp
 check sub/archive_multi.cpp 3<<< "a;"
+
+
+# input from a compressed srcml file
+srcml https://github.com/srcML/test-data/blob/master/srcml/archive-multi.xml.bz2?raw=true
+check 3<<< "a;"
+
+srcml https://github.com/srcML/test-data/blob/master/srcml/archive-multi.xml.gz?raw=true
+check 3<<< "a;"
+
+srcml https://github.com/srcML/test-data/blob/master/srcml/archive-multi.xml.bz2.gz?raw=true
+check 3<<< "a;"
+
+srcml https://github.com/srcML/test-data/blob/master/srcml/archive-multi.xml.gz.bz2?raw=true
+check 3<<< "a;"
+
+
+# input from an archived srcml file
+srcml https://github.com/srcML/test-data/raw/master/srcml/archive-multi.xml.cpio
+check 3<<< "a;"
+
+srcml https://github.com/srcML/test-data/blob/master/srcml/archive-multi.xml.cpio.bz2?raw=true
+check 3<<< "a;"
+
+srcml https://github.com/srcML/test-data/blob/master/srcml/archive-multi.xml.cpio.gz?raw=true
+check 3<<< "a;"
+
+srcml https://github.com/srcML/test-data/blob/master/srcml/archive-multi.xml.tar.bz2?raw=true
+check 3<<< "a;"
+
+srcml https://github.com/srcML/test-data/blob/master/srcml/archive-multi.xml.tar.gz?raw=true
+check 3<<< "a;"
+
+srcml https://github.com/srcML/test-data/raw/master/srcml/archive-multi.xml.tar
+check 3<<< "a;"
+
+srcml https://github.com/srcML/test-data/blob/master/srcml/archive-multi.xml.tbz2?raw=true
+check 3<<< "a;"
+
+srcml https://github.com/srcML/test-data/blob/master/srcml/archive-multi.xml.tgz?raw=true
+check 3<<< "a;"
+
+srcml https://github.com/srcML/test-data/blob/master/srcml/archive-multi.xml.zip?raw=true
+check 3<<< "a;"
+
+srcml https://github.com/srcML/test-data/blob/master/srcml/archive-multi.xml.zip.bz2?raw=true
+check 3<<< "a;"
+
+srcml https://github.com/srcML/test-data/blob/master/srcml/archive-multi.xml.zip.gz?raw=true
+check 3<<< "a;"
--- cli/testsuite/remote_srcml_archive_single.sh
+++ cli/testsuite/remote_srcml_archive_single.sh
@@ -16,3 +16,51 @@ check 3<<< "a;"
 
 srcml https://raw.githubusercontent.com/srcML/test-data/master/srcml/archive-single.xml --unit 1 -o sub/archive_single.cpp
 check sub/archive_single.cpp 3<<< "a;"
+
+# input from a compressed srcml file
+srcml https://github.com/srcML/test-data/blob/master/srcml/archive-single.xml.bz2?raw=true
+check 3<<< "a;"
+
+srcml https://github.com/srcML/test-data/blob/master/srcml/archive-single.xml.gz?raw=true
+check 3<<< "a;"
+
+srcml https://github.com/srcML/test-data/blob/master/srcml/archive-single.xml.bz2.gz?raw=true
+check 3<<< "a;"
+
+srcml https://github.com/srcML/test-data/blob/master/srcml/archive-single.xml.gz.bz2?raw=true
+check 3<<< "a;"
+
+
+# input from an archived srcml file
+srcml https://github.com/srcML/test-data/raw/master/srcml/archive-single.xml.cpio
+check 3<<< "a;"
+
+srcml https://github.com/srcML/test-data/blob/master/srcml/archive-single.xml.cpio.bz2?raw=true
+check 3<<< "a;"
+
+srcml https://github.com/srcML/test-data/blob/master/srcml/archive-single.xml.cpio.gz?raw=true
+check 3<<< "a;"
+
+srcml https://github.com/srcML/test-data/blob/master/srcml/archive-single.xml.tar.bz2?raw=true
+check 3<<< "a;"
+
+srcml https://github.com/srcML/test-data/blob/master/srcml/archive-single.xml.tar.gz?raw=true
+check 3<<< "a;"
+
+srcml https://github.com/srcML/test-data/raw/master/srcml/archive-single.xml.tar
+check 3<<< "a;"
+
+srcml https://github.com/srcML/test-data/blob/master/srcml/archive-single.xml.tbz2?raw=true
+check 3<<< "a;"
+
+srcml https://github.com/srcML/test-data/blob/master/srcml/archive-single.xml.tgz?raw=true
+check 3<<< "a;"
+
+srcml https://github.com/srcML/test-data/blob/master/srcml/archive-single.xml.zip?raw=true
+check 3<<< "a;"
+
+srcml https://github.com/srcML/test-data/blob/master/srcml/archive-single.xml.zip.bz2?raw=true
+check 3<<< "a;"
+
+srcml https://github.com/srcML/test-data/blob/master/srcml/archive-single.xml.zip.gz?raw=true
+check 3<<< "a;"
