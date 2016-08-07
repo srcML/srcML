23d54af2a3d8662526275d3fc529377072418706
Heather M. Michaud <hmm34@zips.uakron.edu>
2016-08-06 21:19:17 -0400

    Fix xpath empty input test expecting wrong output #1411
    * Using stdin redirction reads this as an empty source code file,
    so it should output an empty unit.

--- cli/testsuite/xpath_empty_input.sh
+++ cli/testsuite/xpath_empty_input.sh
@@ -15,10 +15,6 @@ define xpath_empty <<- 'STDOUT'
 	<unit xmlns="http://www.srcML.org/srcML/src" revision="REVISION"/>
 	STDOUT
 
-define output <<- 'STDOUT'
-	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-	<unit xmlns="http://www.srcML.org/srcML/src" revision="REVISION"/>
-	STDOUT
 
 xmlcheck "$xpath_empty"
 xmlcheck "$output"
@@ -34,6 +30,6 @@ check_exit 1
 srcml2src -l C++ --xpath=src:unit -o sub/b.cpp.xml sub/a.cpp.xml
 check_exit 1
 
-# TODO: issue #973
+# equivalent as inputting an empty source file
 srcml2src -l C++ --xpath=src:unit < sub/a.cpp.xml
-check_exit 1
+check 3<<< "$xpath_empty"
