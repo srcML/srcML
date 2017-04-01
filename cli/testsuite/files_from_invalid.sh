2cc0df4273da8da906340a3083613093ab41cb9e
Michael L. Collard, Ph.D <collard@uakron.edu>
2017-04-01 17:02:15 -0400

    Fix test case

--- cli/testsuite/files_from_invalid.sh
+++ cli/testsuite/files_from_invalid.sh
@@ -32,7 +32,7 @@ check empty-remote.xml 3<<< "$empty_srcml"
 # file list of non-existent files
 define open_error <<- 'STDERR'
 	srcml: Unable to open file nonexistent1.txt
-STDERR
+	STDERR
 
 createfile nonexistent_files.txt "nonexistent1.txt"
 
