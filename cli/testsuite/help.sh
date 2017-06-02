86fecf39443a2cacb725d2680a7298edba279de7
Drew Guarnera <dguarner@kent.edu>
2017-06-02 15:39:31 -0400

    Add test cases that show help usage cases

--- cli/testsuite/help.sh
+++ cli/testsuite/help.sh
@@ -8,3 +8,10 @@ source $(dirname "$0")/framework_test.sh
 src2srcml -h
 src2srcml --help
 
+src2srcml -h src2srcml
+src2srcml --help src2srcml
+src2srcml --help="src2srcml"
+
+src2srcml -h srcml2src
+src2srcml --help srcml2src
+src2srcml --help="srcml2src"
