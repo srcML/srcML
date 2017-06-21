b46eae8d33f8ea74e55d1b1a69afdebea946f2f4
Michael L. Collard <collard@uakron.edu>
2017-06-20 22:30:20 -0400

    Change from BUILD_CLI_TESTS to BUILD_CLIENT_TESTS for issue 1455

--- CMake/config.cmake
+++ CMake/config.cmake
@@ -53,7 +53,7 @@ endif()
 # Turn ON/OFF tests
 option(BUILD_TIMING_TESTS "Run timing tests with ctest" ON)
 option(BUILD_UNIT_TESTS "Build unit tests for srcML/libsrcml" ON)
-option(BUILD_CLI_TESTS "Build cli tests" ON)
+option(BUILD_CLIENT_TESTS "Build cli tests" ON)
 option(BUILD_LARGE_SYSTEMS_TESTS "Build tests run on large systems" OFF)
 
 
