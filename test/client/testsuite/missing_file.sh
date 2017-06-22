3c5d75f49f1bd47c7cc2774e4b62ac91be3ccfd1
Mike Weyandt <mike@mikeweyandt.com>
2017-06-22 09:54:02 -0400

    redirected stderr to stdout and updated value

--- test/client/testsuite/missing_file.sh
+++ test/client/testsuite/missing_file.sh
@@ -7,20 +7,21 @@ source $(dirname "$0")/framework_test.sh
 ##
 # check missingfile
 
-src2srcml foo.c
+src2srcml foo.c 2>&1
 
-checkv2 4<<- 'STDERR'
-	srcml: Unable to open file foo.c
-	STDERR
+checkv2 "WARNING srcml: Unable to open file foo.c
 
-src2srcml abc.c
+"
 
-checkv2 4<<- 'STDERR'
-	srcml: Unable to open file abc.c
-	STDERR
 
-src2srcml ../src/foo.c
+src2srcml abc.c 2>&1
 
-checkv2 4<<- 'STDERR'
-	srcml: Unable to open file ../src/foo.c
-	STDERR
+checkv2 "WARNING srcml: Unable to open file abc.c
+
+"
+
+src2srcml ../src/foo.c 2>&1
+
+checkv2 "WARNING srcml: Unable to open file ../src/foo.c
+
+"
