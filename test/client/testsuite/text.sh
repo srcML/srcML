bda5f2dc2a9df727dc61befb83f8f724ca47eca3
Michael L. Collard, Ph.D <collard@uakron.edu>
2017-06-23 17:00:51 -0400

    Improve testing of escape characters for --text input

--- test/client/testsuite/text.sh
+++ test/client/testsuite/text.sh
@@ -136,3 +136,75 @@ check sub/a.cpp.xml "$ansrcml"
 
 src2srcml -l C++ --text="a;\n" -o sub/a.cpp.xml
 check sub/a.cpp.xml "$ansrcml"
+
+# embedded \a
+define atsrcml <<- 'STDOUT'
+	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+	<unit xmlns="http://www.srcML.org/srcML/src" xmlns:cpp="http://www.srcML.org/srcML/cpp" revision="REVISION" language="C++"><escape char="0x07"/><expr_stmt><expr><name>a</name></expr>;</expr_stmt><escape char="0x07"/></unit>
+	STDOUT
+
+src2srcml -l C++ --text="\aa;\a" -o sub/a.cpp.xml
+check sub/a.cpp.xml "$atsrcml"
+
+# embedded \b
+define atsrcml <<- 'STDOUT'
+	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+	<unit xmlns="http://www.srcML.org/srcML/src" xmlns:cpp="http://www.srcML.org/srcML/cpp" revision="REVISION" language="C++"><escape char="0x08"/><expr_stmt><expr><name>a</name></expr>;</expr_stmt><escape char="0x08"/></unit>
+	STDOUT
+
+src2srcml -l C++ --text="\ba;\b" -o sub/a.cpp.xml
+check sub/a.cpp.xml "$atsrcml"
+
+# embedded \f
+define atsrcml <<- 'STDOUT'
+	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+	<unit xmlns="http://www.srcML.org/srcML/src" xmlns:cpp="http://www.srcML.org/srcML/cpp" revision="REVISION" language="C++"><escape char="0x0c"/><expr_stmt><expr><name>a</name></expr>;</expr_stmt><escape char="0x0c"/></unit>
+	STDOUT
+
+src2srcml -l C++ --text="\fa;\f" -o sub/a.cpp.xml
+check sub/a.cpp.xml "$atsrcml"
+
+# embedded \r
+define atsrcml <<- 'STDOUT'
+	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+	<unit xmlns="http://www.srcML.org/srcML/src" xmlns:cpp="http://www.srcML.org/srcML/cpp" revision="REVISION" language="C++">
+	<expr_stmt><expr><name>a</name></expr>;</expr_stmt>
+	</unit>
+	STDOUT
+
+src2srcml -l C++ --text="\ra;\r" -o sub/a.cpp.xml
+check sub/a.cpp.xml "$atsrcml"
+
+# embedded \t
+define atsrcml <<- 'STDOUT'
+	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+	<unit xmlns="http://www.srcML.org/srcML/src" xmlns:cpp="http://www.srcML.org/srcML/cpp" revision="REVISION" language="C++">	<expr_stmt><expr><name>a</name></expr>;</expr_stmt>	</unit>
+	STDOUT
+
+src2srcml -l C++ --text="\ta;\t" -o sub/a.cpp.xml
+check sub/a.cpp.xml "$atsrcml"
+
+# embedded \v
+define atsrcml <<- 'STDOUT'
+	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+	<unit xmlns="http://www.srcML.org/srcML/src" xmlns:cpp="http://www.srcML.org/srcML/cpp" revision="REVISION" language="C++"><escape char="0x0b"/><expr_stmt><expr><name>a</name></expr>;</expr_stmt><escape char="0x0b"/></unit>
+	STDOUT
+
+src2srcml -l C++ --text="\va;\v" -o sub/a.cpp.xml
+check sub/a.cpp.xml "$atsrcml"
+
+# embedded \e
+define atsrcml <<- 'STDOUT'
+	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+	<unit xmlns="http://www.srcML.org/srcML/src" xmlns:cpp="http://www.srcML.org/srcML/cpp" revision="REVISION" language="C++"><escape char="0x1b"/><expr_stmt><expr><name>a</name></expr>;</expr_stmt><escape char="0x1b"/></unit>
+	STDOUT
+
+src2srcml -l C++ --text="\ea;\e" -o sub/a.cpp.xml
+check sub/a.cpp.xml "$atsrcml"
+
+
+
+
+
+
+
