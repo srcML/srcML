92e2dc0117f17ff6fa5515795b2d14d503647360
Michael L. Collard <collard@uakron.edu>
2017-06-30 06:07:09 -0400

    Fix test cases for client remove srcml archive

--- test/client/testsuite/remote_srcml_archive_multi.sh
+++ test/client/testsuite/remote_srcml_archive_multi.sh
@@ -3,71 +3,72 @@
 # test framework
 source $(dirname "$0")/framework_test.sh
 
-define srca <<- 'STDOUT'a;
-	return b;
+# have to get null byte into test case result
+printf "a;\n\0return b;\n" > tfile
+
+define srca <<- 'STDOUT'
+	a;
 	STDOUT
 
 # input from a valid remote srcML archive multiple units
 srcml https://raw.githubusercontent.com/srcML/test-data/master/srcml/archive-multi.xml
-check "$srca"
+check tfile
 
 mkdir sub
 
-srcml https://raw.githubusercontent.com/srcML/test-data/master/srcml/archive-multi.xml -o sub/archive_multi.cpp
-check sub/archive_multi.cpp "a;
-return b;"
+#srcml https://raw.githubusercontent.com/srcML/test-data/master/srcml/archive-multi.xml -o sub/archive_multi.cpp
+#check sub/archive_multi.cpp tfile
 
 srcml https://raw.githubusercontent.com/srcML/test-data/master/srcml/archive-multi.xml --unit 1
-check "a;"
+check "$srca"
 
 srcml https://raw.githubusercontent.com/srcML/test-data/master/srcml/archive-multi.xml --unit 1 -o sub/archive_multi.cpp
-check sub/archive_multi.cpp "a;"
-
+check sub/archive_multi.cpp "$srca"
 
 # input from a compressed srcml file
-srcml https://github.com/srcML/test-data/blob/master/srcml/archive-multi.xml.bz2?raw=true
-check "a;"
+srcml https://raw.githubusercontent.com/srcML/test-data/master/srcml/archive-multi.xml.bz2
+check tfile
 
-srcml https://github.com/srcML/test-data/blob/master/srcml/archive-multi.xml.gz?raw=true
-check "a;"
+srcml https://raw.githubusercontent.com/srcML/test-data/master/srcml/archive-multi.xml.gz
+check tfile
 
-srcml https://github.com/srcML/test-data/blob/master/srcml/archive-multi.xml.bz2.gz?raw=true
-check "a;"
+srcml https://raw.githubusercontent.com/srcML/test-data/master/srcml/archive-multi.xml.bz2.gz
+check tfile
 
-srcml https://github.com/srcML/test-data/blob/master/srcml/archive-multi.xml.gz.bz2?raw=true
-check "a;"
+srcml https://raw.githubusercontent.com/srcML/test-data/master/srcml/archive-multi.xml.gz.bz2
+check tfile
 
 
 # input from an archived srcml file
-srcml https://github.com/srcML/test-data/raw/master/srcml/archive-multi.xml.cpio
-check "a;"
+srcml https://raw.githubusercontent.com/srcML/test-data/master/srcml/archive-multi.xml.cpio
+check tfile
 
-srcml https://github.com/srcML/test-data/blob/master/srcml/archive-multi.xml.cpio.bz2?raw=true
-check "a;"
+srcml https://raw.githubusercontent.com/srcML/test-data/master/srcml/archive-multi.xml.cpio.bz2
+check tfile
 
-srcml https://github.com/srcML/test-data/blob/master/srcml/archive-multi.xml.cpio.gz?raw=true
-check "a;"
+srcml https://raw.githubusercontent.com/srcML/test-data/master/srcml/archive-multi.xml.cpio.gz
+check tfile
 
-srcml https://github.com/srcML/test-data/blob/master/srcml/archive-multi.xml.tar.bz2?raw=true
-check "a;"
+srcml https://raw.githubusercontent.com/srcML/test-data/master/srcml/archive-multi.xml.tar.bz2
+check tfile
 
-srcml https://github.com/srcML/test-data/blob/master/srcml/archive-multi.xml.tar.gz?raw=true
-check "a;"
+srcml https://raw.githubusercontent.com/srcML/test-data/master/srcml/archive-multi.xml.tar.gz
+check tfile
 
-srcml https://github.com/srcML/test-data/raw/master/srcml/archive-multi.xml.tar
-check "a;"
+srcml https://raw.githubusercontent.com/srcML/test-data/raw/master/srcml/archive-multi.xml.tar
+check tfile
 
-srcml https://github.com/srcML/test-data/blob/master/srcml/archive-multi.xml.tbz2?raw=true
-check "a;"
+srcml https://raw.githubusercontent.com/srcML/test-data/master/srcml/archive-multi.xml.tbz2
+check tfile
 
-srcml https://github.com/srcML/test-data/blob/master/srcml/archive-multi.xml.tgz?raw=true
-check "a;"
+srcml https://raw.githubusercontent.com/srcML/test-data/master/srcml/archive-multi.xml.tgz
+check tfile
 
-srcml https://github.com/srcML/test-data/blob/master/srcml/archive-multi.xml.zip?raw=true
-check "a;"
+srcml https://raw.githubusercontent.com/srcML/test-data/master/srcml/archive-multi.xml.zip
+check tfile
 
-srcml https://github.com/srcML/test-data/blob/master/srcml/archive-multi.xml.zip.bz2?raw=true
-check "a;"
+srcml https://raw.githubusercontent.com/srcML/test-data/master/srcml/archive-multi.xml.zip.bz2
+check tfile
 
-srcml https://github.com/srcML/test-data/blob/master/srcml/archive-multi.xml.zip.gz?raw=true
-check "a;"
+srcml https://raw.githubusercontent.com/srcML/test-data/master/srcml/archive-multi.xml.zip.gz
+check tfile
