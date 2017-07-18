64aa7dcb96b3ed5bea7338e7b519403d2c325ac4
Michael L. Collard <collard@uakron.edu>
2017-07-17 22:27:49 -0400

    Code towards encoding checking

--- src/parser/UTF8CharBuffer.cpp
+++ src/parser/UTF8CharBuffer.cpp
@@ -33,12 +33,29 @@
 #include <io.h>
 #endif
 
+namespace {
+    template<typename T>
+    struct Context {
+        Context(T v) : value(v) {}
+        T value;
+    };
 
-template<typename T>
-struct Context {
-    Context(T v) : value(v) {}
-    T value;
-};
+    // indicates if the two given encodings are basically the same,
+    // with a trivial encoding process
+    bool compatibleEncodings(const char* encoding1, const char* encoding2) {
+        fprintf(stderr, "DEBUG:  %s %s %d\n", __FILE__,  __FUNCTION__, __LINE__);
+
+        // setup encoder from encoding to UTF-8
+        iconv_t ce = iconv_open(encoding1, encoding2);
+        if (ce == (iconv_t) -1)
+            return false;
+
+        // see if encoding is trivial
+        bool trivial = false;
+        iconvctl(ce, ICONV_TRIVIALP, &trivial);
+        return trivial;
+    }
+}
 
 /**
  * UTF8CharBuffer
@@ -242,8 +259,12 @@ ssize_t UTF8CharBuffer::readChars() {
             pos += 3;
 
             // no encoding specified (by user) then UTF-8 it is
-            if (encoding.empty())
+            // no encoding found yet
+            if (encoding.empty()) {
                 encoding = "UTF-8";
+            } else if (!compatibleEncodings(encoding.c_str(), "UTF-8")) {
+                fprintf(stderr, "Warning: the encoding %s was specified, but the source code has a UTF-8 BOM", encoding.c_str());
+            }
         }
 
         // auto-detect UTF-16 based on BOM
@@ -253,8 +274,11 @@ ssize_t UTF8CharBuffer::readChars() {
             (data.i & 0x0000FFFF) == 0x0000FEFF) {
 
             // no encoding found yet
-            if (encoding.empty())
+            if (encoding.empty()) {
                 encoding = "UTF-16";
+            } else if (!compatibleEncodings(encoding.c_str(), "UTF-16")) {
+                fprintf(stderr, "Warning: the encoding %s was specified, but the source code has a UTF-16 BOM", encoding.c_str());
+            }
         }
 
         // auto-detect UTF-32 based on BOM
@@ -264,8 +288,11 @@ ssize_t UTF8CharBuffer::readChars() {
             data.i == 0xFEFF0000) {
 
             // no encoding found yet
-            if (encoding.empty())
+            if (encoding.empty()) {
                 encoding = "UTF-32";
+            } else if (!compatibleEncodings(encoding.c_str(), "UTF-32")) {
+                fprintf(stderr, "Warning: the encoding %s was specified, but the source code has a UTF-32 BOM", encoding.c_str());
+            }
         }
 
         // if no encoding found or specified, assume ISO-8859-1
