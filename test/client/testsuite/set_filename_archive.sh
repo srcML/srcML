3dcb0329dcf98ca9d5fa677ab4c8c083c4f954d5
Michael L. Collard <collard@uakron.edu>
2017-07-23 12:00:34 -0400

    Fix regression after changes to test framework

--- test/client/testsuite/set_filename_archive.sh
+++ test/client/testsuite/set_filename_archive.sh
@@ -10,41 +10,41 @@ source $(dirname "$0")/framework_test.sh
 createfile sub/a.cpp "a;"
 
 srcml sub/a.cpp --archive -f "compressed.tar.gz"
-check "" "Attribute filename cannot be set for a srcML archive
+check_exit 2 "Attribute filename cannot be set for a srcML archive
 "
 
 srcml sub/a.cpp --archive --filename "compressed.tar.gz"
-check "" "Attribute filename cannot be set for a srcML archive"
+check_exit 2 "Attribute filename cannot be set for a srcML archive"
 
 srcml sub/a.cpp --archive --filename="compressed.tar.gz"
-check "" "Attribute filename cannot be set for a srcML archive"
+check_exit 2 "Attribute filename cannot be set for a srcML archive"
 
 srcml -l C++ --archive -f 'compressed.tar.gz' -o sub/a.cpp.xml sub/a.cpp
-check "" "Attribute filename cannot be set for a srcML archive"
+check_exit 2 "Attribute filename cannot be set for a srcML archive"
 
 srcml --archive  -f 'compressed.tar.gz' sub/a.cpp -o sub/a.cpp.xml
-check "" "Attribute filename cannot be set for a srcML archive"
+check_exit 2 "Attribute filename cannot be set for a srcML archive"
 
 
 # standard input
 echo -n "a;" | srcml -l C++ --archive -f compressed.tar.gz
-check "" "Attribute filename cannot be set for a srcML archive"
+check_exit 2 "Attribute filename cannot be set for a srcML archive"
 
 echo -n "a;" | srcml -l C++ --archive --filename compressed.tar.gz
-check "" "Attribute filename cannot be set for a srcML archive"
+check_exit 2 "Attribute filename cannot be set for a srcML archive"
 
 echo -n "a;" | srcml -l C++ --archive --filename=compressed.tar.gz
-check "" "Attribute filename cannot be set for a srcML archive"
+check_exit 2 "Attribute filename cannot be set for a srcML archive"
 
 
 # filename flag on archive of multiple units
 createfile sub/b.cpp "b;"
 
 srcml sub/a.cpp sub/b.cpp --in-order -f "compressed.tar.gz"
-check "" "Attribute filename cannot be set for a srcML archive"
+check_exit 2 "Attribute filename cannot be set for a srcML archive"
 
 srcml sub/a.cpp sub/b.cpp --in-order -f=compressed.tar.gz
-check "" "Attribute filename cannot be set for a srcML archive"
+check_exit 2 "Attribute filename cannot be set for a srcML archive"
 
 srcml --in-order -f "compressed.tar.gz" sub/a.cpp sub/b.cpp
-check "" "Attribute filename cannot be set for a srcML archive"
+check_exit 2 "Attribute filename cannot be set for a srcML archive"
