e78bbcbb2609354bf54ed7219539f7d03296a6b8
Michael L. Collard <collard@uakron.edu>
2017-08-03 08:21:34 -0400

    Now works in non-position mode

--- src/parser/srcMLOutput.cpp
+++ src/parser/srcMLOutput.cpp
@@ -201,63 +201,62 @@ void srcMLOutput::consume(const char* language, const char* revision, const char
     unit_hash = hash;
     unit_encoding = encoding;
 
+    bool isposition = isoption(options, SRCML_OPTION_POSITION);
+    bool isinteractive = isoption(options, SRCML_OPTION_INTERACTIVE);
+
     int open = 0;
-    unsigned long maxqsize = 0;
     std::stack<const antlr::RefToken> ends;
-    if (!isoption(options, SRCML_OPTION_INTERACTIVE)) {
+    while (1) {
+        const antlr::RefToken& token = input->nextToken();
+        if (token->getType() == antlr::Token::EOF_TYPE)
+            break;
 
-        while (1) {
-            const antlr::RefToken& token = input->nextToken();
-            if (token->getType() == antlr::Token::EOF_TYPE)
-                break;
+        // if we aren't determining position, then just output and go on
+        if (!isposition) {
+            outputToken(token);
+            if (isinteractive) 
+                xmlTextWriterFlush(xout);
+            continue;
+        }
 
-            tokenlist.emplace(token);
+        // since we are determining position, need to queue up until
+        // back at top document level (i.e., directly inside of <unit>)
+        tokenlist.emplace(token);
 
-            // end tokens are put on queue, then all tokens on down are output
-            if (isend(token)) {
-                --open;
+        // end tokens are put on queue, then all tokens on down are output
+        if (ispurestart(token)) {
+            ++open;
 
-                ends.emplace(token);
+        } else if (isend(token)) {
+            --open;
+            ends.emplace(token);
 
-                if (open == 0) {
-                    while (!tokenlist.empty()) {
-                        const antlr::RefToken& qtoken = tokenlist.front();
+            if (open == 0) {
+                while (!tokenlist.empty()) {
+                    const antlr::RefToken qtoken = std::move(tokenlist.front());
+                    tokenlist.pop();
 
-                        // need to transfer line/column from end token to start token
-                        if (ispurestart(qtoken)) {
-                            const antlr::RefToken& qetoken = ends.top();
-                            static_cast<srcMLToken*>(&(*qtoken))->endline = qetoken->getLine();
-                            static_cast<srcMLToken*>(&(*qtoken))->endcolumn = qetoken->getColumn();
-                            ends.pop();
-                        }
-
-                        outputToken(qtoken);
-                        tokenlist.pop();
+                    // need to transfer line/column from end token to start token
+                    if (ispurestart(qtoken)) {
+                        const antlr::RefToken qetoken = std::move(ends.top());
+                        static_cast<srcMLToken*>(&(*qtoken))->endline = qetoken->getLine();
+                        static_cast<srcMLToken*>(&(*qtoken))->endcolumn = qetoken->getColumn();
+                        ends.pop();
                     }
+
+                    outputToken(qtoken);
+                    if (isinteractive) 
+                        xmlTextWriterFlush(xout);
                 }
-
-            } else if (ispurestart(token)) {
-
-                ++open;
             }
-
-            maxqsize = std::max(tokenlist.size(), maxqsize);
         }
+    }
 
-        // may be tokens left in the queue
-        while (!tokenlist.empty()) {
-            outputToken(tokenlist.front());
-            tokenlist.pop();
-        }
-
-    } else {
-
-        // consume all input until EOF
-        while (consume_next() != antlr::Token::EOF_TYPE) {
-
-            // in interactive mode flush after each token is discovered
-            xmlTextWriterFlush(xout);
-        }
+    // may be tokens left in the queue
+    while (!tokenlist.empty()) {
+        const antlr::RefToken qtoken = std::move(tokenlist.front());
+        outputToken(qtoken);
+        tokenlist.pop();
     }
 }
 
