34c0a0fd4dc9112dc1d7f967c142a7de7b44b295
Michael L. Collard <collard@uakron.edu>
2017-08-16 14:50:07 -0400

    Missing define in client test file

--- test/client/testsuite/invalid_input.sh
+++ test/client/testsuite/invalid_input.sh
@@ -3,6 +3,21 @@
 # test framework
 source $(dirname "$0")/framework_test.sh
 
+define nestedfile <<- 'STDIN'
+	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+	<unit xmlns="http://www.srcML.org/srcML/src" revision="REVISION">
+
+	<unit xmlns:cpp="http://www.srcML.org/srcML/cpp" revision="REVISION" language="C++" filename="sub/a.cpp" hash="1a2c5d67e6f651ae10b7673c53e8c502c97316d6">
+	<expr_stmt><expr><name>a</name></expr>;</expr_stmt>
+	</unit>
+
+	<unit xmlns:cpp="http://www.srcML.org/srcML/cpp" revision="REVISION" language="C++" filename="sub/b.cpp" hash="aecf18b52d520ab280119febd8ff6c803135ddfc">
+	<expr_stmt><expr><name>b</name></expr>;</expr_stmt>
+	</unit>
+
+	</unit>
+	STDIN
+	
 # test invalid_input
 echo "a;" | srcml -l C++ --strip
 check_exit 3
