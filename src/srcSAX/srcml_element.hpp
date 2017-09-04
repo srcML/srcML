6774a685480c3aad24e3fc86b1522229e6a3465f
Michael L. Collard, Ph.D <collard@uakron.edu>
2017-09-04 17:20:48 -0400

    Remove unneeded parts of member init list

--- src/srcSAX/srcml_element.hpp
+++ src/srcSAX/srcml_element.hpp
@@ -115,10 +115,7 @@ struct srcml_element {
 
     /** Copy constructor */
     srcml_element(const srcml_element & element)
-        : context(element.context), localname(0), prefix(0), URI(0),
-          nb_namespaces(0), namespaces(0),
-          nb_attributes(0), nb_defaulted(0),
-          attributes(0) {
+        : context(element.context) {
 
         // save all the info in case this is not a srcML archive
         this->localname = element.localname ? (xmlChar*) STRDUP((const char*) element.localname) : 0;
