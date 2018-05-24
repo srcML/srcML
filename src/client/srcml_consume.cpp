ccbf91d5c44df74bc5d2d942ed9fb4865e563fd3
Michael L. Collard <collard@uakron.edu>
2018-05-23 20:02:32 -0400

    Remove use of non-needed temp

--- src/client/srcml_consume.cpp
+++ src/client/srcml_consume.cpp
@@ -58,6 +58,7 @@ void srcml_consume(ParseRequest* request, WriteQueue* write_queue) {
             if (request->disk_dir->back() != '/')
                 xml_filename += '/';
         }
+        // @todo Should this be .srcml?
         xml_filename += *request->filename + ".xml";
 
         srcml_archive_write_open_filename(request->srcml_arch, xml_filename.c_str(), 0);
@@ -66,21 +67,20 @@ void srcml_consume(ParseRequest* request, WriteQueue* write_queue) {
     std::string original_filename;
 
     // construct and parse the unit
-    srcml_unit* unit = request->unit;
     int status = SRCML_STATUS_OK;
     try {
 
         // create the unit start tag
-        if (!unit) {
-            if (!(unit = srcml_unit_create(request->srcml_arch))) {
+        if (!request->unit) {
+            if (!(request->unit = srcml_unit_create(request->srcml_arch))) {
                 throw SRCML_STATUS_ERROR;
             }
         }
 
         // language attribute, required if from memory
         // @todo if request has a language different from input, need to srcml->src->srcml 
-        if (srcml_unit_get_language(unit) == 0 || srcml_unit_get_language(unit)[0] == '\0')
-            if ((status = srcml_unit_set_language(unit, request->language.c_str())) != SRCML_STATUS_OK)
+        if (srcml_unit_get_language(request->unit) == 0 || srcml_unit_get_language(request->unit)[0] == '\0')
+            if ((status = srcml_unit_set_language(request->unit, request->language.c_str())) != SRCML_STATUS_OK)
                 throw status;
 
         // (optional) filename attribute
@@ -93,27 +93,27 @@ void srcml_consume(ParseRequest* request, WriteQueue* write_queue) {
                 request->filename->erase(0,1);
             }
             
-            if ((status = srcml_unit_set_filename(unit, request->filename->c_str())) != SRCML_STATUS_OK)
+            if ((status = srcml_unit_set_filename(request->unit, request->filename->c_str())) != SRCML_STATUS_OK)
                 throw status;
         }
 
         // (optional) version attribute
-        if (request->version && ((status = srcml_unit_set_version(unit, request->version->c_str())) != SRCML_STATUS_OK))
+        if (request->version && ((status = srcml_unit_set_version(request->unit, request->version->c_str())) != SRCML_STATUS_OK))
             throw status;
 
         // (optional) timestamp attribute
         if (request->time_stamp)
-            srcml_unit_set_timestamp(unit, request->time_stamp->c_str());
+            srcml_unit_set_timestamp(request->unit, request->time_stamp->c_str());
 
         // parse the buffer/file, timing as we go
         Timer parsetime;
 
         if (request->disk_filename) {
-            status = srcml_unit_parse_filename(unit, request->disk_filename->c_str());
+            status = srcml_unit_parse_filename(request->unit, request->disk_filename->c_str());
         }
         else if (!request->unit) {
 
-            status = srcml_unit_parse_memory(unit, &request->buffer.front(), request->buffer.size());
+            status = srcml_unit_parse_memory(request->unit, &request->buffer.front(), request->buffer.size());
         }
         if (status != SRCML_STATUS_OK) {
             request->status = status;
@@ -124,7 +124,7 @@ void srcml_consume(ParseRequest* request, WriteQueue* write_queue) {
 
         // perform any transformations and add them to the request
         srcml_unit** results = nullptr;
-        srcml_unit_apply_transforms(request->srcml_arch, unit, &results);
+        srcml_unit_apply_transforms(request->srcml_arch, request->unit, &results);
         if (results) {
             for (auto p = results; *p; ++p) {
                 request->units.push_back(*p);
@@ -134,13 +134,12 @@ void srcml_consume(ParseRequest* request, WriteQueue* write_queue) {
     } catch (...) {
 
         request->errormsg = "srcml: Unable to open file " + original_filename;
-        if (unit)
-            srcml_unit_free(unit);
-        unit = 0;
+        if (request->unit)
+            srcml_unit_free(request->unit);
+        request->unit = 0;
     }
 
     // schedule unit for output
-    request->unit = unit;
     request->status = status;
     write_queue->schedule(request);
 }
