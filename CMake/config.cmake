11350c573d32181c699e927fad8d90010db4f4bf
Michael L. Collard, Ph.D <collard@uakron.edu>
2018-06-25 10:14:34 -0400

    Fix for last commit of libarchive build on macOS

--- CMake/config.cmake
+++ CMake/config.cmake
@@ -106,10 +106,11 @@ else()
     if (APPLE AND EXISTS /usr/local/opt/libarchive)
         set(LibArchive_INCLUDE_DIR /usr/local/opt/libarchive/include)
         set(LibArchive_LIBRARIES /usr/local/opt/libarchive/lib/libarchive.dylib)
-    endif()
+    else()
+        find_package(LibArchive 3 REQUIRED)
+   endif()
 
     # Locating packages.e
-    find_package(LibArchive 3 REQUIRED)
     find_package(LibXml2 REQUIRED)
     find_package(CURL REQUIRED)
     find_package(Iconv REQUIRED)
