551e7e0cab4b30a98128793b05a7b17e205654e5
Michael L. Collard, Ph.D <collard@uakron.edu>
2018-06-25 21:51:25 -0400

    First attempt at libarchive file read

--- src/client/src_input_filesystem.cpp
+++ src/client/src_input_filesystem.cpp
@@ -26,14 +26,21 @@
 
 #include <src_input_libarchive.hpp>
 #include <src_input_filesystem.hpp>
+#if ARCHIVE_VERSION_NUMBER < 3000000
 #include <boost/filesystem.hpp>
+#endif
 #include <list>
+#include <deque>
 #include <vector>
+#include <archive.h>
+#include <archive_entry.h>
 
 int src_input_filesystem(ParseQueue& queue,
                           srcml_archive* srcml_arch,
                           const srcml_request_t& srcml_request,
                           const std::string& input) {
+
+#if ARCHIVE_VERSION_NUMBER < 3000000
 
     // list of directories with the input as the first one
     std::list<boost::filesystem::path> dirs(1, boost::filesystem::path(input));
@@ -73,6 +80,61 @@ int src_input_filesystem(ParseQueue& queue,
             }
         }
     }
+#else
+
+    // list of directories with the input as the first one
+    std::deque<std::string> dirs(1, input);
+
+    while (!dirs.empty()) {
+
+        // get a list of files (including directories) from the current directory
+        std::vector<std::string> files;
+
+        auto darchive = archive_read_disk_new();
+        archive_read_disk_open(darchive, dirs.front().c_str());
+        archive_read_disk_descend(darchive);
+
+        archive_entry* entry;
+        int status = ARCHIVE_OK;
+        int count = 0;
+        while ((status = archive_read_next_header(darchive, &entry)) == ARCHIVE_OK) {
+fprintf(stderr, "DEBUG:  archive_entry_pathname(entry): %s\n", archive_entry_pathname(entry));
+
+       //     if (archive_read_disk_can_descend(darchive)) {
+fprintf(stderr, "DEBUG:  %s %s %d \n", __FILE__,  __FUNCTION__, __LINE__);
+
+//                dirs.push_back(archive_entry_pathname(entry));
+
+//                continue;
+//            }
+
+            files.push_back(archive_entry_pathname(entry));
+        }
+fprintf(stderr, "DEBUG:  %s %s %d \n", __FILE__,  __FUNCTION__, __LINE__);
+
+        archive_read_close(darchive);
+
+        std::sort(files.begin(), files.end());
+
+        for (auto filename : files) {
+
+            //src_input_libarchive(queue, srcml_arch, srcml_request, file.string());
+            srcml_input_src input_file(filename);
+
+            // If a directory contains archives skip them
+            if (!(input_file.archives.empty())) {
+                input_file.skip = true;
+            }
+
+            src_input_libarchive(queue, srcml_arch, srcml_request, input_file);
+        }
+
+        dirs.pop_front();
+
+        std::sort(dirs.begin(), dirs.end());
+    }
+
+#endif
 
     return 1;
 }
