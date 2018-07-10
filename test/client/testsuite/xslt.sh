7caf7587654d76459ecdece77257d1c13c72643e
Michael L. Collard <collard@uakron.edu>
2018-07-10 13:25:57 -0400

    Add client test cases for xslt parameters

--- test/client/testsuite/xslt.sh
+++ test/client/testsuite/xslt.sh
@@ -32,10 +32,48 @@ define copyxslt <<- 'STDOUT'
 	</xsl:stylesheet>
 	STDOUT
 
+define setlanguage <<- 'STDOUT'
+	<xsl:stylesheet
+	xmlns="http://www.srcML.org/srcML/src"
+	xmlns:src="http://www.srcML.org/srcML/src"
+	xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+	xmlns:cpp="http://www.srcML.org/srcML/cpp"
+	version="1.0">
+
+	<!--
+	setlanguage.xsl
+
+	Identity transformation.
+
+	Michael L. Collard
+	collard@uakron.edu
+	-->
+
+	<xsl:output method="xml" version="1.0" encoding="UTF-8" standalone="yes"/>
+
+	<xsl:param name="language"/>
+
+	<xsl:template match="src:unit/@language"><xsl:attribute name="language"><xsl:value-of select="$language"/></xsl:attribute></xsl:template>
+
+	<xsl:template match="@*|node()"><xsl:copy><xsl:apply-templates select="@*|node()"/></xsl:copy></xsl:template>
+
+	</xsl:stylesheet>
+	STDOUT
+
+define srcml <<- 'STDOUT'
+	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+	<unit xmlns="http://www.srcML.org/srcML/src" xmlns:cpp="http://www.srcML.org/srcML/cpp" revision="REVISION" language="C++"/>
+	STDOUT
+
+define srcmljava <<- 'STDOUT'
+	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+	<unit xmlns="http://www.srcML.org/srcML/src" xmlns:cpp="http://www.srcML.org/srcML/cpp" revision="REVISION" language="Java"/>
+	STDOUT
+
 xmlcheck "$srcml"
 createfile sub/a.cpp.xml "$srcml"
 createfile copy.xsl "$copyxslt"
-
+createfile setlanguage.xsl "$setlanguage"
 
 # xslt copy xpathparam NAME=VALUE
 srcml --xslt=copy.xsl --xpathparam 'NAME=VALUE' sub/a.cpp.xml
@@ -69,4 +107,21 @@ check sub/b.cpp.xml "$srcml"
 
 srcml --xslt=copy.xsl --xpathparam 'name="a"' -o sub/b.cpp.xml < sub/a.cpp.xml
 check sub/b.cpp.xml "$srcml"
+
+
+srcml --xslt=setlanguage.xsl --xpathparam 'language="Java"' sub/a.cpp.xml
+check "$srcmljava"
+
+srcml --xslt=setlanguage.xsl --xpathparam 'language="Java"' < sub/a.cpp.xml
+check "$srcmljava"
+
+srcml --xslt=setlanguage.xsl --xpathparam 'language="Java"' sub/a.cpp.xml -o sub/b.java.xml
+check sub/b.java.xml "$srcmljava"
+
+srcml --xslt=setlanguage.xsl --xpathparam 'language="Java"' -o sub/b.java.xml sub/a.cpp.xml
+check sub/b.java.xml "$srcmljava"
+
+srcml --xslt=setlanguage.xsl --xpathparam 'language="Java"' -o sub/b.java.xml < sub/a.cpp.xml
+check sub/b.java.xml "$srcmljava"
+
 
