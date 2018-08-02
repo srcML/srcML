8b0a11002010fd556774beef0e8b954169bc7903
Michael L. Collard <collard@uakron.edu>
2018-08-02 12:10:40 -0400

    Move to libarchive for creating output directories

--- src/client/src_output_filesystem.cpp
+++ src/client/src_output_filesystem.cpp
@@ -23,7 +23,40 @@
 #include <src_output_filesystem.hpp>
 #include <srcml.h>
 #include <iostream>
-#include <boost/filesystem.hpp>
+#include <archive.h>
+#include <archive_entry.h>
+
+static __LA_MODE_T curperm() {
+
+    // permissions of current directory
+    auto darchive = archive_read_disk_new();
+    archive_read_disk_set_behavior(darchive, ARCHIVE_READDISK_NO_ACL | ARCHIVE_READDISK_NO_XATTR | ARCHIVE_READDISK_NO_FFLAGS);
+    archive_read_disk_open(darchive, ".");
+    archive_entry* dentry = nullptr;
+    archive_read_next_header(darchive, &dentry);
+    auto perm = archive_entry_perm(dentry);
+//    archive_read_free(darchive);
+//    archive_entry_free(dentry);
+
+    return perm;
+}
+
+static void ourmkdir(const std::string& path, __LA_MODE_T perm) {
+
+    // assume path is directory name (don't split yet)
+    std::string curdirname = path;
+
+    // create new directory
+    auto a = archive_write_disk_new();
+    auto entry = archive_entry_new();
+    archive_entry_set_pathname(entry, curdirname.c_str());
+    archive_entry_set_filetype(entry, AE_IFDIR);
+    archive_entry_set_perm(entry, perm);
+    archive_write_header(a, entry);
+    archive_write_finish_entry(a);
+    archive_write_free(a);
+    archive_entry_free(entry);
+}
 
 void src_output_filesystem(srcml_archive* srcml_arch, const std::string& output_dir, TraceLog& log) {
 
@@ -32,22 +65,38 @@ void src_output_filesystem(srcml_archive* srcml_arch, const std::string& output_
     if (output_dir != "." && output_dir != "./")
         prefix = output_dir;
 
+    auto perm = curperm();
+
     int count = 0;
     while (srcml_unit* unit = srcml_archive_read_unit(srcml_arch)) {
 
-        // construct the relative directory
-        boost::filesystem::path out(prefix);
-        if (const char* filename = srcml_unit_get_filename(unit))
-            out /= filename;
+        const char* cfilename = srcml_unit_get_filename(unit);
+        // @todo What do we do with no filename?
+        if (!cfilename)
+            continue;
 
-        // create the path
-        if (out.has_parent_path() && !is_directory(out.parent_path()))
-            boost::filesystem::create_directories(out.parent_path());
+        std::string filename = cfilename;
+
+        // separate pathname from filename
+        std::string path = filename.rfind('/') != std::string::npos ? filename.substr(0, filename.rfind('/')) : "";
+
+        // construct the relative directory
+        std::string fullpath = prefix;
+        if (!path.empty()) {
+            fullpath += '/';
+            fullpath += path;
+        }
+
+        ourmkdir(fullpath, perm);
+
+        std::string fullfilename = prefix;
+        fullfilename += "/";
+        fullfilename += filename;
 
         // unparse directory to filename
-        log << ++count << out.string();
+        log << ++count << fullfilename;
 
-        srcml_unit_unparse_filename(unit, (const char *)out.c_str());
+        srcml_unit_unparse_filename(unit, fullfilename.c_str());
 
         srcml_unit_free(unit);
     }
