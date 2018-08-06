1359c908fd62c5218f131831848f99fcf083746d
Michael L. Collard <collard@uakron.edu>
2018-08-06 14:58:37 -0400

    Rename Cmake test build option from BUILD_UNIT_TESTS to BUILD_LIBSRCML_TESTS

--- CMake/config.cmake
+++ CMake/config.cmake
@@ -51,9 +51,9 @@ if(NOT DYNAMIC_LOAD_ENABLED)
 endif()
 
 # Turn ON/OFF tests
-option(BUILD_PARSER_TESTS "Include tests for parser" ON)
 option(BUILD_CLIENT_TESTS "Build srcml client tests" ON)
-option(BUILD_UNIT_TESTS "Build unit tests for libsrcml" OFF)
+option(BUILD_LIBSRCML_TESTS "Build unit tests for libsrcml" OFF)
+option(BUILD_PARSER_TESTS "Include tests for parser" ON)
 
 option(BUILD_EXAMPLES "Build examples usage files for libsrcml" OFF)
 option(BUILD_PYTHON_BINDINGS "Build Python language bindings/wrapper" OFF)
