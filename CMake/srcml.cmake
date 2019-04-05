a939dee4d7ad33787fd4783cfb80fe7c885f62a7
Michael L. Collard, Ph.D <collard@uakron.edu>
2019-04-05 15:36:19 -0400

    Move the last out of config.cmake and into src/CMakeLists.txt, and get rid of that file

--- CMake/srcml.cmake
+++ CMake/srcml.cmake
@@ -17,7 +17,6 @@
 # along with the srcML Toolkit; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-include (config)
 include (packageConfig)
 include (doc)
 include (install)
