2de798d5bd4689bf899fd3a5b857f59d2ebcf91f
Michael L. Collard, Ph.D <collard@uakron.edu>
2019-04-09 17:06:40 -0400

    Change for experiments with cdash

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -1,11 +1,3 @@
-## This file should be placed in the root directory of your project.
-## Then modify the CMakeLists.txt file in the root directory of your
-## project to incorporate the testing dashboard.
-##
-## # The following are required to submit to the CDash dashboard:
-##   ENABLE_TESTING()
-##   INCLUDE(CTest)
-
 set(CTEST_PROJECT_NAME "srcML")
 set(CTEST_NIGHTLY_START_TIME "01:00:00 UTC")
 
@@ -14,3 +6,6 @@ set(CTEST_DROP_SITE "localhost:8080")
 set(CTEST_DROP_LOCATION "/submit.php?project=srcML")
 set(CTEST_DROP_SITE_CDASH TRUE)
 
+set(CTEST_PROJECT_SUBPROJECTS
+develop
+)
