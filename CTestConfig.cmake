727e86b85f89406ec4092b8c28da03408ff50692
Michael L. Collard, Ph.D <collard@uakron.edu>
2019-04-09 14:54:26 -0400

    Change for experiments with cdash

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -10,8 +10,7 @@ set(CTEST_PROJECT_NAME "srcML")
 set(CTEST_NIGHTLY_START_TIME "01:00:00 UTC")
 
 set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "localhost:8080")
-set(CTEST_SUBMIT_URL "localhost:8080")
+set(CTEST_DROP_SITE "localhost")
 set(CTEST_DROP_LOCATION "/submit.php?project=srcML")
 set(CTEST_DROP_SITE_CDASH TRUE)
-set(CTEST_PROJECT_SUBPROJECTS develop)
+
