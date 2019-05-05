02c1b50fc7f9a9cb8e5970522a24019c5feaf87b
Michael L. Collard, Ph.D <collard@uakron.edu>
2019-05-04 23:02:07 -0400

    Fix output format

--- test/parser/runtest.cpp
+++ test/parser/runtest.cpp
@@ -11,10 +11,12 @@
 #include <list>
 #include <deque>
 #include <vector>
+#include <unordered_map>
+#include <map>
 #include <archive.h>
 #include <archive_entry.h>
 
-const int FIELD_WIDTH_LANGUAGE = 15;
+const int FIELD_WIDTH_LANGUAGE = 12;
 const int FIELD_WIDTH_URL = 30;
 
 int main(int argc, char* argv[]) {
@@ -59,83 +61,88 @@ int main(int argc, char* argv[]) {
 
     std::sort(files.begin(), files.end());
 
-    std::cerr << "FILES: " << files[1] << '\n';
+    std::map<std::string, int> ltotal;
+    std::map<std::string, int> misses;
 
-    srcml_archive* outarchive = srcml_archive_create();
-    char* outbuffer = 0;
-    size_t outsize = 0;
-    srcml_archive_write_open_memory(outarchive, &outbuffer, &outsize);
-    srcml_unit* outunit = srcml_unit_create(outarchive);
     int line_count = 0;
-    for (auto& filename : files)
-    {
+    int total = 0;
+    int failed = 0;
+    for (auto& filename : files) {
+
+        bool found = true;
         srcml_archive* archive = srcml_archive_create();
         srcml_archive_read_open_filename(archive, filename.c_str());
         bool first = true;
         int count = 0;
-        std::string url = srcml_archive_get_url(archive);
+        std::string url = srcml_archive_get_url(archive) ? srcml_archive_get_url(archive) : "";
+        std::string unit_language;
         while (srcml_unit* unit = srcml_archive_read_unit(archive)) {
 
             if (first) {
-                std::string unit_language = srcml_unit_get_language(unit);
-                if (!language.empty() && language != unit_language)
+                unit_language = srcml_unit_get_language(unit);
+                if (!language.empty() && language != unit_language) {
+                    found = false;
                     break;
+                }
                 std::cout << std::setw(FIELD_WIDTH_LANGUAGE) << std::left << srcml_unit_get_language(unit);
                 std::cout << std::setw(FIELD_WIDTH_URL) << std::left << url;
                 line_count = 0;
             }
             first = false;
             ++count;
+            ++total;
+
+            ++ltotal[unit_language];
 
             // get the src
             char* buffer = 0;
             size_t size = 0;
+            srcml_unit_set_src_encoding(unit, "UTF-8");
             srcml_unit_unparse_memory(unit, &buffer, &size);
-            std::string s(buffer, size);
-            //fprintf(stderr, "DEBUG:  %s %s %d s: %s\n", __FILE__,  __FUNCTION__, __LINE__,  s.c_str());
 
             // get the srcml
-            std::string xml = srcml_unit_get_srcml(unit);
+            std::string xml = srcml_unit_get_srcml_raw(unit);
 
-            //fprintf(stderr, "DEBUG:  %s %s %d s: %s\n", __FILE__,  __FUNCTION__, __LINE__,  s.c_str());
-            //fprintf(stderr, "DEBUG:  %s %s %d xml: %s\n", __FILE__,  __FUNCTION__, __LINE__,  xml.c_str());
-
-            // parse the source
+            srcml_unit* outunit = srcml_unit_clone(unit);
             srcml_unit_set_language(outunit, srcml_unit_get_language(unit));
-            int n = srcml_unit_parse_memory(outunit, buffer, size);
-            //fprintf(stderr, "DEBUG:  %s %s %d n: %d\n", __FILE__,  __FUNCTION__, __LINE__,  (int) n);
 
-            std::string sout = srcml_unit_get_srcml(outunit);
-            //fprintf(stderr, "DEBUG:  %s %s %d sout: %s\n", __FILE__,  __FUNCTION__, __LINE__, sout);
+            srcml_unit_parse_memory(outunit, buffer, size);
+
+            std::string sout = srcml_unit_get_srcml_raw(outunit);
 
             if (line_count >= 75) {
                 std::cout << '\n' << std::setw(FIELD_WIDTH_LANGUAGE) << " " << std::setw(FIELD_WIDTH_URL) << std::left << "...";
                 line_count = 0;
             }
 
-            if (count > 99)
-                line_count += 3;
-            else if (count > 9)
-                line_count += 2;
-            else
-                line_count += 1;
+            line_count += std::to_string(count).size() + 1;
 
             if (sout == xml) {
-                std::cout << "\033[0;31m" << count << "\033[0m";
-            } else {
                 std::cout << "\033[0;33m" << count << "\033[0m";
+            } else {
+                ++failed;
+                ++misses[unit_language];
+                std::cout << "\033[0;31m" << count << "\033[0m";
             }
             std::cout << " ";
 
+            free(buffer);
+            srcml_unit_free(outunit);
             srcml_unit_free(unit);
         }
-        std::cout << '\n';
+        if (found)
+            std::cout << '\n';
 
         srcml_archive_close(archive);
-
     }
 
-    srcml_archive_close(outarchive);
+    double percent = double(failed * 100) / total;
+    std::cout << "\nErrors: " << std::setw(FIELD_WIDTH_LANGUAGE) << std::left << "Total" << std::setw(6) << std::right << failed << std::setw(6) << std::right << total << '\t' << std::setprecision(2) << percent << "%" << '\n';
+
+    for (auto& kv : misses) {
+        double percent = double(kv.second * 100) / ltotal[kv.first];
+        std::cout << "        " << std::setw(FIELD_WIDTH_LANGUAGE) << std::left << kv.first << std::setw(6) << std::right << kv.second << std::setw(6) << std::right << ltotal[kv.first] << '\t' << std::setprecision(2) << percent << "%" << '\n';
+    }
 
     return 0;
 }
