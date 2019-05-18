743455c7efe5fe24fd15f621a8401b79a6daff40
Michael L. Collard, Ph.D <collard@uakron.edu>
2019-05-17 23:10:24 -0400

    Change error code in test cases to be consistent

--- test/client/testsuite/tabs_fail.sh
+++ test/client/testsuite/tabs_fail.sh
@@ -22,7 +22,7 @@ createfile sub/a.cpp ""
 # immediately following '--tabs' must be either a number or another option,
 # or boost won't know whether the argument is an input file or argument
 srcml --tabs sub/a.cpp
-check_exit 7
+check_exit 4
 
 srcml --tabs sub/a.cpp -o sub/a.cpp.xml
-check_exit 7
+check_exit 4
