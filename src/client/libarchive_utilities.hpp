27256ddd5c48108140bec8cde418ae9c87c20470
Michael L. Collard, Ph.D <collard@uakron.edu>
2019-05-25 22:50:29 -0400

    Fix typo in #ifndef/#define

--- src/client/libarchive_utilities.hpp
+++ src/client/libarchive_utilities.hpp
@@ -18,8 +18,8 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#ifndef INCLUDED_LIBARCHIVE_UTILTIES_HPP
-#define INCLUDED_LIBARCHIVE_UTILTIES_HPP
+#ifndef INCLUDED_LIBARCHIVE_UTILITIES_HPP
+#define INCLUDED_LIBARCHIVE_UTILITIES_HPP
 
 #include <archive.h>
 #include <archive_entry.h>
