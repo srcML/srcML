ea3ceaafca62135cd6daea5c32a4a12fecbe56ad
Michael L. Collard, Ph.D <collard@uakron.edu>
2019-06-03 11:19:31 -0400

    Remove use of try-catch in srcml_consume()

--- src/client/srcml_consume.cpp
+++ src/client/srcml_consume.cpp
@@ -37,13 +37,12 @@
 void srcml_consume(int /* n */, std::shared_ptr<ParseRequest> request, WriteQueue* write_queue) {
 
     // error passthrough to output for proper output in trace
-    if (/* request->input_archive || */ request->status) {
-        request->unit = 0;
+    if (request->status) {
+        request->unit.reset(0);
         write_queue->schedule(request);
         return;
     }
 
-    // NOTE: thread task cannot throw exception
     if (option(SRCML_COMMAND_NOARCHIVE)) {
 
         srcml_archive_enable_solitary_unit(request->srcml_arch);
@@ -66,77 +65,82 @@ void srcml_consume(int /* n */, std::shared_ptr<ParseRequest> request, WriteQueu
     std::string original_filename;
 
     // construct and parse the unit
-    int status = SRCML_STATUS_OK;
-    try {
+    request->status = SRCML_STATUS_OK;
 
-        // create the unit start tag
+    // create the unit if needed
+    if (!request->unit) {
+        request->unit.reset(srcml_unit_create(request->srcml_arch));
         if (!request->unit) {
-            request->unit.reset(srcml_unit_create(request->srcml_arch));
-            if (!request->unit) {
-                throw SRCML_STATUS_ERROR;
-            }
+            request->status = SRCML_STATUS_ERROR;
+            write_queue->schedule(request);
+            return;
         }
+    }
 
-        // language attribute, required if from memory
-        // @todo if request has a language different from input, need to srcml->src->srcml 
-        if (srcml_unit_get_language(request->unit.get()) == 0 || srcml_unit_get_language(request->unit.get())[0] == '\0')
-            if ((status = srcml_unit_set_language(request->unit.get(), request->language.c_str())) != SRCML_STATUS_OK)
-                throw status;
-
-        // (optional) filename attribute
-        if (request->filename) {
-
-            original_filename = *request->filename;
-            
-            // Cleanup filename
-            auto it = request->filename->begin();
-            while (*it == '.' && std::next(it) != request->filename->end() && *std::next(it) == '/') {
-                request->filename->erase(it, std::next(std::next(it)));
-                it = request->filename->begin();
-            }
-            
-            if ((status = srcml_unit_set_filename(request->unit.get(), request->filename->c_str())) != SRCML_STATUS_OK)
-                throw status;
-        }
-
-        // (optional) version attribute
-        if (request->version && ((status = srcml_unit_set_version(request->unit.get(), request->version->c_str())) != SRCML_STATUS_OK))
-            throw status;
-
-        // (optional) timestamp attribute
-        if (request->time_stamp)
-            srcml_unit_set_timestamp(request->unit.get(), request->time_stamp->c_str());
-
-        // parse the buffer/file, timing as we go
-        Timer parsetime;
-
-        if (request->disk_filename) {
-            status = srcml_unit_parse_filename(request->unit.get(), request->disk_filename->c_str());
-        }
-        else if (request->needsparsing) {
-            status = srcml_unit_parse_memory(request->unit.get(), request->buffer.data(), request->buffer.size());
-        }
-        if (status != SRCML_STATUS_OK) {
-            request->status = status;
-            throw status;
-        }
-
-        request->runtime = parsetime.cpu_time_elapsed();
-        request->results.type = SRCML_RESULTS_UNITS;
-
-        // perform any transformations and add them to the request
-        srcml_unit_apply_transforms(request->srcml_arch, request->unit.get(), &(request->results));
-        if (request->results.type == SRCML_RESULTS_NONE) {
+    // language attribute, required if from memory
+    // @todo if request has a language different from input, need to srcml->src->srcml 
+    if (srcml_unit_get_language(request->unit.get()) == 0 || srcml_unit_get_language(request->unit.get())[0] == '\0')
+        if ((request->status = srcml_unit_set_language(request->unit.get(), request->language.c_str())) != SRCML_STATUS_OK) {
             request->unit.reset();
+            write_queue->schedule(request);
+            return;
         }
 
-    } catch (...) {
+    // (optional) filename attribute
+    if (request->filename) {
 
+        original_filename = *request->filename;
+        
+        // Cleanup filename
+        auto it = request->filename->begin();
+        while (*it == '.' && std::next(it) != request->filename->end() && *std::next(it) == '/') {
+            request->filename->erase(it, std::next(std::next(it)));
+            it = request->filename->begin();
+        }
+        
+        if ((request->status = srcml_unit_set_filename(request->unit.get(), request->filename->c_str())) != SRCML_STATUS_OK) {
+            request->unit.reset();
+            write_queue->schedule(request);
+            return;
+        }
+    }
+
+    // (optional) version attribute
+    if (request->version && ((request->status = srcml_unit_set_version(request->unit.get(), request->version->c_str())) != SRCML_STATUS_OK)) {
+        request->unit.reset();
+        write_queue->schedule(request);
+        return;
+    }
+
+    // (optional) timestamp attribute
+    if (request->time_stamp)
+        srcml_unit_set_timestamp(request->unit.get(), request->time_stamp->c_str());
+
+    // parse the buffer/file, timing as we go
+    Timer parsetime;
+
+    if (request->disk_filename) {
+        request->status = srcml_unit_parse_filename(request->unit.get(), request->disk_filename->c_str());
+    }
+    else if (request->needsparsing) {
+        request->status = srcml_unit_parse_memory(request->unit.get(), request->buffer.data(), request->buffer.size());
+    }
+    if (request->status != SRCML_STATUS_OK) {
         request->errormsg = "srcml: Unable to open file " + original_filename;
+        request->unit.reset();
+        write_queue->schedule(request);
+        return;
+    }
+
+    request->runtime = parsetime.cpu_time_elapsed();
+    request->results.type = SRCML_RESULTS_UNITS;
+
+    // perform any transformations and add them to the request
+    srcml_unit_apply_transforms(request->srcml_arch, request->unit.get(), &(request->results));
+    if (request->results.type == SRCML_RESULTS_NONE) {
         request->unit.reset();
     }
 
     // schedule unit for output
-    request->status = status;
     write_queue->schedule(request);
 }
