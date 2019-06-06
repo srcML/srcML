def0b10f67b00ad2f030f2fa0b7b5907c3f9601b
Michael L. Collard, Ph.D <collard@uakron.edu>
2019-06-06 07:31:23 -0400

    Change FSF address to header copyright to recent address

--- src/client/srcml_consume.cpp
+++ src/client/srcml_consume.cpp
@@ -17,7 +17,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with the srcml command-line client; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  *
  * Thread-designed function to parse and create a unit.
  * Output is scheduled for writing on the write queue
