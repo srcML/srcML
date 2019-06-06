6a2d0e0e946ed96ed7b52d86fff2812d674a8896
Michael L. Collard, Ph.D <collard@uakron.edu>
2019-06-05 22:08:25 -0400

    Change packaging to multiple components for .rpm and .deb

--- package/debian.cmake
+++ package/debian.cmake
@@ -1,7 +1,7 @@
 ##
 # @file debian.cmake
 #
-# @copyright Copyright (C) 2013-2014 srcML, LLC. (www.srcML.org)
+# @copyright Copyright (C) 2013-2019 srcML, LLC. (www.srcML.org)
 # 
 # The srcML Toolkit is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -17,30 +17,70 @@
 # along with the srcML Toolkit; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
-# Debian package name
-set(CPACK_DEBIAN_PACKAGE_NAME "srcML")
+# Turn ON/OFF internal CPackDeb debugging
+set(CPACK_DEBIAN_PACKAGE_DEBUG OFF)
 
-set(CPACK_DEBIAN_PACKAGE_HOMEPAGE https://srcMl.org)
+# Generate user and development rpms
+# Note: Yes, this is CPACK_DEB_, not CPACK_DEBIAN
+set(CPACK_DEB_COMPONENT_INSTALL ON)
 
-# architecture
-# if(CMAKE_SYSTEM_PROCESSOR STREQUAL "i686")
-#     set(CPACK_DEBIAN_PACKAGE_ARCHITECTURE "i386")
-# else()
-#     set(CPACK_DEBIAN_PACKAGE_ARCHITECTURE "amd64")
-# endif()
+# Debian package names
+# Package names should be lowercase
+set(CPACK_DEBIAN_CLIENT_PACKAGE_NAME "srcml")
+set(CPACK_DEBIAN_DEVELOPMENT_PACKAGE_NAME "srcml-dev")
 
-# autogenerate dependency information
-set(CPACK_DEBIAN_PACKAGE_DEBUG ON)
+# Package release number (NOT srcml or libsrcml release)
+# Note: Update when package is updated, but not contents
+set(CPACK_DEBIAN_PACKAGE_RELEASE "${SRCML_PACKAGE_RELEASE}")
+
+# Package filenames
+set(CPACK_DEBIAN_CLIENT_FILE_NAME DEB-DEFAULT)
+set(CPACK_DEBIAN_DEVELOPMENT_FILE_NAME DEB-DEFAULT)
+set(CPACK_ARCHIVE_CLIENT_FILE_NAME "${CPACK_DEBIAN_CLIENT_PACKAGE_NAME}_${PROJECT_VERSION}-${CPACK_DEBIAN_PACKAGE_RELEASE}")
+set(CPACK_ARCHIVE_DEVELOPMENT_FILE_NAME "${CPACK_DEBIAN_DEVELOPMENT_PACKAGE_NAME}_${PROJECT_VERSION}-${CPACK_DEBIAN_PACKAGE_RELEASE}")
+
+# Package types
+# Current CPack defaults
+# Note: unclear whether CPACK_DEBIAN_PACKAGE_SECTION is default for components
+set(CPACK_DEBIAN_CLIENT_PACKAGE_SECTION "devel")
+set(CPACK_DEBIAN_DEVELOPMENT_PACKAGE_SECTION "devel")
+
+# Package priorities
+# Current CPack defaults
+# Note: unclear whether CPACK_DEBIAN_PACKAGE_PRIORITY is default for components
+set(CPACK_DEBIAN_CLIENT_PACKAGE_PRIORITY "optional")
+set(CPACK_DEBIAN_DEVELOPMENT_PACKAGE_PRIORITY "optional")
+
+# Copyright file - required by lintian
+# @todo create real copyright file
+# install(FILES ${CMAKE_SOURCE_DIR}/COPYING.txt DESTINATION share/doc/srcml RENAME copyright COMPONENT CLIENT)
+# install(FILES ${CMAKE_SOURCE_DIR}/COPYING.txt DESTINATION share/doc/srcml RENAME copyright COMPONENT DEVELOPMENT)
+
+# Changelog file - required by lintian
+# file(WRITE ${CMAKE_BINARY_DIR}/changelog.Debian
+# "srcml (1.0.0-1) UNRELEASED; urgency=medium
+
+#  * Initial release
+
+# -- Michael L. Collard <collard@uakron.edu>  Mon, 10 Jun 2109 15:48:00 +0400
+# ")
+# execute_process(COMMAND gzip -n -9 -f ${CMAKE_BINARY_DIR}/changelog.Debian)
+# install(FILES ${CMAKE_BINARY_DIR}/changelog.Debian.gz DESTINATION share/doc/srcml COMPONENT CLIENT)
+# install(FILES ${CMAKE_BINARY_DIR}/changelog.Debian.gz DESTINATION share/doc/srcml COMPONENT DEVELOPMENT)
+
+# Autogenerate dependency information
 set(CPACK_DEBIAN_PACKAGE_SHLIBDEPS ON)
+set(CPACK_DEBIAN_PACKAGE_GENERATE_SHLIBS ON)
+set(CPACK_DEBIAN_PACKAGE_GENERATE_SHLIBS_POLICY ">=")
 
-# package type
-set(CPACK_DEBIAN_PACKAGE_SECTION "devel")
+# Dependency for srcml-dev package on srcml package
+set(CPACK_DEBIAN_DEVELOPMENT_PACKAGE_DEPENDS "srcml (>= ${PROJECT_VERSION})")
 
-# package priority
-set(CPACK_DEBIAN_PACKAGE_PRIORITY "optional")
-
-# recommended package
+# Recommended packages
+# Shared between packages srcml and srcml-dev
 set(CPACK_DEBIAN_PACKAGE_RECOMMENDS "libxslt, zip, unzip, cpio, tar, man")
 
-# package suggests
-#set(CPACK_DEBIAN_PACKAGE_SUGGESTS "yes")
+# Trigger required for library installed in client to initiate ldconfig
+set(TRIGGERS_FILE "${CMAKE_CURRENT_BINARY_DIR}/triggers")
+file(WRITE "${TRIGGERS_FILE}" "activate-noawait ldconfig\n")
+set(CPACK_DEBIAN_CLIENT_PACKAGE_CONTROL_EXTRA "${TRIGGERS_FILE}")
--- package/rpm.cmake
+++ package/rpm.cmake
@@ -1,7 +1,7 @@
 ##
 # @file rpm.cmake
 #
-# @copyright Copyright (C) 2013-2014 srcML, LLC. (www.srcML.org)
+# @copyright Copyright (C) 2013-2019 srcML, LLC. (www.srcML.org)
 # 
 # The srcML Toolkit is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -17,16 +17,62 @@
 # along with the srcML Toolkit; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
-# RPM package name
-set(CPACK_RPM_PACKAGE_NAME "srcML")
+# Turn ON/OFF internal CPackRPM debugging
+set(CPACK_RPM_PACKAGE_DEBUG ON)
 
-# package release number (NOT srcML release)
-set(CPACK_RPM_PACKAGE_RELEASE 1)
+# Generate user and development rpms
+set(CPACK_RPM_COMPONENT_INSTALL ON)
 
-# post-install command for ldconfig
-set(CPACK_RPM_PACKAGE_REQUIRES_POSTUN "ldconfig")
+# Package release number (NOT srcml or libsrcml release)
+# Note: Update when package is updated, but not contents
+set(CPACK_RPM_PACKAGE_RELEASE "${SRCML_PACKAGE_RELEASE}")
 
-# cpack puts directories leading to the installed files into the RPM. Since they are already
-# created, this leads to a conflict with other packages. This excludes these (which are mostly documentation)
-# Note that there is no problem with this list including directories that are not needed
-set(CPACK_RPM_EXCLUDE_FROM_AUTO_FILELIST_ADDITION ${CMAKE_INSTALL_MANDIR}/man1 ${CMAKE_INSTALL_MANDIR} /usr/local /usr/local/man /usr/local/man/man1 /usr/local/share /usr/local/share/man /usr/local/share/man/man1 /usr/share/man /usr/share/man/man1 /usr/share/man1)
+# Package names
+set(CPACK_RPM_CLIENT_PACKAGE_NAME "srcml")
+set(CPACK_RPM_DEVELOPMENT_PACKAGE_NAME "srcml-devel")
+set(CPACK_ARCHIVE_CLIENT_FILE_NAME "${CPACK_RPM_CLIENT_PACKAGE_NAME}_${PROJECT_VERSION}-${CPACK_RPM_PACKAGE_RELEASE}")
+set(CPACK_ARCHIVE_DEVELOPMENT_FILE_NAME "${CPACK_RPM_DEVELOPMENT_PACKAGE_NAME}_${PROJECT_VERSION}-${CPACK_RPM_PACKAGE_RELEASE}")
+
+# CLIENT package is main, so no extension is added
+set(CPACK_RPM_MAIN_COMPONENT CLIENT)
+
+# srcml-devel package requires srcml package
+set(CPACK_RPM_DEVELOPMENT_PACKAGE_REQUIRES "srcml >= ${PROJECT_VERSION}")
+
+# Homepage URLs
+# Note: Default should be CMAKE_PROJECT_HOMEPAGE_URL, but isn't due to components
+set(CPACK_RPM_CLIENT_PACKAGE_URL ${CMAKE_PROJECT_HOMEPAGE_URL})
+set(CPACK_RPM_DEVELOPMENT_PACKAGE_URL ${CMAKE_PROJECT_HOMEPAGE_URL})
+
+# Package groups
+# @todo Perhaps both packages should be in "Development/Tools"
+set(CPACK_RPM_CLIENT_PACKAGE_GROUP "Development/Tools")
+set(CPACK_RPM_DEVELOPMENT_PACKAGE_GROUP "Development/Libraries")
+
+# Package filenames
+set(CPACK_RPM_CLIENT_FILE_NAME RPM-DEFAULT)
+set(CPACK_RPM_DEVELOPMENT_FILE_NAME RPM-DEFAULT)
+
+# Adds to the release the distribution
+set(CPACK_RPM_PACKAGE_RELEASE_DIST ON)
+
+# Required changelog
+set(CPACK_RPM_CHANGELOG_FILE "${CMAKE_CURRENT_SOURCE_DIR}/ChangeLog.txt")
+
+# License
+set(CPACK_RPM_PACKAGE_LICENSE "GPLv2+")
+
+# post install script for ldconfig
+# Note: Believe that newline is required
+file(WRITE ${CPACK_PACKAGE_DIRECTORY}/post.sh "/sbin/ldconfig\n")
+set(CPACK_RPM_POST_INSTALL_SCRIPT_FILE   ${CPACK_PACKAGE_DIRECTORY}/post.sh)
+set(CPACK_RPM_POST_UNINSTALL_SCRIPT_FILE ${CPACK_PACKAGE_DIRECTORY}/post.sh)
+
+# CPack puts directories of installed files into the RPM
+# Since they already exist, this is a conflict with other packages
+# This excludes the following, mostly for documentation
+# Note: There is no issue with this list including directories that are not needed
+set(CPACK_RPM_EXCLUDE_FROM_AUTO_FILELIST_ADDITION ${CMAKE_INSTALL_MANDIR}/man1 ${CMAKE_INSTALL_MANDIR}
+		/usr/local /usr/local/include /usr/local/lib64 /usr/local/bin /usr/local/man /usr/local/man/man1 
+		/usr/local/share /usr/local/share/man /usr/local/share/man/man1 /usr/share/man /usr/share/man/man1
+		/usr/share/man1)
