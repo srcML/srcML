fce1cde39eae64b91cf56d1a0ef9cefaf891ef3d
Michael L. Collard, Ph.D <collard@uakron.edu>
2019-06-12 10:00:58 -0400

    Add background image to macos installer

--- package/macos.cmake
+++ package/macos.cmake
@@ -18,18 +18,7 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
 # Custom template CPack.distribution.dist.in
-file(WRITE ${CMAKE_BINARY_DIR}/pkg_resources/CPack.distribution.dist.in
-'<?xml version="1.0" encoding="UTF-8"?>
-<installer-gui-script minSpecVersion="1.0">
-    <title>@CPACK_PACKAGE_NAME@</title>
-    <welcome file="@CPACK_RESOURCE_FILE_WELCOME_NOPATH@"/>
-    <license file="@CPACK_RESOURCE_FILE_LICENSE_NOPATH@"/>
-    <background file="BACKGROUND.png"/>
-    <options allow-external-scripts="no" customize="allow" rootVolumeOnly="false"></options>
-    @CPACK_PACKAGEMAKER_CHOICES@
-</installer-gui-script>
-')
-set(CMAKE_MODULE_PATH "${CMAKE_BINARY_DIR}/pkg_resources")
+set(CMAKE_MODULE_PATH "${CMAKE_SOURCE_DIR}/package")
 
 set(CPACK_COMPONENTS_ONE_PACKAGE_PER_GROUP ON)
 
@@ -40,4 +29,4 @@ set(CPACK_RESOURCE_FILE_LICENSE ${CMAKE_SOURCE_DIR}/COPYING.txt)
 configure_file(${CMAKE_SOURCE_DIR}/README.md ${CMAKE_BINARY_DIR}/README.txt COPYONLY)
 set(CPACK_RESOURCE_FILE_README ${CMAKE_BINARY_DIR}/README.txt)
 configure_file(${CMAKE_SOURCE_DIR}/package/background.png ${CMAKE_BINARY_DIR}/pkg_resources/BACKGROUND.png COPYONLY)
-set(CPACK_PRODUCTBUILD_RESOURCE_DIR ${CMAKE_BINARY_DIR}/pkg_resources)
+set(CPACK_PRODUCTBUILD_RESOURCES_DIR ${CMAKE_BINARY_DIR}/pkg_resources)
