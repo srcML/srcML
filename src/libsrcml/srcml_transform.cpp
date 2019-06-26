7f62bf6c524190f114f2011af62788718e62d594
Michael L. Collard, Ph.D <collard@uakron.edu>
2019-06-26 11:44:03 -0400

    Fix xpath results for text, comment, etc. that still had cpp namespaces

--- src/libsrcml/srcml_transform.cpp
+++ src/libsrcml/srcml_transform.cpp
@@ -598,6 +598,22 @@ int srcml_unit_apply_transforms(struct srcml_archive* archive, struct srcml_unit
             nunit->hash = boost::none;
         }
 
+        doc->children = fullresults->nodeTab[i];
+
+        if (!nunit->namespaces)
+            nunit->namespaces = starting_namespaces;
+
+        // mark unused cpp and omp until we examine the query result
+        auto& view = nunit->namespaces->get<nstags::uri>();
+        auto itcpp = view.find(SRCML_CPP_NS_URI);
+        if (itcpp != view.end()) {
+            view.modify(itcpp, [](Namespace& thisns){ thisns.flags &= ~NS_USED; });
+        }
+        auto itomp = view.find(SRCML_OPENMP_NS_URI);
+        if (itomp != view.end()) {
+            view.modify(itomp, [](Namespace& thisns){ thisns.flags &= ~NS_USED; });
+        }
+
         // special case for XML comment as it does not get written to the tree
         switch (fullresults->nodeTab[i]->type) {
         case XML_COMMENT_NODE:
@@ -636,20 +652,6 @@ int srcml_unit_apply_transforms(struct srcml_archive* archive, struct srcml_unit
             // very important to flush to make sure the unit contents are all present
             // also performs a free of resources
             xmlOutputBufferClose(output);
-
-            if (!nunit->namespaces)
-                nunit->namespaces = starting_namespaces;
-
-            // mark unused cpp and omp until we examine the query result
-            auto& view = nunit->namespaces->get<nstags::uri>();
-            auto itcpp = view.find(SRCML_CPP_NS_URI);
-            if (itcpp != view.end()) {
-                view.modify(itcpp, [](Namespace& thisns){ thisns.flags &= ~NS_USED; });
-            }
-            auto itomp = view.find(SRCML_OPENMP_NS_URI);
-            if (itomp != view.end()) {
-                view.modify(itomp, [](Namespace& thisns){ thisns.flags &= ~NS_USED; });
-            }
 
             if (usesURI(xmlDocGetRootElement(doc.get()), SRCML_CPP_NS_URI)) {
 
