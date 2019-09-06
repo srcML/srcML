4221ce782e2f7ef960238d1c22da56e01ffa2482
Michael L. Collard, Ph.D <collard@uakron.edu>
2019-09-06 15:00:33 -0400

    Add back in SSIZE_t for Windows

--- src/libsrcml/srcml.h
+++ src/libsrcml/srcml.h
@@ -51,6 +51,7 @@ extern "C" {
 #if defined(WIN32) && !defined(__MINGW32__)
 #define LIBSRCML_DECL __declspec(dllexport)
 #include <BaseTsd.h>
+typedef SSIZE_T ssize_t
 #else
 #define LIBSRCML_DECL
 #endif
