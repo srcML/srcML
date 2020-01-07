632eee02518ea03f39117f2122d3550ce1029567
Michael L. Collard, Ph.D <collard@uakron.edu>
2020-01-07 08:15:51 -0500

    Fix XSLT tranformation for non well-formed XML units

--- src/libsrcml/xsltTransformation.cpp
+++ src/libsrcml/xsltTransformation.cpp
@@ -156,9 +156,13 @@ TransformationResult xsltTransformation::apply(xmlDocPtr doc, int /* position */
 
     // apply the style sheet to the document, which is the individual unit
     std::shared_ptr<xmlDoc> res(xsltApplyStylesheetUser(stylesheet, doc, cparams.data(), 0, 0, 0), [](xmlDoc* doc) { xmlFreeDoc(doc); });
-    if (!res || !res->children) {
+    if (!res) {
         fprintf(stderr, "libsrcml:  Error in applying stylesheet\n");
 
+        return TransformationResult();
+    }
+    // empty (but valid) result
+    if (!res->children) {
         return TransformationResult();
     }
 
@@ -166,6 +170,26 @@ TransformationResult xsltTransformation::apply(xmlDocPtr doc, int /* position */
     TransformationResult result(xmlXPathNodeSetCreate(res->children), true);
     result.doc = res;
     result.nodeType = res->children->type == 1 ? SRCML_RESULT_UNITS : SRCML_RESULT_STRING;
+
+    // transformation may produce more than one unit, but not be well-formed due to lack of root element
+    // so create a nodeset of those parts, and change the type since libxml2 has this as text
+    if (res->children->next && res->children->next->type == 1) {
+
+        // libxml2 types this as text since there is no root element
+        result.nodeType = SRCML_RESULT_UNITS;
+
+        // remove the res->children since it may not be a unit
+        xmlXPathNodeSetRemove(result.nodeset.get(), 0);
+
+        // add each unit to the nodeset
+        for (auto p = res->children; p; p = p->next) {
+            if (p->type != 1)
+                continue;
+            xmlXPathNodeSetMerge(result.nodeset.get(), xmlXPathNodeSetCreate(p));
+        }
+    }
+
+    // text result
     if (result.nodeType == SRCML_RESULT_STRING) {
         auto str = (char*) xmlNodeListGetRawString(res.get(), res->children, 0);
         result.stringValue = str;
