4f66f0d76c9a07b2610d93f6fe245a049290f270
Michael L. Collard <collard@uakron.edu>
2021-06-08 12:24:23 -0400

    Disable specific Windows warnings

--- src/parser/srcMLOutput.hpp
+++ src/parser/srcMLOutput.hpp
@@ -29,7 +29,9 @@
 
 #ifdef _MSC_VER
 #   pragma warning(push, 0)
-#   pragma warning(disable : 4619)
+#   pragma warning(disable : 4619) // #pragma warning : there is no warning number 'number'
+#   pragma warning(disable : 4365) // conversion from 'int' to 'const unsigned __int64', signed/unsigned mismatch
+#   pragma warning(disable : 4100) // 'ex': unreferenced formal parameter
 #endif
 #include <antlr/Token.hpp>
 #ifdef _MSC_VER
