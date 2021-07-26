87c4cdeb0c613842f045360e1d20637715e522b8
Michael L. Collard <collard@uakron.edu>
2021-07-26 10:30:52 -0400

    Add CPUCount() and use it to determine number of parsing threads

---
+++ src/client/CPUCount.cpp
@@ -0,0 +1,52 @@
+/**
+ * @file CPUCount.cpp
+ *
+ * @copyright Copyright (C) 2021 srcML, LLC. (www.srcML.org)
+ *
+ * This file is part of the srcml command-line client.
+ *
+ * The srcML Toolkit is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * The srcML Toolkit is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the srcml command-line client; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#include <CPUCount.hpp>
+
+#if _WIN64
+    #include <windows.h>
+#elif __APPLE__
+    #include <sys/types.h>
+    #include <sys/sysctl.h>
+#else
+    #include <sched.h>
+#endif
+
+// Number of CPU cores
+int CPUCount() {
+
+#ifdef _WIN64
+    SYSTEM_INFO sysinfo;
+    GetSystemInfo(&sysinfo);
+    return sysinfo.dwNumberOfProcessors;
+#elif __APPLE__
+    int count = 0;
+    size_t len = sizeof(count);
+    sysctlbyname("machdep.cpu.core_count", &count, &len, NULL, 0);
+    return count;
+#else
+    cpu_set_t cs;
+    CPU_ZERO(&cs);
+    sched_getaffinity(0, sizeof(cs), &cs);
+    return CPU_COUNT(&cs);
+#endif
+}
