f7ae6c6dd90d5d2f8847c0862c578aaca2465c8e
Michael L. Collard <collard@uakron.edu>
2022-09-29 10:48:00 -0400

    Remove ldconfig for rpm's. May fail on older platforms

--- package/rpm.cmake
+++ package/rpm.cmake
@@ -96,12 +96,6 @@ set(CPACK_RPM_CHANGELOG_FILE "${CMAKE_CURRENT_SOURCE_DIR}/ChangeLog.txt")
 # License
 set(CPACK_RPM_PACKAGE_LICENSE "GPLv2+")
 
-# post install script for ldconfig
-# Note: Believe that newline is required
-file(WRITE ${CPACK_BINARY_DIR}/post.sh "/sbin/ldconfig\n")
-set(CPACK_RPM_POST_INSTALL_SCRIPT_FILE   ${CPACK_BINARY_DIR}/post.sh)
-set(CPACK_RPM_POST_UNINSTALL_SCRIPT_FILE ${CPACK_BINARY_DIR}/post.sh)
-
 # CPack puts directories of installed files into the RPM
 # Since they already exist, this is a conflict with other packages
 # This excludes the following, mostly for documentation
