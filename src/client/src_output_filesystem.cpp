7d2c7665a1ea07fb2dd129a977feba004ea4c1c2
Michael L. Collard <collard@uakron.edu>
2022-11-24 07:37:42 -0500

    Replace own mkDir using libarchive with std::filesystem to create directories

--- src/client/src_output_filesystem.cpp
+++ src/client/src_output_filesystem.cpp
@@ -11,7 +11,8 @@
 #include <srcml.h>
 #include <iostream>
 #include <srcml_utilities.hpp>
-#include <mkDir.hpp>
+#include <SRCMLStatus.hpp>
+#include <filesystem>
 
 void src_output_filesystem(srcml_archive* srcml_arch, std::string_view output_dir, TraceLog& log) {
 
@@ -20,35 +21,37 @@ void src_output_filesystem(srcml_archive* srcml_arch, std::string_view output_di
     if (output_dir != "." && output_dir != "./")
         prefix = output_dir;
 
-    // create output directory structure as needed
-    mkDir dir;
-
+    // read and output each unit
     int count = 0;
-    std::string last;
     while (std::unique_ptr<srcml_unit> unit{srcml_archive_read_unit(srcml_arch)}) {
 
+        // extract the filename from the unit
         const char* cfilename = srcml_unit_get_filename(unit.get());
+
+        // skip if no filename
         if (!cfilename)
             continue;
 
+        // construct the full, relative filename
         std::string filename = cfilename;
+        std::filesystem::path path(prefix);
+        path /= cfilename;
 
-        // separate pathname from filename
-        std::string path = prefix;
-        path += '/';
-        auto pos = filename.rfind('/');
-        path += pos != std::string::npos ? filename.substr(0, pos) : "";
+        // create the file path
+        auto directory = path;
+        directory.remove_filename();
+        std::error_code ec;
+        if (!std::filesystem::create_directories(directory, ec) && ec.value() != 0) {
+            SRCMLstatus(ERROR_MSG, "Unable to create directory %s", directory.c_str());
+            continue;
+        }
 
-        std::string fullfilename = prefix;
-        fullfilename += '/';
-        fullfilename += filename;
-
-        // use libarchive to create the file path
-        dir.mkdir(path);
-
-        // unparse directory to filename
-        log << ++count << fullfilename;
-
-        srcml_unit_unparse_filename(unit.get(), fullfilename.data());
+        // unparse directly to the file
+        // log before so error is on last shown filename
+        log << ++count << path.c_str();
+        int result = srcml_unit_unparse_filename(unit.get(), path.c_str());
+        if (result != SRCML_STATUS_OK) {
+           SRCMLstatus(ERROR_MSG, "Unable to extract file %s", path.c_str());
+        }
     }
 }
