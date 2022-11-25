e4fe22e4d6ef89652ca008d7c663d45c157197ae
Michael L. Collard <collard@uakron.edu>
2022-11-25 13:18:45 -0500

    Another attempt to fix MSVC with std::filesystem::path::c_str()

--- src/client/src_output_filesystem.cpp
+++ src/client/src_output_filesystem.cpp
@@ -47,7 +47,7 @@ void src_output_filesystem(srcml_archive* srcml_arch, std::string_view output_di
 
         // unparse directly to the file
         // log before so error is on last shown filename
-        log << ++count << std::string_view(path.c_str());
+        log << ++count << path.string().c_str();
         int result = srcml_unit_unparse_filename(unit.get(), path.c_str());
         if (result != SRCML_STATUS_OK) {
            SRCMLstatus(ERROR_MSG, "Unable to extract file %s", path.c_str());
