bd95bf2a58417786e4eb3eb4978f0bf71caef368
Michael L. Collard <collard@uakron.edu>
2022-12-09 16:22:18 -0500

    Convert output to a filesystem to use threads

--- src/client/src_output_filesystem.cpp
+++ src/client/src_output_filesystem.cpp
@@ -13,6 +13,29 @@
 #include <srcml_utilities.hpp>
 #include <SRCMLStatus.hpp>
 #include <filesystem>
+#include <ctpl_stl.h>
+#include <CPUCount.hpp>
+
+// creates the source-code file in the in unit in the given filename path
+static void create_file(int, std::shared_ptr<srcml_unit> unit, std::filesystem::path path) {
+
+    // create the file path
+    auto directory = path;
+    directory.remove_filename();
+    std::error_code ec;
+    if (!std::filesystem::create_directories(directory, ec) && ec.value() != 0) {
+        SRCMLstatus(ERROR_MSG, "Unable to create directory %s", directory.c_str());
+        return;
+    }
+
+    // unparse directly to the file
+    // log before so error is on last shown filename
+    // log << ++count << path.string().c_str();
+    int result = srcml_unit_unparse_filename(unit.get(), path.string().c_str());
+    if (result != SRCML_STATUS_OK) {
+       SRCMLstatus(ERROR_MSG, "Unable to extract file %s", path.string().c_str());
+    }
+}
 
 void src_output_filesystem(srcml_archive* srcml_arch, std::string_view output_dir, TraceLog& log) {
 
@@ -21,9 +44,18 @@ void src_output_filesystem(srcml_archive* srcml_arch, std::string_view output_di
     if (output_dir != "." && output_dir != "./")
         prefix = output_dir;
 
+    // determine default max threads
+    int processorCount = CPUCount();
+    int maxThreads = 2;
+    if (processorCount == 2)
+        maxThreads = 3;
+    else
+        maxThreads = processorCount + 2;
+    ctpl::thread_pool pool(maxThreads);
+
     // read and output each unit
     int count = 0;
-    while (std::unique_ptr<srcml_unit> unit{srcml_archive_read_unit(srcml_arch)}) {
+    while (std::shared_ptr<srcml_unit> unit{srcml_archive_read_unit(srcml_arch)}) {
 
         // extract the filename from the unit
         const char* cfilename = srcml_unit_get_filename(unit.get());
@@ -36,21 +68,11 @@ void src_output_filesystem(srcml_archive* srcml_arch, std::string_view output_di
         std::filesystem::path path(prefix);
         path /= cfilename;
 
-        // create the file path
-        auto directory = path;
-        directory.remove_filename();
-        std::error_code ec;
-        if (!std::filesystem::create_directories(directory, ec) && ec.value() != 0) {
-            SRCMLstatus(ERROR_MSG, "Unable to create directory %s", directory.c_str());
-            continue;
-        }
+        // create the file using a thread pool
+        pool.push(create_file, std::move(unit), path);
 
-        // unparse directly to the file
-        // log before so error is on last shown filename
         log << ++count << path.string().c_str();
-        int result = srcml_unit_unparse_filename(unit.get(), path.string().c_str());
-        if (result != SRCML_STATUS_OK) {
-           SRCMLstatus(ERROR_MSG, "Unable to extract file %s", path.string().c_str());
-        }
     }
+
+    pool.stop(true);
 }
