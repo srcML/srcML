a5a81bb6c1a146693c21762897e413faaaf31d21
Michael L. Collard <collard@uakron.edu>
2023-06-05 11:23:29 -0400

    Try to unset rather than assign to empty

--- package/nsis.cmake
+++ package/nsis.cmake
@@ -28,7 +28,7 @@ install(FILES ${CMAKE_SOURCE_DIR}/package/srcml_icon.ico DESTINATION . COMPONENT
 set(CPACK_WIX_PRODUCT_ICON ${CMAKE_SOURCE_DIR}/package/srcml_icon.ico)
 
 # Prevent start menu shortcut
-set(CPACK_WIX_PROGRAM_MENU_FOLDER "")
+unset(CPACK_WIX_PROGRAM_MENU_FOLDER)
 
 # directory name
 set(CPACK_PACKAGE_INSTALL_DIRECTORY "srcML")
