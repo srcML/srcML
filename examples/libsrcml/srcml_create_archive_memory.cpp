8a5fa5f288af8f9a7bccc6e4249d954f296a5986
Michael L. Collard <collard@uakron.edu>
2023-07-24 12:41:23 -0400

    Remove need for C++17 for example file

--- examples/libsrcml/srcml_create_archive_memory.cpp
+++ examples/libsrcml/srcml_create_archive_memory.cpp
@@ -2,7 +2,7 @@
 /**
  * @file srcml_archive_create_memory.cpp
  *
- * @copyright Copyright (C) 2013-2019 srcML, LLC. (www.srcML.org)
+ * @copyright Copyright (C) 2013-2023 srcML, LLC. (www.srcML.org)
  *
  * Example program of the use of the libsrcml C API.
  *
@@ -10,43 +10,37 @@
 */
 
 #include <srcml.h>
-#include <stdlib.h>
 #include <iostream>
-#include <fstream>
-#include <iterator>
+#include <cstring>
 
 int main(int argc, char* argv[]) {
 
-    // open a srcML archive for output
+    // open a srcML archive for memory output
     srcml_archive* archive = srcml_archive_create();
     char* s = nullptr;
     size_t size;
     srcml_archive_write_open_memory(archive, &s, &size);
 
-    // add all the files to the archive
-    for (int i = 1; i < argc; ++i) {
+    // translate to the code to srcml and append to the archive
+    const char* code = "int n = 0;";
+    srcml_unit* unit = srcml_unit_create(archive);
+    srcml_unit_set_language(unit, "C++");
+    srcml_unit_parse_memory(unit, code, strlen(code));
+    srcml_archive_write_unit(archive, unit);
 
-        // read the contents of the input file
-        std::ifstream ifs(argv[i]);
-        std::string contents(std::istreambuf_iterator<char>{ifs}, {});
-
-        // Translate to srcml and append to the archive
-        srcml_unit* unit = srcml_unit_create(archive);
-        srcml_unit_set_language(unit, srcml_archive_check_extension(archive, argv[i]));
-        srcml_unit_parse_memory(unit, contents.data(), contents.size());
-        srcml_archive_write_unit(archive, unit);
-
-        srcml_unit_free(unit);
-    }
+    // free the unit
+    srcml_unit_free(unit);
 
     // close the srcML archive
     srcml_archive_close(archive);
 
-    // free the archives
+    // free the archive
     srcml_archive_free(archive);
 
-    // now dump the contents of the archive
+    // output the contents of the archive
     std::cout << s;
+
+    // free the generated archive
     free(s);
 
     return 0;
