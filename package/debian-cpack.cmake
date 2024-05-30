786286326e2acc6c4ecfe6117a31e16b991b917e
Michael L. Collard <collard@uakron.edu>
2024-05-30 12:48:54 -0400

    Remove Debian recommended package libxslt as it is now required

--- package/debian-cpack.cmake
+++ package/debian-cpack.cmake
@@ -94,7 +94,7 @@ set(CPACK_DEBIAN_DEVLIBS_PACKAGE_DEPENDS "${CPACK_PACKAGE_NAME} (>= ${PROJECT_VE
 
 # Recommended packages
 # Shared between client and dev packages
-set(CPACK_DEBIAN_PACKAGE_RECOMMENDS "libxslt, zip, unzip, cpio, tar, man")
+set(CPACK_DEBIAN_PACKAGE_RECOMMENDS "zip, unzip, cpio, tar, man")
 
 # Trigger required for library installed in client to initiate ldconfig
 set(TRIGGERS_FILE "${CMAKE_CURRENT_BINARY_DIR}/triggers")
