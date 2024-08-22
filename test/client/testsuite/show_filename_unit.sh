f3c33ef0e78bc3bd8b28e407c8c352d54ebb1763
Michael L. Collard <collard@uakron.edu>
2024-08-22 14:04:39 -0400

    Restore test cases and fix show_filename_unit test

--- test/client/testsuite/show_filename_unit.sh
+++ test/client/testsuite/show_filename_unit.sh
@@ -12,23 +12,23 @@ source $(dirname "$0")/framework_test.sh
 define input <<- 'STDOUT'
 	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 	<unit xmlns="http://www.srcML.org/srcML/src" revision="REVISION" language="C++" filename="sub/a.cpp"><expr_stmt><expr><name>a</name></expr>;</expr_stmt></unit>
-  STDOUT
+STDOUT
 
 xmlcheck "$input"
 createfile sub/a.cpp.xml "$input"
 createfile sub/a.cpp "a;"
 
 srcml sub/a.cpp.xml --show-filename
-check "sub/a.cpp"
+check "sub/a.cpp\n"
 
 srcml --show-filename sub/a.cpp.xml
-check "sub/a.cpp"
+check "sub/a.cpp\n"
 
 srcml --show-filename < sub/a.cpp.xml
-check "sub/a.cpp"
+check "sub/a.cpp\n"
 
 srcml sub/a.cpp --show-filename
-check "sub/a.cpp"
+check "sub/a.cpp\n"
 
 srcml --show-filename sub/a.cpp
-check "sub/a.cpp"
+check "sub/a.cpp\n"
