1d50ca764a19fb8e1f3036a25e23b420943baef0
Michael L. Collard <collard@uakron.edu>
2024-08-21 22:10:14 -0400

    Fix skipped tests for the src-version attribute

--- test/client/testsuite/show_src-version_unit.sh
+++ test/client/testsuite/show_src-version_unit.sh
@@ -12,7 +12,7 @@ source $(dirname "$0")/framework_test.sh
 define input <<- 'STDOUT'
 	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 	<unit xmlns="http://www.srcML.org/srcML/src" revision="REVISION" language="C++" directory="bar" filename="foo" version="1.0"/>
-  STDOUT
+	STDOUT
 
 # test on archive of one unit
 define archive <<- 'STDOUT'
@@ -24,7 +24,7 @@ define archive <<- 'STDOUT'
 	</unit>
 
 	</unit>
-  STDOUT
+	STDOUT
 
 xmlcheck "$input"
 xmlcheck "$archive"
@@ -33,22 +33,22 @@ createfile sub/a.cpp.xml "$input"
 createfile sub/archive.cpp.xml "$archive"
 
 srcml --show-src-version sub/a.cpp.xml
-check "1.0"
+check "1.0\n"
 
 srcml --show-src-version < sub/a.cpp.xml
-check "1.0"
+check "1.0\n"
 
 srcml --show-src-version sub/archive.cpp.xml
-check "1.0"
+check "1.0\n"
 
 srcml --show-src-version < sub/archive.cpp.xml
-check "1.0"
+check "1.0\n"
 
 # test src version on single unit with empty version
 define empty <<- 'STDIN'
 	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 	<unit xmlns="http://www.srcML.org/srcML/src" revision="REVISION" language="" directory="" filename="" version=""/>
-  STDIN
+	STDIN
 
 # test on archive of one unit with an empty version
 define emptyarchive <<- 'STDOUT'
@@ -60,7 +60,7 @@ define emptyarchive <<- 'STDOUT'
 	</unit>
 
 	</unit>
-  STDOUT
+	STDOUT
 
 xmlcheck "$empty"
 xmlcheck "$emptyarchive"
@@ -69,22 +69,22 @@ createfile sub/a.cpp.xml "$empty"
 createfile sub/archive.cpp.xml "$emptyarchive"
 
 srcml --show-src-version sub/a.cpp.xml
-check ""
+check "\n"
 
 srcml --show-src-version < sub/a.cpp.xml
-check ""
+check "\n"
 
 srcml --show-src-version sub/archive.cpp.xml
-check ""
+check "\n"
 
 srcml --show-src-version < sub/archive.cpp.xml
-check ""
+check "\n"
 
 # test on empty archive with no version
 define noneempty <<- 'STDIN'
 	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 	<unit xmlns="http://www.srcML.org/srcML/src" xmlns:cpp="http://www.srcML.org/srcML/cpp"/>
-  STDIN
+	STDIN
 
 # test on archive of one unit with no version
 define none <<- 'STDIN'
@@ -96,7 +96,7 @@ define none <<- 'STDIN'
 	</unit>
 
 	</unit>
-  STDIN
+	STDIN
 
 xmlcheck "$noneempty"
 xmlcheck "$none"
