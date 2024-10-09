6dd1a1d789e20d9241141d3aabb84c6267218b5c
Michael L. Collard <collard@uakron.edu>
2024-10-09 08:07:31 -0700

    Remove heredoc end indent

--- test/client/testsuite/cpp_java.sh
+++ test/client/testsuite/cpp_java.sh
@@ -15,13 +15,13 @@ define output <<- 'STDOUT'
 	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 	<unit xmlns="http://www.srcML.org/srcML/src" xmlns:cpp="http://www.srcML.org/srcML/cpp" revision="REVISION" language="Java"><cpp:define>#<cpp:directive>define</cpp:directive></cpp:define>
 	</unit>
-	STDOUT
+STDOUT
 
 define foutput <<- 'STDOUT'
 	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 	<unit xmlns="http://www.srcML.org/srcML/src" xmlns:cpp="http://www.srcML.org/srcML/cpp" revision="REVISION" language="Java" filename="sub/a.java"><cpp:define>#<cpp:directive>define</cpp:directive></cpp:define>
 	</unit>
-	STDOUT
+STDOUT
 
 xmlcheck "$output"
 xmlcheck "$foutput"
