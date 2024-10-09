29dde89052610c246a5fc6a222c4cd03c83c7923
Michael L. Collard <collard@uakron.edu>
2024-10-09 08:10:59 -0700

    Remove heredoc end indent

--- test/client/testsuite/expression.sh
+++ test/client/testsuite/expression.sh
@@ -12,22 +12,22 @@ source $(dirname "$0")/framework_test.sh
 define sxmlfile <<- 'STDOUT'
 	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 	<unit xmlns="http://www.srcML.org/srcML/src" revision="REVISION" language="C++"><expr><name>a</name></expr></unit>
-	STDOUT
+STDOUT
 
 define fsxmlfile <<- 'STDOUT'
 	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 	<unit xmlns="http://www.srcML.org/srcML/src" revision="REVISION" language="C++" filename="sub/a.cpp"><expr><name>a</name></expr></unit>
-	STDOUT
+STDOUT
 
 define expr_stmt_sxmlfile <<- 'STDOUT'
 	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 	<unit xmlns="http://www.srcML.org/srcML/src" revision="REVISION" language="C++"><expr_stmt><expr><name>b</name></expr>;</expr_stmt></unit>
-	STDOUT
+STDOUT
 
 define expr_stmt_fsxmlfile <<- 'STDOUT'
 	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 	<unit xmlns="http://www.srcML.org/srcML/src" revision="REVISION" language="C++" filename="sub/b.cpp"><expr_stmt><expr><name>b</name></expr>;</expr_stmt></unit>
-	STDOUT
+STDOUT
 
 xmlcheck "$sxmlfile"
 xmlcheck "$fsxmlfile"
