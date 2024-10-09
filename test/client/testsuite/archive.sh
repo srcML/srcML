9648cd8860126c2311a5560504b7b9fb14e30cbb
Michael L. Collard <collard@uakron.edu>
2024-10-09 08:09:18 -0700

    Remove heredoc end indent

--- test/client/testsuite/archive.sh
+++ test/client/testsuite/archive.sh
@@ -16,7 +16,7 @@ define fileasrcml <<- 'STDOUT'
 	<unit revision="REVISION" language="C++" filename="sub/a.cpp" hash="a301d91aac4aa1ab4e69cbc59cde4b4fff32f2b8"><expr_stmt><expr><name>a</name></expr>;</expr_stmt></unit>
 
 	</unit>
-	STDOUT
+STDOUT
 xmlcheck "$fileasrcml"
 
 define asrcml <<- 'STDOUT'
@@ -26,7 +26,7 @@ define asrcml <<- 'STDOUT'
 	<unit revision="REVISION" language="C++" hash="a301d91aac4aa1ab4e69cbc59cde4b4fff32f2b8"><expr_stmt><expr><name>a</name></expr>;</expr_stmt></unit>
 
 	</unit>
-	STDOUT
+STDOUT
 xmlcheck "$asrcml"
 
 define nestedfile <<- 'STDOUT'
@@ -38,7 +38,7 @@ define nestedfile <<- 'STDOUT'
 	<unit revision="REVISION" language="C++" filename="sub/b.cpp" hash="9a1e1d3d0e27715d29bcfbf72b891b3ece985b36"><expr_stmt><expr><name>b</name></expr>;</expr_stmt></unit>
 
 	</unit>
-	STDOUT
+STDOUT
 xmlcheck "$nestedfile"
 
 createfile sub/a.cpp "a;"
