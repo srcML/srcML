b2463e09ecc8a55e06875736a15f8915dc9539bc
Mike Weyandt <mike@mikeweyandt.com>
2026-01-29 15:27:11 -0500

    Add tests to ensure empty filenames return SRCML_STATUS_IO_ERROR

--- test/libsrcml/testsuite/test_srcml_convenience.cpp
+++ test/libsrcml/testsuite/test_srcml_convenience.cpp
@@ -147,6 +147,14 @@ int main(int, char* argv[]) {
     }
 
     {
+        dassert(srcml("", "foo.xml"), SRCML_STATUS_IO_ERROR);
+    }
+
+    {
+        dassert(srcml("foo.c", ""), SRCML_STATUS_IO_ERROR);
+    }
+
+    {
         dassert(srcml("foo.c", "foo.xml"), SRCML_STATUS_IO_ERROR);
     }
 
