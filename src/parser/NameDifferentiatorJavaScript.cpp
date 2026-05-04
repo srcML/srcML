7ae25558d5c07cf64e3ec5e15127cfbb6bbfb781
Kyle776R <kyle01nicholas@roadrunner.com>
2026-05-04 12:44:54 -0400

    Fix bug with JavaScript contextual keywords and "?." operator

--- src/parser/NameDifferentiatorJavaScript.cpp
+++ src/parser/NameDifferentiatorJavaScript.cpp
@@ -240,6 +240,7 @@ bool NameDifferentiatorJavaScript::isNameToken(antlr::RefToken token, antlr::Ref
             )
         )
         || (prevToken->getType() == srcMLParser::PERIOD || nextToken->getType() == srcMLParser::PERIOD)
+        || (prevToken->getType() == srcMLParser::QMARK_PERIOD || nextToken->getType() == srcMLParser::QMARK_PERIOD)
         || (
             bracketBuffer.front() == "*"
             && (
