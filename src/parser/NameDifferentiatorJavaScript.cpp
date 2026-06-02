0a2c2a744d296387aab18a4e627afa1e9cd4b019
Kyle776R <kyle01nicholas@roadrunner.com>
2026-06-02 13:15:48 -0400

    Fix bug with contextual keyword "async" in JavaScript

--- src/parser/NameDifferentiatorJavaScript.cpp
+++ src/parser/NameDifferentiatorJavaScript.cpp
@@ -250,8 +250,11 @@ bool NameDifferentiatorJavaScript::isNameToken(antlr::RefToken token, antlr::Ref
         )
         || (prevToken->getType() == srcMLParser::PERIOD || nextToken->getType() == srcMLParser::PERIOD)
         || (prevToken->getType() == srcMLParser::QMARK_PERIOD || nextToken->getType() == srcMLParser::QMARK_PERIOD)
-        || (token->getType() == srcMLParser::JS_ASYNC && nextToken->getType() == srcMLParser::QMARK)
         || (token->getType() == srcMLParser::JS_VOID && nextToken->getType() == srcMLParser::JS_ARROW)
+        || (
+            token->getType() == srcMLParser::JS_ASYNC
+            && (nextToken->getType() == srcMLParser::QMARK || nextToken->getType() == srcMLParser::JS_RANGE_IN)
+        )
         || (
             prevNonWhitespaceToken->getType() == srcMLParser::CLASS
             && (
